89dc3fe583050ff54b403fb9ec7a27c8e8c1644a
dmitriev0 <no_reply@jetbrains.com>
2004-01-16 13:49:21 +0300

    no message

---
+++ source/jetbrains/mps/nodeEditor/PropertyAccessor.java
@@ -0,0 +1,32 @@
+package jetbrains.mps.nodeEditor;
+
+import jetbrains.mps.semanticModel.SemanticNode;
+
+/**
+ * User: Dmitriev.
+ * Date: Jan 16, 2004
+ */
+public class PropertyAccessor implements ModelAccessor {
+  private SemanticNode myNode;
+  private String myPropertyName;
+  private String myDefaultValue;
+  private boolean myWritable;
+
+  public PropertyAccessor(SemanticNode node, String propertyName, String defaultValue, boolean writable) {
+    myNode = node;
+    myPropertyName = propertyName;
+    myDefaultValue = defaultValue;
+    myWritable = writable;
+  }
+
+  public String getText() {
+    String value = myNode.getProperty(myPropertyName);
+    return value == null ? myDefaultValue : value;
+  }
+
+  public void setText(String text) {
+    if(myWritable) {
+      myNode.setProperty(myPropertyName, text);
+    }
+  }
+}
