13fe1b51b369bebbb47132923c1db1917c5d8844
dmitriev0 <no_reply@jetbrains.com>
2004-03-04 17:19:15 +0300

    editor lang gen : CellModel_Property : gen the "allowEmptyText" code re-gen base lang editor

--- source/jetbrains/mps/nodeEditor/PropertyAccessor.java
+++ source/jetbrains/mps/nodeEditor/PropertyAccessor.java
@@ -10,11 +10,17 @@ public class PropertyAccessor implements ModelAccessor {
   private SemanticNode myNode;
   private String myPropertyName;
   private boolean myWritable;
+  private boolean myAllowEmptyText;
 
   public PropertyAccessor(SemanticNode node, String propertyName, boolean writable) {
+    this(node, propertyName, writable, false);
+  }
+
+  public PropertyAccessor(SemanticNode node, String propertyName, boolean writable, boolean allowEmptyText) {
     myNode = node;
     myPropertyName = propertyName;
     myWritable = writable;
+    myAllowEmptyText = allowEmptyText;
   }
 
   public String getText() {
@@ -23,6 +29,9 @@ public class PropertyAccessor implements ModelAccessor {
 
   public void setText(String text) {
     if(myWritable) {
+      if(!myAllowEmptyText && text != null && text.length() == 0) {
+        text = null;
+      }
       myNode.setProperty(myPropertyName, text);
     }
   }
