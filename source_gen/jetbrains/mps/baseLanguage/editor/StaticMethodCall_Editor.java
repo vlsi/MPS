f14a5a1943bd65f74e0cb082fdb1e7c216950f6d
dmitriev0 <no_reply@jetbrains.com>
2004-04-19 20:49:57 +0400

    change some action sets, clean context model

--- source_gen/jetbrains/mps/baseLanguage/editor/StaticMethodCall_Editor.java
+++ source_gen/jetbrains/mps/baseLanguage/editor/StaticMethodCall_Editor.java
@@ -15,7 +15,7 @@ import jetbrains.mps.nodeEditor.ModelAccessor;
 import jetbrains.mps.nodeEditor.EditorCell_Property;
 
 public class StaticMethodCall_Editor extends SemanticNodeEditor {
-  public static String MATCHING_TEXT = ".<static method>";
+  public static String MATCHING_TEXT = ". <static method>";
   public static String PRESENTATION_NAME = "static method call";
 
   private AbstractCellProvider my_BaseMethodCallArgListEditor;
