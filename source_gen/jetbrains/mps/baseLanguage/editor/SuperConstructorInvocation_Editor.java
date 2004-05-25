120b0cbc50fd6128ecbdd2668ea4070464e1f386
jeff <no_reply@jetbrains.com>
2004-05-25 12:19:56 +0400

    - latest changes for template based code-generation

--- source_gen/jetbrains/mps/baseLanguage/editor/SuperConstructorInvocation_Editor.java
+++ source_gen/jetbrains/mps/baseLanguage/editor/SuperConstructorInvocation_Editor.java
@@ -4,23 +4,46 @@ package jetbrains.mps.baseLanguage.editor;
 
 
 import jetbrains.mps.nodeEditor.SemanticNodeEditor;
+import jetbrains.mps.nodeEditor.EditorCellListHandler;
 import jetbrains.mps.semanticModel.SemanticNode;
 import jetbrains.mps.nodeEditor.EditorCell;
 import jetbrains.mps.nodeEditor.EditorContext;
-import jetbrains.mps.nodeEditor.EditorCell_Error;
+import jetbrains.mps.nodeEditor.EditorCell_Collection;
+import jetbrains.mps.nodeEditor.EditorCell_Constant;
 
 public class SuperConstructorInvocation_Editor extends SemanticNodeEditor {
   public static String MATCHING_TEXT = "super";
   public static String PRESENTATION_NAME = "super constructor call";
 
+  private EditorCellListHandler myActualArgumentListHandler;
+
   public void setSemanticNode(SemanticNode node) {
     super.setSemanticNode(node);
+    myActualArgumentListHandler = new SuperConstructorInvocation_Editor_ActualArgumentListHandler(node, "actualArgument", "aggregation");
   }
   public EditorCell createEditorCell(EditorContext editorContext, SemanticNode node) {
-    return this.createErrorCell(editorContext, node);
+    return this.createRowCell(editorContext, node);
   }
-  public EditorCell createErrorCell(EditorContext editorContext, SemanticNode node) {
-    EditorCell_Error editorCell = EditorCell_Error.create(editorContext, node, "SuperConstructorInvocation");
+  public EditorCell createRowCell(EditorContext editorContext, SemanticNode node) {
+    EditorCell_Collection editorCell = EditorCell_Collection.createHorizontal(editorContext, node);
+    editorCell.setGridLayout(false);
+    SuperConstructorInvocation_NodeBoxActions.setCellActions(editorCell, node);
+    editorCell.addEditorCell(this.createConstantCell(editorContext, node, "super("));
+    editorCell.addEditorCell(this.createActualArgumentList(editorContext, node));
+    editorCell.addEditorCell(this.createConstantCell1(editorContext, node, ")"));
+    return editorCell;
+  }
+  public EditorCell createConstantCell(EditorContext editorContext, SemanticNode node, String text) {
+    EditorCell_Constant editorCell = EditorCell_Constant.create(editorContext, node, text, false);
+    return editorCell;
+  }
+  public EditorCell createActualArgumentList(EditorContext editorContext, SemanticNode node) {
+    EditorCell_Collection cellCollection = myActualArgumentListHandler.createCells_Horizontal(editorContext);
+    cellCollection.setGridLayout(false);
+    return cellCollection;
+  }
+  public EditorCell createConstantCell1(EditorContext editorContext, SemanticNode node, String text) {
+    EditorCell_Constant editorCell = EditorCell_Constant.create(editorContext, node, text, false);
     return editorCell;
   }
 }
