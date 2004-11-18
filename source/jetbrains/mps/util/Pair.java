f6237f29b3dfd061660ce6d78fb33f394804de06
dmitriev0 <no_reply@jetbrains.com>
2004-11-18 22:10:14 +0300

    update cml templates

---
+++ source/jetbrains/mps/util/Pair.java
@@ -0,0 +1,17 @@
+/*
+ * Created by IntelliJ IDEA.
+ * User: alshan
+ * Date: Nov 18, 2004
+ * Time: 7:47:55 PM
+ */
+package jetbrains.mps.util;
+
+public class Pair<T1, T2> {
+  public T1 o1;
+  public T2 o2;
+
+  public Pair(T1 o1, T2 o2) {
+    this.o1 = o1;
+    this.o2 = o2;
+  }
+}
