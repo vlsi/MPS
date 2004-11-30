e4c60747beae52153e12cc7cd32b6ffe04f8de12
Konstantin Solomatov <kostik@jetbrains.com>
2004-11-30 13:13:33 +0300

    NodeComparator completed

--- source/jetbrains/mps/nodeEditor/nodeComparator/NodeComparator.java
+++ source/jetbrains/mps/nodeEditor/nodeComparator/NodeComparator.java
@@ -1,6 +1,7 @@
 package jetbrains.mps.nodeEditor.nodeComparator;
 
 import jetbrains.mps.semanticModel.SemanticNode;
+import jetbrains.mps.semanticModel.SemanticReference;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -22,7 +23,10 @@ public class NodeComparator {
   public boolean compare() {
     if (!compareNodes(myFirstNode, mySecondNode)) {
       return false;
-    };
+    }
+    if (!compareReferences(myFirstNode, mySecondNode)) {
+      return false;
+    }
     return true;
   }
 
@@ -30,6 +34,7 @@ public class NodeComparator {
     if (one.getClass() != another.getClass()) return false;
     if (!compareProperties(one, another)) return false;
     if (!compareChildren(one, another)) return false;
+    myNodeMap.put(one, another);
     return true;
   }
 
@@ -45,9 +50,30 @@ public class NodeComparator {
     if (one.getChildren().size() != another.getChildren().size()) return false;
 
     for (int i = 0; i < one.getChildren().size(); i++) {
-      if (!compareNodes(one.getChildren().get(i), another.getChildren().get(i))) return false;      
+      if (!compareNodes(one.getChildren().get(i), another.getChildren().get(i))) return false;
+    }
+    return true;
+  }
+
+  private boolean compareReferences(SemanticNode one, SemanticNode another) {
+    if (one.getReferences().size() != another.getReferences().size()) return false;
+
+    for (int i = 0; i < one.getReferences().size(); i++) {
+      SemanticReference oneReference = one.getReferences().get(i);
+      SemanticReference anotherReference = another.getReferences().get(i);
+      if (!compareTargets(oneReference.getTargetNode(), anotherReference.getTargetNode())) return false;
     }
 
+
+    for (int i = 0; i < one.getChildren().size(); i++) {
+      if (!compareReferences(one.getChildren().get(i), another.getChildren().get(i))) return false;
+    }
     return true;
+  }
+
+  private boolean compareTargets(SemanticNode one, SemanticNode another) {
+    if (one == another) return true;
+    if (myNodeMap.get(one) == another) return true;
+    return false;
   }
 }
