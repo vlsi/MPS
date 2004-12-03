ab252e605db78c2299db1e9b06b1ec7dd7bda2a4
Konstantin Solomatov <kostik@jetbrains.com>
2004-12-03 14:44:22 +0300

    Roots-related methods moved from MPSProject to RootManager

--- source/jetbrains/mps/project/MPSProject.java
+++ source/jetbrains/mps/project/MPSProject.java
@@ -29,30 +29,15 @@ import java.util.*;
 public class MPSProject implements ModelLocator {
   private File myProjectFile;
   private File myWorkspaceFile;
-  private ArrayList<SemanticModel> myProjectModels = new ArrayList<SemanticModel>();
-  private ArrayList<SemanticModel> myLibraryModels = new ArrayList<SemanticModel>();
-  private ArrayList<ModelRoot> myProjectModelRoots = new ArrayList<ModelRoot>();
-  private ArrayList<ModelRoot> myLibraryModelRoots = new ArrayList<ModelRoot>();
-  private ArrayList<File> myLanguageRoots = new ArrayList<File>();
-
-  public static final String LANGUAGES = "languages";
-  public static final String LANGUAGE_ROOT = "languageRoot";
-  public static final String PROJECT_MODELS = "projectModels";
-  public static final String LIBRARY_MODELS = "libraryModels";
-  public static final String MODEL_ROOT = "modelRoot";
-  public static final String PROJECT = "project";
 
   public static final String COMPONENTS = "components";
   public static final String COMPONENT = "component";
   public static final String CLASS = "class";
 
-  public static final String PATH_MACRO_MODELS_ROOT = "${models_root}" + File.separatorChar;
-  public static final String PATH_MACRO_PROJECT = "${project}" + File.separatorChar;
-  public static final String NAMESPACE_PREFIX = "namespacePrefix";
-  public static final String ROOT_PATH = "rootPath";
   private ExecutionPoint myEntryPoint;
   private HashMap<String, Language> myNamespaceToLanguageMap = new HashMap<String, Language>();
   private Map<Class, Object> myComponents = new HashMap<Class, Object>();
+  private RootManager myRootManager = new RootManager(this);
 
   public MPSProject(File file) {
     initComponent();
@@ -74,15 +59,15 @@ public class MPSProject implements ModelLocator {
   }
 
   public List<ModelRoot> getProjectModelRoots() {
-    return Collections.unmodifiableList(myProjectModelRoots);
+    return myRootManager.getProjectModelRoots();
   }
 
   public List<ModelRoot> getLibraryModelRoots() {
-    return Collections.unmodifiableList(myLibraryModelRoots);
+    return myRootManager.getLibraryModelRoots();
   }
 
   public List<File> getLanguageRoots() {
-    return Collections.unmodifiableList(myLanguageRoots);
+    return myRootManager.getLanguageRoots();
   }
 
   public List<Object> getComponents() {
@@ -98,75 +83,7 @@ public class MPSProject implements ModelLocator {
   }
 
   public void read(File file) {
-    if (!myProjectFile.exists()) {
-      return;
-    }
-    Document document;
-    try {
-      document = JDOMUtil.loadDocument(myProjectFile);
-    } catch (JDOMException e) {
-      e.printStackTrace();
-      return;
-    } catch (IOException e) {
-      e.printStackTrace();
-      return;
-    }
-    Element rootElement = document.getRootElement();
-
-    Element languageRootsElement = rootElement.getChild(LANGUAGES);
-    if (languageRootsElement != null) {
-      Iterator languageRootElements = languageRootsElement.getChildren(LANGUAGE_ROOT).iterator();
-      while (languageRootElements.hasNext()) {
-        Element element = (Element) languageRootElements.next();
-        String rootFileName = element.getAttributeValue(ROOT_PATH);
-        String rootAbsolutePath = null;
-        if (rootFileName.startsWith(PATH_MACRO_PROJECT)) {
-          String modelRelativePath = rootFileName.substring(PATH_MACRO_PROJECT.length());
-          rootAbsolutePath = PathManager.getAbsolutePathByRelational(myProjectFile, modelRelativePath);
-        } else { // default
-          rootAbsolutePath = PathManager.getAbsolutePathByRelational(myProjectFile, rootFileName);
-        }
-        File dir = new File(rootAbsolutePath);
-        myLanguageRoots.add(dir);
-        if (dir.exists()) {
-          readLanguageDescriptors(dir);
-        } else {
-          throw new RuntimeException("Couldn't load languages from " + rootAbsolutePath +
-                  "\nDirectory doesn't exist: " + rootAbsolutePath);
-        }
-      }
-    }
-
-
-    Element libraryModelsElement = rootElement.getChild(LIBRARY_MODELS);
-    readModelRoots(myLibraryModelRoots, libraryModelsElement);
-    Element projectModelsElement = rootElement.getChild(PROJECT_MODELS);
-    readModelRoots(myProjectModelRoots, projectModelsElement);
-
-    // check roots consistency
-    Iterator<ModelRoot> projectModelRoots = myProjectModelRoots.iterator();
-    while (projectModelRoots.hasNext()) {
-      ModelRoot modelRoot = projectModelRoots.next();
-      String error = modelRoot.checkConsistency();
-      if (error != null) {
-        (new Exception("Error reading project " + myProjectFile.getAbsolutePath() + ". Inconsistent project model root:\n" + error)).printStackTrace();
-        projectModelRoots.remove();
-      }
-    }
-    Iterator<ModelRoot> libraryModelRoots = myLibraryModelRoots.iterator();
-    while (libraryModelRoots.hasNext()) {
-      ModelRoot modelRoot = libraryModelRoots.next();
-      String error = modelRoot.checkConsistency();
-      if (error != null) {
-        (new Exception("Error reading project " + myProjectFile.getAbsolutePath() + ". Inconsistent library model root:\n" + error)).printStackTrace();
-        libraryModelRoots.remove();
-      }
-    }
-
-//    readModels(myLibraryModelRoots, myLibraryModels);
-    readModels(myProjectModelRoots, myProjectModels);
-
-    getSemanticModels().flushModelInfos();
+    myRootManager.read(file);
   }
 
   public void readComponents() {
@@ -193,156 +110,9 @@ public class MPSProject implements ModelLocator {
     }
   }
 
-  private void readModels(ArrayList<ModelRoot> modelRoots, ArrayList<SemanticModel> models) {
-    for (Iterator<ModelRoot> iterator = modelRoots.iterator(); iterator.hasNext();) {
-      ModelRoot modelRoot = iterator.next();
-      File dir = modelRoot.root;
-      if (dir.exists()) {
-        readModels(models, dir);
-      } else {
-        throw new RuntimeException("Couldn't load models from " + dir.getAbsolutePath() +
-                "\nDirectory doesn't exist: " + dir.getAbsolutePath());
-      }
-    }
-  }
-
-  private void readModelRoots(ArrayList<ModelRoot> modelRoots, Element modelsElement) {
-    if (modelsElement == null) {
-      return;
-    }
-    Iterator modelRootElements = modelsElement.getChildren(MODEL_ROOT).iterator();
-    while (modelRootElements.hasNext()) {
-      Element modelRootElement = (Element) modelRootElements.next();
-      String rootFileName = modelRootElement.getAttributeValue(ROOT_PATH);
-      String rootAbsolutePath = null;
-      if (rootFileName.startsWith(PATH_MACRO_PROJECT)) {
-        String modelRelativePath = rootFileName.substring(PATH_MACRO_PROJECT.length());
-        rootAbsolutePath = PathManager.getAbsolutePathByRelational(myProjectFile, modelRelativePath);
-      } else { // default
-        rootAbsolutePath = PathManager.getAbsolutePathByRelational(myProjectFile, rootFileName);
-      }
-      File dir = new File(rootAbsolutePath);
-      String namespacePrefix = modelRootElement.getAttributeValue(NAMESPACE_PREFIX);
-      ModelRoot modelRoot = new ModelRoot(namespacePrefix, dir);
-      modelRoots.add(modelRoot);
-    }
-  }
-
-  private void readModels(ArrayList<SemanticModel> models, File dir) {
-    if (!dir.isDirectory()) {
-      return;
-    }
-    File[] files = dir.listFiles(new FilenameFilter() {
-      public boolean accept(File dir, String name) {
-        return name.endsWith(".mps");
-      }
-    });
-    for (int i = 0; i < files.length; i++) {
-      File file = files[i];
-      SemanticModel semanticModel = loadModel(file.getAbsolutePath());
-      models.add(semanticModel);
-    }
-    File[] dirs = dir.listFiles();
-    for (int i = 0; i < dirs.length; i++) {
-      File childDir = dirs[i];
-      if (childDir.isDirectory()) {
-        readModels(models, childDir);
-      }
-    }
-  }
-
-  protected void readLanguageDescriptors(File dir) {
-    if (!dir.isDirectory()) {
-      return;
-    }
-    File[] files = dir.listFiles(new FilenameFilter() {
-      public boolean accept(File dir, String name) {
-        return name.endsWith(".mpl");
-      }
-    });
-    for (int i = 0; i < files.length; i++) {
-      File file = files[i];
-      Language language = Language.loadFromFile(file, this);
-      System.out.println("MPSProject add language : " + language.getNamespace());
-      addLanguage(language.getNamespace(), language);
-    }
-    File[] dirs = dir.listFiles();
-    for (int i = 0; i < dirs.length; i++) {
-      File childDir = dirs[i];
-      if (childDir.isDirectory()) {
-        readLanguageDescriptors(childDir);
-      }
-    }
-
-  }
-
   public void save() {
     getSemanticModels().saveAll();
 
-/**
-    Element rootElement = new Element(PROJECT);
-    Document document = new Document();
-    document.setRootElement(rootElement);
-
-    Element languages = new Element(LANGUAGES);
-    for (Iterator<File> iterator = myLanguageRoots.iterator(); iterator.hasNext();) {
-      File languageRoot = iterator.next();
-      String absolutePath = languageRoot.getAbsolutePath();
-      String fileName = null;
-      if (absolutePath.startsWith(myProjectFile.getParent())) {
-        String modelRelationalPath = PathManager.getRelationalPathByAbsolute(myProjectFile, absolutePath);
-        fileName = PATH_MACRO_PROJECT + modelRelationalPath;
-      } else {
-        String modelRelationalPath = PathManager.getRelationalPathByAbsolute(new File(PathManager.getModelPath()), absolutePath);
-        fileName = PATH_MACRO_MODELS_ROOT + modelRelationalPath;
-      }
-      Element languageRootElement = new Element(LANGUAGE_ROOT);
-      languageRootElement.setAttribute(ROOT_PATH, fileName);
-      languages.addContent(languageRootElement);
-    }
-    rootElement.addContent(languages);
-
-    Element projectModelsElement = new Element(PROJECT_MODELS);
-    saveModelRoots(myProjectModelRoots, projectModelsElement);
-    rootElement.addContent(projectModelsElement);
-    Element libraryModelsElement = new Element(LIBRARY_MODELS);
-    saveModelRoots(myProjectModelRoots, libraryModelsElement);
-    rootElement.addContent(libraryModelsElement);
-
-    if (!myProjectFile.exists()) {
-      try {
-        myProjectFile.createNewFile();
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-
-    }
-    try {
-      JDOMUtil.writeDocument(document, myProjectFile);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
-
-  private void saveModelRoots(ArrayList<ModelRoot> modelRoots, Element modelsElement) {
-    for (Iterator<ModelRoot> iterator = modelRoots.iterator(); iterator.hasNext();) {
-      ModelRoot modelRoot = iterator.next();
-      String absolutePath = modelRoot.root.getAbsolutePath();
-      String fileName = null;
-      if (absolutePath.startsWith(myProjectFile.getParent())) {
-        String modelRelationalPath = PathManager.getRelationalPathByAbsolute(myProjectFile, absolutePath);
-        fileName = PATH_MACRO_PROJECT + modelRelationalPath;
-      } else {
-        String modelRelationalPath = PathManager.getRelationalPathByAbsolute(new File(PathManager.getModelPath()), absolutePath);
-        fileName = PATH_MACRO_MODELS_ROOT + modelRelationalPath;
-      }
-      Element modelRootElement = new Element(MODEL_ROOT);
-      modelRootElement.setAttribute(ROOT_PATH, fileName);
-      if(modelRoot.namespacePrefix != null) {
-        modelRootElement.setAttribute(NAMESPACE_PREFIX, modelRoot.namespacePrefix);
-      }
-      modelsElement.addContent(modelRootElement);
-    } **/
   }
 
   public void saveComponents() {
@@ -369,11 +139,11 @@ public class MPSProject implements ModelLocator {
 
 
   public String findPath(String modelFQName) {
-    String modelPath = PathManager.findModelPath(myProjectModelRoots, modelFQName);
+    String modelPath = PathManager.findModelPath(myRootManager.getProjectModelRoots(), modelFQName);
     if (modelPath != null && (new File(modelPath)).exists()) {
       return modelPath;
     }
-    modelPath = PathManager.findModelPath(myLibraryModelRoots, modelFQName);
+    modelPath = PathManager.findModelPath(myRootManager.getLibraryModelRoots(), modelFQName);
     if (modelPath != null && (new File(modelPath)).exists()) {
       return modelPath;
     }
