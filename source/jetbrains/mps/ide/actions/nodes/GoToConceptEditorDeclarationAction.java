9ae159373e69a8a605766ee8c3a08b09c4869c8c
Konstantin Solomatov <kostik@jetbrains.com>
2005-01-01 16:58:36 +0300

    Fixed SCR #77

--- source/jetbrains/mps/ide/actions/nodes/GoToConceptEditorDeclarationAction.java
+++ source/jetbrains/mps/ide/actions/nodes/GoToConceptEditorDeclarationAction.java
@@ -19,7 +19,7 @@ import java.util.Iterator;
  */
 public class GoToConceptEditorDeclarationAction extends MPSAction {
   public GoToConceptEditorDeclarationAction() {
-    super("Go to concept declaration");
+    super("Go to concept editor declaration");
   }
 
   public boolean isVisible(ActionContext context) {
