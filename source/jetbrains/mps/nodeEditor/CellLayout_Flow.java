891bb16b0e46cf1b19bb63982394ba4721c2fcae
Konstantin Solomatov <kostik@jetbrains.com>
2005-02-14 12:01:28 +0300

    Bibliography language completed

--- source/jetbrains/mps/nodeEditor/CellLayout_Flow.java
+++ source/jetbrains/mps/nodeEditor/CellLayout_Flow.java
@@ -23,7 +23,7 @@ hStart |--------|                  |  | hEnd
   private int myWEnd = 0;
   private int myHEnd = 0;
   private int myRowCount = 1;
-  private int myMaxX = 200;
+  private int myMaxX = 400;
 
 
   public void setWStart(int WStart) {
