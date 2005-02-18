08b33fb4f6fd2af5af099abe2f01545803fbb7be
Konstantin Solomatov <kostik@jetbrains.com>
2005-02-18 17:01:18 +0300

    Initial version of plugin

---
+++ MPSPlugin/source/jetbrains/mps/plugin/MPSSupportHandler.java
@@ -0,0 +1,33 @@
+package jetbrains.mps.plugin;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.psi.PsiManager;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.search.GlobalSearchScope;
+
+/**
+ * @author Kostik
+ */
+public class MPSSupportHandler {
+  private Project myProject;
+
+  public MPSSupportHandler(Project project) {
+    myProject = project;
+  }
+
+  public boolean isAspectFileExist(final String namespace) {
+
+    final PsiClass[] aspects = new PsiClass[1];
+
+    ApplicationManager.getApplication().runReadAction(new Runnable() {
+      public void run() {
+        final PsiManager psiManager = PsiManager.getInstance(myProject);
+        aspects[0] = psiManager.findClass(namespace + ".Aspects", GlobalSearchScope.projectScope(myProject));
+      }
+    });
+
+    return aspects[0] != null;
+
+  }
+}
