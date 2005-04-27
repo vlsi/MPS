b904c47222ba5a0528422decd2630133f09510f4
Konstantin Solomatov <kostik@jetbrains.com>
2005-04-27 15:07:16 +0400

    Fixed MPS-112. DirectoryUtil

---
+++ source/jetbrains/mps/util/DirectoryUtil.java
@@ -0,0 +1,23 @@
+package jetbrains.mps.util;
+
+import jetbrains.mps.project.ApplicationComponents;
+import jetbrains.mps.ide.IdeMain;
+
+import javax.swing.*;
+import java.io.File;
+
+/**
+ * @author Kostik
+ */
+public class DirectoryUtil {
+  public static boolean askToCreateNewDirectory(File dir) {
+    if (!dir.exists()) {
+      int result = JOptionPane.showConfirmDialog(ApplicationComponents.getInstance()  .getComponent(IdeMain.class).getMainFrame(), "Do you want to create directory " + dir, "Directory doesn't exist", JOptionPane.INFORMATION_MESSAGE);
+      if(result != JOptionPane.OK_OPTION) {
+        return false;
+      }
+      dir.mkdirs();
+    }
+    return true;    
+  }
+}
