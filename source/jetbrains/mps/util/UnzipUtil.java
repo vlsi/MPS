1b11a7ca33ff3781f49b8913c219ad7e3b3e8860
Konstantin Solomatov <kostik@jetbrains.com>
2005-05-11 14:50:31 +0400

    Install plugin action

--- source/jetbrains/mps/util/UnzipUtil.java
+++ source/jetbrains/mps/util/UnzipUtil.java
@@ -9,28 +9,24 @@ import java.util.zip.ZipEntry;
  */
 public class UnzipUtil {
 
-  public static void unzip(File zipFile, File outputDir) {
-    try {
-      InputStream input = new BufferedInputStream(new FileInputStream(zipFile));
-      ZipInputStream zipInput = new ZipInputStream(input);
-      ZipEntry entry = null;
-      while ((entry = zipInput.getNextEntry()) != null) {
-        File entryFile = new File(outputDir.getPath() + File.separatorChar + entry.getName());
-        if (entry.isDirectory()) {
-          entryFile.mkdirs();
-        } else {
-          entryFile.getParentFile().mkdirs();
-          FileOutputStream output = new FileOutputStream(entryFile, false);
-          byte[] b = new byte[512];
-          int len = 0;
-          while ((len=zipInput.read(b))!= -1) {
-            output.write(b,0,len);
-          }
-          output.close();
+  public static void unzip(File zipFile, File outputDir) throws IOException {
+    InputStream input = new BufferedInputStream(new FileInputStream(zipFile));
+    ZipInputStream zipInput = new ZipInputStream(input);
+    ZipEntry entry = null;
+    while ((entry = zipInput.getNextEntry()) != null) {
+      File entryFile = new File(outputDir.getPath() + File.separatorChar + entry.getName());
+      if (entry.isDirectory()) {
+        entryFile.mkdirs();
+      } else {
+        entryFile.getParentFile().mkdirs();
+        FileOutputStream output = new FileOutputStream(entryFile, false);
+        byte[] b = new byte[512];
+        int len = 0;
+        while ((len=zipInput.read(b))!= -1) {
+          output.write(b,0,len);
         }
+        output.close();
       }
-    } catch (IOException e) {
-      e.printStackTrace();
     }
   }
 }
