fbab11d2d65db515d1a3e2fd84c445ba64c43870
Konstantin Solomatov <kostik@jetbrains.com>
2005-05-19 21:38:18 +0400

    Fixed find usage problem

--- source/jetbrains/mps/project/MPSProject.java
+++ source/jetbrains/mps/project/MPSProject.java
@@ -68,26 +68,7 @@ public class MPSProject implements ModelLocator, ModelOwner, LanguageOwner {
   public Set<SModelDescriptor> getAllModelDescriptors() {
     init();
 
-    //do not comment out this code
-    //language models loading different from project models
-    //languages load only needed models, so not all models may be listed here
-    Set<SModelDescriptor> result = new HashSet<SModelDescriptor>();
-
-    RootManager rootManager = getRootManager();
-
-    result.addAll(rootManager.getProjectModelDescriptors());
-    result.addAll(rootManager.getLibraryModelDescriptors());
-
-    for (Language l : rootManager.getLanguages()) {
-      if (l.getActionsModelDescriptor() != null) result.add(l.getActionsModelDescriptor());
-      if (l.getEditorContextModelDescriptor() != null) result.add(l.getEditorContextModelDescriptor());
-      if (l.getEditorDescriptor() != null) result.add(l.getEditorDescriptor());
-      if (l.getStructureModelDescriptor() != null) result.add(l.getStructureModelDescriptor());
-      if (l.getTypesystemModelDescriptor() != null) result.add(l.getTypesystemModelDescriptor());
-      result.addAll(l.getLibraryModels());
-    }
-
-    return result;
+    return SModelRepository.getInstance().getAllModelDescriptors();
   }
 
   public Collection<Language> getLanguages() {
