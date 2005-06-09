42cafd2efc0cf66bca27cd8c76ccf8559e2674fa
Konstantin Solomatov <kostik@jetbrains.com>
2005-06-09 16:46:41 +0400

    More icons

--- source/jetbrains/mps/ide/projectPane/Icons.java
+++ source/jetbrains/mps/ide/projectPane/Icons.java
@@ -13,6 +13,9 @@ public class Icons {
   public static final Icon DEFAULT_ICON = new ImageIcon(Icons.class.getResource("nodes/default.png"));
   public static final Icon STRUCTURE_ICON = new ImageIcon(Icons.class.getResource("nodes/structure.png"));
   public static final Icon ENUM_ICON = new ImageIcon(Icons.class.getResource("nodes/enum.png"));
+  public static final Icon TEMPLATE_ICON = new ImageIcon(Icons.class.getResource("nodes/template.png"));
+  public static final Icon MAPPING_ICON = new ImageIcon(Icons.class.getResource("nodes/mapping.png"));
+  public static final Icon SWITCH_ICON = new ImageIcon(Icons.class.getResource("nodes/switch.png"));
 
   public static final Icon OPENED_FOLDER = new ImageIcon(Icons.class.getResource("other/openedFolder.png"));
   public static final Icon CLOSED_FOLDER = new ImageIcon(Icons.class.getResource("other/closedFolder.png"));
