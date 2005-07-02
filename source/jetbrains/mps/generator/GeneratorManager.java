e83a90a60886ba1ef41031550b2a349f90927ade
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2005-07-02 22:43:08 +0400

    update modelQueryLanguage

--- source/jetbrains/mps/generator/GeneratorManager.java
+++ source/jetbrains/mps/generator/GeneratorManager.java
@@ -49,7 +49,7 @@ import java.util.List;
 public class GeneratorManager implements ExternalizableComponent, ComponentWithPreferences, LanguageOwner, ModelOwner {
   public static final Logger LOG = Logger.getLogger(GeneratorManager.class);
 
-  private static final boolean SAVE_TRANSIENT_MODELS = false;
+  private static final boolean SAVE_TRANSIENT_MODELS = true; //false;
   public static final String COMPILE_ON_GENERATION = "compile-on-generation";
 
   private MPSProject myProject;
