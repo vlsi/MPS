7355571d9ad8c824f64cf5dbae095025b5b233ba
Cyril Konopko <Cyril.Konopko@jetbrains.com>
2005-07-13 20:13:20 +0400

    textual copy-paste added (Shift-Ctrl-c, Shift-Ctrl-v)

---
+++ source/jetbrains/mps/datatransfer/TextCopyPasteUtil.java
@@ -0,0 +1,59 @@
+package jetbrains.mps.datatransfer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Cyril.Konopko
+ * Date: 13.07.2005
+ * Time: 19:29:37
+ * To change this template use File | Settings | File Templates.
+ */
+import java.awt.datatransfer.*;
+import java.awt.*;
+import java.io.IOException;
+
+public class TextCopyPasteUtil {
+
+  static class StringContent implements Transferable  {
+
+    private String myString;
+
+    public StringContent(String s) {
+      myString = new String(s);
+    }
+
+    public DataFlavor[] getTransferDataFlavors() {
+      DataFlavor[] dataFlavors = new DataFlavor[1];
+      dataFlavors[0] = DataFlavor.stringFlavor;
+      return dataFlavors;
+    }
+
+    public boolean isDataFlavorSupported(DataFlavor flavor) {
+      return (flavor == DataFlavor.stringFlavor);
+    }
+
+    public Object getTransferData(DataFlavor flavor) throws UnsupportedFlavorException, IOException {
+      if (!isDataFlavorSupported(flavor)) throw new UnsupportedFlavorException(flavor);
+      return myString;
+    }
+  }
+
+  public static void copyStringToClipboard(String s) {
+    Clipboard cb = Toolkit.getDefaultToolkit().getSystemClipboard();
+    StringContent stringContent = new StringContent(s);
+    cb.setContents(stringContent, null);
+  };
+
+  public static String getStringFromClipboard () {
+    Clipboard cb = Toolkit.getDefaultToolkit().getSystemClipboard();
+    Transferable content = cb.getContents(null);
+    if (content == null) return null;
+    String result;
+    try {
+      result = (String) content.getTransferData(DataFlavor.stringFlavor);
+    } catch(Exception e) {
+      return null;
+    }
+    return result;
+  };
+
+}
