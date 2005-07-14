9757922d4ea179d230e44187989012b12b1cbd99
Cyril Konopko <Cyril.Konopko@jetbrains.com>
2005-07-14 15:09:29 +0400

    shift and ctrl-shift replaced with each other

--- source/jetbrains/mps/nodeEditor/NodeRangeSelection.java
+++ source/jetbrains/mps/nodeEditor/NodeRangeSelection.java
@@ -34,7 +34,7 @@ public class NodeRangeSelection implements IKeyboardHandler {
 
   public boolean isSelectionKeystroke(KeyEvent keyEvent) {
     // multiple selection...
-    boolean selectionKeystroke = keyEvent.isShiftDown() && !(keyEvent.isAltDown() || keyEvent.isControlDown()) &&
+    boolean selectionKeystroke = keyEvent.isShiftDown() && keyEvent.isControlDown() && !(keyEvent.isAltDown()) &&
             (keyEvent.getKeyCode() == KeyEvent.VK_UP ||
             keyEvent.getKeyCode() == KeyEvent.VK_DOWN ||
             keyEvent.getKeyCode() == KeyEvent.VK_LEFT ||
