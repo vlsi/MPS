ff4127a5a34896a8cb419f38fc5edabd4f575239
Cyril Konopko <Cyril.Konopko@jetbrains.com>
2005-07-31 14:02:49 +0400

    external resolver written

--- source/jetbrains/mps/resolve/ExternalResolver.java
+++ source/jetbrains/mps/resolve/ExternalResolver.java
@@ -4,6 +4,10 @@ import jetbrains.mps.smodel.SReference;
 import jetbrains.mps.smodel.ExternalReference;
 import jetbrains.mps.smodel.SModel;
 import jetbrains.mps.smodel.SNode;
+import jetbrains.mps.logging.Logger;
+
+import java.lang.reflect.Method;
+import java.util.Collection;
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,14 +18,53 @@ import jetbrains.mps.smodel.SNode;
  */
 public class ExternalResolver {
 
+  public static final String METHOD_NAME_PREFIX  = "getExtResolveInfoForTargetClass";
+  public static final String CLASS_NAME_SUFFIX = ".resolve.ExternalResolver";
+
+  private static final Logger LOG = Logger.getLogger(ExternalResolver.class);
+
+
+
   public static String createExternalResolveInfo(SReference reference) {
-    //todo! implement
-    SNode sourceNode = reference.getSourceNode();
-    String packageName = sourceNode.getClass().getPackage().getName();
 
+    LOG.assertLog(reference instanceof ExternalReference, "reference to resolve externally is not external");
 
+    ExternalReference externalReference = (ExternalReference) reference;
+
+    SNode targetNode = reference.getTargetNode();
+    if (targetNode == null) {
+      return externalReference.getExtResolveInfo();
+    }
+
+    return getExternalResolveInfo_internal(targetNode);
+
+  }
+
+  private static String getExternalResolveInfo_internal(SNode targetNode) {
+    String packageName = targetNode.getClass().getPackage().getName();
+    Class externalResolver = null;
+    try {
+      externalResolver = Class.forName(packageName + CLASS_NAME_SUFFIX);
+    } catch (ClassNotFoundException e) {
+      LOG.error("external resolver not found in package " + packageName, e);
+      return null;
+    }
+
+    Class targetCls = targetNode.getClass();
+    while (targetCls != SNode.class) {
+      try {
+        String targetClassName = targetCls.getName();
+        targetClassName = targetClassName.substring(targetClassName.lastIndexOf(".") + 1);
+        Method externalResolveMethod = externalResolver.getMethod(METHOD_NAME_PREFIX + targetClassName, targetCls);
+        return (String) externalResolveMethod.invoke(null, targetNode);
+      } catch (Exception e) {
+        targetCls = targetCls.getClass();
+      }
+    }
     return null;
   }
+
+
 
   public static void setExternalResolveInfo(SReference reference) {
     if (!(reference instanceof ExternalReference)) return;
@@ -29,9 +72,17 @@ public class ExternalResolver {
     externalReference.setExtResolveInfo(createExternalResolveInfo(reference));
   }
 
-  public static SNode getTargetNode(SModel model, String extResolveInfo) {
-    return null; //todo: implement
 
+  
+  public static SNode getTargetNode(SModel model, String extResolveInfo) {
+    if (extResolveInfo == null) return null;
+
+    Collection<? extends SNode> nodes = model.getAllNodes();
+    for (SNode node : nodes) {
+      String nodeExtResolveInfo = getExternalResolveInfo_internal(node);
+      if (extResolveInfo.equals(nodeExtResolveInfo)) return node;
+    }
+    return null;
   }
 
 }
