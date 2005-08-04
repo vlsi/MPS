5c08f3c26145c9089d3b3bfa0da9aa855025b417
Cyril Konopko <Cyril.Konopko@jetbrains.com>
2005-08-04 16:53:10 +0400

    sreference assertion bug fixed

--- source/jetbrains/mps/smodel/SReference.java
+++ source/jetbrains/mps/smodel/SReference.java
@@ -116,7 +116,7 @@ public abstract class SReference {
 
       localNodeId = targetNodeId.substring(offset + 1);
 
-      if (extResolveInfo != null) {
+      if (extResolveInfo != null || !extResolveInfo.equals("")) {
         int offset1 = extResolveInfo.indexOf('.');
         LOG.assertLog(targetNodeId.substring(0, offset).equals(extResolveInfo.substring(0, offset1)));
         extResolveInfo = extResolveInfo.substring(offset1 + 1);
