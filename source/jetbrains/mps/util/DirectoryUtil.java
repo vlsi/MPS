febf77ededfc7e971272f3997ee60f308e6f7cfb
Konstantin Solomatov <kostik@jetbrains.com>
2005-09-21 21:04:33 +0400

    Remove some usage of ProjectWindow as ApplicationComponent

--- source/jetbrains/mps/util/DirectoryUtil.java
+++ source/jetbrains/mps/util/DirectoryUtil.java
@@ -5,19 +5,20 @@ import jetbrains.mps.ide.ProjectWindow;
 
 import javax.swing.*;
 import java.io.File;
+import java.awt.*;
 
 /**
  * @author Kostik
  */
 public class DirectoryUtil {
-  public static boolean askToCreateNewDirectory(File dir) {
+  public static boolean askToCreateNewDirectory(Frame mainFrame, File dir) {
     if (!dir.exists()) {
-      int result = JOptionPane.showConfirmDialog(ApplicationComponents.getInstance().getComponent(ProjectWindow.class).getMainFrame(), "Do you want to create directory " + dir, "Directory doesn't exist", JOptionPane.INFORMATION_MESSAGE);
+      int result = JOptionPane.showConfirmDialog(mainFrame, "Do you want to create directory " + dir, "Directory doesn't exist", JOptionPane.INFORMATION_MESSAGE);
       if(result != JOptionPane.OK_OPTION) {
         return false;
       }
       dir.mkdirs();
     }
-    return true;    
+    return true;
   }
 }
