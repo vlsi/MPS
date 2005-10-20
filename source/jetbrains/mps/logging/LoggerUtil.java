fdf49a9c559eb3499961ed627bc9772516b3d253
Konstantin Solomatov <kostik@jetbrains.com>
2005-10-20 18:22:04 +0400

    Basic webapp functionality completed

--- source/jetbrains/mps/logging/LoggerUtil.java
+++ source/jetbrains/mps/logging/LoggerUtil.java
@@ -8,14 +8,9 @@ import jetbrains.mps.util.PathManager;
 
 public class LoggerUtil {
   public static void configureLogger() {
-    System.err.println("MPS home = " + PathManager.getHomePath());
-
     if (new File(PathManager.getHomePath() + File.separator + "log" + File.separator + "userlog.xml").exists()) {
       DOMConfigurator.configure(PathManager.getHomePath() + File.separator + "log" + File.separator + "userlog.xml");
     } else {
-
-      System.err.println("configuring with : " + PathManager.getHomePath() + File.separator + "log" + File.separator + "log.xml");
-
       DOMConfigurator.configure(PathManager.getHomePath() + File.separator + "log" + File.separator + "log.xml");
     }
   }
