5a5acd44568507cd4128f8db2aa8e016a7bd60db
Konstantin Solomatov <kostik@jetbrains.com>
2005-10-26 20:11:40 +0400

    MPSWiki update

---
+++ source/jetbrains/mps/smodel/event/SModelEventVisitorAdapter.java
@@ -0,0 +1,19 @@
+package jetbrains.mps.smodel.event;
+
+public class SModelEventVisitorAdapter implements SModelEventVisitor {
+  public void visitRootEvent(SModelRootEvent event) {
+
+  }
+
+  public void visitChildEvent(SModelChildEvent event) {
+
+  }
+
+  public void visitPropertyEvent(SModelPropertyEvent event) {
+
+  }
+
+  public void visitReferenceEvent(SModelReferenceEvent event) {
+
+  }
+}
