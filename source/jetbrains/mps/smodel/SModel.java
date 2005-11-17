3f3af26d830888d3bf420022f938da99571ce438
Konstantin Solomatov <kostik@jetbrains.com>
2005-11-17 15:10:04 +0300

    Get rid of unnecessary locks. Created command filter

--- source/jetbrains/mps/smodel/SModel.java
+++ source/jetbrains/mps/smodel/SModel.java
@@ -478,8 +478,7 @@ public class SModel implements Iterable<SNode> {
     return modelsList.iterator();
   }
 
-  /*package*/
-  Iterator<ImportElement> importElements() {
+  /*package*/ Iterator<ImportElement> importElements() {
     return myImports.iterator();
   }
 
