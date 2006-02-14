f596c3dcf4421feda0084829f8199b8c77bf3157
Cyril Konopko <Cyril.Konopko@jetbrains.com>
2006-02-14 20:10:12 +0300

    left highlighter undone

--- source/jetbrains/mps/nodeEditor/LeftEditorHighlighter.java
+++ source/jetbrains/mps/nodeEditor/LeftEditorHighlighter.java
@@ -1,6 +1,7 @@
 package jetbrains.mps.nodeEditor;
 
 import jetbrains.mps.logging.Logger;
+import jetbrains.mps.util.Pair;
 
 import java.util.*;
 import java.awt.Color;
@@ -21,37 +22,28 @@ public class LeftEditorHighlighter {
   private AbstractEditorComponent myEditorComponent;
   private int myMinWidth = 10;
   private int myWidth = myMinWidth;
-  private HighlighterBracket myRootBracket;
+
+  private WeakHashMap<CellInfo, HighlighterBracket> myBrackets;
+
 
 
   public LeftEditorHighlighter(AbstractEditorComponent abstractEditorComponent) {
     myEditorComponent = abstractEditorComponent;
-    myRootBracket = new HighlighterBracket(0, myEditorComponent.getHeight(), Color.BLACK, abstractEditorComponent);
   }
 
 
   public void paint(Graphics g) {   //does not paint root bracket
-    for (HighlighterBracket bracket : myRootBracket) {
-      bracket.paint(g, true);
-    }
+    //todo
   }
 
-
-  private HighlighterBracket findBracket(EditorCell cell) {
-    return myRootBracket.findBracket(cell);
-  }
 
   public void unHighlight(EditorCell cell) {
-    HighlighterBracket bracket = findBracket(cell);
-    if (bracket != null) {
-      bracket.delete();
-    }
+    //todo
   }
 
   public void highlight(EditorCell cell, Color c) {
     if (cell.getEditor() != myEditorComponent) throw new IllegalArgumentException("cell must be from my editor");
-    if (findBracket(cell) != null) return;
-    myRootBracket.addBracket(new HighlighterBracket(cell, c, myEditorComponent));
+    //todo
   }
 
   public int getWidth() {
@@ -64,22 +56,17 @@ public class LeftEditorHighlighter {
   }
 
   public void relayoutWidth() {
-    myRootBracket.paint(null, false);
-    int currentWidth = myRootBracket.getCurrentWidth();
-    currentWidth += BRACKETS_WIDTH;
-    myWidth = currentWidth;
+     //todo
+   // myWidth = currentWidth;
     if (myWidth < myMinWidth) myWidth = myMinWidth;
-    myRootBracket.setX(myWidth);
   }
 
   public void relayoutHeight() {
-    myRootBracket.setY2(myEditorComponent.getHeight());
-    myRootBracket.relayout();
+
   }
 
-  private static class HighlighterBracket implements Iterable<HighlighterBracket> {
-    private List<HighlighterBracket> myBrackets = new ArrayList<HighlighterBracket>();
-    private HighlighterBracket myParent;
+  private static class HighlighterBracket {
+
     private int myY1;
     private int myY2;
     private int myX;//right edge
@@ -89,10 +76,14 @@ public class LeftEditorHighlighter {
     private AbstractEditorComponent myEditor;
 
     public HighlighterBracket(EditorCell cell, Color c, AbstractEditorComponent editorComponent) {
+      this(new CellInfo(cell), c, editorComponent);
+    }
+
+    public HighlighterBracket(CellInfo cellInfo, Color c, AbstractEditorComponent editorComponent) {
       myColor = c;
       myEditor = editorComponent;
-      myEditorCellInfo = new CellInfo(cell);
-      doRelayout();
+      myEditorCellInfo = cellInfo;
+      relayout();
     }
 
     public  HighlighterBracket(int y1, int y2, Color c, AbstractEditorComponent editorComponent) {
@@ -104,47 +95,21 @@ public class LeftEditorHighlighter {
       myY2 = y2;
     }
 
-    private void doRelayout() {
+    public void relayout() {
       if (myEditorCellInfo == null) return;
       EditorCell cell = getCell();
       if (cell != null) {
         myY1 = cell.getY();
         myY2 = cell.getY() + cell.getHeight();
-      } else {
-        delete();
       }
-    }
-
-    public HighlighterBracket findBracket(EditorCell cell) {
-      CellInfo cellInfo = new CellInfo(cell);
-      if (myEditorCellInfo != null && myEditorCellInfo.equals(cellInfo)) return this;
-      for (HighlighterBracket child : this) {
-        HighlighterBracket bracket = child.findBracket(cell);
-        if (bracket != null) return bracket;
-      }
-      return null;
     }
 
     private EditorCell getCell() {
       return myEditorCellInfo.findCell(myEditor);
     }
 
-    public void relayout() {
-      doRelayout();
-      for (HighlighterBracket highlighterBracket : this) {
-        highlighterBracket.relayout();
-      }
-    }
-
-    public Iterator<HighlighterBracket> iterator() {
-      return myBrackets.iterator();
-    }
-
     public void setX(int x) {
       myX = x;
-      for (HighlighterBracket bracket : myBrackets) {
-        bracket.setX(x);
-      }
     }
 
     public void setY2(int y2) {
@@ -152,30 +117,8 @@ public class LeftEditorHighlighter {
       myY2 = y2;
     }
 
-    public HighlighterBracket getParent() {
-      return myParent;
-    }
-
-    public boolean isEmpty() {
-      return myBrackets.isEmpty();
-    }
-
-    public HighlighterBracket getRoot() {
-      HighlighterBracket root = this;
-      while (root.myParent != null) {
-        root = root.myParent;
-      }
-      return root;
-    }
-
     public int getCurrentWidth() {
       return myCurrentWidth;
-    }
-
-    private void detach() {
-      if (getParent() == null) return;
-      getParent().myBrackets.remove(this);
-      myParent = null;
     }
 
     private boolean isInside(HighlighterBracket bracket) {
@@ -186,57 +129,12 @@ public class LeftEditorHighlighter {
       return bracket.myY1 >= myY1 && bracket.myY2 <= myY2;
     }
 
-    //adds as if this is root
-    public void addBracket(HighlighterBracket newBracket) {
-      newBracket.detach();
-      for (HighlighterBracket bracket : myBrackets) {
-        if (newBracket.isInside(bracket)) {
-          bracket.addBracket(newBracket);
-          return;
-        }
-      }
-
-      if (!isOutside(newBracket)) {
-        LOG.error("can't add overlapping bracket");
-        return;
-      }
-      myBrackets.add(newBracket);
-      newBracket.myParent = this;
-      if (newBracket.myY1 - myY1 <= BRACKETS_WIDTH + 1) newBracket.myY1 = myY1 + BRACKETS_WIDTH + 1;
-      if (myY2 - newBracket.myY2 <= BRACKETS_WIDTH + 1) newBracket.myY2 = myY2 - (BRACKETS_WIDTH + 1);
-    }
-
-
-    // parent must not be null: never call delete on the root cell
-    public void delete() {
-      if (myParent == null) {
-        LOG.error("parent is null, delete is called from inconsistent state");
-      }
-      myParent.myBrackets.remove(this);
-      for (HighlighterBracket child : this) {
-        child.myParent = myParent;
-        myParent.myBrackets.add(child);
-      }
-      myBrackets.clear();
-      myParent = null;
-    }
-
-    public void paint(Graphics g, boolean reallyPaint) {
+    public void paint(Graphics g) {
       int currentWidth = myCurrentWidth;
-      for (HighlighterBracket child : myBrackets) {
-        child.paint(g, reallyPaint);
-      }
-      for (HighlighterBracket child : myBrackets) {
-        currentWidth = Math.max(currentWidth, child.myCurrentWidth + 1 + BRACKETS_WIDTH);
-      }
-      myCurrentWidth = currentWidth;
-
-      if (reallyPaint) {
-        g.setColor(myColor);
-        g.fillRect(myX - currentWidth + BRACKETS_WIDTH, myY1, currentWidth - BRACKETS_WIDTH, BRACKETS_WIDTH);
-        g.fillRect(myX - currentWidth, myY1, BRACKETS_WIDTH, myY2 - myY1);
-        g.fillRect(myX - currentWidth + BRACKETS_WIDTH, myY2 - BRACKETS_WIDTH, currentWidth - BRACKETS_WIDTH, BRACKETS_WIDTH);
-      }
+      g.setColor(myColor);
+      g.fillRect(myX - currentWidth + BRACKETS_WIDTH, myY1, currentWidth - BRACKETS_WIDTH, BRACKETS_WIDTH);
+      g.fillRect(myX - currentWidth, myY1, BRACKETS_WIDTH, myY2 - myY1);
+      g.fillRect(myX - currentWidth + BRACKETS_WIDTH, myY2 - BRACKETS_WIDTH, currentWidth - BRACKETS_WIDTH, BRACKETS_WIDTH);
     }
   }
 
