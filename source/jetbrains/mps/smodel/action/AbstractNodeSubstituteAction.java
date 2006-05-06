5ba68b34a8c92ec6ba5bb85ff9a3b4a175dec22f
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2006-05-06 04:30:10 +0400

    editor lang editor: rewrite actions

---
+++ source/jetbrains/mps/smodel/action/AbstractNodeSubstituteAction.java
@@ -0,0 +1,14 @@
+package jetbrains.mps.smodel.action;
+
+import jetbrains.mps.nodeEditor.AbstractNodeSubstituteItem;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Igoor
+ * Date: May 5, 2006
+ * Time: 7:41:32 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public abstract class AbstractNodeSubstituteAction extends AbstractNodeSubstituteItem implements INodeSubstituteAction {
+  
+}
