de8d6fb4b95f7caf397748c7ffddf14e4711f835
Cyril Konopko <Cyril.Konopko@jetbrains.com>
2006-06-23 18:02:18 +0400

    icons added

--- source/jetbrains/mps/ide/projectPane/Icons.java
+++ source/jetbrains/mps/ide/projectPane/Icons.java
@@ -63,6 +63,7 @@ public class Icons {
   public static final Icon DERIVATION_NODE_ICON = new ImageIcon(Icons.class.getResource("nodes/gadget.png"));
   public static final Icon INFER_FOUND_ICON = new ImageIcon(Icons.class.getResource("nodes/inferFound.png"));
   public static final Icon INFER_FAILED_ICON = new ImageIcon(Icons.class.getResource("nodes/inferFailed.png"));
+  public static final Icon INFER_QUESTION_ICON = new ImageIcon(Icons.class.getResource("nodes/inferQuestion.png"));
 
   public static final Icon INSPECTOR_ICON = new ImageIcon(Icons.class.getResource("inspector.png"));
 
