e76450a0857d4c86f89e51697d8e4bd1be010436
Konstantin Solomatov <kostik@jetbrains.com>
2006-07-14 18:05:31 +0400

    add nice antialiasing for lines

--- source/jetbrains/mps/nodeEditor/AbstractEditorComponent.java
+++ source/jetbrains/mps/nodeEditor/AbstractEditorComponent.java
@@ -1161,7 +1161,10 @@ public abstract class AbstractEditorComponent extends JComponent implements Scro
     if (EditorSettings.getInstance().isUseAntialiasing()) {
       g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
               RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+      g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+      g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
     }
+
 
 
     g.setColor(getBackground());
