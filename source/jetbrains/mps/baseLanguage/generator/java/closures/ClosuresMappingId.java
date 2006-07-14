9acb934f10a03e56b7e452a6bbc9ce718af55cc6
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2006-07-14 23:15:17 +0400

    BL: implementing closures

--- source/jetbrains/mps/baseLanguage/generator/java/closures/ClosuresMappingId.java
+++ source/jetbrains/mps/baseLanguage/generator/java/closures/ClosuresMappingId.java
@@ -6,8 +6,8 @@ package jetbrains.mps.baseLanguage.generator.java.closures;
  */
 public interface ClosuresMappingId {
   static String CLOSURE__ADAPTER_CLASS = "closure_adapterClass";
-  static String METHOD__CLOSURE_CONTEXT__CLASS = "method_closureContext_class";
-  static String METHOD__CLOSURE_CONTEXT__VARIABLE_DECL_STMT = "method_closureContext_variable";
+  static String CONTEXT_OWNER__CLOSURE_CONTEXT__CLASS = "contextOwner_closureContext_class";
+  static String CONTEXT_OWNER__CLOSURE_CONTEXT__VARIABLE_DECL_STMT = "contextOwner_closureContext_variable";
   static String VARIABLE__CLOSURE_CONTEXT__CLASS_FIELD = "variable_closureContext_classField";
 
   static String NAME__CLOSURE_ADAPTER__ENCLOSING_CLASS_FIELD = "_enclosingClass";
