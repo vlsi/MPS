4610fc00af0e5dff30247966df5d8a527de6b693
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2006-09-22 00:04:58 +0400

    NodeFactoryManager: adding extra parameters to factory method

--- source/jetbrains/mps/smodel/action/NodeFactoryManager.java
+++ source/jetbrains/mps/smodel/action/NodeFactoryManager.java
@@ -20,21 +20,25 @@ import java.util.LinkedList;
  */
 public class NodeFactoryManager extends NodeFactoryManager_deprecated {
 
-  public static SNode createNode(String conceptFqName, SNode sampleNode, SModel model, IScope scope) {
-    ConceptDeclaration conceptDeclaration = SModelUtil.findConceptDeclaration(conceptFqName, scope);
-    return createNode(conceptDeclaration, sampleNode, model);
+  public static SNode createNode(ConceptDeclaration concept, SNode sampleNode, SModel model) {
+    return createNode(concept, sampleNode, null, model);
   }
 
-  public static SNode createNode(ConceptDeclaration nodeConcept, SNode sampleNode, SModel model) {
+  public static SNode createNode(String conceptFqName, SNode sampleNode, SNode enclosingNode, SModel model, IScope scope) {
+    ConceptDeclaration conceptDeclaration = SModelUtil.findConceptDeclaration(conceptFqName, scope);
+    return createNode(conceptDeclaration, sampleNode, enclosingNode, model);
+  }
+
+  public static SNode createNode(ConceptDeclaration nodeConcept, SNode sampleNode, SNode enclosingNode, SModel model) {
     SNode node = SModelUtil.instantiateConceptDeclaration(nodeConcept, model);
-    boolean done = setupNode(nodeConcept, node, sampleNode);
+    boolean done = setupNode(nodeConcept, node, sampleNode, enclosingNode, model);
     if (!done) {
       setupNode_deprecated(nodeConcept, node, sampleNode);
     }
     return node;
   }
 
-  private static boolean setupNode(ConceptDeclaration nodeConcept, SNode newNode, SNode sampleNode) {
+  private static boolean setupNode(ConceptDeclaration nodeConcept, SNode newNode, SNode sampleNode, SNode enclosingNode, SModel model) {
 
     // find node factory
     List<NodeFactory> nodeFactories = new LinkedList<NodeFactory>();
@@ -58,18 +62,17 @@ public class NodeFactoryManager extends NodeFactoryManager_deprecated {
 
     // setup node
     for (NodeFactory factory : nodeFactories) {
-      invokeNodeSetupFunction(factory, newNode, sampleNode);
+      invokeNodeSetupFunction(factory, newNode, sampleNode, enclosingNode, model);
     }
     return true;
   }
 
-  private static void invokeNodeSetupFunction(NodeFactory factory, SNode newNode, SNode sampleNode) {
+  private static void invokeNodeSetupFunction(NodeFactory factory, SNode newNode, SNode sampleNode, SNode enclosingNode, SModel model) {
     NodeSetupFunction setupFunction = factory.getSetupFunction();
     if (setupFunction == null) return;
 
     String methodName = ActionQueryMethodName.nodeFactory_NodeSetupFunction(factory);
     Object[] args = new Object[]{newNode, sampleNode};
-    SModel model = factory.getModel();
-    QueryMethodGenerated.invoke(methodName, args, model, true);
+    QueryMethodGenerated.invoke(methodName, args, factory.getModel(), true);
   }
 }
