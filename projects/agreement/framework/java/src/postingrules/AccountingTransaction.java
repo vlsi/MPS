a72d4ea59dea68369bed32a591ce68f72dcb43f5
Konstantin Solomatov <kostik@jetbrains.com>
2006-10-13 15:56:17 +0400

    fix more inspections warnings

--- projects/agreement/framework/java/src/postingrules/AccountingTransaction.java
+++ projects/agreement/framework/java/src/postingrules/AccountingTransaction.java
@@ -1,17 +1,13 @@
 package postingrules;
 
-import mf.*;
-
-import java.util.*;
+import mf.MfDate;
+import mf.Money;
 
 public class AccountingTransaction {
-    private Collection<Entry> myEntries = new HashSet<Entry>();
     public AccountingTransaction(Money amount, Account from, Account to, MfDate date) {
         Entry fromEntry = new Entry(amount.negate(), date);
         from.addEntry(fromEntry);
-        myEntries.add(fromEntry);
         Entry toEntry = new Entry(amount, date);
         to.addEntry(toEntry);
-        myEntries.add(toEntry);
     }
 }
