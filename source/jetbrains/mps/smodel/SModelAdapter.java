9d1d601c088c6c46f41ff7e3de59488b3a568d1a
Cyril Konopko <Cyril.Konopko@jetbrains.com>
2006-11-07 23:22:19 +0300

    helgins highlighter bug fixed

--- source/jetbrains/mps/smodel/SModelAdapter.java
+++ source/jetbrains/mps/smodel/SModelAdapter.java
@@ -7,53 +7,65 @@ import jetbrains.mps.smodel.event.*;
  */
 public class SModelAdapter implements SModelListener {
   public void languageAdded(SModeLanguageEvent event) {
+    eventFired(event);
     modelChanged(event.getModel());
   }
 
   public void languageRemoved(SModeLanguageEvent event) {
+    eventFired(event);
     modelChanged(event.getModel());
   }
 
   public void importAdded(SModeImportEvent event) {
+    eventFired(event);
     modelChanged(event.getModel());
   }
 
   public void importRemoved(SModeImportEvent event) {
+    eventFired(event);
     modelChanged(event.getModel());
   }
 
   public void rootAdded(SModelRootEvent event) {
+    eventFired(event);
     modelChangedDramatically(event.getModel());
   }
 
   public void rootRemoved(SModelRootEvent event) {
+    eventFired(event);
     modelChangedDramatically(event.getModel());
   }
 
   public void beforeRootRemoved(SModelRootEvent event) {
-
   }
 
   public void propertyChanged(SModelPropertyEvent event) {
+    eventFired(event);
     modelChanged(event.getModel());
   }
 
   public void childAdded(SModelChildEvent event) {
+    eventFired(event);
     modelChangedDramatically(event.getModel());
   }
 
   public void childRemoved(SModelChildEvent event) {
+    eventFired(event);
     modelChangedDramatically(event.getModel());
   }
 
   public void referenceAdded(SModelReferenceEvent event) {
+    eventFired(event);
     modelChangedDramatically(event.getModel());
   }
 
   public void referenceRemoved(SModelReferenceEvent event) {
+    eventFired(event);
     modelChangedDramatically(event.getModel());
   }
 
+  public void eventFired(SModelEvent event) {
+  }
 
   public void modelChanged(SModel model) {
   }
