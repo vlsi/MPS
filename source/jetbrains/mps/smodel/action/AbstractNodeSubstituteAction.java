f6621b705b824a2b266abb6e6d65d3d9ddd0b550
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2006-11-28 19:16:25 +0300

    removeing deprecations from editor

--- source/jetbrains/mps/smodel/action/AbstractNodeSubstituteAction.java
+++ source/jetbrains/mps/smodel/action/AbstractNodeSubstituteAction.java
@@ -1,17 +1,16 @@
 package jetbrains.mps.smodel.action;
 
-import jetbrains.mps.nodeEditor.AbstractNodeSubstituteItem;
+import jetbrains.mps.logging.Logger;
 import jetbrains.mps.smodel.SNode;
 import jetbrains.mps.smodel.presentation.NodePresentationUtil;
 
 /**
- * Created by IntelliJ IDEA.
- * User: Igoor
- * Date: May 5, 2006
- * Time: 7:41:32 PM
- * To change this template use File | Settings | File Templates.
+ * Igor Alshannikov
+ * May 5, 2006
  */
-public abstract class AbstractNodeSubstituteAction extends AbstractNodeSubstituteItem implements INodeSubstituteAction {
+public abstract class AbstractNodeSubstituteAction implements INodeSubstituteAction {
+  private static final Logger LOG = Logger.getLogger(AbstractNodeSubstituteAction.class);
+
   private SNode mySourceNode;
   private SNode myParameterNode;
 
@@ -44,5 +43,33 @@ public abstract class AbstractNodeSubstituteAction extends AbstractNodeSubstitut
 
   public String getDescriptionText(String pattern) {
     return NodePresentationUtil.descriptionText(getParameterNode());
+  }
+
+  public boolean canSubstituteStrictly(String pattern) {
+    if (pattern == null || getMatchingText(pattern) == null) return false;
+    return getMatchingText(pattern).equals(pattern);
+  }
+
+  /**
+   * @param pattern . NULL if pattern is not available yet
+   */
+  public boolean canSubstitute(String pattern) {
+    if (pattern == null || pattern.length() == 0) {
+      return true;
+    }
+    String matchingText = null;
+    try {
+      matchingText = getMatchingText(pattern);
+    } catch (Exception e) {
+      LOG.error(e);
+    }
+    if (matchingText == null || matchingText.length() == 0) return false;
+    // first char must be same
+    if (matchingText.charAt(0) != pattern.charAt(0)) return false;
+    return matchingText.toUpperCase().startsWith(pattern.toUpperCase());
+  }
+
+  public String toString() {
+    return getMatchingText("");
   }
 }
