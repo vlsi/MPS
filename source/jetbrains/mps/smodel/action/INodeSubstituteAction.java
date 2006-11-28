67fdc3c348ce84c87a0ad4d8c11b8e5df1bed225
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2006-11-28 21:51:04 +0300

    removeing deprecations from editor

--- source/jetbrains/mps/smodel/action/INodeSubstituteAction.java
+++ source/jetbrains/mps/smodel/action/INodeSubstituteAction.java
@@ -1,16 +1,30 @@
 package jetbrains.mps.smodel.action;
 
-import jetbrains.mps.nodeEditor.INodeSubstituteItem;
 import jetbrains.mps.smodel.SNode;
 
 /**
- * Created by IntelliJ IDEA.
- * User: Igoor
- * Date: Mar 29, 2005
- * Time: 1:18:56 PM
- * To change this template use File | Settings | File Templates.
+ * Igor Alshannikov
+ * Mar 29, 2005
  */
-public interface INodeSubstituteAction extends INodeSubstituteItem {
+public interface INodeSubstituteAction {
   SNode getSourceNode();
+
   SNode getParameterNode();
+
+  String getMatchingText(String pattern);
+
+  String getDescriptionText(String pattern);
+
+  boolean canSubstituteStrictly(String pattern);
+
+  /**
+   * @param pattern . NULL if pattern is not available yet
+   */
+  boolean canSubstitute(String pattern);
+
+  /**
+   * @param pattern . NULL if pattern is not available yet
+   * @return node which this action has created. Returns NULL if no new nodes have been created.
+   */
+  SNode doSubstitute(String pattern);
 }
