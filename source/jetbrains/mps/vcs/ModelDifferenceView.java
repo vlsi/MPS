a70f89f7175d8831ca05580a19966561fba68e8c
Konstantin Solomatov <kostik@jetbrains.com>
2007-01-10 16:18:00 +0300

    create ModelDiffView

--- source/jetbrains/mps/vcs/ModelDifferenceView.java
+++ source/jetbrains/mps/vcs/ModelDifferenceView.java
@@ -1,13 +1,28 @@
 package jetbrains.mps.vcs;
 
 import jetbrains.mps.smodel.SModel;
+import jetbrains.mps.ide.ui.MPSTree;
+import jetbrains.mps.ide.ui.MPSTreeNode;
+import jetbrains.mps.ide.ui.TextTreeNode;
 
 import javax.swing.JPanel;
-import java.util.List;
+import javax.swing.JScrollPane;
+import java.awt.BorderLayout;
 
 public class ModelDifferenceView extends JPanel {
+  private MPSTree myTree = new MPSTree() {
+    protected MPSTreeNode rebuild() {
+      return new TextTreeNode("Test");
+    }
+  };
 
-  public ModelDifferenceView(SModel m, List<Change> chagnes) {
+  public ModelDifferenceView() {
+    setLayout(new BorderLayout());
+    myTree.rebuildTree();
+    add(new JScrollPane(myTree), BorderLayout.CENTER);
+  }
+
+  public void showDifference(SModel m1, SModel m2) {
     
   }
 }
