9d3266d7e4fb13aa41ad006c4269e58a67cba890
Cyril Konopko <Cyril.Konopko@jetbrains.com>
2007-01-11 12:14:47 +0300

    exception handling in reduction rules (temporary hack)

---
+++ source/jetbrains/mps/generator/template/ReductionNotNeededException.java
@@ -0,0 +1,14 @@
+package jetbrains.mps.generator.template;
+
+import jetbrains.mps.util.annotation.Hack;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Cyril.Konopko
+ * Date: 11.01.2007
+ * Time: 12:10:48
+ * To change this template use File | Settings | File Templates.
+ */
+@Hack
+public class ReductionNotNeededException extends RuntimeException {
+}
