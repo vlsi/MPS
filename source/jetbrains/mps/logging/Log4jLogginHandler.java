cd35a856525f6d1992b4aaf314ce5c3a31624292
Konstantin Solomatov <kostik@jetbrains.com>
2007-01-24 11:43:47 +0300

    fix log4j bug

--- source/jetbrains/mps/logging/Log4jLogginHandler.java
+++ source/jetbrains/mps/logging/Log4jLogginHandler.java
@@ -10,18 +10,18 @@ class Log4jLogginHandler implements ILoggingHandler {
   }
 
   public void warning(LogEntry e) {
-    getLogger(e.getSourceClass()).info(e.getMessage(), e.getThrowable());
+    getLogger(e.getSourceClass()).warn(e.getMessage(), e.getThrowable());
   }
 
   public void debug(LogEntry e) {
-    getLogger(e.getSourceClass()).info(e.getMessage(), e.getThrowable());
+    getLogger(e.getSourceClass()).debug(e.getMessage(), e.getThrowable());
   }
 
   public void error(LogEntry e) {
-    getLogger(e.getSourceClass()).info(e.getMessage(), e.getThrowable());
+    getLogger(e.getSourceClass()).error(e.getMessage(), e.getThrowable());
   }
 
   public void fatal(LogEntry e) {
-    getLogger(e.getSourceClass()).info(e.getMessage(), e.getThrowable());
+    getLogger(e.getSourceClass()).fatal(e.getMessage(), e.getThrowable());
   }
 }
