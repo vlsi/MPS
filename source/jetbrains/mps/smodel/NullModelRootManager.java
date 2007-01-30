4b02ac98127b070a2425cb6b388e3f79cf976ada
Konstantin Solomatov <kostik@jetbrains.com>
2007-01-30 14:24:31 +0300

    more robust model root manager

--- source/jetbrains/mps/smodel/NullModelRootManager.java
+++ source/jetbrains/mps/smodel/NullModelRootManager.java
@@ -1,15 +1,19 @@
 package jetbrains.mps.smodel;
 
 import jetbrains.mps.projectLanguage.ModelRoot;
+import jetbrains.mps.logging.Logger;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Set;
+import java.util.HashSet;
 
 class NullModelRootManager extends AbstractModelRootManager {
+  public static final Logger LOG = Logger.getLogger(NullModelRootManager.class);
 
   @NotNull
   public Set<SModelDescriptor> read(@NotNull ModelRoot root, @NotNull ModelOwner owner) {
-    throw new RuntimeException();
+    LOG.error("Can't read from Null Model Root Manager");
+    return new HashSet<SModelDescriptor>();
   }
 
   @NotNull
@@ -20,13 +24,14 @@ class NullModelRootManager extends AbstractModelRootManager {
   public void saveModel(@NotNull SModelDescriptor modelDescriptor) {
     String message = "saving model " + modelDescriptor + " using null model root manager \n";
     message += "this model is owned by " + SModelRepository.getInstance().getOwners(modelDescriptor);
-    throw new RuntimeException(message);
+    LOG.error(message);
   }
 
   public boolean containsString(@NotNull SModelDescriptor modelDescriptor, @NotNull String string) {
     String message = "trying to find usages in " + modelDescriptor + " , model with NullModelRootManager\n";
     message += "this model is owned by " + SModelRepository.getInstance().getOwners(modelDescriptor);
-    throw new RuntimeException(message);
+    LOG.error(message);
+    return false;
   }
 
   public boolean isFindUsagesSupported() {
