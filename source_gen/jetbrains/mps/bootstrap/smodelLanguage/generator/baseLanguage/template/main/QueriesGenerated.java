d5ab7356a1fa7a1b3cf4a2369c7f9b6b91cbf312
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2007-02-05 22:32:56 +0300

    update smodel gen

--- source_gen/jetbrains/mps/bootstrap/smodelLanguage/generator/baseLanguage/template/main/QueriesGenerated.java
+++ source_gen/jetbrains/mps/bootstrap/smodelLanguage/generator/baseLanguage/template/main/QueriesGenerated.java
@@ -388,6 +388,9 @@ public class QueriesGenerated {
   public static SNode sourceNodeQuery_1170443922573(SNode node, SModel sourceModel, ITemplateGenerator generator, IScope scope, IOperationContext operationContext) {
     return SLinkOperations.getTarget(SNodeOperations.getParent(node, null, false, false), "leftExpression", true);
   }
+  public static SNode sourceNodeQuery_1170703889501(SNode node, SModel sourceModel, ITemplateGenerator generator, IScope scope, IOperationContext operationContext) {
+    return SLinkOperations.getTarget(node, "leftExpression", true);
+  }
   public static SNode sourceNodeQuery_1169055764567(SNode node, SModel sourceModel, ITemplateGenerator generator, IScope scope, IOperationContext operationContext) {
     // <expr-to-copy> . link-access . set-target-op
     SNode lexpr1 = SLinkOperations.getTarget(SNodeOperations.getParent(node, null, false, false), "leftExpression", true);
