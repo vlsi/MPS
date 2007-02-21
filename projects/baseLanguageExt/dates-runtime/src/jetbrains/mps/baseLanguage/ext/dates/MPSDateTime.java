c8a6326e0c76d8e97c8dde61f41fb2a4992af6a5
Maxim Mazin <Maxim.Mazin@jetbrains.com>
2007-02-21 21:49:49 +0300

    Fixed Charisma generation

--- projects/baseLanguageExt/dates-runtime/src/jetbrains/mps/baseLanguage/ext/dates/MPSDateTime.java
+++ projects/baseLanguageExt/dates-runtime/src/jetbrains/mps/baseLanguage/ext/dates/MPSDateTime.java
@@ -3,6 +3,9 @@ package jetbrains.mps.baseLanguage.ext.dates;
 import org.joda.time.format.DateTimeFormatter;
 import org.joda.time.DateTime;
 
+import java.util.Date;
+import java.util.Calendar;
+
 /**
  * @author Maxim.Mazin at date: 21.02.2007 time: 17:25:49
  */
@@ -19,5 +22,17 @@ public class MPSDateTime {
 
   public static String print(MPSDateTime dateTime, DateTimeFormatter formatter) {
     return formatter.print(dateTime != null ? new DateTime(dateTime.myValue) : Constants.NULL_DATE_TIME);
+  }
+
+  public static MPSDateTime convert(DateTime dateTime) {
+    return dateTime != null ? new MPSDateTime(dateTime.getMillis()) : null;
+  }
+
+  public static MPSDateTime convert(Date date) {
+    return date != null ? new MPSDateTime(date.getTime()) : null;
+  }
+
+  public static MPSDateTime convert(Calendar calendar) {
+    return calendar != null ? new MPSDateTime(calendar.getTimeInMillis()) : null;
   }
 }
