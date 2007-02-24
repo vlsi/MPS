eae96480b9d17835bbadb1677d26425100c86529
Timur Zambalayev <Timur.Zambalayev@jetbrains.com>
2007-02-24 03:10:20 +0300

    script action

--- workbench/ideLanguages/scriptLanguage/source_gen/jetbrains/mps/ide/scriptLanguage/generator/baseLanguage/template/main/QueriesGenerated.java
+++ workbench/ideLanguages/scriptLanguage/source_gen/jetbrains/mps/ide/scriptLanguage/generator/baseLanguage/template/main/QueriesGenerated.java
@@ -8,10 +8,18 @@ import jetbrains.mps.generator.template.ITemplateGenerator;
 import jetbrains.mps.smodel.IScope;
 import jetbrains.mps.smodel.IOperationContext;
 import jetbrains.mps.bootstrap.smodelLanguage.generator.smodelAdapter.SPropertyOperations;
+import java.util.List;
+import jetbrains.mps.bootstrap.smodelLanguage.generator.smodelAdapter.SLinkOperations;
 
 public class QueriesGenerated {
 
   public static String propertyMacro_GetPropertyValue_1172274241857(SNode node, String templateValue, SNode templateNode, SModel sourceModel, ITemplateGenerator generator, IScope scope, IOperationContext operationContext) {
     return SPropertyOperations.getString(node, "scriptName") + "_Script";
+  }
+  public static String propertyMacro_GetPropertyValue_1172275716010(SNode node, String templateValue, SNode templateNode, SModel sourceModel, ITemplateGenerator generator, IScope scope, IOperationContext operationContext) {
+    return "action_" + node.getId();
+  }
+  public static List sourceNodesQuery_1172275596624(SNode node, SModel sourceModel, ITemplateGenerator generator, IScope scope, IOperationContext operationContext) {
+    return SLinkOperations.getTargets(node, "scriptAction", true);
   }
 }
