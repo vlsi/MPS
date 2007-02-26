62afe28e441028786150ac8384e61f85950c8f4e
Maxim Mazin <Maxim.Mazin@jetbrains.com>
2007-02-26 20:15:50 +0300

    WD-77: Create interval simple type in dates language

--- projects/baseLanguageExt/dates-runtime/src/jetbrains/mps/baseLanguage/ext/dates/DateTimeOperations.java
+++ projects/baseLanguageExt/dates-runtime/src/jetbrains/mps/baseLanguage/ext/dates/DateTimeOperations.java
@@ -1,8 +1,7 @@
 package jetbrains.mps.baseLanguage.ext.dates;
 
 import org.joda.time.format.DateTimeFormatter;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeFieldType;
+import org.joda.time.*;
 
 import java.util.Date;
 import java.util.Calendar;
@@ -75,5 +74,93 @@ public class DateTimeOperations {
 
   public static Long with(Long datetime, DateTimeFieldType type, int value) {
     return datetime != null ? new DateTime(datetime).property(type).setCopy(value).getMillis() : null;
+  }
+
+  public static Period plus(Period leftExpression, Period rightExpression) {
+    if (leftExpression == null) {
+      leftExpression = Period.ZERO;
+    }
+    if (rightExpression == null){
+      rightExpression = Period.ZERO;
+    }
+    return new Period(
+            leftExpression.getYears() + rightExpression.getYears(),
+            leftExpression.getMonths() + rightExpression.getMonths(),
+            leftExpression.getWeeks() + rightExpression.getWeeks(),
+            leftExpression.getDays() + rightExpression.getDays(),
+            leftExpression.getHours() + rightExpression.getHours(),
+            leftExpression.getMinutes() + rightExpression.getMinutes(),
+            leftExpression.getSeconds() + rightExpression.getSeconds(),
+            leftExpression.getMillis() + rightExpression.getMillis());
+  }
+
+  public static Period minus(Period leftExpression, Period rightExpression) {
+    if (leftExpression == null) {
+      leftExpression = Period.ZERO;
+    }
+    if (rightExpression == null){
+      rightExpression = Period.ZERO;
+    }
+    return new Period(
+            leftExpression.getYears() - rightExpression.getYears(),
+            leftExpression.getMonths() - rightExpression.getMonths(),
+            leftExpression.getWeeks() - rightExpression.getWeeks(),
+            leftExpression.getDays() - rightExpression.getDays(),
+            leftExpression.getHours() - rightExpression.getHours(),
+            leftExpression.getMinutes() - rightExpression.getMinutes(),
+            leftExpression.getSeconds() - rightExpression.getSeconds(),
+            leftExpression.getMillis() - rightExpression.getMillis());
+  }
+
+  public static Long plus(Long leftExpression, Period rightExpression) {
+    Long result;
+    if (leftExpression == null) {
+      result = null;
+    } else {
+      result = new DateTime(leftExpression).plus(rightExpression).getMillis();
+    }
+    return result;
+  }
+
+  public static Long minus(Long leftExpression, Period rightExpression) {
+    Long result;
+    if (leftExpression == null) {
+      result = null;
+    } else {
+      result = new DateTime(leftExpression).minus(rightExpression).getMillis();
+    }
+    return result;
+  }
+
+  public static Period minus(Long leftExpression, Long rightExpression) {
+    Period result;
+    if (leftExpression == null || rightExpression == null) {
+      result = null;
+    } else {
+      result = new Period(rightExpression, leftExpression);
+    }
+    return result;
+  }
+
+  public static boolean equals(Period leftExpression, Period rightExpression) {
+    boolean result;
+    if (leftExpression == null || rightExpression == null) {
+      result = leftExpression == rightExpression;
+    } else {
+      DateTime now = new DateTime();
+      result = leftExpression.toDurationFrom(now).equals(rightExpression.toDurationFrom(now));
+    }
+    return result;
+  }
+
+  public static boolean less(Period leftExpression, Period rightExpression) {
+    boolean result;
+    if (leftExpression == null || rightExpression == null) {
+      throw new NullPointerException("Operands shouldn't be null");
+    } else {
+      DateTime now = new DateTime();
+      result = leftExpression.toDurationFrom(now).compareTo(rightExpression.toDurationFrom(now)) < 0;
+    }
+    return result;
   }
 }
