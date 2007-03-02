87fa84cbdaf711a3c53fb16075fa9e32f13223b0
Cyril Konopko <Cyril.Konopko@jetbrains.com>
2007-03-02 20:24:13 +0300

    autoscrolling restored

--- source/jetbrains/mps/nodeEditor/AbstractEditorComponent.java
+++ source/jetbrains/mps/nodeEditor/AbstractEditorComponent.java
@@ -1327,9 +1327,21 @@ public abstract class AbstractEditorComponent extends JComponent implements Scro
     if (mySelectedCell != null) {
       mySelectedCell.setSelected(true);
     }
-   // if (mySelectedCell != null) {
-   //
-   // }
+    if (mySelectedCell != null) {
+      EditorCell cell = getDeepestSelectedCell();
+      Rectangle selectionRect;
+      if (cell instanceof EditorCell_Label) {
+        EditorCell_Label cellLabel = (EditorCell_Label) cell;
+        int caretX = cellLabel.getCaretX();
+        int charWidth = cellLabel.getCharWidth();
+        selectionRect = new Rectangle(caretX-2*charWidth, cellLabel.getY(), 4*charWidth, cellLabel.getHeight());
+      } else {
+        selectionRect = new Rectangle(cell.getX(), cell.getY(), 30, cell.getHeight());
+      }
+      //Rectangle fakeRect = new Rectangle(0,cell.getY(), 30, cell.getHeight());
+      //scrollRectToVisible(fakeRect);
+      scrollRectToVisible(selectionRect);
+    }
     repaint();
 
     fireCellSelectionChanged(oldSelection, newSelectedCell);
