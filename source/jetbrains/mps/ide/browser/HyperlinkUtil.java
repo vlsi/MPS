166ada16ae71d050f566f73b893b3b1b66116916
Timur Zambalayev <Timur.Zambalayev@jetbrains.com>
2007-03-04 00:41:56 +0300

    HyperlinkUtil

---
+++ source/jetbrains/mps/ide/browser/HyperlinkUtil.java
@@ -0,0 +1,28 @@
+package jetbrains.mps.ide.browser;
+
+import javax.swing.JComponent;
+import javax.swing.JEditorPane;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+
+public final class HyperlinkUtil {
+
+  private HyperlinkUtil() {
+  }
+
+  public static JComponent getHyperlinkComponent(final String url) {
+    JEditorPane editorPane = new JEditorPane();
+    editorPane.setContentType("text/html");
+    editorPane.setText("<a href=\"" + url + "\">" + url + "</a>");
+    editorPane.setEditable(false);
+    editorPane.addHyperlinkListener(new HyperlinkListener() {
+      public void hyperlinkUpdate(HyperlinkEvent e) {
+        if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+          BrowserUtil.launchBrowser(url);
+        }
+      }
+    });
+    return editorPane;
+  }
+
+}
