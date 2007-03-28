3a0bd44b3e8bd880bb7b836e315d73fd3e583cf8
Timur Zambalayev <Timur.Zambalayev@jetbrains.com>
2007-03-28 04:44:08 +0400

    ClassOptionUtil

--- platform/webr/languages/xml/source_gen/webr/xml/constraints/Attribute_attributeDeclaration_ReferentConstraint.java
+++ platform/webr/languages/xml/source_gen/webr/xml/constraints/Attribute_attributeDeclaration_ReferentConstraint.java
@@ -8,8 +8,9 @@ import jetbrains.mps.smodel.constraints.ModelConstraintsManager;
 import jetbrains.mps.smodel.SModel;
 import jetbrains.mps.smodel.SNode;
 import jetbrains.mps.smodel.IScope;
-import jetbrains.mps.bootstrap.smodelLanguage.generator.smodelAdapter.SNodeOperations;
 import jetbrains.mps.smodel.search.ISearchScope;
+import jetbrains.mps.bootstrap.smodelLanguage.generator.smodelAdapter.SNodeOperations;
+import jetbrains.mps.smodel.search.SModelSearchUtil_new;
 import jetbrains.mps.smodel.search.SimpleSearchScope;
 import webr.xml.util.AttributeUtil;
 
@@ -25,11 +26,18 @@ public class Attribute_attributeDeclaration_ReferentConstraint implements IModel
     manager.unRegisterNodeReferentSearchScopeProvider("webr.xml.structure.Attribute", "attributeDeclaration");
   }
   public boolean canCreateNodeReferentSearchScope(SModel model, SNode enclosingNode, SNode referenceNode, IScope scope) {
-    return (SNodeOperations.getAncestor(enclosingNode, "webr.xml.structure.Element", false, false) != null);
+    return true;
   }
   public ISearchScope createNodeReferentSearchScope(SModel model, SNode enclosingNode, SNode referenceNode, IScope scope) {
+    ISearchScope searchScope;
     SNode element = SNodeOperations.getAncestor(enclosingNode, "webr.xml.structure.Element", false, false);
-    return new SimpleSearchScope(AttributeUtil.getAttributeDeclarations_knownElement(element));
+    if((element == null)) {
+      searchScope = SModelSearchUtil_new.createModelAndImportedModelsScope(model, scope);
+    } else 
+    {
+      searchScope = new SimpleSearchScope(AttributeUtil.getAttributeDeclarations_knownElement(element));
+    }
+    return searchScope;
   }
   public String getNodeReferentSearchScopeDescription() {
     return "<no description>";
