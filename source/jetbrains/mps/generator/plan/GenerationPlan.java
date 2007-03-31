b7e5c57a7505b6a5703ee821f1c872a836cb4e30
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2007-03-31 04:46:37 +0400

    mapping priorities

--- source/jetbrains/mps/generator/plan/GenerationPlan.java
+++ source/jetbrains/mps/generator/plan/GenerationPlan.java
@@ -1,9 +1,9 @@
 package jetbrains.mps.generator.plan;
 
+import jetbrains.mps.logging.Logger;
+import jetbrains.mps.projectLanguage.structure.*;
 import jetbrains.mps.smodel.*;
 import jetbrains.mps.transformation.TLBase.structure.MappingConfiguration;
-import jetbrains.mps.projectLanguage.structure.*;
-import jetbrains.mps.logging.Logger;
 import jetbrains.mps.util.CollectionUtil;
 
 import java.util.*;
@@ -15,7 +15,7 @@ import java.util.*;
 public class GenerationPlan {
   private static Logger LOG = Logger.getLogger(GenerationPlan.class);
 
-  public static List<GenerationStep> createSteps(List<Generator> generators) {
+  public static List<List<MappingConfiguration>> createSteps(List<Generator> generators) {
     GenerationPlan plan = new GenerationPlan(generators);
 
     Map<MappingConfiguration, Set<MappingConfiguration>> priorityMap = plan.newPriorityMap();
@@ -29,17 +29,37 @@ public class GenerationPlan {
     return createSteps(priorityMap);
   }
 
-  private static List<GenerationStep> createSteps(Map<MappingConfiguration, Set<MappingConfiguration>> priorityMap) {
-    List<GenerationStep> steps = new ArrayList<GenerationStep>();
+  private static List<List<MappingConfiguration>> createSteps(Map<MappingConfiguration, Set<MappingConfiguration>> priorityMap) {
+    List<List<MappingConfiguration>> steps = new ArrayList<List<MappingConfiguration>>();
     while (!priorityMap.isEmpty()) {
-      steps.add(createStep(priorityMap));
+      List<MappingConfiguration> step = createStep(priorityMap);
+      if(step.isEmpty()) {
+        // error!!!
+        return steps;
+      }
+      steps.add(step);
     }
     return steps;
   }
 
-  private static GenerationStep createStep(Map<MappingConfiguration, Set<MappingConfiguration>> priorityMap) {
+  private static List<MappingConfiguration> createStep(Map<MappingConfiguration, Set<MappingConfiguration>> priorityMap) {
+    List<MappingConfiguration> step = new ArrayList<MappingConfiguration>();
+    for (MappingConfiguration mappingConfig : priorityMap.keySet()) {
+      // no greater priority mappings?
+      if(priorityMap.get(mappingConfig).isEmpty()) {
+        step.add(mappingConfig);
+      }
+    }
 
-    return null;  //To change body of created methods use File | Settings | File Templates.
+    // clean-up
+    for (MappingConfiguration mappingConfig : step) {
+      priorityMap.remove(mappingConfig);
+      for (Set<MappingConfiguration> set : priorityMap.values()) {
+         set.remove(mappingConfig);
+      }
+    }
+
+    return step;
   }
 
 
@@ -70,6 +90,7 @@ public class GenerationPlan {
     MappingConfig_AbstractRef lesserPriMappingRef = rule.getLesserPriorityMapping();
     if (greaterPriMappingRef == null || lesserPriMappingRef == null) return;
 
+    // map: lesser mapping -> {greater mapping, .... , greater mapping }
     List<MappingConfiguration> greaterPriMappings = getMappingsFromRef(greaterPriMappingRef, generator, generator.getScope());
     List<MappingConfiguration> lesserPriMappings = getMappingsFromRef(lesserPriMappingRef, generator, generator.getScope());
     List<MappingConfiguration> lesserPriMappingsComplement = CollectionUtil.substruct(lesserPriMappings, greaterPriMappings);
@@ -81,13 +102,26 @@ public class GenerationPlan {
         }
       }
     }
-    return;
   }
 
   private List<MappingConfiguration> getMappingsFromRef(MappingConfig_AbstractRef mappingRef, Generator refGenerator, IScope scope) {
     if (mappingRef instanceof MappingConfig_RefAllGlobal) {
       return myAllMappings;
     }
+
+    if (mappingRef instanceof MappingConfig_RefAllLocal) {
+      return refGenerator.getOwnMappings();
+    }
+
+    if (mappingRef instanceof MappingConfig_SimpleRefSet) {
+      List<MappingConfiguration> result = new ArrayList<MappingConfiguration>();
+      MappingConfig_SimpleRefSet simpleRefSet = ((MappingConfig_SimpleRefSet) mappingRef);
+      for (MappingConfig_SimpleRef simpleRef : simpleRefSet.getMappingConfigs()) {
+        result.addAll(getMappingsFromRef(simpleRef, refGenerator, scope));
+      }
+      return result;
+    }
+
     if (mappingRef instanceof MappingConfig_ExtRef) {
       GeneratorReference generatorRef = ((MappingConfig_ExtRef) mappingRef).getGenerator();
       if (generatorRef != null) {
@@ -103,6 +137,7 @@ public class GenerationPlan {
       }
       return new ArrayList();
     }
+
     if (mappingRef instanceof MappingConfig_SimpleRef) {
       String modelUID = ((MappingConfig_SimpleRef) mappingRef).getModelUID();
       String nodeID = ((MappingConfig_SimpleRef) mappingRef).getNodeID();
@@ -127,9 +162,7 @@ public class GenerationPlan {
       }
       return new ArrayList();
     }
-    if (mappingRef instanceof MappingConfig_RefAllLocal) {
-      return refGenerator.getOwnMappings();
-    }
+
     return new ArrayList();
   }
 }
