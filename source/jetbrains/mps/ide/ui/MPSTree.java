a2f0139156a8a340198de2c3e5e3fafc9248b4d5
Konstantin Solomatov <kostik@jetbrains.com>
2007-04-17 15:54:43 +0400

    fix NPE

--- source/jetbrains/mps/ide/ui/MPSTree.java
+++ source/jetbrains/mps/ide/ui/MPSTree.java
@@ -164,7 +164,9 @@ public abstract class MPSTree extends JTree {
         for (int i = 0; i < menu.getItemCount(); i++) {
           JMenuItem item = (JMenuItem) menu.getItem(i);
 
-          if (item == null) continue;
+          if (item == null) {            
+            continue;
+          }
 
           KeyStroke keyStroke = item.getAccelerator();
           if (eventKeyStroke.equals(keyStroke)) {
