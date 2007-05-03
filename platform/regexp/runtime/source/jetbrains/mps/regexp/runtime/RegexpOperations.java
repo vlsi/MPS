6533cc139c8cd69757a69ac6c9c423ffb5020a6a
Konstantin Solomatov <kostik@jetbrains.com>
2007-05-03 13:54:55 +0400

    work on regexp overview remove unnecessary code

--- platform/regexp/runtime/source/jetbrains/mps/regexp/runtime/RegexpOperations.java
+++ platform/regexp/runtime/source/jetbrains/mps/regexp/runtime/RegexpOperations.java
@@ -22,18 +22,4 @@ public class RegexpOperations {
   public static List<String> split(String s, Pattern pattern) {    
     return Arrays.asList(pattern.split(s));
   }
-
-
-  public static void main(String[] args) {
-    String text = "* [http://www.sciencedirect.com/science?_ob=ArticleURL&amp;_udi=B6TCY-4J555R7-1&amp;_coverDate=03%2F31%2F2006&amp;_alid=504087490&amp;_rdoc=1&amp;_fmt=&amp;_orig=search&amp;_qd=1&amp;_cdi=5183&amp;_sort=d&amp;view=c&amp;_acct=C000050221&amp;_version=1&amp;_urlVersion=0&amp;_userid=10&amp;md5=dff7066bdd49f3033b0dae9090673c26 Genomics of MicroRNA] by Kim and Nam";
-    Pattern p = Pattern.compile("(?:\\[)(?:(?:((?:(?:(?:(?:http)|(?:(?:https)|(?:ftp))))(?:(?:://)(?:(?:(?:(?:(?:[\\-\\w])+)(?:(?:(?:(?:.)(?:(?:\\w)+)))*)))(?:(?:(?:(?:/)(?:(?:(?:[\\w\\p{Punct}])+))))?))))))(?:(?:(?:\\s)+)(?:(?:((?:.)*?))(?:\\]))))");
-
-    replace(text, p, new Replacer() {
-
-      public String replace(Matcher m) {
-        return "abc";
-      }
-    });
-
-  }
 }
