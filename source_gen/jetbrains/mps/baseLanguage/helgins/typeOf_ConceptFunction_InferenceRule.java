7ad7ae2b88caabbd43921bfec7bd146f72eec74c
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2007-05-08 23:39:57 +0400

    rollback: remove typeOf  statement list, return statement

--- source_gen/jetbrains/mps/baseLanguage/helgins/typeOf_ConceptFunction_InferenceRule.java
+++ source_gen/jetbrains/mps/baseLanguage/helgins/typeOf_ConceptFunction_InferenceRule.java
@@ -38,16 +38,16 @@ public class typeOf_ConceptFunction_InferenceRule implements InferenceRule_Runti
     if(noReturnExpected) {
       // shouldn't return any values
       {
-        ICursor<SNode> _zCursor = CursorFactory.createCursor(returnStatements);
+        ICursor<SNode> _zCursor1 = CursorFactory.createCursor(returnStatements);
         try {
-          while(_zCursor.moveToNext()) {
-            SNode returnStatement = _zCursor.getCurrent();
+          while(_zCursor1.moveToNext()) {
+            SNode returnStatement = _zCursor1.getCurrent();
             if((SLinkOperations.getTarget(returnStatement, "expression", true) != null)) {
               TypeChecker.getInstance().reportTypeError(returnStatement, "no return value expected");
             }
           }
         } finally {
-          _zCursor.release();
+          _zCursor1.release();
         }
       }
     } else 
@@ -55,10 +55,10 @@ public class typeOf_ConceptFunction_InferenceRule implements InferenceRule_Runti
       // should return subtypes of the 'expected type'
       // if 'expected typeis null - should still return some value (of any type)
       {
-        ICursor<SNode> _zCursor1 = CursorFactory.createCursor(returnStatements);
+        ICursor<SNode> _zCursor2 = CursorFactory.createCursor(returnStatements);
         try {
-          while(_zCursor1.moveToNext()) {
-            SNode returnStatement = _zCursor1.getCurrent();
+          while(_zCursor2.moveToNext()) {
+            SNode returnStatement = _zCursor2.getCurrent();
             if((SLinkOperations.getTarget(returnStatement, "expression", true) == null)) {
               TypeChecker.getInstance().reportTypeError(returnStatement, "should return value");
             } else 
@@ -67,7 +67,7 @@ public class typeOf_ConceptFunction_InferenceRule implements InferenceRule_Runti
             }
           }
         } finally {
-          _zCursor1.release();
+          _zCursor2.release();
         }
       }
       if(leastCommonSupertype == null) {
