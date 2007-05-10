6db9517aa685e51599a5f4518ca7cba44ac7050e
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2007-05-10 05:46:26 +0400

    helgins in collections

--- projects/baseLanguageExt/dev/source_gen/jetbrains/mps/baseLanguage/ext/collections/lang/helgins/supertypesOf_ListType_ClassifierTypeList_SubtypingRule.java
+++ projects/baseLanguageExt/dev/source_gen/jetbrains/mps/baseLanguage/ext/collections/lang/helgins/supertypesOf_ListType_ClassifierTypeList_SubtypingRule.java
@@ -5,7 +5,7 @@ package jetbrains.mps.baseLanguage.ext.collections.lang.helgins;
 import jetbrains.mps.bootstrap.helgins.runtime.SubtypingRule_Runtime;
 import jetbrains.mps.bootstrap.helgins.runtime.ISubtypingRule_Runtime;
 import jetbrains.mps.smodel.SNode;
-import jetbrains.mps.baseLanguage.ext.collections.lang.helgins.QuotationClass_29;
+import jetbrains.mps.baseLanguage.ext.collections.lang.helgins.QuotationClass_30;
 import jetbrains.mps.bootstrap.smodelLanguage.generator.smodelAdapter.SLinkOperations;
 import jetbrains.mps.bootstrap.helgins.structure.ApplicableNodeCondition;
 import jetbrains.mps.smodel.SModel;
@@ -20,7 +20,7 @@ public class supertypesOf_ListType_ClassifierTypeList_SubtypingRule extends Subt
   }
 
   public SNode getSubOrSuperType(SNode type) {
-    return new QuotationClass_29().createNode(SLinkOperations.getTarget(type, "elementType", true));
+    return new QuotationClass_30().createNode(SLinkOperations.getTarget(type, "elementType", true));
   }
   public String getApplicableConceptFQName() {
     return "jetbrains.mps.baseLanguage.ext.collections.lang.structure.ListType";
