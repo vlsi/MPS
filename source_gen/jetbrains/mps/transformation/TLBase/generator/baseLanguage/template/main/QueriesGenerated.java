f9e1c538698310310e5f1d8a576fd66b9e97428d
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2007-05-12 00:10:02 +0400

    helgins in TLBase generator

--- source_gen/jetbrains/mps/transformation/TLBase/generator/baseLanguage/template/main/QueriesGenerated.java
+++ source_gen/jetbrains/mps/transformation/TLBase/generator/baseLanguage/template/main/QueriesGenerated.java
@@ -16,9 +16,6 @@ import jetbrains.mps.baseLanguage.ext.collections.internal.CursorFactory;
 import jetbrains.mps.bootstrap.smodelLanguage.generator.smodelAdapter.SLinkOperations;
 import jetbrains.mps.bootstrap.smodelLanguage.generator.smodelAdapter.SPropertyOperations;
 import jetbrains.mps.bootstrap.smodelLanguage.generator.smodelAdapter.SNodeOperations;
-import jetbrains.mps.bootstrap.structureLanguage.structure.AbstractConceptDeclaration;
-import jetbrains.mps.transformation.TemplateLanguageTypesUtil;
-import jetbrains.mps.transformation.TLBase.structure.ReferenceMacro_GetReferent;
 import jetbrains.mps.transformation.TLBase.helgins.Util_TLBase_types;
 
 public class QueriesGenerated {
@@ -76,13 +73,6 @@ public class QueriesGenerated {
   public static SNode referenceMacro_GetReferent_1167771845166(SNode node, SNode templateValue, SNode templateNode, SModel sourceModel, ITemplateGenerator generator, IScope scope, IOperationContext operationContext) {
     return SLinkOperations.getTarget(SNodeOperations.getAncestor(node, "jetbrains.mps.transformation.TLBase.structure.BaseMappingRule", false, false), "applicableConcept", false);
   }
-  public static SNode referenceMacro_GetReferent_1170795079215(SNode node, SNode templateValue, SNode templateNode, SModel sourceModel, ITemplateGenerator generator, IScope scope, IOperationContext operationContext) {
-    AbstractConceptDeclaration concept = TemplateLanguageTypesUtil.get_sourceNode_concept(((ReferenceMacro_GetReferent)SNodeOperations.getAdapter(node)), generator.getTypeChecker());
-    if(concept != null) {
-      return concept.getNode();
-    }
-    return templateValue;
-  }
   public static SNode sourceNodeQuery_1168025917226(SNode node, SModel sourceModel, ITemplateGenerator generator, IScope scope, IOperationContext operationContext) {
     return SLinkOperations.getTarget(node, "body", true);
   }
@@ -108,6 +98,9 @@ public class QueriesGenerated {
     return SLinkOperations.getTarget(node, "body", true);
   }
   public static SNode mapSrcMacro_mapper_1178910515264(SNode node, SModel sourceModel, ITemplateGenerator generator, IScope scope, IOperationContext operationContext) {
+    return SNodeOperations.copyNode(Util_TLBase_types.get_templateFunction_inputNodeType(node));
+  }
+  public static SNode mapSrcMacro_mapper_1178914145434(SNode node, SModel sourceModel, ITemplateGenerator generator, IScope scope, IOperationContext operationContext) {
     return SNodeOperations.copyNode(Util_TLBase_types.get_templateFunction_inputNodeType(node));
   }
   public static SNode mapSrcMacro_mapper_1178914006342(SNode node, SModel sourceModel, ITemplateGenerator generator, IScope scope, IOperationContext operationContext) {
