464dc75c0aa4ee7fe2ba37e24ffad550b7118b23
Fedor Isakov <Fedor.Isakov@jetbrains.com>
2007-05-15 15:58:49 +0400

    reduction of treepath<?> -> ITreeTraversal<?>

--- projects/YPath/solutions/sandbox/source_gen/sample2/FileDemo.java
+++ projects/YPath/solutions/sandbox/source_gen/sample2/FileDemo.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.ArrayList;
 import jetbrains.mps.baseLanguage.ext.collections.internal.SequenceWithSupplier;
 import sample2.zValueSupplier;
+import ypath.util.ITreeTraversal;
 
 public class FileDemo {
 
@@ -38,6 +39,6 @@ public class FileDemo {
     for(File d : TreeTraversalFactory.Filter(new File_TreePath().startTraversal(sequenceOfFiles), new TreePath_nodeKind_1172248995401())) {
       System.out.println("Is a directory too");
     }
-    Iterable<File> foo = TreeTraversalFactory.Filter(new File_TreePath().startTraversal(_zClosureContext.f), new TreePath_nodeKind_1172248995401());
+    ITreeTraversal<File> foo = TreeTraversalFactory.Filter(new File_TreePath().startTraversal(_zClosureContext.f), new TreePath_nodeKind_1172248995401());
   }
 }
