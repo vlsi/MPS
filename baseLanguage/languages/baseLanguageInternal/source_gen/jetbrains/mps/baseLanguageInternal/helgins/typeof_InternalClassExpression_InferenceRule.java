e2acc63a861561aa24152157017ffae8d7fd79ab
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2007-05-24 05:26:09 +0400

    regen types in baseLangInternal

--- baseLanguage/languages/baseLanguageInternal/source_gen/jetbrains/mps/baseLanguageInternal/helgins/typeof_InternalClassExpression_InferenceRule.java
+++ baseLanguage/languages/baseLanguageInternal/source_gen/jetbrains/mps/baseLanguageInternal/helgins/typeof_InternalClassExpression_InferenceRule.java
@@ -7,11 +7,6 @@ import jetbrains.mps.smodel.SNode;
 import jetbrains.mps.helgins.inference.TypeChecker;
 import jetbrains.mps.baseLanguageInternal.helgins.QuotationClass_;
 import jetbrains.mps.bootstrap.smodelLanguage.generator.smodelAdapter.SLinkOperations;
-import jetbrains.mps.bootstrap.helgins.structure.ApplicableNodeCondition;
-import jetbrains.mps.smodel.SModel;
-import jetbrains.mps.smodel.SModelRepository;
-import jetbrains.mps.smodel.SModelUID;
-import jetbrains.mps.smodel.BaseAdapter;
 import jetbrains.mps.smodel.SModelUtil_new;
 
 public class typeof_InternalClassExpression_InferenceRule implements InferenceRule_Runtime {
@@ -24,10 +19,6 @@ public class typeof_InternalClassExpression_InferenceRule implements InferenceRu
   }
   public String getApplicableConceptFQName() {
     return "jetbrains.mps.baseLanguageInternal.structure.InternalClassExpression";
-  }
-  public ApplicableNodeCondition getNodeCondition() {
-    SModel model = SModelRepository.getInstance().getModelDescriptor(SModelUID.fromString("jetbrains.mps.baseLanguageInternal.helgins")).getSModel();
-    return (ApplicableNodeCondition)BaseAdapter.fromNode(model.getNodeById("1179333045894"));
   }
   public boolean isApplicable(SNode argument) {
     return SModelUtil_new.isAssignableConcept(argument.getConceptFqName(), this.getApplicableConceptFQName());
--- baseLanguage/languages/baseLanguageInternal/source_gen/jetbrains/mps/baseLanguageInternal/helgins/typeof_InternalPartialFieldReference_InferenceRule.java
+++ baseLanguage/languages/baseLanguageInternal/source_gen/jetbrains/mps/baseLanguageInternal/helgins/typeof_InternalPartialFieldReference_InferenceRule.java
@@ -6,11 +6,6 @@ import jetbrains.mps.bootstrap.helgins.runtime.InferenceRule_Runtime;
 import jetbrains.mps.smodel.SNode;
 import jetbrains.mps.helgins.inference.TypeChecker;
 import jetbrains.mps.bootstrap.smodelLanguage.generator.smodelAdapter.SLinkOperations;
-import jetbrains.mps.bootstrap.helgins.structure.ApplicableNodeCondition;
-import jetbrains.mps.smodel.SModel;
-import jetbrains.mps.smodel.SModelRepository;
-import jetbrains.mps.smodel.SModelUID;
-import jetbrains.mps.smodel.BaseAdapter;
 import jetbrains.mps.smodel.SModelUtil_new;
 
 public class typeof_InternalPartialFieldReference_InferenceRule implements InferenceRule_Runtime {
@@ -23,10 +18,6 @@ public class typeof_InternalPartialFieldReference_InferenceRule implements Infer
   }
   public String getApplicableConceptFQName() {
     return "jetbrains.mps.baseLanguageInternal.structure.InternalPartialFieldReference";
-  }
-  public ApplicableNodeCondition getNodeCondition() {
-    SModel model = SModelRepository.getInstance().getModelDescriptor(SModelUID.fromString("jetbrains.mps.baseLanguageInternal.helgins")).getSModel();
-    return (ApplicableNodeCondition)BaseAdapter.fromNode(model.getNodeById("1179333045897"));
   }
   public boolean isApplicable(SNode argument) {
     return SModelUtil_new.isAssignableConcept(argument.getConceptFqName(), this.getApplicableConceptFQName());
