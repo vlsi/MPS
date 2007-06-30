610e24b0216af3622261e00f00c5d134e18ef9d0
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2007-06-30 04:47:21 +0400

    generator AbstractConceptDecl support

--- source/jetbrains/mps/generator/template/FastRuleFinder.java
+++ source/jetbrains/mps/generator/template/FastRuleFinder.java
@@ -1,9 +1,7 @@
 package jetbrains.mps.generator.template;
 
-import jetbrains.mps.bootstrap.structureLanguage.structure.ConceptDeclaration;
 import jetbrains.mps.bootstrap.structureLanguage.structure.AbstractConceptDeclaration;
-import jetbrains.mps.bootstrap.structureLanguage.structure.InterfaceConceptReference;
-import jetbrains.mps.bootstrap.structureLanguage.structure.InterfaceConceptDeclaration;
+import jetbrains.mps.bootstrap.structureLanguage.structure.ConceptDeclaration;
 import jetbrains.mps.smodel.BaseAdapter;
 import jetbrains.mps.smodel.SModelUtil_new;
 import jetbrains.mps.smodel.SNode;
@@ -17,9 +15,9 @@ import java.util.*;
  */
 public class FastRuleFinder {
   private List<Reduction_MappingRule> myRuleList;
-  private Map<AbstractConceptDeclaration, List<Reduction_MappingRule>> myRules_all = new HashMap<AbstractConceptDeclaration, List<Reduction_MappingRule>>();
+  private Map<ConceptDeclaration, List<Reduction_MappingRule>> myRules_all = new HashMap<ConceptDeclaration, List<Reduction_MappingRule>>();
   private Map<AbstractConceptDeclaration, List<Reduction_MappingRule>> myRules_applicableExactly = new HashMap<AbstractConceptDeclaration, List<Reduction_MappingRule>>();
-  private Map<AbstractConceptDeclaration, List<Reduction_MappingRule>> myRules_applicableInheritor = new HashMap<AbstractConceptDeclaration, List<Reduction_MappingRule>>();
+  private Map<ConceptDeclaration, List<Reduction_MappingRule>> myRules_applicableInheritor = new HashMap<ConceptDeclaration, List<Reduction_MappingRule>>();
 
   public FastRuleFinder(List<Reduction_MappingRule> reductionRules) {
     myRuleList = reductionRules;
@@ -37,7 +35,7 @@ public class FastRuleFinder {
 
   private void addRule_applicableExactly(AbstractConceptDeclaration concept, Reduction_MappingRule rule) {
     if (!myRules_applicableExactly.containsKey(concept)) {
-      myRules_applicableExactly.put(concept, new ArrayList<Reduction_MappingRule>());
+      myRules_applicableExactly.put(concept, new ArrayList<Reduction_MappingRule>(3));
     }
     myRules_applicableExactly.get(concept).add(rule);
   }
@@ -52,77 +50,70 @@ public class FastRuleFinder {
   private void cacheAllApplicableRules(ConceptDeclaration inputConcept, ConceptDeclaration baseConcept) {
     if (myRules_all.containsKey(inputConcept)) return;
 
-    List<AbstractConceptDeclaration> hierarchy = new ArrayList<AbstractConceptDeclaration>(5);
+    // create partial hierarhy where last concept is always 'processed'
+    List<ConceptDeclaration> partialHierarchy = new ArrayList<ConceptDeclaration>(5);
     while (inputConcept != null) {
-      hierarchy.add(inputConcept);
+      partialHierarchy.add(inputConcept);
       if (myRules_all.containsKey(inputConcept)) {
         break;
       }
-      myRules_applicableInheritor.put(inputConcept, new ArrayList<Reduction_MappingRule>(5));
       inputConcept = getExtendedConcept(inputConcept, baseConcept);
     }
 
-    // collect interfaces in the right order
-    List<AbstractConceptDeclaration> interfacesHierarchy = new ArrayList<AbstractConceptDeclaration>(5);
-    for (AbstractConceptDeclaration hrrConcept : hierarchy) {
-      addInterfacesToHierarchy(((ConceptDeclaration)hrrConcept).getImplementses(), interfacesHierarchy);
-    }
+    Collections.reverse(partialHierarchy); // first concept is always 'processed'
 
-    hierarchy.addAll(interfacesHierarchy);
-    Collections.reverse(hierarchy);
+    // for each concept: all rules rules applicable to it and rules applicable to inheritor 
+    List<Reduction_MappingRule> rulesForInheritor = new ArrayList(myRules_applicableInheritor.get(partialHierarchy.get(0)));
+    for (ConceptDeclaration hrrConcept : partialHierarchy) {
+      if (myRules_all.containsKey(hrrConcept)) {
+        continue;  // skip first
+      }
 
-//    // test
-//    if(!interfacesHierarchy.isEmpty()) {
-//      System.out.println("---------");
-//      for (AbstractConceptDeclaration cd : hierarchy) {
-//        System.out.println(cd.getName());
-//      }
-//      System.out.println("---------");
-//    }
-//    // test
+      myRules_all.put(hrrConcept, new ArrayList<Reduction_MappingRule>(rulesForInheritor));
 
-    // rules for inheritor
-    List<Reduction_MappingRule> rulesForInheritor = null;
-    for (AbstractConceptDeclaration hrrConcept : hierarchy) {
-      rulesForInheritor = updateRulesForInheritor(hrrConcept, rulesForInheritor);
-      List<Reduction_MappingRule> list = myRules_applicableExactly.get(hrrConcept);
-      if (list != null) {
-        for (Reduction_MappingRule rule : list) {
+      List<Reduction_MappingRule> rulesApplicableExactly = myRules_applicableExactly.get(hrrConcept);
+      if (rulesApplicableExactly != null) {
+        myRules_all.get(hrrConcept).addAll(rulesApplicableExactly);
+        for (Reduction_MappingRule rule : rulesApplicableExactly) {
           if (rule.getApplyToConceptInheritors()) {
             rulesForInheritor.add(rule);
           }
         }
       }
+
+      // add all rules applicable to implemented interfaces
+      Set<AbstractConceptDeclaration> implInterfaces = SModelUtil_new.getDirectlyImplementedInterfaces(hrrConcept);
+      for (AbstractConceptDeclaration implInterface : implInterfaces) {
+        List<Reduction_MappingRule> rulesApplicableToInterface = myRules_applicableExactly.get(implInterface);
+        if (rulesApplicableToInterface != null) {
+          for (Reduction_MappingRule rule : rulesApplicableToInterface) {
+            if (!rulesForInheritor.contains(rule)) {
+              myRules_all.get(hrrConcept).add(rule);
+              rulesForInheritor.add(rule);
+            }
+          }
+        }
+      }
+
+      myRules_applicableInheritor.put(hrrConcept, new ArrayList<Reduction_MappingRule>(rulesForInheritor));
     }
 
     // all rules
-    for (AbstractConceptDeclaration hrrConcept : hierarchy) {
-      if (myRules_all.containsKey(hrrConcept)) {
-        continue;
-      }
-      myRules_all.put(hrrConcept, new ArrayList<Reduction_MappingRule>(5));
-      List<Reduction_MappingRule> rulesExectly = myRules_applicableExactly.get(hrrConcept);
-      if (rulesExectly != null) {
-        myRules_all.get(hrrConcept).addAll(rulesExectly);
-      }
-      myRules_all.get(hrrConcept).addAll(myRules_applicableInheritor.get(hrrConcept));
-    }
+//    for (ConceptDeclaration hrrConcept : partialHierarchy) {
+//      if (myRules_all.containsKey(hrrConcept)) {
+//        continue; // skip first
+//      }
+//      myRules_all.put(hrrConcept, new ArrayList<Reduction_MappingRule>(5));
+//      List<Reduction_MappingRule> rulesExectly = myRules_applicableExactly.get(hrrConcept);
+//      if (rulesExectly != null) {
+//        myRules_all.get(hrrConcept).addAll(rulesExectly);
+//      }
+//      myRules_all.get(hrrConcept).addAll(myRules_applicableInheritor.get(hrrConcept));
+//
+//    }
   }
 
-  private void addInterfacesToHierarchy(List<InterfaceConceptReference> interfacesToAdd, List<AbstractConceptDeclaration> interfacesHierarchy) {
-    for (InterfaceConceptReference interfaceConceptReference : interfacesToAdd) {
-      InterfaceConceptDeclaration interfaceConcept = interfaceConceptReference.getIntfc();
-      if(interfaceConcept == null) continue;
-      if (myRules_all.containsKey(interfaceConcept)) continue;
-      // make sure that more abstract interfaces are in the end of list
-      interfacesHierarchy.remove(interfaceConcept);
-      interfacesHierarchy.add(interfaceConcept);
-      myRules_applicableInheritor.put(interfaceConcept, new ArrayList<Reduction_MappingRule>(5));
-      addInterfacesToHierarchy(interfaceConcept.getExtendses(), interfacesHierarchy);
-    }
-  }
-
-  private List<Reduction_MappingRule> updateRulesForInheritor(AbstractConceptDeclaration concept, List<Reduction_MappingRule> addRulesForInheritor) {
+  private List<Reduction_MappingRule> updateRulesForInheritor(ConceptDeclaration concept, List<Reduction_MappingRule> addRulesForInheritor) {
     List<Reduction_MappingRule> currentRulesForInheritor = myRules_applicableInheritor.get(concept);
     if (addRulesForInheritor != null) {
       currentRulesForInheritor.addAll(addRulesForInheritor);
@@ -143,7 +134,6 @@ public class FastRuleFinder {
     if (allRules == null) {
       return null;
     }
-
 
     for (Reduction_MappingRule rule : allRules) {
       if (TemplateGenUtil.checkConditionForBaseMappingRule(node, rule, generator)) {
