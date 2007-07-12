ecc9b55946f5aa5b59def535d2d855ac5b45b400
Timur Zambalayev <Timur.Zambalayev@jetbrains.com>
2007-07-12 06:29:29 +0400

    IInspectorEditorComponent

--- source/jetbrains/mps/nodeEditor/inspector/InspectorPaneEditorComponent.java
+++ source/jetbrains/mps/nodeEditor/inspector/InspectorPaneEditorComponent.java
@@ -1,48 +1,51 @@
 package jetbrains.mps.nodeEditor.inspector;
 
 import jetbrains.mps.ide.IEditor;
-import jetbrains.mps.nodeEditor.*;
+import jetbrains.mps.nodeEditor.EditorCell;
+import jetbrains.mps.nodeEditor.IEditorComponent;
 import jetbrains.mps.smodel.IOperationContext;
 import jetbrains.mps.smodel.SNode;
-import jetbrains.mps.smodel.event.SModelEvent;
 
+import javax.swing.JComponent;
+import javax.swing.JPanel;
 import java.util.ArrayList;
 import java.util.List;
 
-final class InspectorPaneEditorComponent extends AbstractEditorComponent implements IInspectorEditorComponent {
+final class InspectorPaneEditorComponent extends JComponent implements IInspectorEditorComponent {
 
   private final List<InspectorEditorComponent> myInspectorEditorComponents = new ArrayList<InspectorEditorComponent>();
+  private final JComponent myContainer = new JPanel();
 
   InspectorPaneEditorComponent() {
-    super(null);
-    reinitEditor();
   }
 
-  private void reinitEditor() {
-    setEditorContext(new EditorContext(this, null, null));
-    rebuildEditorContent();
+  public JComponent getExternalComponent() {
+    return myContainer;
   }
 
-  public EditorCell createRootCell() {
-    return createRootCell(null);
+  public EditorCell findNodeCell(SNode currentTargetNode) {
+    throw new UnsupportedOperationException();
   }
 
-  public EditorCell createRootCell(List<SModelEvent> events) {
-    EditorContext editorContext = getEditorContext();
-    EditorCell_Collection editorCell_collection = EditorCell_Collection.createVertical(editorContext, null);
-    for (IInspectorEditorComponent inspectorEditorComponent : myInspectorEditorComponents) {
-      editorCell_collection.addEditorCell(inspectorEditorComponent.createRootCell(events));
-    }
-    return editorCell_collection;
+  public EditorCell getRootCell() {
+    throw new UnsupportedOperationException();
+  }
+
+  public void rebuildEditorContent() {
+    throw new UnsupportedOperationException();
+  }
+
+  public void changeSelection(EditorCell cellInInspector) {
+    throw new UnsupportedOperationException();
   }
 
   public void inspectNode(SNode node, IOperationContext context) {
     myInspectorEditorComponents.clear();
+    myContainer.removeAll();
     InspectorEditorComponent inspectorEditorComponent = new InspectorEditorComponent();
     inspectorEditorComponent.inspectNode(node, context);
     myInspectorEditorComponents.add(inspectorEditorComponent);
-    reinitEditor();
-    repaint();
+    myContainer.add(inspectorEditorComponent);
   }
 
   private InspectorEditorComponent getInspectorEditorComponent() {
