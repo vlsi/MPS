fcd56388e83044e8065c8bce37c229f047b3d523
Timur Zambalayev <Timur.Zambalayev@jetbrains.com>
2007-07-12 07:08:51 +0400

    myPanel

--- source/jetbrains/mps/nodeEditor/inspector/InspectorPaneEditorComponent.java
+++ source/jetbrains/mps/nodeEditor/inspector/InspectorPaneEditorComponent.java
@@ -7,7 +7,9 @@ import jetbrains.mps.smodel.IOperationContext;
 import jetbrains.mps.smodel.SNode;
 
 import javax.swing.JComponent;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import java.awt.GridLayout;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,8 +17,10 @@ final class InspectorPaneEditorComponent extends JComponent implements IInspecto
 
   private final List<InspectorEditorComponent> myInspectorEditorComponents = new ArrayList<InspectorEditorComponent>();
   private final JScrollPane myScrollPane = new JScrollPane();
+  private final JComponent myPanel = new JPanel(new GridLayout(0, 1));
 
   InspectorPaneEditorComponent() {
+    myScrollPane.setViewportView(myPanel);
   }
 
   public JComponent getExternalComponent() {
@@ -24,27 +28,32 @@ final class InspectorPaneEditorComponent extends JComponent implements IInspecto
   }
 
   public EditorCell findNodeCell(SNode currentTargetNode) {
+    System.out.println("InspectorPaneEditorComponent.findNodeCell");
     throw new UnsupportedOperationException();
   }
 
   public EditorCell getRootCell() {
+    System.out.println("InspectorPaneEditorComponent.getRootCell");
     throw new UnsupportedOperationException();
   }
 
   public void rebuildEditorContent() {
+    System.out.println("InspectorPaneEditorComponent.rebuildEditorContent");
     throw new UnsupportedOperationException();
   }
 
   public void changeSelection(EditorCell cellInInspector) {
+    System.out.println("InspectorPaneEditorComponent.changeSelection");
     throw new UnsupportedOperationException();
   }
 
   public void inspectNode(SNode node, IOperationContext context) {
     myInspectorEditorComponents.clear();
+    myPanel.removeAll();
     InspectorEditorComponent inspectorEditorComponent = new InspectorEditorComponent();
     inspectorEditorComponent.inspectNode(node, context);
     myInspectorEditorComponents.add(inspectorEditorComponent);
-    myScrollPane.setViewportView(inspectorEditorComponent);
+    myPanel.add(inspectorEditorComponent);
   }
 
   private InspectorEditorComponent getInspectorEditorComponent() {
