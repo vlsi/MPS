7501ce4c4980261d25ac1b5e4da3003eeb3a764d
Timur Zambalayev <Timur.Zambalayev@jetbrains.com>
2007-07-12 20:03:36 +0400

    using SModelUtil_new.getConceptHierarchy(concept)

--- source/jetbrains/mps/smodel/behaviour/BehaviorManager.java
+++ source/jetbrains/mps/smodel/behaviour/BehaviorManager.java
@@ -5,6 +5,7 @@ import jetbrains.mps.bootstrap.structureLanguage.structure.ConceptDeclaration;
 import jetbrains.mps.logging.Logger;
 import jetbrains.mps.reloading.ClassLoaderManager;
 import jetbrains.mps.smodel.SNode;
+import jetbrains.mps.smodel.SModelUtil_new;
 import jetbrains.mps.util.NameUtil;
 
 import java.lang.reflect.InvocationTargetException;
@@ -12,6 +13,7 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 public final class BehaviorManager {
   private static final Logger LOG = Logger.getLogger(BehaviorManager.class);
@@ -60,41 +62,7 @@ public final class BehaviorManager {
     return behaviorClass;
   }
 
-  public<T> T invoke(Class<T> returnType, SNode node, String methodName, List<Class> parametersTypes, Object... parameters) {
-    AbstractConceptDeclaration concept = node.getConceptDeclarationAdapter();
-
-    Method method = null;
-    List<Class> paramTypes = new ArrayList<Class>();
-    paramTypes.add(SNode.class);
-    paramTypes.addAll(parametersTypes);
-    Class[] parameterTypeArray = paramTypes.toArray(new Class[0]);
-    while (concept != null) {
-      method = getMethod(concept, methodName, parameterTypeArray);
-      if (method != null) {
-        break;
-      }
-      concept = ((ConceptDeclaration) concept).getExtends();
-    }
-    
-    if (method != null) {
-      List<Object> params = new ArrayList<Object>();
-      params.add(node);
-      params.addAll(Arrays.asList(parameters));
-
-      if (params.size() != paramTypes.size()) {
-        System.out.println("!!!!");
-      }
-
-      try {
-        return (T) method.invoke(null, params.toArray());
-      } catch (IllegalAccessException e) {
-        throw new RuntimeException(e);
-      } catch (InvocationTargetException e) {
-        throw new RuntimeException(e);
-      }
-    }
-
-    throw new RuntimeException("Can't invoke_old a method " + methodName + " on node " + node);
+  public void reloadAll() {
   }
 
   private Method getMethod(AbstractConceptDeclaration concept, String methodName, Class[] parameterTypes) {
@@ -114,8 +82,41 @@ public final class BehaviorManager {
     return method;
   }
 
+  public<T> T invoke(Class<T> returnType, SNode node, String methodName, List<Class> parametersTypes, Object... parameters) {
+    AbstractConceptDeclaration concept = node.getConceptDeclarationAdapter();
 
-  public void reloadAll() {
+    Method method = null;
+    List<Class> paramTypes = new ArrayList<Class>();
+    paramTypes.add(SNode.class);
+    paramTypes.addAll(parametersTypes);
+    Class[] parameterTypeArray = paramTypes.toArray(new Class[0]);
+
+    for (AbstractConceptDeclaration conceptDeclaration : SModelUtil_new.getConceptHierarchy(concept)) {
+      method = getMethod(conceptDeclaration, methodName, parameterTypeArray);
+      if (method != null) {
+        break;
+      }
+    }
+
+    if (method != null) {
+      List<Object> params = new ArrayList<Object>();
+      params.add(node);
+      params.addAll(Arrays.asList(parameters));
+
+      if (params.size() != paramTypes.size()) {
+        System.out.println("!!!!");
+      }
+
+      try {
+        return (T) method.invoke(null, params.toArray());
+      } catch (IllegalAccessException e) {
+        throw new RuntimeException(e);
+      } catch (InvocationTargetException e) {
+        throw new RuntimeException(e);
+      }
+    }
+
+    throw new RuntimeException("Can't invoke_old a method " + methodName + " on node " + node);
   }
 
 }
