476c28e2377482cadfdcee4593731170a09a13d2
Konstantin Solomatov <kostik@jetbrains.com>
2007-07-18 22:27:06 +0400

    memory leaks tests

--- tests/jetbrains/mps/MemoryLeaksTest.java
+++ tests/jetbrains/mps/MemoryLeaksTest.java
@@ -7,9 +7,11 @@ import jetbrains.mps.project.MPSProject;
 import java.io.File;
 
 public class MemoryLeaksTest extends TestCase {
-  public void testMemoryLeaks() {
-    assertTrue(TestMain.testProjectForLeaks(new File("languages/bootstrap/editorLanguage/editorLanguage.mpr")));
+  public void testEditorLanguageGenerationLeaks() {
+    assertTrue(TestMain.testProjectForLeaks(new File("languages/bootstrap/editorLanguage/editorLanguage.mpr"), 3));
   }
 
-
+  public void testPubMedDBLeaks() {
+    assertTrue(TestMain.testProjectForLeaks(new File("app/PubMedDB/pubMedDB.mpr"), 20));
+  }
 }
