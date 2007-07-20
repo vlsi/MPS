406010803367f98a622a1b73f16ad9431ae917fc
Konstantin Solomatov <kostik@jetbrains.com>
2007-07-20 17:07:47 +0400

    one more leaks test

--- tests/jetbrains/mps/MemoryLeaksTest.java
+++ tests/jetbrains/mps/MemoryLeaksTest.java
@@ -2,22 +2,24 @@ package jetbrains.mps;
 
 import junit.framework.TestCase;
 import jetbrains.mps.ide.TestMain;
-import jetbrains.mps.project.MPSProject;
 
 import java.io.File;
-import java.lang.reflect.Method;
 
 public class MemoryLeaksTest extends TestCase {
   public void testEditorLanguageGenerationLeaks() {
-    assertTrue(TestMain.testProjectForLeaks(new File("languages/bootstrap/editorLanguage/editorLanguage.mpr"), 100));
+    assertTrue(TestMain.testProjectGenerationForLeaks(new File("languages/bootstrap/editorLanguage/editorLanguage.mpr")));
   }
 
-  public void testPubMedDBLeaks() {
-    assertTrue(TestMain.testProjectForLeaks(new File("app/PubMedDB/pubMedDB.mpr"), 100));
+  public void testPubMedDBGenerationLeaks() {
+    assertTrue(TestMain.testProjectGenerationForLeaks(new File("app/PubMedDB/pubMedDB.mpr")));
   }
 
-  public void testCharismaLeaks() {
-    assertTrue(TestMain.testProjectForLeaks(new File("app/charisma/charisma-app/charisma-app.mpr"), 100));
+  public void testCharismaGenerationLeaks() {
+    assertTrue(TestMain.testProjectGenerationForLeaks(new File("app/charisma/charisma-app/charisma-app.mpr")));
+  }
+
+  public void testWebrProjectReloadingLeaks() {
+    assertTrue(TestMain.testProjectReloadForLeaks(new File("platform/webr/webr.mpr"), 100));
   }
 }
 
