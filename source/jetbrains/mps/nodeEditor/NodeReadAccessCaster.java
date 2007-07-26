e335bc3fce3ee69a3b47e9e40f3f4f112cc13bf6
Konstantin Solomatov <kostik@jetbrains.com>
2007-07-26 20:15:57 +0400

    fix exception

--- source/jetbrains/mps/nodeEditor/NodeReadAccessCaster.java
+++ source/jetbrains/mps/nodeEditor/NodeReadAccessCaster.java
@@ -77,7 +77,6 @@ public class NodeReadAccessCaster {
   }
 
   public static void fireNodeReadAccessed(SNode node) {    
-    if (node.getModel().isLoading()) return;
     if(!node.isRegistered()) return;
     if (ourReadAccessListener != null) ourReadAccessListener.readAccess(node);
     if (ourAbstractReadAccessListener != null) ourAbstractReadAccessListener.readAccess(node);
