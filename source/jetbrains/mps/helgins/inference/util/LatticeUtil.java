013d43596b47b61cddb1f4e9b2ce2305d84e240f
Cyril Konopko <Cyril.Konopko@jetbrains.com>
2007-08-27 19:54:07 +0400

    lattice util

--- source/jetbrains/mps/helgins/inference/util/LatticeUtil.java
+++ source/jetbrains/mps/helgins/inference/util/LatticeUtil.java
@@ -1,6 +1,7 @@
 package jetbrains.mps.helgins.inference.util;
 
 import jetbrains.mps.helgins.inference.IWrapper;
+import jetbrains.mps.helgins.inference.JoinWrapper;
 
 /**
  * Created by IntelliJ IDEA.
@@ -11,7 +12,25 @@ import jetbrains.mps.helgins.inference.IWrapper;
  */
 public class LatticeUtil {
   public static IWrapper join(IWrapper wrapper1, IWrapper wrapper2) {
-
-    return null;
+    JoinWrapper joinWrapper = new JoinWrapper();
+    if (wrapper1 instanceof JoinWrapper) {
+      JoinWrapper joinWrapper1 = (JoinWrapper) wrapper1;
+      joinWrapper.addArguments(joinWrapper1);
+      if (wrapper2 instanceof JoinWrapper) {
+        JoinWrapper joinWrapper2 = (JoinWrapper) wrapper2;
+        joinWrapper.addArguments(joinWrapper2);
+      } else {
+        joinWrapper.addArgument(wrapper2);
+      }
+    } else
+    if (wrapper2 instanceof JoinWrapper) {
+      JoinWrapper joinWrapper2 = (JoinWrapper) wrapper2;
+      joinWrapper.addArgument(wrapper1);
+      joinWrapper.addArguments(joinWrapper2);
+    } else {
+      joinWrapper.addArgument(wrapper1);
+      joinWrapper.addArgument(wrapper2);
+    }
+    return joinWrapper;
   }
 }
