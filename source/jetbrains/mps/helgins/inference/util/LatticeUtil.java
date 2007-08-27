353758613539371240f959c1b9cca5094a3614a8
Cyril Konopko <Cyril.Konopko@jetbrains.com>
2007-08-27 19:10:20 +0400

    fix external resolve

---
+++ source/jetbrains/mps/helgins/inference/util/LatticeUtil.java
@@ -0,0 +1,17 @@
+package jetbrains.mps.helgins.inference.util;
+
+import jetbrains.mps.helgins.inference.IWrapper;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Cyril.Konopko
+ * Date: 27.08.2007
+ * Time: 15:27:37
+ * To change this template use File | Settings | File Templates.
+ */
+public class LatticeUtil {
+  public static IWrapper join(IWrapper wrapper1, IWrapper wrapper2) {
+
+    return null;
+  }
+}
