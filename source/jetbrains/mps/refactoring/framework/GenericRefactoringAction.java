b4a4f20f66e44c131b9f91d1d6fd450856d9d29d
Cyril Konopko <Cyril.Konopko@jetbrains.com>
2007-09-26 16:41:23 +0400

    refactoring lang in progress

--- source/jetbrains/mps/refactoring/framework/GenericRefactoringAction.java
+++ source/jetbrains/mps/refactoring/framework/GenericRefactoringAction.java
@@ -4,6 +4,9 @@ import jetbrains.mps.ide.action.MPSAction;
 import jetbrains.mps.ide.action.ActionContext;
 import jetbrains.mps.refactoring.IRefactoring;
 import jetbrains.mps.smodel.SModel;
+import jetbrains.mps.smodel.SModelDescriptor;
+import jetbrains.mps.smodel.SModelRepository;
+import jetbrains.mps.smodel.SModelStereotype;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
@@ -27,6 +30,23 @@ public class GenericRefactoringAction extends MPSAction {
     Map<String, String> args = myRefactoring.askForInfo(context);
     if (args == null) return;
     myRefactoring.doRefactor(context, args);
+    SModelDescriptor modelDescriptor = context.getModel();
+    if (modelDescriptor == null) return;
+    SModel model = modelDescriptor.getSModel();
+    for (SModelDescriptor anotherDescriptor : SModelRepository.getInstance().getAllModelDescriptors()) {
+      String stereotype = anotherDescriptor.getStereotype();
+      if (!stereotype.equals(SModelStereotype.NONE) && !stereotype.equals(SModelStereotype.TEMPLATES)) {
+        continue;
+      }
+      if (!anotherDescriptor.isInitialized()) continue;
+      SModel anotherModel = anotherDescriptor.getSModel();
+      if (!anotherModel.getImportedModelUIDs().contains(model.getUID())) continue;
+      processModel(anotherModel, args);
+    }
+  }
+
+  private void processModel(SModel model, Map<String, String> args) {
+    myRefactoring.updateModel(model, args);
   }
 
   public void writeIntoLog(SModel model) {
