cccf2b1f8a7158bbf96ff77b6d77de713c793c8a
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2007-11-20 18:07:23 +0300

    MPS - refactoring - user code checking added (execute method exception handling)

--- workbench/ideLanguages/scriptLanguage/runtime/source/jetbrains/mps/ide/scriptLanguage/runtime/AbstractMigrationRefactoring.java
+++ workbench/ideLanguages/scriptLanguage/runtime/source/jetbrains/mps/ide/scriptLanguage/runtime/AbstractMigrationRefactoring.java
@@ -3,21 +3,15 @@ package jetbrains.mps.ide.scriptLanguage.runtime;
 import jetbrains.mps.bootstrap.structureLanguage.structure.ConceptDeclaration;
 import jetbrains.mps.findUsages.FindUsagesManager;
 import jetbrains.mps.ide.progress.IAdaptiveProgressMonitor;
+import jetbrains.mps.logging.Logger;
 import jetbrains.mps.project.ApplicationComponents;
 import jetbrains.mps.project.GlobalScope;
 import jetbrains.mps.refactoring.DefaultRefactoring;
 import jetbrains.mps.refactoring.RefactoringContext;
-import jetbrains.mps.smodel.IScope;
-import jetbrains.mps.smodel.SModel;
-import jetbrains.mps.smodel.SModelDescriptor;
-import jetbrains.mps.smodel.SModelUID;
-import jetbrains.mps.smodel.SModelUtil_new;
-import jetbrains.mps.smodel.SNode;
-import jetbrains.mps.smodel.IOperationContext;
+import jetbrains.mps.smodel.*;
 
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
@@ -26,9 +20,11 @@ import java.util.Set;
  * Date: Apr 24, 2007
  */
 public abstract class AbstractMigrationRefactoring extends DefaultRefactoring {
+  private static final Logger LOG = Logger.getLogger(AbstractMigrationRefactoring.class);
+
   private static final String SMODEL_UID = "SMODEL_UID";
 
-public AbstractMigrationRefactoring(IOperationContext context) {
+  public AbstractMigrationRefactoring(IOperationContext context) {
     super(new RefactoringContext(context));
   }
 
@@ -40,7 +36,7 @@ public AbstractMigrationRefactoring(IOperationContext context) {
 
   public String getAdditionalInfo() {
     return super.getAdditionalInfo();
-  }                                                   
+  }
 
   public abstract String getFqNameOfConceptToSearchInstances();
 
@@ -54,21 +50,24 @@ public AbstractMigrationRefactoring(IOperationContext context) {
     ConceptDeclaration conceptDeclaration = SModelUtil_new.findConceptDeclaration(getFqNameOfConceptToSearchInstances(), scope);
     Set<SNode> instances = usages.findInstances(conceptDeclaration, scope, monitor);
     HashSet<SNode> affectedInstances = new HashSet<SNode>();
-    
-    List<SModel> matchModels = new ArrayList<SModel> ();
+
+    List<SModel> matchModels = new ArrayList<SModel>();
     String uids = getRefactoringContext().get(SMODEL_UID);
     if (uids != null) {
-        for (String uidString : uids.split("\n")) {
-            SModelDescriptor desc = scope.getModelDescriptor(SModelUID.fromString(uidString));
-            matchModels.add (desc.getSModel());
-        }
+      for (String uidString : uids.split("\n")) {
+        SModelDescriptor desc = scope.getModelDescriptor(SModelUID.fromString(uidString));
+        matchModels.add(desc.getSModel());
+      }
     }
-    
+
     for (SNode instance : instances) {
-      if ((matchModels.isEmpty() || matchModels.contains(instance.getModel())) 
-              && isApplicableInstanceNode(instance)) 
-      {
-        affectedInstances.add(instance);
+      try {
+        if ((matchModels.isEmpty() || matchModels.contains(instance.getModel()))
+                && isApplicableInstanceNode(instance)) {
+          affectedInstances.add(instance);
+        }
+      } catch (Throwable t) {
+        LOG.error("Refactoring failed: " + t.getMessage(), t);
       }
     }
     return affectedInstances;
