2249f028dd829440c12a2c0b3c63c1200210742b
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2007-12-08 04:49:24 +0300

    strict assertions

--- source/jetbrains/mps/smodel/ModelChange.java
+++ source/jetbrains/mps/smodel/ModelChange.java
@@ -1,33 +1,28 @@
 package jetbrains.mps.smodel;
 
 import jetbrains.mps.ide.command.CommandProcessor;
-import jetbrains.mps.logging.Logger;
 
 /**
  * Igor Alshannikov
  * Jul 18, 2007
  */
 /*package*/ class ModelChange {
-  private final static Logger LOG = Logger.getLogger(ModelChange.class);
 
   static void assertLegalNodeChange(SNode node) {
     if (!(node.getModel().isLoading())) {
-//      LOG.assertLog(!node.isRegistered() || CommandProcessor.instance().isInsideUndoableCommand(), "registered node can only be modified inside undoable command or in 'loading' model " + node.getDebugText());
-      assertTrue(!node.isRegistered() || CommandProcessor.instance().isInsideCommand(), "registered node can only be modified inside undoable command or in 'loading' model " + node.getDebugText());
+      assertTrue(!node.isRegistered() || CommandProcessor.instance().isInsideUndoableCommand(), "registered node can only be modified inside undoable command or in 'loading' model " + node.getDebugText());
     }
   }
 
   static void assertLegalNodeRegistration(SModel model, SNode node) {
     if (!(model.isLoading())) {
-//      LOG.assertLog(CommandProcessor.instance().isInsideUndoableCommand(), "node registration is only allowed inside undoable command  or in 'loading' model " + node.getDebugText());
-      assertTrue(CommandProcessor.instance().isInsideCommand(), "node registration is only allowed inside undoable command  or in 'loading' model " + node.getDebugText());
+      assertTrue(CommandProcessor.instance().isInsideUndoableCommand(), "node registration is only allowed inside undoable command  or in 'loading' model " + node.getDebugText());
     }
   }
 
   static void assertLegalNodeUnRegistration(SModel model, SNode node) {
     if (!(model.isLoading())) {
-//      LOG.assertLog(CommandProcessor.instance().isInsideUndoableCommand(), "node un-registration is only allowed inside undoable command or in 'loading' model" + node.getDebugText());
-      assertTrue(CommandProcessor.instance().isInsideCommand(), "node un-registration is only allowed inside undoable command or in 'loading' model" + node.getDebugText());
+      assertTrue(CommandProcessor.instance().isInsideUndoableCommand(), "node un-registration is only allowed inside undoable command or in 'loading' model" + node.getDebugText());
     }
   }
 
