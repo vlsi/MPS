a943531afd2f12dfd03c72c55bf2f0025abed537
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2007-12-12 22:17:02 +0300

    buildlanguage: inheritance and redefinition of attributes

--- platform/buildlanguage/languages/buildlanguage/source_gen/jetbrains/mps/buildlanguage/constraints/PropertyReference_propertyDeclaration_ReferentConstraint.java
+++ platform/buildlanguage/languages/buildlanguage/source_gen/jetbrains/mps/buildlanguage/constraints/PropertyReference_propertyDeclaration_ReferentConstraint.java
@@ -38,14 +38,14 @@ public class PropertyReference_propertyDeclaration_ReferentConstraint implements
   public ISearchScope createNodeReferentSearchScope(final SModel model, final SNode enclosingNode, final SNode referenceNode, final IScope scope) {
     List<SNode> result = new ArrayList<SNode>();
     {
-      ICursor<SNode> _zCursor = CursorFactory.createCursor(SNodeOperations.getAncestors(enclosingNode, "jetbrains.mps.buildlanguage.structure.IPropertyHolder", true));
+      ICursor<SNode> _zCursor3 = CursorFactory.createCursor(SNodeOperations.getAncestors(enclosingNode, "jetbrains.mps.buildlanguage.structure.IPropertyHolder", true));
       try {
-        while(_zCursor.moveToNext()) {
-          SNode holder = _zCursor.getCurrent();
+        while(_zCursor3.moveToNext()) {
+          SNode holder = _zCursor3.getCurrent();
           ListOperations.addAllElements(result, SLinkOperations.getTargets(holder, "propertyList", true));
         }
       } finally {
-        _zCursor.release();
+        _zCursor3.release();
       }
     }
     return new SimpleSearchScope(result);
