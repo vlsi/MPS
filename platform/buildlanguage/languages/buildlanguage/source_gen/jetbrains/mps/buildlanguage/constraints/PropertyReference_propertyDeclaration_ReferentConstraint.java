a613c9ab628734334c95eb84a9a846513e0e3525
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2007-12-28 15:01:14 +0300

    buildlanguage: import nested elements

--- platform/buildlanguage/languages/buildlanguage/source_gen/jetbrains/mps/buildlanguage/constraints/PropertyReference_propertyDeclaration_ReferentConstraint.java
+++ platform/buildlanguage/languages/buildlanguage/source_gen/jetbrains/mps/buildlanguage/constraints/PropertyReference_propertyDeclaration_ReferentConstraint.java
@@ -38,14 +38,14 @@ public class PropertyReference_propertyDeclaration_ReferentConstraint implements
   public ISearchScope createNodeReferentSearchScope(final SModel model, final SNode enclosingNode, final SNode referenceNode, final IScope scope) {
     List<SNode> result = new ArrayList<SNode>();
     {
-      ICursor<SNode> _zCursor5 = CursorFactory.createCursor(SNodeOperations.getAncestors(enclosingNode, "jetbrains.mps.buildlanguage.structure.IPropertyHolder", true));
+      ICursor<SNode> _zCursor6 = CursorFactory.createCursor(SNodeOperations.getAncestors(enclosingNode, "jetbrains.mps.buildlanguage.structure.IPropertyHolder", true));
       try {
-        while(_zCursor5.moveToNext()) {
-          SNode holder = _zCursor5.getCurrent();
+        while(_zCursor6.moveToNext()) {
+          SNode holder = _zCursor6.getCurrent();
           ListOperations.addAllElements(result, SLinkOperations.getTargets(holder, "propertyList", true));
         }
       } finally {
-        _zCursor5.release();
+        _zCursor6.release();
       }
     }
     return new SimpleSearchScope(result);
