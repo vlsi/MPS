1b9dd9ddc9f5d9906c8b99cd88dac443445c8e5e
Cyril Konopko <Cyril.Konopko@jetbrains.com>
2007-12-28 16:34:20 +0300

    icons

--- source/jetbrains/mps/ide/findusages/view/icons/Icons.java
+++ source/jetbrains/mps/ide/findusages/view/icons/Icons.java
@@ -9,9 +9,16 @@ public class Icons {
   public static final Icon EXPAND_ICON = new ImageIcon(Icons.class.getResource("expandall.png"));
   public static final Icon COLLAPSE_ICON = new ImageIcon(Icons.class.getResource("collapseall.png"));
   public static final Icon CLOSE_ICON = new ImageIcon(Icons.class.getResource("close.png"));
-  public static final Icon ROOT_ICON = new ImageIcon(Icons.class.getResource("root.png"));
-  public static final Icon PATH_ICON = new ImageIcon(Icons.class.getResource("path.png"));
-  public static final Icon NUM_ICON = new ImageIcon(Icons.class.getResource("usagesNum.png"));
-  public static final Icon INFO_ICON = new ImageIcon(Icons.class.getResource("info.png"));
-  public static final Icon MODULE_ICON = new ImageIcon(Icons.class.getResource("module.png"));
+  
+  public static final Icon ROOT_ICON_OLD = new ImageIcon(Icons.class.getResource("root.png"));
+  public static final Icon PATH_ICON_OLD = new ImageIcon(Icons.class.getResource("path.png"));
+  public static final Icon NUM_ICON_OLD = new ImageIcon(Icons.class.getResource("usagesNum.png"));
+  public static final Icon INFO_ICON_OLD = new ImageIcon(Icons.class.getResource("info.png"));
+  public static final Icon MODULE_ICON_OLD = new ImageIcon(Icons.class.getResource("module.png"));
+
+  public static final Icon NUM_ICON = new ImageIcon(Icons.class.getResource("showNumber.png"));
+  public static final Icon INFO_ICON = new ImageIcon(Icons.class.getResource("showInfo.png"));
+  public static final Icon MODULE_ICON = new ImageIcon(Icons.class.getResource("groupByModule.png"));
+  public static final Icon MODEL_ICON = new ImageIcon(Icons.class.getResource("showModels.png"));
+  public static final Icon CATEGORY_ICON = new ImageIcon(Icons.class.getResource("groupByCategory.png"));
 }
