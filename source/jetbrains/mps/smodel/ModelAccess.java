aa08b133c2034aab882fccb5881f55bbc166bc0e
Konstantin Solomatov <kostik@jetbrains.com>
2008-01-11 16:38:17 +0300

    tmp disable read asserts

--- source/jetbrains/mps/smodel/ModelAccess.java
+++ source/jetbrains/mps/smodel/ModelAccess.java
@@ -11,7 +11,7 @@ class ModelAccess {
         return;
       }
 
-      throw new IllegalModelAccessError("You can read model only inside command. Use lightweight command to read model");
+//      throw new IllegalModelAccessError("You can read model only inside command. Use lightweight command to read model");
     }
   }
 }
