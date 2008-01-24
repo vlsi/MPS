59ae2bdccf1bb7064804e1a20f38013a1fa2df17
Konstantin Solomatov <kostik@jetbrains.com>
2008-01-24 18:25:49 +0300

    fix assert fix MPS-679

--- source/jetbrains/mps/nodeEditor/NodeInformationDialog.java
+++ source/jetbrains/mps/nodeEditor/NodeInformationDialog.java
@@ -3,6 +3,9 @@ package jetbrains.mps.nodeEditor;
 import jetbrains.mps.smodel.SNode;
 import jetbrains.mps.smodel.SReference;
 import jetbrains.mps.smodel.presentation.NodePresentationUtil;
+import jetbrains.mps.ide.command.CommandProcessor;
+import jetbrains.mps.util.Calculable;
+import jetbrains.mps.util.NameUtil;
 
 import java.awt.Frame;
 import java.awt.Point;
@@ -12,27 +15,35 @@ public class NodeInformationDialog extends AbstractNodeInformationDialog {
     super(owner, location, node);
   }
 
-  protected String createNodeInfo(SNode node) {
-    StringBuilder result = new StringBuilder();
+  protected String createNodeInfo(final SNode node) {
+    return CommandProcessor.instance().executeLightweightCommand(new Calculable<String>() {
+      public String calculate() {
+        StringBuilder result = new StringBuilder();
 
-    result.append("Node from model ").append(node.getModel().getUID()).append("\n\n");
+        result.append("model = ").append(node.getModel().getUID()).append("\n");
+        result.append("concept = ").append(NameUtil.shortNameFromLongName(node.getConceptFqName())).append("\n");
+        result.append("language = ").append(node.getLanguageNamespace());
 
-    for (SReference ref : node.getReferences()) {
-      SNode target = ref.getTargetNode();
 
-      if (ref.getTargetNode() == null) {
-        result.append("Bad Reference\n");
+        result.append("\n\nReferences:");
+        for (SReference ref : node.getReferences()) {
+          SNode target = ref.getTargetNode();
 
-      } else {
-        result.append("Model = ").append(target.getModel().getUID()).append("\n");
-        result.append("Node = ").append(NodePresentationUtil.getPathToRoot(target)).append("\n");
-        result.append("ID  = ").append(target.getId()).append("\n");
+          if (ref.getTargetNode() == null) {
+            result.append("Bad Reference\n");
+
+          } else {
+            result.append("Model = ").append(target.getModel().getUID()).append("\n");
+            result.append("Node = ").append(NodePresentationUtil.getPathToRoot(target)).append("\n");
+            result.append("ID  = ").append(target.getId()).append("\n");
+          }
+
+          result.append("\n");
+        }
+
+        return result.toString();
       }
-
-      result.append("\n");
-    }
-
-    return result.toString();
+    });
   }
 
 }
