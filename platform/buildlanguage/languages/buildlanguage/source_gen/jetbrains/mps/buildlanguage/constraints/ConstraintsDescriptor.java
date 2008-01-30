c06c8570d69a7e4ab86fb995e4b034213b164626
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2008-01-30 13:42:42 +0300

    fixed problem with tasks scopes in buildlanguage

--- platform/buildlanguage/languages/buildlanguage/source_gen/jetbrains/mps/buildlanguage/constraints/ConstraintsDescriptor.java
+++ platform/buildlanguage/languages/buildlanguage/source_gen/jetbrains/mps/buildlanguage/constraints/ConstraintsDescriptor.java
@@ -13,7 +13,6 @@ public class ConstraintsDescriptor implements IModelConstraints {
 
   public  ConstraintsDescriptor() {
     this.myConstraints.add(new StringLiteral_name_PropertyConstraint());
-    this.myConstraints.add(new GenericCall_pseudonymProperty_PropertyConstraint());
     this.myConstraints.add(new GenericCall_name_PropertyConstraint());
     this.myConstraints.add(new TargetReference_targetDeclaration_ReferentConstraint());
     this.myConstraints.add(new PropertyReference_propertyDeclaration_ReferentConstraint());
@@ -21,7 +20,6 @@ public class ConstraintsDescriptor implements IModelConstraints {
     this.myConstraints.add(new GenericAttribute_attributeDeclaration_ReferentConstraint());
     this.myConstraints.add(new Reference_declaration_ReferentConstraint());
     this.myConstraints.add(new PathReference_call_ReferentConstraint());
-    this.myConstraints.add(new StringLiteralReference_ref_ReferentConstraint());
     this.myConstraints.add(new GenericCall_DefaultSearchScope());
   }
 
