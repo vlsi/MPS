534135da4a137879ce8a2f469800164e2824de61
Konstantin Solomatov <kostik@jetbrains.com>
2008-01-30 20:07:18 +0300

    fix MPS-709: The "Find Method Usages In MPS" doesn't work

--- core/kernel/source/jetbrains/mps/plugin/MPSProjectIDEHandler.java
+++ core/kernel/source/jetbrains/mps/plugin/MPSProjectIDEHandler.java
@@ -7,6 +7,7 @@ import jetbrains.mps.ide.EditorsPane;
 import jetbrains.mps.ide.IDEProjectFrame;
 import jetbrains.mps.ide.IEditor;
 import jetbrains.mps.ide.ThreadUtils;
+import jetbrains.mps.ide.command.CommandProcessor;
 import jetbrains.mps.ide.findusages.findalgorithm.finders.AspectMethodsFinder;
 import jetbrains.mps.ide.findusages.findalgorithm.resultproviders.TreeBuilder;
 import jetbrains.mps.ide.findusages.view.optionseditor.FindUsagesOptions;
@@ -21,6 +22,7 @@ import jetbrains.mps.logging.Logger;
 import jetbrains.mps.project.MPSProject;
 import jetbrains.mps.project.ModuleContext;
 import jetbrains.mps.project.ProjectOperationContext;
+import jetbrains.mps.project.GlobalScope;
 import jetbrains.mps.smodel.*;
 import jetbrains.mps.util.FrameUtil;
 import jetbrains.mps.util.IDisposable;
@@ -56,69 +58,99 @@ public class MPSProjectIDEHandler extends UnicastRemoteObject implements IMPSIDE
   public void showNode(final String namespace, final String id) throws RemoteException {
     ThreadUtils.runInUIThreadAndWait(new Runnable() {
       public void run() {
-        List<SModelDescriptor> modelDescriptors = myProject.getScope().getModelDescriptors(namespace);
-        for (SModelDescriptor descriptor : modelDescriptors) {
-          if (descriptor.getStereotype().equals(SModelStereotype.JAVA_STUB)) continue;
+        CommandProcessor.instance().executeLightweightCommand(new Runnable() {
+          public void run() {
+            List<SModelDescriptor> modelDescriptors = GlobalScope.getInstance().getModelDescriptors(namespace);
+            for (SModelDescriptor descriptor : modelDescriptors) {
+              if (descriptor.getStereotype().equals(SModelStereotype.JAVA_STUB)) continue;
 
-          SNode node = descriptor.getSModel().getNodeById(id);
-          if (node != null) {
-            IDEProjectFrame frame = getProjectWindow();
-            ModuleContext operationContext = ModuleContext.create(node, getProjectWindow());
-            EditorsPane pane = frame.getEditorsPane();
-            IEditor editor = pane.openEditor(node, operationContext);
-            NavigationActionProcessor.executeNavigationAction(new EditorNavigationCommand(node, editor, pane), operationContext.getProject());
+              SNode node = descriptor.getSModel().getNodeById(id);
+              if (node != null) {
+                IDEProjectFrame frame = getProjectWindow();
+                ModuleContext operationContext = ModuleContext.create(node, getProjectWindow());
+                EditorsPane pane = frame.getEditorsPane();
+                IEditor editor = pane.openEditor(node, operationContext);
+                NavigationActionProcessor.executeNavigationAction(new EditorNavigationCommand(node, editor, pane), operationContext.getProject());
+              }
+            }
+
+            FrameUtil.activateFrame(getMainFrame());
+          }
+        });
+      }
+    });
+  }
+
+  public void showAspectMethodUsages(final String namespace, final String name) throws RemoteException {
+    CommandProcessor.instance().executeLightweightCommand(new Runnable() {
+      public void run() {
+        List<SModelDescriptor> modelDescriptors = GlobalScope.getInstance().getModelDescriptors(namespace);
+        final List<SModel> applicableModelDescriptors = new ArrayList<SModel>();
+        for (final SModelDescriptor descriptor : modelDescriptors) {
+          if (!descriptor.getStereotype().equals(SModelStereotype.JAVA_STUB)) {
+            applicableModelDescriptors.add(descriptor.getSModel());
           }
         }
+        new Thread() {
+          public void run() {
+            SearchQuery searchQuery = new SearchQuery(new SNodePointer((SNode) null), GlobalScope.getInstance());
+            IResultProvider resultProvider = TreeBuilder.forFinder(new AspectMethodsFinder(applicableModelDescriptors, name));
+            getProjectWindow().getUsagesView().findUsages(resultProvider, searchQuery, false, true, true);
+          }
+        }.start();
+      }
+    });
+  }
 
+  public void showConceptNode(final String fqName) throws RemoteException {
+    CommandProcessor.instance().executeLightweightCommand(new Runnable() {
+      public void run() {
+        ConceptDeclaration concept = SModelUtil_new.findConceptDeclaration(fqName, GlobalScope.getInstance());
+        IDEProjectFrame projectWindow = getProjectWindow();
+        projectWindow.getEditorsPane().openEditor(concept.getNode(), new ProjectOperationContext(projectWindow.getProject()));
         FrameUtil.activateFrame(getMainFrame());
       }
     });
   }
 
-  public void showAspectMethodUsages(String namespace, final String name) throws RemoteException {
-    List<SModelDescriptor> modelDescriptors = myProject.getScope().getModelDescriptors(namespace);
-    final List<SModel> applicableModelDescriptors = new ArrayList<SModel>();
-    for (final SModelDescriptor descriptor : modelDescriptors) {
-      if (!descriptor.getStereotype().equals(SModelStereotype.JAVA_STUB)) {
-        applicableModelDescriptors.add(descriptor.getSModel());
-      }
-    }
-    new Thread() {
+  public void showClassUsages(final String fqName) throws RemoteException {
+    CommandProcessor.instance().executeLightweightCommand(new Runnable() {
       public void run() {
-        SearchQuery searchQuery = new SearchQuery(new SNodePointer((SNode) null), myProject.getScope());
-        IResultProvider resultProvider = TreeBuilder.forFinder(new AspectMethodsFinder(applicableModelDescriptors, name));
-        getProjectWindow().getUsagesView().findUsages(resultProvider, searchQuery, false, true, true);
+        Classifier cls = SModelUtil_new.findNodeByFQName(fqName, Classifier.class, GlobalScope.getInstance());
+        if (cls == null) {
+          LOG.error("Can't find a class " + fqName);
+          return;
+        }
+        FrameUtil.activateFrame(getMainFrame());
+        getProjectWindow().findUsages(cls.getNode(), GlobalScope.getInstance());
       }
-    }.start();
+    });
   }
 
-  public void showConceptNode(String fqName) throws RemoteException {
-    ConceptDeclaration concept = SModelUtil_new.findConceptDeclaration(fqName, myProject.getScope());
-    IDEProjectFrame projectWindow = getProjectWindow();
-    projectWindow.getEditorsPane().openEditor(concept.getNode(), new ProjectOperationContext(projectWindow.getProject()));
-    FrameUtil.activateFrame(getMainFrame());
-  }
+  public void showMethodUsages(final String classFqName, final String methodName, final int parameterCount) throws RemoteException {
+    CommandProcessor.instance().executeLightweightCommand(new Runnable() {
+      public void run() {
+        Classifier cls = SModelUtil_new.findNodeByFQName(classFqName, Classifier.class, GlobalScope.getInstance());
+        if (cls == null) {
+          LOG.error("Can't find a class " + classFqName);
+          return;
+        }
+        BaseMethodDeclaration m = null;
+        for (BaseMethodDeclaration method : cls.getChildren(BaseMethodDeclaration.class)) {
+          if (methodName.equals(method.getName()) && method.getParametersCount() == parameterCount) {
+            m = method;
+            break;
+          }
+        }
+        if (m == null) {
+          LOG.error("Can't find a method " + classFqName + "." + methodName);
+          return;
+        }
+        FrameUtil.activateFrame(getMainFrame());
 
-  public void showClassUsages(String fqName) throws RemoteException {
-    Classifier cls = SModelUtil_new.findNodeByFQName(fqName, Classifier.class, myProject.getScope());
-    if (cls == null) return;
-    FrameUtil.activateFrame(getMainFrame());
-    getProjectWindow().findUsages(cls.getNode(), new ProjectOperationContext(myProject));
-  }
-
-  public void showMethodUsages(String classFqName, String methodName, int parameterCount) throws RemoteException {
-    Classifier cls = SModelUtil_new.findNodeByFQName(classFqName, Classifier.class, myProject.getScope());
-    if (cls == null) return;
-    BaseMethodDeclaration m = null;
-    for (BaseMethodDeclaration method : cls.getChildren(BaseMethodDeclaration.class)) {
-      if (methodName.equals(method.getName()) && method.getParametersCount() == parameterCount) {
-        m = method;
-        break;
+        getProjectWindow().findUsages(m.getNode(), GlobalScope.getInstance());
       }
-    }
-    if (m == null) return;
-    FrameUtil.activateFrame(getMainFrame());
-    getProjectWindow().findUsages(m.getNode(), new ProjectOperationContext(myProject));
+    });
   }
 
   public void dispose() {
