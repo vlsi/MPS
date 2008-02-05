db68864532fc03c17813583020a5551e32cbafa2
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2008-02-05 21:29:46 +0300

    MPS - file chooser - update functionality & sh button

--- workbench/source/jetbrains/mps/ide/ui/filechoosers/treefilechooser/icons/Icons.java
+++ workbench/source/jetbrains/mps/ide/ui/filechoosers/treefilechooser/icons/Icons.java
@@ -7,4 +7,6 @@ public class Icons {
   public static final Icon MODULE_HOME_ICON = new ImageIcon(Icons.class.getResource("moduleHome.png"));
   public static final Icon NEW_FOLDER_ICON = new ImageIcon(Icons.class.getResource("newFolder.png"));
   public static final Icon DELETE_ICON = new ImageIcon(Icons.class.getResource("delete.png"));
+  public static final Icon UPDATE_ICON = new ImageIcon(Icons.class.getResource("delete.png"));
+  public static final Icon SHOW_HIDDEN_ICON = new ImageIcon(Icons.class.getResource("delete.png"));
 }
