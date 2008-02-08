55bc4989a3f15d44af04fb0570f29789f7d320f7
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2008-02-09 02:12:01 +0300

    move 'outputDir' parm in 'handleOutput'

--- core/languageDesign/transformation/source/jetbrains/mps/generator/GeneratorManager.java
+++ core/languageDesign/transformation/source/jetbrains/mps/generator/GeneratorManager.java
@@ -543,7 +543,7 @@ public class GeneratorManager implements IExternalizableComponent, IComponentWit
 
           checkMonitorCanceled(progress);
           if (status.getOutputModel() != null) {
-            generationType.handleOutput(status, invocationContext, progress, outputFolder, messages);
+            generationType.handleOutput(status, outputFolder, invocationContext, progress, messages);
           } else {
             //marks as generated models without applicable generators
             FileGenerationUtil.updateLastGenerationTime(inputModel.getSModel());
