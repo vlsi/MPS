2225c53fd80e583ec97dc24e44d9acc5573c8201
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2008-02-16 02:50:16 +0300

    remove TemplateFragment lable costraint

--- core/languageDesign/transformation/source_gen/jetbrains/mps/transformation/TLBase/constraints/ConstraintsDescriptor.java
+++ core/languageDesign/transformation/source_gen/jetbrains/mps/transformation/TLBase/constraints/ConstraintsDescriptor.java
@@ -18,7 +18,6 @@ public class ConstraintsDescriptor implements IModelConstraints {
     this.myConstraints.add(new CreateRootRule_templateNode_ReferentConstraint());
     this.myConstraints.add(new NodeMacro_mappingLabel_ReferentConstraint());
     this.myConstraints.add(new Node_FindOutputNode_label_ReferentConstraint());
-    this.myConstraints.add(new TemplateFragment_labelDeclaration_ReferentConstraint());
     this.myConstraints.add(new BaseMappingRule_labelDeclaration_ReferentConstraint());
   }
 
