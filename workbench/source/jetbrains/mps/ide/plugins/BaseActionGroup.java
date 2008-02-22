c62c0ee480671b9b83f03e8f0ab38b36df36c141
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2008-02-22 19:33:58 +0300

    MPS - plugin update - module added

---
+++ workbench/source/jetbrains/mps/ide/plugins/BaseActionGroup.java
@@ -0,0 +1,9 @@
+package jetbrains.mps.ide.plugins;
+
+import jetbrains.mps.ide.action.ActionGroup;
+
+public class BaseActionGroup extends ActionGroup {
+  public BaseActionGroup(String name) {
+    super(name);
+  }
+}
