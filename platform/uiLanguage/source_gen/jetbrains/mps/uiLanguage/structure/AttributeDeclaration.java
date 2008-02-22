42d3f17da9a87c8826df232b4ae9428b50e41184
Konstantin Solomatov <kostik@jetbrains.com>
2008-02-22 13:17:49 +0300

    uiLanguage: onChange blocks for attributes

--- platform/uiLanguage/source_gen/jetbrains/mps/uiLanguage/structure/AttributeDeclaration.java
+++ platform/uiLanguage/source_gen/jetbrains/mps/uiLanguage/structure/AttributeDeclaration.java
@@ -9,14 +9,16 @@ import jetbrains.mps.smodel.SModel;
 import jetbrains.mps.smodel.SModelUtil_new;
 import jetbrains.mps.project.GlobalScope;
 import jetbrains.mps.baseLanguage.structure.Type;
+import jetbrains.mps.baseLanguage.structure.Expression;
 
 public class AttributeDeclaration extends BaseConcept implements INamedConcept {
   public static final String concept = "jetbrains.mps.uiLanguage.structure.AttributeDeclaration";
-  public static String TYPE = "type";
   public static String NAME = "name";
   public static String SHORT_DESCRIPTION = "shortDescription";
   public static String ALIAS = "alias";
   public static String VIRTUAL_PACKAGE = "virtualPackage";
+  public static String TYPE = "type";
+  public static String ON_CHANGE = "onChange";
 
   public  AttributeDeclaration(SNode node) {
     super(node);
@@ -30,14 +32,6 @@ public class AttributeDeclaration extends BaseConcept implements INamedConcept {
     return AttributeDeclaration.newInstance(sm, false);
   }
 
-
-  public Type getType() {
-    return (Type)this.getChild(AttributeDeclaration.TYPE);
-  }
-
-  public void setType(Type node) {
-    super.setChild(AttributeDeclaration.TYPE, node);
-  }
 
   public String getName() {
     return this.getProperty(AttributeDeclaration.NAME);
@@ -69,6 +63,22 @@ public class AttributeDeclaration extends BaseConcept implements INamedConcept {
 
   public void setVirtualPackage(String value) {
     this.setProperty(AttributeDeclaration.VIRTUAL_PACKAGE, value);
+  }
+
+  public Type getType() {
+    return (Type)this.getChild(AttributeDeclaration.TYPE);
+  }
+
+  public void setType(Type node) {
+    super.setChild(AttributeDeclaration.TYPE, node);
+  }
+
+  public Expression getOnChange() {
+    return (Expression)this.getChild(AttributeDeclaration.ON_CHANGE);
+  }
+
+  public void setOnChange(Expression node) {
+    super.setChild(AttributeDeclaration.ON_CHANGE, node);
   }
 
 }
