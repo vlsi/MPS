91ff7d2859fe15d5691c49b649272e8194eae877
Konstantin Solomatov <kostik@jetbrains.com>
2008-02-26 16:18:05 +0300

    use more sophisticated way to intern strings

--- core/kernel/source/jetbrains/mps/util/InternUtil.java
+++ core/kernel/source/jetbrains/mps/util/InternUtil.java
@@ -8,7 +8,22 @@ public class InternUtil {
   private static final Map<String, String> internCache = new HashMap<String, String>();
 
   public static String intern(String s) {
-    if (s == null) return null;
-    return s.intern();
+    if (s == null) {
+      return null;
+    }
+
+    if (internCache.containsKey(s)) {
+      return internCache.get(s);
+    }
+
+    String result = s.intern();
+
+    internCache.put(result, result);
+
+    return result;
+  }
+
+  public static void clear() {
+    internCache.clear();
   }
 }
