3d8cba5b6e859647c9fda7abb5270ff8d797185d
Konstantin Solomatov <kostik@jetbrains.com>
2008-02-26 12:45:01 +0300

    speed up editor loading

--- core/languageDesign/editorLanguage/source/jetbrains/mps/nodeEditor/EditorsFinderManager.java
+++ core/languageDesign/editorLanguage/source/jetbrains/mps/nodeEditor/EditorsFinderManager.java
@@ -2,27 +2,29 @@ package jetbrains.mps.nodeEditor;
 
 import jetbrains.mps.smodel.SNode;
 import jetbrains.mps.util.NameUtil;
-import jetbrains.mps.reloading.ClassLoaderManager;
 import jetbrains.mps.logging.Logger;
 
 import java.util.*;
 
-/**
- * Created by IntelliJ IDEA.
- * User: Cyril.Konopko
- * Date: 01.02.2006
- * Time: 17:31:48
- * To change this template use File | Settings | File Templates.
- */
 public class EditorsFinderManager {
   private static final Logger LOG = Logger.getLogger(EditorsFinderManager.class);
 
-  private static Map<String, IGeneralizingEntityEditorFinder> ourLanguageNamespacesToGEEditorFinders = new HashMap<String, IGeneralizingEntityEditorFinder>();
+  private static Map<String, IGeneralizingEntityEditorFinder> ourLanguageNamespacesToGEEditorFinders = new HashMap<String, IGeneralizingEntityEditorFinder>();  
+  private static Map<String, INodeEditor> ourCachedEditors = new HashMap<String, INodeEditor>();
 
   public static INodeEditor loadEditor(EditorContext context, SNode node) {
     IGeneralizingEntityEditorFinder finder = getGEFinder(node);
-    if (finder == null) finder = new DefaultGeneralizingEntityEditorFinder();
-    return finder.findEditor(node, context);
+    if (finder == null) {
+      finder = new DefaultGeneralizingEntityEditorFinder();
+    }
+
+    if (ourCachedEditors.containsKey(node.getConceptFqName())) {
+      return ourCachedEditors.get(node.getConceptFqName());
+    }
+
+    INodeEditor result = finder.findEditor(node, context);    
+    ourCachedEditors.put(node.getConceptFqName(), result);
+    return result;
   }
 
   public static IGeneralizingEntityEditorFinder getGEFinder(SNode node) {
@@ -38,15 +40,7 @@ public class EditorsFinderManager {
     ourLanguageNamespacesToGEEditorFinders.remove(languageNamespace);
   }
 
-  static {
-/*    try {
-      Class cls = Class.forName("jetbrains.semanticweb.editorLanguage.OWLInstanceGEFinder", true, ClassLoaderManager.getInstance().getMPSClassLoader());
-      registerEditorJavaClassesFinder("jetbrains.semanticweb.core", (IGeneralizingEntityEditorFinder) cls.newInstance());
-    } catch (ClassNotFoundException cnfe) {
-      //it's ok if we didn't find it we're not editing OWL 
-    } catch (Throwable t) {
-      LOG.error(t);
-    } */
+  public static void clear() {
+    ourCachedEditors.clear();
   }
-
 }
