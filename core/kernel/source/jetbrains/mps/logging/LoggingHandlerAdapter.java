9fdf67fc2457e9bad1b51ece4c30022b9acb677c
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2008-02-27 22:27:17 +0300

    check logging level in MPS logger

--- core/kernel/source/jetbrains/mps/logging/LoggingHandlerAdapter.java
+++ core/kernel/source/jetbrains/mps/logging/LoggingHandlerAdapter.java
@@ -9,36 +9,23 @@ import org.apache.log4j.Level;
 public abstract class LoggingHandlerAdapter implements ILoggingHandler {
   public abstract void addLogEntry(LogEntry e);
 
-  private org.apache.log4j.Logger getLogger(String fqName) {
-    return org.apache.log4j.Logger.getLogger(fqName);
-  }
   public final void info(LogEntry e) {
-    if(Level.INFO.isGreaterOrEqual(getLogger(e.getSourceClass()).getEffectiveLevel())) {
-      addLogEntry(e);
-    }
+    addLogEntry(e);
   }
 
   public final void warning(LogEntry e) {
-    if(Level.WARN.isGreaterOrEqual(getLogger(e.getSourceClass()).getEffectiveLevel())) {
-      addLogEntry(e);
-    }
+    addLogEntry(e);
   }
 
   public final void debug(LogEntry e) {
-    if(Level.DEBUG.isGreaterOrEqual(getLogger(e.getSourceClass()).getEffectiveLevel())) {
-      addLogEntry(e);
-    }
+    addLogEntry(e);
   }
 
   public final void error(LogEntry e) {
-    if(Level.ERROR.isGreaterOrEqual(getLogger(e.getSourceClass()).getEffectiveLevel())) {
-      addLogEntry(e);
-    }
+    addLogEntry(e);
   }
 
   public final void fatal(LogEntry e) {
-    if(Level.FATAL.isGreaterOrEqual(getLogger(e.getSourceClass()).getEffectiveLevel())) {
-      addLogEntry(e);
-    }
+    addLogEntry(e);
   }
 }
