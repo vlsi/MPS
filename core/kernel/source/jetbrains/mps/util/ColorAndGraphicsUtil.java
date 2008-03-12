5a89fa927db4bc2d077ae42db6e36b3ea07715c1
Konstantin Solomatov <kostik@jetbrains.com>
2008-03-12 17:14:35 +0300

    trying to optimize waved line drawing

--- core/kernel/source/jetbrains/mps/util/ColorAndGraphicsUtil.java
+++ core/kernel/source/jetbrains/mps/util/ColorAndGraphicsUtil.java
@@ -1,17 +1,11 @@
 package jetbrains.mps.util;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
-/**
- * Created by IntelliJ IDEA.
- * User: User
- * Date: 18.01.2006
- * Time: 16:10:52
- * To change this template use File | Settings | File Templates.
- */
 public class ColorAndGraphicsUtil {
   public static final int WAVE_HEIGHT = 2;
-  public static final int WAVE_SEGMENT_LENGTH = 4;
+  public static final int WAVE_SEGMENT_LENGTH = WAVE_HEIGHT * 2;
 
   public static Color saturateColor(Color c, float factor) {
     float[] hsb = new float[3];
@@ -45,8 +39,15 @@ public class ColorAndGraphicsUtil {
     return new BasicStroke(1.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 1.0f, dash, 0.0f);
   }
 
-
   public static void drawWave(Graphics g, int xStart, int xEnd, int y) {
+    Graphics2D g2d = (Graphics2D) g;
+    Paint oldPaint = g2d.getPaint();
+    g2d.setPaint(createPaintForLine(g.getColor()));
+    g2d.fillRect(xStart, y, xEnd - xStart, WAVE_HEIGHT);
+    g2d.setPaint(oldPaint);
+  }
+
+  public static void drawWaveOld(Graphics g, int xStart, int xEnd, int y) {
     int startSegment = xStart / WAVE_SEGMENT_LENGTH;
     int endSegment = xEnd / WAVE_SEGMENT_LENGTH;
     for (int i = startSegment; i < endSegment; i++) {
@@ -60,6 +61,16 @@ public class ColorAndGraphicsUtil {
   public static void drawWaveSegment(Graphics g, int x, int y) {
     g.drawLine(x, y + WAVE_HEIGHT, x + WAVE_SEGMENT_LENGTH / 2, y);
     g.drawLine(x + WAVE_SEGMENT_LENGTH / 2, y, x + WAVE_SEGMENT_LENGTH, y + WAVE_HEIGHT);
+  }
+
+  private static TexturePaint createPaintForLine(Color c) {
+    BufferedImage image = new BufferedImage(WAVE_SEGMENT_LENGTH, WAVE_HEIGHT, BufferedImage.TYPE_INT_ARGB);
+    Graphics g = image.getGraphics();
+    g.setColor(new Color(0, 0, 0, 0));
+    g.fillRect(0, 0, WAVE_SEGMENT_LENGTH, WAVE_HEIGHT);
+    g.setColor(c);
+    drawWaveSegment(g, 0, 0);
+    return new TexturePaint(image, new Rectangle(0, 0, WAVE_SEGMENT_LENGTH , WAVE_HEIGHT));
   }
 
   public static Point getCentralPosition(Component parent, Component component) {
