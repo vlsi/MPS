ec5729fd77e44032779b17f9aeb03489f3fe9892
Konstantin Solomatov <kostik@jetbrains.com>
2008-03-31 14:12:26 +0400

    more benchmarks

--- platform/uiLanguage/source/jetbrains/mps/uiLanguage/IterationBenchmark.java
+++ platform/uiLanguage/source/jetbrains/mps/uiLanguage/IterationBenchmark.java
@@ -6,14 +6,34 @@ import jetbrains.mps.util.Folder;
 
 import java.util.*;
 
-public class Sandbox {
+public class IterationBenchmark {
   public static void main(String[] args) {
-    final List<Integer> a = Arrays.asList(1,2,3);
+    final List<Integer> a = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+    final Integer[] ar = a.toArray(new Integer[a.size()]);
 
     final Map m = new HashMap();
     for (int i = 0; i < 100000; i++) {
       m.put(i, i);      
     }
+
+    measure(new Runnable() {
+      public void run() {
+        int sum = 0;
+        int size = ar.length;
+        for (int i = 0; i < size; i++) {
+          sum += ar[i];
+        }
+      }
+    });
+
+    measure(new Runnable() {
+      public void run() {
+        int sum = 0;
+        for (int i : ar) {
+          sum += i;
+        }
+      }
+    });
 
     measure(new Runnable() {
       public void run() {
@@ -57,7 +77,7 @@ public class Sandbox {
   private static void measure(Runnable r) {
     long start = System.currentTimeMillis();
 
-    for (int i = 0; i < 1000000; i++) {
+    for (int i = 0; i < 10000000; i++) {
       r.run();
     }
 
