1c057bf8e2017ab8f2a4878c5ad24a381ba7cc82
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2008-04-04 02:57:54 +0400

    cleanup

--- core/baseLanguage/baseLanguage/source/jetbrains/mps/baseLanguage/helgins/Queries.java
+++ core/baseLanguage/baseLanguage/source/jetbrains/mps/baseLanguage/helgins/Queries.java
@@ -51,98 +51,98 @@ public class Queries {
     return leastCommonSupertypes.iterator().next();
   }
 
-  public static Object CustomExpression_getBinaryOperationType(Object... args) {
-    SNode leftType = (SNode) args[0];
-    SNode rightType = (SNode) args[1];
-    boolean mayBeString = false;
-    if (args.length >= 3) {
-      mayBeString = (Boolean) args[2];
-    }
-    return getBinaryOperationType(leftType, rightType, mayBeString);
-  }
-
-  public static Object CustomExpression_hasConceptProperty_lvalue(Object... args) {
-    SNode node = (SNode) args[0];
-    return node.hasConceptProperty("lvalue");
-  }
-
-  @Hack
-  public static Object CustomExpression_classifierTypeSuperTypes(Object... args) {
-    ClassifierType clt = (ClassifierType) BaseAdapter.fromNode((SNode) args[0]);
-    List<SNode> result = new ArrayList<SNode>();
-
-    Classifier classifier = clt.getClassifier();
-    if (classifier instanceof ClassConcept) {
-      for (ClassifierType interfaceType : ((ClassConcept)classifier).getImplementedInterfaces()) {
-        result.add(BaseAdapter.fromAdapter(supertypeByDeclaration(clt, interfaceType)));
-      }
-      ClassifierType superclass = ((ClassConcept) classifier).getSuperclass();
-      if (superclass != null) {
-        result.add(BaseAdapter.fromAdapter(supertypeByDeclaration(clt, superclass)));
-      }
-    }
-    if (classifier instanceof Interface) {
-      for (ClassifierType interfaceType : ((Interface) classifier).getExtendedInterfaces()) {
-        result.add(BaseAdapter.fromAdapter(supertypeByDeclaration(clt, interfaceType)));
-      }
-    }
-    SModel model = TypeChecker.getInstance().getRuntimeTypesModel();
-    ClassifierType erasedClt = ClassifierType.newInstance(model);
-    erasedClt.setClassifier(classifier);
-    result.add(BaseAdapter.fromAdapter(erasedClt));
-    return result;
-  }
-
-  @Hack
-  private static ClassifierType supertypeByDeclaration(ClassifierType subType,  ClassifierType declaredSupertype) {
-    Classifier cls = subType.getClassifier();
-    List<TypeVariableDeclaration> declarations = cls.getTypeVariableDeclarations();
-    List<Type> typeParameters = subType.getParameters();
-    SModel model = TypeChecker.getInstance().getRuntimeTypesModel();
-    ClassifierType result = CopyUtil.copy(declaredSupertype);
-    for (Type t : result.getParameters()) {
-      if (t instanceof TypeVariableReference) {
-        TypeVariableReference ref = (TypeVariableReference) t;
-        int i = declarations.indexOf(ref.getTypeVariableDeclaration());
-        Type newT = null;
-        if (i > -1 && i < typeParameters.size()) {
-          newT = typeParameters.get(i);
-        }
-        if (newT != null) {
-          newT = CopyUtil.copy(newT);
-          result.replaceChild(t, newT);
-        } else {
-          result.removeChild(t);
-        }
-      }
-    }
-    return result;
-  }
-
-  @Hack
-  public static Object CustomExpression_arrayTypeSuperTypes(Object... args) {   //todo
-    ArrayType art = (ArrayType) BaseAdapter.fromNode((SNode) args[0]);
-    SModel model = TypeChecker.getInstance().getRuntimeTypesModel();
-    List<SNode> result = new ArrayList<SNode>();
-    for (SNode type : TypeChecker.getInstance().getSubtypingManager().
-            collectImmediateSupertypes(BaseAdapter.fromAdapter(art.getComponentType()))) {
-      if (BaseAdapter.isInstance(type, Type.class)) {
-        ArrayType arrayType = ArrayType.newInstance(model);
-        arrayType.setComponentType((Type) BaseAdapter.fromNode(CopyUtil.copy(type)));
-        result.add(arrayType.getNode());
-      }
-    }
-    return result;
-  }
-
-  public static Object CustomExpression_allReturnStatements(Object... args) {
-    SNode statementList = ((SNode) args[0]);
-    List<SNode> result = new ArrayList<SNode>();
-    for (SNode node : CollectionUtil.iteratorAsIterable(statementList.depthFirstChildren())) {
-      if (BaseAdapter.isInstance(node, ReturnStatement.class)) {
-        result.add(node);
-      }
-    }
-    return result;
-  }
+//  public static Object CustomExpression_getBinaryOperationType(Object... args) {
+//    SNode leftType = (SNode) args[0];
+//    SNode rightType = (SNode) args[1];
+//    boolean mayBeString = false;
+//    if (args.length >= 3) {
+//      mayBeString = (Boolean) args[2];
+//    }
+//    return getBinaryOperationType(leftType, rightType, mayBeString);
+//  }
+//
+//  public static Object CustomExpression_hasConceptProperty_lvalue(Object... args) {
+//    SNode node = (SNode) args[0];
+//    return node.hasConceptProperty("lvalue");
+//  }
+//
+//  @Hack
+//  public static Object CustomExpression_classifierTypeSuperTypes(Object... args) {
+//    ClassifierType clt = (ClassifierType) BaseAdapter.fromNode((SNode) args[0]);
+//    List<SNode> result = new ArrayList<SNode>();
+//
+//    Classifier classifier = clt.getClassifier();
+//    if (classifier instanceof ClassConcept) {
+//      for (ClassifierType interfaceType : ((ClassConcept)classifier).getImplementedInterfaces()) {
+//        result.add(BaseAdapter.fromAdapter(supertypeByDeclaration(clt, interfaceType)));
+//      }
+//      ClassifierType superclass = ((ClassConcept) classifier).getSuperclass();
+//      if (superclass != null) {
+//        result.add(BaseAdapter.fromAdapter(supertypeByDeclaration(clt, superclass)));
+//      }
+//    }
+//    if (classifier instanceof Interface) {
+//      for (ClassifierType interfaceType : ((Interface) classifier).getExtendedInterfaces()) {
+//        result.add(BaseAdapter.fromAdapter(supertypeByDeclaration(clt, interfaceType)));
+//      }
+//    }
+//    SModel model = TypeChecker.getInstance().getRuntimeTypesModel();
+//    ClassifierType erasedClt = ClassifierType.newInstance(model);
+//    erasedClt.setClassifier(classifier);
+//    result.add(BaseAdapter.fromAdapter(erasedClt));
+//    return result;
+//  }
+//
+//  @Hack
+//  private static ClassifierType supertypeByDeclaration(ClassifierType subType,  ClassifierType declaredSupertype) {
+//    Classifier cls = subType.getClassifier();
+//    List<TypeVariableDeclaration> declarations = cls.getTypeVariableDeclarations();
+//    List<Type> typeParameters = subType.getParameters();
+//    SModel model = TypeChecker.getInstance().getRuntimeTypesModel();
+//    ClassifierType result = CopyUtil.copy(declaredSupertype);
+//    for (Type t : result.getParameters()) {
+//      if (t instanceof TypeVariableReference) {
+//        TypeVariableReference ref = (TypeVariableReference) t;
+//        int i = declarations.indexOf(ref.getTypeVariableDeclaration());
+//        Type newT = null;
+//        if (i > -1 && i < typeParameters.size()) {
+//          newT = typeParameters.get(i);
+//        }
+//        if (newT != null) {
+//          newT = CopyUtil.copy(newT);
+//          result.replaceChild(t, newT);
+//        } else {
+//          result.removeChild(t);
+//        }
+//      }
+//    }
+//    return result;
+//  }
+//
+//  @Hack
+//  public static Object CustomExpression_arrayTypeSuperTypes(Object... args) {   //todo
+//    ArrayType art = (ArrayType) BaseAdapter.fromNode((SNode) args[0]);
+//    SModel model = TypeChecker.getInstance().getRuntimeTypesModel();
+//    List<SNode> result = new ArrayList<SNode>();
+//    for (SNode type : TypeChecker.getInstance().getSubtypingManager().
+//            collectImmediateSupertypes(BaseAdapter.fromAdapter(art.getComponentType()))) {
+//      if (BaseAdapter.isInstance(type, Type.class)) {
+//        ArrayType arrayType = ArrayType.newInstance(model);
+//        arrayType.setComponentType((Type) BaseAdapter.fromNode(CopyUtil.copy(type)));
+//        result.add(arrayType.getNode());
+//      }
+//    }
+//    return result;
+//  }
+//
+//  public static Object CustomExpression_allReturnStatements(Object... args) {
+//    SNode statementList = ((SNode) args[0]);
+//    List<SNode> result = new ArrayList<SNode>();
+//    for (SNode node : CollectionUtil.iteratorAsIterable(statementList.depthFirstChildren())) {
+//      if (BaseAdapter.isInstance(node, ReturnStatement.class)) {
+//        result.add(node);
+//      }
+//    }
+//    return result;
+//  }
 }
