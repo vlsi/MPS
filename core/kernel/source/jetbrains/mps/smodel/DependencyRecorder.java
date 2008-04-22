70cb0233c75021c533fae24b79db2a25148d6801
Konstantin Solomatov <kostik@jetbrains.com>
2008-04-22 17:47:32 +0400

    many to many map use it in Dependency recorder

--- core/kernel/source/jetbrains/mps/smodel/DependencyRecorder.java
+++ core/kernel/source/jetbrains/mps/smodel/DependencyRecorder.java
@@ -1,12 +1,12 @@
 package jetbrains.mps.smodel;
 
 import jetbrains.mps.bootstrap.helgins.runtime.incremental.INodesReadListener;
+import jetbrains.mps.util.ManyToManyMap;
 
 import java.util.*;
 
 public class DependencyRecorder<T> {
-  private Map<T, Set<SNode>> myObjectsToNodes = new HashMap<T, Set<SNode>>();
-  private Map<SNode, Set<T>> myNodesToObjects = new HashMap<SNode, Set<T>>();
+  private ManyToManyMap<T, SNode> myObjectsToNodes = new ManyToManyMap<T, SNode>();
 
   public void rebuild(T t, Runnable r) {
     MyNodeReadListener listener = new MyNodeReadListener();
@@ -16,59 +16,22 @@ public class DependencyRecorder<T> {
     } finally {
       NodeReadEventsCaster.removeNodesReadListener();
     }
-    clear(t);
+    myObjectsToNodes.clearFirst(t);
     for (SNode n : listener.getDependencies()) {
-      addLink(t, n);
+      myObjectsToNodes.addLink(t, n);
     }
   }
 
   public void remove(T t) {
-    clear(t);
-  }
-
-  private void clear(T t) {
-    if (!myObjectsToNodes.containsKey(t)) return;
-    for (SNode n : (Set<SNode>) new HashSet<SNode>(myObjectsToNodes.get(t))) {
-      removeLink(t, n);
-    }
-  }
-
-  private void addLink(T t, SNode n) {
-    if (!myObjectsToNodes.containsKey(t)) {
-      myObjectsToNodes.put(t, new HashSet<SNode>());
-    }
-    if (!myNodesToObjects.containsKey(n)) {
-      myNodesToObjects.put(n, new HashSet<T>());
-    }
-    myObjectsToNodes.get(t).add(n);
-    myNodesToObjects.get(n).add(t);
-  }
-
-  private void removeLink(T t, SNode n) {
-    myObjectsToNodes.get(t).remove(n);
-    myNodesToObjects.get(n).remove(t);
-    if (myObjectsToNodes.get(t).isEmpty()) {
-      myObjectsToNodes.remove(t);
-    }
-    if (myNodesToObjects.get(n).isEmpty()) {
-      myNodesToObjects.remove(n);
-    }
+    myObjectsToNodes.clearFirst(t);
   }
 
   public Set<SNode> getDependencies(T t) {
-    if (myObjectsToNodes.containsKey(t)) {
-      return Collections.unmodifiableSet(myObjectsToNodes.get(t));
-    } else {
-      return Collections.emptySet();
-    }
+    return myObjectsToNodes.getByFirst(t);
   }
 
   public Set<T> getDependOn(SNode n) {
-    if (myNodesToObjects.containsKey(n)) {
-      return Collections.unmodifiableSet(myNodesToObjects.get(n));
-    } else {
-      return Collections.emptySet();
-    }    
+    return myObjectsToNodes.getBySecond(n);
   }
 
   private class MyNodeReadListener implements INodesReadListener {
---
+++ core/kernel/source/jetbrains/mps/util/ManyToManyMap.java
@@ -0,0 +1,68 @@
+package jetbrains.mps.util;
+
+import java.util.*;
+
+public class ManyToManyMap<F, S> {
+  private Map<F, Set<S>> myFToS = new HashMap<F, Set<S>>();
+  private Map<S, Set<F>> mySToF = new HashMap<S, Set<F>>();
+
+  public void clearFirst(F f) {
+    if (!myFToS.containsKey(f)) return;
+    for (S s : new HashSet<S>(myFToS.get(f))) {
+      removeLink(f, s);
+    }
+  }
+
+  public void clearSecond(S s) {
+    if (!mySToF.containsKey(s)) return;
+    for (F f : new HashSet<F>(mySToF.get(s))) {
+      removeLink(f, s);
+    }
+  }
+
+  public void addLink(F f, S s) {
+    if (!myFToS.containsKey(f)) {
+      myFToS.put(f, new HashSet<S>());
+    }
+    if (!mySToF.containsKey(s)) {
+      mySToF.put(s, new HashSet<F>());
+    }
+    myFToS.get(f).add(s);
+    mySToF.get(s).add(f);
+  }
+
+  public void removeLink(F f, S s) {
+    myFToS.get(f).remove(s);
+    mySToF.get(s).remove(f);
+    if (myFToS.get(f).isEmpty()) {
+      myFToS.remove(f);
+    }
+    if (mySToF.get(s).isEmpty()) {
+      mySToF.remove(s);
+    }
+  }
+
+  public Set<F> getFirst() {
+    return Collections.unmodifiableSet(myFToS.keySet());
+  }
+
+  public Set<S> getSecond() {
+    return Collections.unmodifiableSet(mySToF.keySet());
+  }
+
+  public Set<S> getByFirst(F f) {
+    if (myFToS.containsKey(f)) {
+      return Collections.unmodifiableSet(myFToS.get(f));
+    } else {
+      return Collections.emptySet();
+    }
+  }
+
+  public Set<F> getBySecond(S s) {
+    if (mySToF.containsKey(s)) {
+      return Collections.unmodifiableSet(mySToF.get(s));
+    } else {
+      return Collections.emptySet();
+    }
+  }
+}
