f4843cc2bda1bdef55c29934e3dbe0527ea14a83
Konstantin Solomatov <kostik@jetbrains.com>
2008-04-24 12:14:22 +0400

    minor

--- core/kernel/source/jetbrains/mps/smodel/ModuleRepositoryListener.java
+++ core/kernel/source/jetbrains/mps/smodel/ModuleRepositoryListener.java
@@ -2,13 +2,6 @@ package jetbrains.mps.smodel;
 
 import jetbrains.mps.project.IModule;
 
-/**
- * Created by IntelliJ IDEA.
- * User: Igoor
- * Date: May 12, 2006
- * Time: 1:11:32 PM
- * To change this template use File | Settings | File Templates.
- */
 public interface ModuleRepositoryListener {
   void moduleAdded(IModule module);
   void beforeModuleRemoved(IModule module);
