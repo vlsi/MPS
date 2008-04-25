f489b1db7435f001a6a05f6fa977b47dd0490f54
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2008-04-25 16:14:53 +0400

    MPS - FU - todo is now a part of BL plugin

---
+++ core/baseLanguage/baseLanguage/source/jetbrains/mps/baseLanguage/plugin/TodoViewer.java
@@ -0,0 +1,67 @@
+package jetbrains.mps.baseLanguage.plugin;
+
+import jetbrains.mps.ide.IDEProjectFrame;
+import jetbrains.mps.ide.findusages.findalgorithm.resultproviders.TreeBuilder;
+import jetbrains.mps.ide.findusages.model.SearchQuery;
+import jetbrains.mps.ide.findusages.model.SearchResults;
+import jetbrains.mps.ide.findusages.view.UsageView;
+import jetbrains.mps.ide.findusages.view.UsageView.ButtonConfiguration;
+import jetbrains.mps.ide.findusages.view.treeholder.treeview.INodeRepresentator;
+import jetbrains.mps.ide.findusages.view.treeholder.treeview.ViewOptions;
+import jetbrains.mps.project.MPSProject;
+import jetbrains.mps.smodel.SNode;
+import jetbrains.mps.smodel.SNodePointer;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class TodoViewer extends JPanel {
+  private UsageView myUsageView;
+
+  public TodoViewer(final IDEProjectFrame projectFrame) {
+    setLayout(new BorderLayout());
+
+    add(new JLabel("Click to find TODOs", JLabel.CENTER), BorderLayout.CENTER);
+
+    addMouseListener(new MouseAdapter() {
+      public void mouseClicked(MouseEvent e) {
+        removeAll();
+
+        myUsageView = new UsageView(projectFrame, new ViewOptions()) {
+          public void close() {
+            //hideTool();
+          }
+        };
+        add(myUsageView.getComponent(), BorderLayout.CENTER);
+
+        new Thread(new Runnable() {
+          public void run() {
+            MPSProject project = projectFrame.getProject();
+
+            if (project == null) return;
+
+            myUsageView.setRunOptions(
+              TreeBuilder.forFinder(new TodoFinder()),
+              new SearchQuery(new SNodePointer((SNode) null), project.getScope()),
+              new ButtonConfiguration(true),
+              new SearchResults()
+            );
+
+            myUsageView.setCustomNodeRepresentator(MyNodeRepresentator.class);
+
+            myUsageView.run();
+          }
+        }).start();
+      }
+    });
+  }
+
+  public static class MyNodeRepresentator implements INodeRepresentator {
+    public String getPresentation(SNode node) {
+      return "<font color=blue>" + node.getProperty("value") + "</font>";
+    }
+  }
+}
