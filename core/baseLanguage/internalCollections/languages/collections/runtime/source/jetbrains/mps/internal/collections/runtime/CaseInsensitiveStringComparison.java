4275eac1273dc657003f673a454c943fcbd2776d
Fedor Isakov <Fedor.Isakov@jetbrains.com>
2008-05-08 03:48:16 +0400

    Reimplemented the sort() operation reduction

---
+++ core/baseLanguage/internalCollections/languages/collections/runtime/source/jetbrains/mps/internal/collections/runtime/CaseInsensitiveStringComparison.java
@@ -0,0 +1,29 @@
+/*
+ * Created May 7, 2008 at 6:40:11 PM
+ */
+package jetbrains.mps.internal.collections.runtime;
+
+
+/**
+ * @author fyodor
+ */
+public class CaseInsensitiveStringComparison {
+
+    @SuppressWarnings("unchecked")
+    public static <T> int compare (T a, T b, ISelector<T, Comparable<?>> selector) {
+        if (a == b) {
+            return 0;
+        }
+        if (a == null || b == null) {
+            return a == null ? -1 : 1; 
+        }
+        // everything should be an Object, right?
+        Object selA = selector.select(a);
+        Object selB = selector.select(b);
+        // a hack implemented by popular demand
+        if ((selA instanceof String) && (selB instanceof String)) {
+            return String.CASE_INSENSITIVE_ORDER.compare((String)selA, (String)selB);
+        }
+        return ((Comparable<Object>) selA).compareTo((Comparable<Object>) selB);
+    }
+}
