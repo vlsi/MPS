4670a61855e7e41cd2fc1f8f787ea231e5efa748
Konstantin Solomatov <kostik@jetbrains.com>
2008-05-07 19:27:59 +0400

    save transient models action

---
+++ idea-app/source/jetbrains/mps/idea/actions/SaveTransientModelsAction.java
@@ -0,0 +1,35 @@
+package jetbrains.mps.idea.actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.ToggleAction;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.Project;
+import jetbrains.mps.idea.MPSProjectHolder;
+import jetbrains.mps.project.MPSProject;
+import jetbrains.mps.generator.GeneratorManager;
+
+public class SaveTransientModelsAction extends ToggleAction {
+  public SaveTransientModelsAction() {
+  }
+
+  public boolean isSelected(AnActionEvent e) {
+    Project project = e.getData(PlatformDataKeys.PROJECT);
+    if (project == null) {
+      return false;
+    }
+    MPSProject mpsProject = project.getComponent(MPSProjectHolder.class).getMPSProject();
+    return mpsProject.getComponentSafe(GeneratorManager.class).isSaveTransientModels();
+  }
+
+  public void update(AnActionEvent e) {
+    e.getPresentation().setVisible(e.getData(PlatformDataKeys.PROJECT) != null);
+  }
+
+  public void setSelected(AnActionEvent e, boolean state) {
+    Project project = e.getData(PlatformDataKeys.PROJECT);
+    assert project != null;
+    MPSProject mpsProject = project.getComponent(MPSProjectHolder.class).getMPSProject();
+    mpsProject.getComponentSafe(GeneratorManager.class).setSaveTransientModels(state);
+  }
+}
