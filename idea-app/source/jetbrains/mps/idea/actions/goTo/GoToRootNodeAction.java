c55742e979217f05c3416bb99ea4098c863c30a2
Konstantin Solomatov <kostik@jetbrains.com>
2008-05-08 13:02:02 +0400

    goto actions

--- idea-app/source/jetbrains/mps/idea/actions/goTo/GoToRootNodeAction.java
+++ idea-app/source/jetbrains/mps/idea/actions/goTo/GoToRootNodeAction.java
@@ -3,10 +3,11 @@ package jetbrains.mps.idea.actions.goTo;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import jetbrains.mps.ide.action.ActionContext;
+import jetbrains.mps.ide.actions.gotoMenu.GoToRootAction;
 
 public class GoToRootNodeAction extends AnAction {
   public void actionPerformed(AnActionEvent e) {
     ActionContext context = new ActionContext();
-    new jetbrains.mps.ide.actions.gotoMenu.GoToRootAction().execute(context);
+    new GoToRootAction().execute(context);
   }
 }
