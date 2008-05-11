a3437a7a89ba15f644c1ee2892fc7411e9713996
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2008-05-11 15:27:12 +0400

    MPS - UI - events work with component inheritance

--- platform/uiLanguage/runtime/source/jetbrains/mps/uiLanguage/runtime/events/Events.java
+++ platform/uiLanguage/runtime/source/jetbrains/mps/uiLanguage/runtime/events/Events.java
@@ -1,17 +1,32 @@
 package jetbrains.mps.uiLanguage.runtime.events;
 
-import java.util.Map;
+import org.jetbrains.annotations.Nullable;
+
 import java.util.HashMap;
+import java.util.Map;
 
 public abstract class Events {
+  @Nullable
+  private Events myParentEvents = null;
   private Map<String, Event> myEvents = new HashMap<String, Event>();
+
+  protected Events() {
+
+  }
+
+  protected Events(@Nullable Events parentEvents) {
+    myParentEvents = parentEvents;
+  }
 
   protected void addEvent(Event e) {
     myEvents.put(e.getName(), e);
   }
 
   public Event getEvent(String name) {
-    return myEvents.get(name);
+    Event event = myEvents.get(name);
+    if (event != null) return event;
+    if (myParentEvents == null) return null;
+    return myParentEvents.getEvent(name);
   }
 
   public void initialize() {
