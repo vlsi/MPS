b748df049ebe85803abf8907ef9ef2cb50b1a6b4
Konstantin Solomatov <kostik@jetbrains.com>
2008-05-12 15:39:07 +0400

    make highlighter work without deadlocks

--- idea-app/source/jetbrains/mps/idea/highlighter/MPSIDEAEditorsProvider.java
+++ idea-app/source/jetbrains/mps/idea/highlighter/MPSIDEAEditorsProvider.java
@@ -26,49 +26,66 @@ import com.intellij.openapi.vfs.VirtualFile;
  * To change this template use File | Settings | File Templates.
  */
 public class MPSIDEAEditorsProvider implements IEditorsProvider {
-  public List<IEditor> getAllEditors(final MPSProject mpsProject) {
-    final List<IEditor> editors = new ArrayList<IEditor>();
-/*    ApplicationManager.getApplication().invokeAndWait(new Runnable() {
-      public void run() { //todo this about deadlocks
-        FileEditorManager fileEditorManager = getFileEditorManager(mpsProject);
-        for (FileEditor fileEditor : fileEditorManager.getAllEditors()) {
-          if (fileEditor instanceof MPSFileNodeEditor) {
-            MPSFileNodeEditor mpsFileNodeEditor = (MPSFileNodeEditor) fileEditor;
-            IEditor nodeEditor = mpsFileNodeEditor.getNodeEditor();
-            if (nodeEditor != null) {
-              editors.add(nodeEditor);
-            }
+  private Project myProject;
+
+  private List<IEditor> myEditors = new ArrayList<IEditor>();
+  private IEditor myCurrentEditor;
+
+  private final Object myLock = new Object();
+
+  public MPSIDEAEditorsProvider(Project project) {
+    myProject = project;
+    FileEditorManager manager = FileEditorManager.getInstance(project);
+    manager.addFileEditorManagerListener(new FileEditorManagerListener() {
+      public void fileOpened(FileEditorManager source, VirtualFile file) {
+        updateInformation();
+      }
+
+      public void fileClosed(FileEditorManager source, VirtualFile file) {
+        updateInformation();
+      }
+
+      public void selectionChanged(FileEditorManagerEvent event) {
+        updateInformation();
+      }
+    });
+  }
+
+  private void updateInformation() {
+    synchronized (myLock) {
+      myEditors.clear();
+
+      FileEditorManager fileEditorManager = FileEditorManager.getInstance(myProject);
+      for (FileEditor fileEditor : fileEditorManager.getAllEditors()) {
+        if (fileEditor instanceof MPSFileNodeEditor) {
+          MPSFileNodeEditor mpsFileNodeEditor = (MPSFileNodeEditor) fileEditor;
+          IEditor nodeEditor = mpsFileNodeEditor.getNodeEditor();
+          if (nodeEditor != null) {
+            myEditors.add(nodeEditor);
           }
         }
       }
-    }, ModalityState.defaultModalityState()); */
-    return editors;
+
+      FileEditor[] selectedEditors = fileEditorManager.getSelectedEditors();
+      if (selectedEditors.length > 0) {
+        FileEditor fileEditor = selectedEditors[0];
+        if (fileEditor instanceof MPSFileNodeEditor) {
+          MPSFileNodeEditor mpsFileNodeEditor = (MPSFileNodeEditor) fileEditor;
+          myCurrentEditor = mpsFileNodeEditor.getNodeEditor();
+        }
+      }
+    }
+  }
+
+  public List<IEditor> getAllEditors(final MPSProject mpsProject) {
+    synchronized (myLock) {
+      return new ArrayList<IEditor>(myEditors);
+    }
   }
 
   public IEditor getCurrentEditor(final MPSProject mpsProject) {
-    final IEditor[] result = new IEditor[1];
-    result[0] = null;
-/*    ApplicationManager.getApplication().invokeAndWait(new Runnable() {
-      public void run() {
-        FileEditorManager fileEditorManager = getFileEditorManager(mpsProject);
-        FileEditor[] selectedEditors = fileEditorManager.getSelectedEditors();
-        if (selectedEditors.length > 0) {
-          FileEditor fileEditor = selectedEditors[0];
-          if (fileEditor instanceof MPSFileNodeEditor) {
-            MPSFileNodeEditor mpsFileNodeEditor = (MPSFileNodeEditor) fileEditor;
-            result[0] = mpsFileNodeEditor.getNodeEditor();
-            return;
-          }
-        }
-      }
-    }, ModalityState.defaultModalityState()); */
-
-    return result[0];
-  }
-
-  private FileEditorManager getFileEditorManager(MPSProject mpsProject) {
-    Project project = mpsProject.getComponent(Project.class);
-    FileEditorManager fileEditorManager = FileEditorManager.getInstance(project);
-    return fileEditorManager;
+    synchronized (myLock) {
+      return myCurrentEditor;
+    }
   }
 }
