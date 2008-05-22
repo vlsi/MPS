1bcc6fb880fbfdc9dd579139ba4e6c23ec471246
Konstantin Solomatov <kostik@jetbrains.com>
2008-05-22 15:56:01 +0400

    fix project pane state persistence

--- workbench/source/jetbrains/mps/ide/projectPane/ProjectPane.java
+++ workbench/source/jetbrains/mps/ide/projectPane/ProjectPane.java
@@ -4,6 +4,7 @@ import com.intellij.openapi.actionSystem.DataProvider;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
+import com.intellij.openapi.components.PersistentStateComponent;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupManager;
 import com.intellij.openapi.wm.ToolWindowAnchor;
@@ -12,6 +13,7 @@ import jetbrains.mps.ide.EditorsPane;
 import jetbrains.mps.ide.IProjectPane;
 import jetbrains.mps.ide.MPSToolBar;
 import jetbrains.mps.ide.ThreadUtils;
+import jetbrains.mps.ide.projectPane.ProjectPane.MyState;
 import jetbrains.mps.ide.action.ActionContext;
 import jetbrains.mps.ide.action.IActionDataProvider;
 import jetbrains.mps.ide.actions.*;
@@ -57,13 +59,13 @@ import java.util.List;
 @State(
   name = "MPSProjectPane",
   storages = {
-  @Storage(
-    id = "other",
-    file = "$WORKSPACE_FILE$"
-  )
-    }
+    @Storage(
+      id = "other",
+      file = "$WORKSPACE_FILE$"
+    )
+  }
 )
-public class ProjectPane extends BaseMPSTool implements IActionDataProvider, DataProvider, IProjectPane, IExternalizableComponent {
+public class ProjectPane extends BaseMPSTool implements IActionDataProvider, DataProvider, IProjectPane, PersistentStateComponent<MyState>, IExternalizableComponent {
   private static final Logger LOG = Logger.getLogger(ProjectPane.class);
 
   public static final String PROJECT_PANE_NODE_ACTIONS = ProjectPaneNodeActions_ActionGroup.ID;
@@ -121,8 +123,6 @@ public class ProjectPane extends BaseMPSTool implements IActionDataProvider, Dat
       });
     }
   };
-
-  private MyState myState;
 
   public ProjectPane(Project project) {
     super(project, "MPS Project Pane", 1, Icons.MPS_SMALL_ICON, ToolWindowAnchor.LEFT, false);
