c25bdf10120c443544a841cc212108cda57a585d
Konstantin Solomatov <kostik@jetbrains.com>
2008-05-28 13:13:24 +0400

    preparing to move to idea locks, command processor and undo

--- core/kernel/source/jetbrains/mps/smodel/ModelAccess.java
+++ core/kernel/source/jetbrains/mps/smodel/ModelAccess.java
@@ -3,7 +3,121 @@ package jetbrains.mps.smodel;
 import jetbrains.mps.ide.command.CommandProcessor;
 import jetbrains.mps.baseLanguage.ext.collections.internal.CursorWithContinuation;
 
-class ModelAccess {  
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.Lock;
+
+import com.intellij.openapi.util.Computable;
+
+public class ModelAccess {
+  private static final ModelAccess ourInstance = new ModelAccess();
+
+  private ReentrantReadWriteLock myReadWriteLock = new ReentrantReadWriteLock();
+
+  public static ModelAccess instance() {
+    return ourInstance;
+  }
+
+  private ModelAccess() {
+  }
+
+  private boolean allowSharedRead() {
+    return false;
+  }
+
+  private Lock getReadLock() {
+    if (allowSharedRead()) {
+      return myReadWriteLock.readLock();
+    } else {
+      return myReadWriteLock.writeLock();
+    }
+  }
+
+  private Lock getWriteLock() {
+    return myReadWriteLock.writeLock();
+  }
+
+  public void runReadAction(Runnable r) {
+    getReadLock().lock();
+    try {
+      r.run();
+    } finally {
+      getReadLock().unlock();
+    }
+  }
+
+  public boolean tryRead(Runnable r) {
+    if (getReadLock().tryLock()) {
+      try {
+        r.run();
+      } finally {
+        getReadLock().unlock();
+      }
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public <T> T runReadAction(final Computable<T> c) {
+    final Object[] result = new Object[1];
+    runReadAction(new Runnable() {
+      public void run() {
+        result[0] = c.compute();
+      }
+    });
+    return (T) result[0];
+  }
+
+  public void runWriteAction(Runnable r) {
+    getWriteLock().lock();
+    try {
+      r.run();
+    } finally {
+      getWriteLock().unlock();
+    }
+  }
+
+  public boolean tryWrite(Runnable r) {
+    if (getWriteLock().tryLock()) {
+      try {
+        r.run();
+      } finally {
+        getWriteLock().unlock();
+      }      
+    } else {
+      return false;
+    }
+  }
+
+  public <T> T runWriteAction(final Computable<T> c) {
+    final Object[] result = new Object[1];
+    runWriteAction(new Runnable() {
+      public void run() {
+        result[0] = c.compute();
+      }
+    });
+    return (T) result[0];
+  }
+
+  public void checkWriteAccess() {
+    if (!myReadWriteLock.isWriteLockedByCurrentThread()) {
+      throw new IllegalStateException();
+    }
+  }
+
+  public void checkReadAccess() {
+    if (allowSharedRead()) {
+      throw new UnsupportedOperationException();
+    } else {
+      if (!myReadWriteLock.isWriteLockedByCurrentThread()) {
+        throw new IllegalStateException();
+      }
+    }
+  }
+
+  
+
   static void assertLegalRead(SNode node) {
     if (!CommandProcessor.instance().isInsideCommand()) {
       if (Thread.currentThread() instanceof CursorWithContinuation) {
