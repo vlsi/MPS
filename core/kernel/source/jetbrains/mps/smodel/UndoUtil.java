d05c263f9ece92fedc9b183db27dd52cfa82e773
Konstantin Solomatov <kostik@jetbrains.com>
2008-06-02 14:44:29 +0400

    better way of wokring with undo

--- core/kernel/source/jetbrains/mps/smodel/UndoUtil.java
+++ core/kernel/source/jetbrains/mps/smodel/UndoUtil.java
@@ -4,15 +4,22 @@ import com.intellij.openapi.command.undo.UndoableAction;
 import com.intellij.openapi.command.undo.UndoManager;
 import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.ide.DataManager;
 
 class UndoUtil {
   static void addUndoableAction(UndoableAction action) {
-    for (Project p : ProjectManager.getInstance().getOpenProjects()) {
-      UndoManager undoManager = UndoManager.getInstance(p);
-      if (undoManager.isUndoInProgress() || undoManager.isRedoInProgress()) {
-        continue;
+
+
+    DataContext dataContext = DataManager.getInstance().getDataContext();
+    Project project = PlatformDataKeys.PROJECT.getData(dataContext);
+
+    if (project != null) {
+      UndoManager undoManager = UndoManager.getInstance(project);
+      if (!undoManager.isUndoInProgress() && !undoManager.isRedoInProgress()) {
+        undoManager.undoableActionPerformed(action);
       }
-      undoManager.undoableActionPerformed(action);
     }
   }
 }
