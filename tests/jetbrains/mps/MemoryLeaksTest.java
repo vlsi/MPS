9473b11c77d3c41b6eb7b671a0d52fed99aa3d32
Konstantin Solomatov <kostik@jetbrains.com>
2008-06-10 12:50:34 +0400

    fix test

--- tests/jetbrains/mps/MemoryLeaksTest.java
+++ tests/jetbrains/mps/MemoryLeaksTest.java
@@ -4,6 +4,6 @@ import java.io.File;
 
 public class MemoryLeaksTest extends BaseMPSTest {
   public void testEditorLanguageGenerationLeaks() {
-    assertTrue(testProjectGenerationForLeaks(new File("core/languageDesign/editorLanguage/editorLanguage.mpr")));
+    assertTrue(testProjectGenerationForLeaks(new File("core/languageDesign/editorLanguage/editorLanguage.ipr")));
   }
 }
