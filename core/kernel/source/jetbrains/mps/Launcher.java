3dafa9ff1c4c752e47685dca59929d8ab178b5bf
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2008-06-18 19:01:35 +0400

    add svn plugin

--- core/kernel/source/jetbrains/mps/Launcher.java
+++ core/kernel/source/jetbrains/mps/Launcher.java
@@ -8,7 +8,7 @@ public class Launcher {
   public static void main(String[] args) {
     System.setProperty("idea.is.internal", "true");
     System.setProperty("idea.no.jre.check", "true");
-    System.setProperty("idea.load.plugins", "false");
+    System.setProperty("idea.load.plugins", "true");
     System.setProperty("idea.platform.prefix", "MPS");
 
     Bootstrap.main(args, MPSMainImpl.class.getName(), "start");
