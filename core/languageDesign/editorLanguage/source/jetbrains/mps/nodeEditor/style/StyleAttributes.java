84f13f658c0521680596d386ed89f72108d21c65
Konstantin Solomatov <kostik@jetbrains.com>
2008-06-23 17:32:19 +0400

    preparing for using styles in TextLine

--- core/languageDesign/editorLanguage/source/jetbrains/mps/nodeEditor/style/StyleAttributes.java
+++ core/languageDesign/editorLanguage/source/jetbrains/mps/nodeEditor/style/StyleAttributes.java
@@ -6,7 +6,9 @@ import java.awt.Color;
 
 public class StyleAttributes {
   public static final StyleAttribute<Color> BACKGROUND_COLOR = new InheritableStyleAttribute<Color>("background-color");
-  public static final StyleAttribute<Color> BRACKETS_COLOR = new InheritableStyleAttribute<Color>("bracket-color", Color.BLACK);  
+  public static final StyleAttribute<Color> BRACKETS_COLOR = new InheritableStyleAttribute<Color>("bracket-color", Color.BLACK);
+  public static final StyleAttribute<Color> TEXT_COLOR = new InheritableStyleAttribute<Color>("text-color", Color.BLACK);
+  public static final StyleAttribute<Color> NULL_TEXT_COLOR = new InheritableStyleAttribute<Color>("null-text-color", Color.GRAY);  
 
   public static final StyleAttribute<Boolean> DRAW_BRACKETS = new SimpleStyleAttribute<Boolean>("draw-brackets", false);
   public static final StyleAttribute<Boolean> DRAW_BORDER = new SimpleStyleAttribute<Boolean>("draw-border", false);
