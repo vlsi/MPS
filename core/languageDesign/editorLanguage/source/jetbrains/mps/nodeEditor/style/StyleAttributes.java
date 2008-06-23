244392ffd12382acd79d265a3a454c1403a6f88f
Konstantin Solomatov <kostik@jetbrains.com>
2008-06-23 18:21:03 +0400

    cleanup

--- core/languageDesign/editorLanguage/source/jetbrains/mps/nodeEditor/style/StyleAttributes.java
+++ core/languageDesign/editorLanguage/source/jetbrains/mps/nodeEditor/style/StyleAttributes.java
@@ -1,6 +1,7 @@
 package jetbrains.mps.nodeEditor.style;
 
 import jetbrains.mps.nodeEditor.FocusPolicy;
+import jetbrains.mps.nodeEditor.CaretPosition;
 
 import java.awt.Color;
 
@@ -17,9 +18,11 @@ public class StyleAttributes {
   public static final StyleAttribute<Boolean> DRAW_BRACKETS = new SimpleStyleAttribute<Boolean>("draw-brackets", false);
   public static final StyleAttribute<Boolean> DRAW_BORDER = new SimpleStyleAttribute<Boolean>("draw-border", false);
   public static final StyleAttribute<Boolean> SELECTABLE = new SimpleStyleAttribute<Boolean>("selectable", true);
+  public static final StyleAttribute<Boolean> EDITABLE = new SimpleStyleAttribute<Boolean>("editable", true);
 
   public static final StyleAttribute<String> RT_ANCHOR_TAG = new SimpleStyleAttribute<String>("rt-anchor-tag");
   public static final StyleAttribute<String> LAYOUT_CONSTRAINT = new SimpleStyleAttribute<String>("layout-constraint");
   public static final StyleAttribute<FocusPolicy> FOCUS_POLICY = new SimpleStyleAttribute<FocusPolicy>("focus-policy", FocusPolicy.NONE);
+  public static final StyleAttribute<CaretPosition> DEFAULT_CARET_POSITON = new SimpleStyleAttribute<CaretPosition>("default-caret-position");
 
 }
