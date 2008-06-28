9088253e537e40ef3e3932dd89481769194e2760
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2008-06-28 21:47:30 +0400

    icon for model in HierarchyView

--- workbench/source/jetbrains/mps/ide/hierarchy/icons/Icons.java
+++ workbench/source/jetbrains/mps/ide/hierarchy/icons/Icons.java
@@ -6,5 +6,5 @@ import javax.swing.ImageIcon;
 public class Icons {
   public static final Icon CHILDREN_ICON = new ImageIcon(Icons.class.getResource("subtypes.png"));
   public static final Icon PARENT_ICON = new ImageIcon(Icons.class.getResource("supertypes.png"));
-  public static final Icon THIS_MODEL_ICON = new ImageIcon(Icons.class.getResource("thisModel.png"));
+  public static final Icon THIS_MODEL_ICON = jetbrains.mps.ide.projectPane.Icons.MODEL_ICON;
 }
