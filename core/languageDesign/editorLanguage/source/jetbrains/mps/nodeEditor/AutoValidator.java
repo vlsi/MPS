84d5b6c06646481f22fd78464232e7da1ac8e40a
Konstantin Solomatov <kostik@jetbrains.com>
2008-07-02 14:19:43 +0400

    cleaner way of auto validation

--- core/languageDesign/editorLanguage/source/jetbrains/mps/nodeEditor/AutoValidator.java
+++ core/languageDesign/editorLanguage/source/jetbrains/mps/nodeEditor/AutoValidator.java
@@ -3,6 +3,8 @@ package jetbrains.mps.nodeEditor;
 import com.intellij.openapi.wm.impl.CommandProcessor;
 import jetbrains.mps.smodel.ModelAccess;
 
+import javax.swing.SwingUtilities;
+
 class AutoValidator {
   private AbstractEditorComponent myEditorComponent;
 
@@ -15,13 +17,17 @@ class AutoValidator {
     public void selectionChanged(final AbstractEditorComponent editor, final EditorCell oldSelection, EditorCell newSelection) {
       if (editor.isCellSwapInProgress()) return;
       if (oldSelection != null && !EditorUtil.isValidCell(oldSelection)) {
-        editor.flushEvents();
-        ModelAccess.instance().runWriteActionInCommand(new Runnable() {
+        final CellInfo cellInfo = oldSelection.getCellInfo();
+        SwingUtilities.invokeLater(new Runnable() {
           public void run() {
-            Object memento = editor.getEditorContext().createMemento();
-            EditorUtil.validateCell(oldSelection, null, true, true, false);
-            editor.flushEvents();
-            editor.getEditorContext().setMemento(memento);
+            ModelAccess.instance().runWriteActionInCommand(new Runnable() {
+              public void run() {
+                Object memento = editor.getEditorContext().createMemento();
+                EditorUtil.validateCell(cellInfo.findCell(editor), null, true, true, false);
+                editor.flushEvents();
+                editor.getEditorContext().setMemento(memento);
+              }
+            });
           }
         });
       }
