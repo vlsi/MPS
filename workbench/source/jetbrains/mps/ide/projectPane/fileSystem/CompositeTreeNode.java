1df8c01ad3dd59544f1ee18e6674d03a71e9cc6d
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2008-07-03 18:00:30 +0400

    fix duplicated roots in file pane

--- workbench/source/jetbrains/mps/ide/projectPane/fileSystem/CompositeTreeNode.java
+++ workbench/source/jetbrains/mps/ide/projectPane/fileSystem/CompositeTreeNode.java
@@ -4,11 +4,15 @@ import jetbrains.mps.ide.ui.MPSTreeNode;
 import jetbrains.mps.smodel.IOperationContext;
 import jetbrains.mps.vfs.FileSystem;
 import jetbrains.mps.vfs.VFileSystem;
+import jetbrains.mps.vfs.IFile;
 import com.intellij.openapi.vcs.ProjectLevelVcsManager;
 import com.intellij.openapi.vcs.VcsDirectoryMapping;
 import com.intellij.openapi.vcs.impl.VcsFileStatusProvider;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
+
+import java.util.Set;
+import java.util.LinkedHashSet;
 
 public class CompositeTreeNode extends MPSTreeNode {
   private Project myProject;
@@ -18,13 +22,23 @@ public class CompositeTreeNode extends MPSTreeNode {
     myProject = operationContext.getProject();
 
     ProjectLevelVcsManager manager = ProjectLevelVcsManager.getInstance(myProject);
+    Set<String> roots = new LinkedHashSet<String>();
     for (VcsDirectoryMapping m : manager.getDirectoryMappings()) {
       if (!m.getDirectory().equals("")) {
-        add(new FolderTreeNode(operationContext, myProject.getComponent(VcsFileStatusProvider.class), FileSystem.getFile(m.getDirectory())));
+        IFile folder = FileSystem.getFile(m.getDirectory());
+        String path = folder.getPath().replace("\\", "/");
+        if (!roots.contains(path)) {
+          roots.add(path);
+          add(new FolderTreeNode(operationContext, myProject.getComponent(VcsFileStatusProvider.class), folder));
+        }
       } else {
         VirtualFile basedir = myProject.getBaseDir();
         if (basedir != null) {
+          String path = basedir.getPath().replace("\\", "/");
+          if (!roots.contains(path)){
+          roots.add(path);
           add(new FolderTreeNode(operationContext, myProject.getComponent(VcsFileStatusProvider.class), VFileSystem.toIFile(basedir)));
+        }
         }
       }
     }
