f0f97e2412c9de4cfff81dd9986a0c5f83311204
Konstantin Solomatov <kostik@jetbrains.com>
2008-07-10 18:32:49 +0400

    disable ypath

--- tests/jetbrains/mps/ApplicationsTest.java
+++ tests/jetbrains/mps/ApplicationsTest.java
@@ -14,10 +14,10 @@ public class ApplicationsTest extends BaseMPSTest {
       assertTrue(res, res == null);
   }
  
-  public void testYPath() {
-    String res = testProject(new File("platform/ypath/YPath.mpr"));
-    assertTrue(res, res == null);
-  }
+//  public void testYPath() {
+//    String res = testProject(new File("platform/ypath/YPath.mpr"));
+//    assertTrue(res, res == null);
+//  }
 
   public void testPanacea() {
     String res = testProject(new File("platform/panacea/Panacea.mpr"));
