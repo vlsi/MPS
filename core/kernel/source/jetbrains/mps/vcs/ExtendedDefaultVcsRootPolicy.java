7dee8cd5549cb200b428bc1eae53cb3734abdb55
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2008-08-04 17:23:07 +0400

    MPS-2388: Illegal argument exception : Argument 0 for @NotNull parameter of com/intellij/openapi/vfs/VfsUtil.isAncestor must not be null

--- core/kernel/source/jetbrains/mps/vcs/ExtendedDefaultVcsRootPolicy.java
+++ core/kernel/source/jetbrains/mps/vcs/ExtendedDefaultVcsRootPolicy.java
@@ -5,7 +5,9 @@ import com.intellij.openapi.vcs.changes.VcsDirtyScopeManagerImpl;
 import com.intellij.openapi.vcs.ProjectLevelVcsManager;
 import com.intellij.openapi.vcs.VcsRoot;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.platform.PlatformProjectOpenProcessor;
+import com.intellij.platform.ProjectBaseDirectory;
 
 public class ExtendedDefaultVcsRootPolicy extends BasicDefaultVcsRootPolicy {
   private Project myIDEAProject;
@@ -22,8 +24,18 @@ public class ExtendedDefaultVcsRootPolicy extends BasicDefaultVcsRootPolicy {
     ProjectLevelVcsManager manager = ProjectLevelVcsManager.getInstance(myIDEAProject);
     VcsRoot[] roots = manager.getAllVcsRoots();
 
-    for (VcsRoot root : roots){
+    for (VcsRoot root : roots) {
       vcsDirtyScopeManager.dirDirtyRecursively(root.path);
     }
+  }
+
+  @Override
+  public boolean matchesDefaultMapping(VirtualFile file, Object matchContext) {
+    if (ProjectBaseDirectory.getInstance(myIDEAProject).getBaseDir() == null) {
+      // fix MPS-2388
+      // java.lang.IllegalArgumentException: Argument 0 for @NotNull parameter of com/intellij/openapi/vfs/VfsUtil.isAncestor must not be null
+      return false;
+    }
+    return super.matchesDefaultMapping(file, matchContext);
   }
 }
