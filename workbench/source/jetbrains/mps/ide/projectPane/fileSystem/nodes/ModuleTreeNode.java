943e869ea3f5905f4a40a32ce1db9723f8633a06
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2008-08-05 22:55:11 +0400

    progress on "New Devkit"

--- workbench/source/jetbrains/mps/ide/projectPane/fileSystem/nodes/ModuleTreeNode.java
+++ workbench/source/jetbrains/mps/ide/projectPane/fileSystem/nodes/ModuleTreeNode.java
@@ -1,23 +1,18 @@
 package jetbrains.mps.ide.projectPane.fileSystem.nodes;
 
-import jetbrains.mps.ide.ui.MPSTreeNode;
-import jetbrains.mps.ide.actions.FileActions_ActionGroup;
+import com.intellij.openapi.actionSystem.ActionGroup;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
 import jetbrains.mps.ide.actions.ModuleActions_ActionGroup;
-import jetbrains.mps.smodel.IOperationContext;
 import jetbrains.mps.project.IModule;
 import jetbrains.mps.vfs.VFileSystem;
 import jetbrains.mps.workbench.action.ActionUtils;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vcs.impl.VcsFileStatusProvider;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.openapi.actionSystem.ActionGroup;
-import com.intellij.ide.ui.customization.CustomizableActionsSchemas;
 
 public class ModuleTreeNode extends AbstractFileTreeNode {
   private final IModule myModule;
 
   public ModuleTreeNode(Project project, IModule m) {
-    super(project, VFileSystem.getFile(m.getDescriptorFile().getParent()));
+    super(project, VFileSystem.refreshAndGetFile(m.getDescriptorFile().getParent().toFile()));
 
     myModule = m;
 
