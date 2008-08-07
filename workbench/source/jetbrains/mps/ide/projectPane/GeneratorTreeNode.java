fe75bc7fd7cd94bed5098d1482041aed494316e4
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2008-08-07 15:43:23 +0400

    MPS-2471: 'generation required' status is not shown for 'generator' node

--- workbench/source/jetbrains/mps/ide/projectPane/GeneratorTreeNode.java
+++ workbench/source/jetbrains/mps/ide/projectPane/GeneratorTreeNode.java
@@ -1,12 +1,12 @@
 package jetbrains.mps.ide.projectPane;
 
+import com.intellij.openapi.actionSystem.ActionGroup;
 import jetbrains.mps.ide.ui.MPSTreeNode;
+import jetbrains.mps.ide.ui.smodel.SModelTreeNode;
 import jetbrains.mps.project.MPSProject;
 import jetbrains.mps.project.ModuleContext;
 import jetbrains.mps.smodel.Generator;
 import jetbrains.mps.workbench.action.ActionUtils;
-
-import com.intellij.openapi.actionSystem.ActionGroup;
 
 class GeneratorTreeNode extends MPSTreeNode {
 
@@ -17,12 +17,54 @@ class GeneratorTreeNode extends MPSTreeNode {
   }
 
   protected void updatePresentation() {
+    if (generationRequired()) {
+      setAdditionalText("generation required");
+    } else if (getGenerator().isPackaged()) {
+      setAdditionalText("packaged");
+    } else {
+      setAdditionalText(null);
+    }
+
+    if (getGenerator().validate().isEmpty()) {
+      setTooltipText(null);
+    } else {
+      String result = "<html>";
+      for (String error : getGenerator().validate()) {
+        result += error + "<br>";
+      }
+      setTooltipText(result);
+    }
+
     setIcon(Icons.GENERATOR_ICON);
     setNodeIdentifier(calculateNodeIdenifier());
     setText(calculateText());
 
     setErrorState(!getGenerator().isValid());
   }
+
+  public boolean generationRequired() {
+    return generationRequired(this);
+  }
+
+  private boolean generationRequired(MPSTreeNode node) {
+    if (getGenerator().isPackaged()) {
+      return false;
+    }
+
+    if (node instanceof SModelTreeNode) {
+      SModelTreeNode smodelTreeNode = (SModelTreeNode) node;
+      return smodelTreeNode.generationRequired();
+    }
+
+    for (int i = 0; i < node.getChildCount(); i++) {
+      if (generationRequired((MPSTreeNode) node.getChildAt(i))) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
 
   public Generator getGenerator() {
     return (Generator) getOperationContext().getModule();
