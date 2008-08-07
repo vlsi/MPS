ca51fd4e7e64e6a7e469b6319e399f234bb098ad
Konstantin Solomatov <kostik@jetbrains.com>
2008-08-07 18:45:15 +0400

    increase expiration date

--- workbench/source/jetbrains/mps/workbench/license/MPSLicenseManager.java
+++ workbench/source/jetbrains/mps/workbench/license/MPSLicenseManager.java
@@ -37,7 +37,7 @@ public class MPSLicenseManager extends LicenseManager {
   }
 
   public Date getEvaluationExpirationDate() {
-    GregorianCalendar calendar = new GregorianCalendar(2008, Calendar.AUGUST, 31);
+    GregorianCalendar calendar = new GregorianCalendar(2008, Calendar.DECEMBER, 31);
     return new Date(calendar.getTimeInMillis());
   }
 
