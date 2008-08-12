144a722887e168c688a37939b8a602c8ce4506dc
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2008-08-12 19:49:27 +0400

    add multi-line strings to buildlanguage

--- platform/buildlanguage/languages/buildlanguage/source_gen/jetbrains/mps/buildlanguage/constraints/ConstraintsDescriptor.java
+++ platform/buildlanguage/languages/buildlanguage/source_gen/jetbrains/mps/buildlanguage/constraints/ConstraintsDescriptor.java
@@ -14,6 +14,7 @@ public class ConstraintsDescriptor implements IModelConstraints {
   public ConstraintsDescriptor() {
     this.myConstraints.add(new GenericCall_name_PropertyConstraint());
     this.myConstraints.add(new StringLiteral_name_PropertyConstraint());
+    this.myConstraints.add(new MultiLineString_name_PropertyConstraint());
     this.myConstraints.add(new TargetReference_targetDeclaration_ReferentConstraint());
     this.myConstraints.add(new GenericCall_declaration_ReferentConstraint());
     this.myConstraints.add(new PathReference_call_ReferentConstraint());
