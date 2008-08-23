ea9003a1c0f5dd4412da76490119c541f5b25553
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2008-08-23 16:58:00 +0400

    MPS-2607: UVT exception on disposing

--- workbench/source/jetbrains/mps/ide/findusages/view/UsagesViewTool.java
+++ workbench/source/jetbrains/mps/ide/findusages/view/UsagesViewTool.java
@@ -14,6 +14,7 @@ import com.intellij.openapi.progress.Task.Modal;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupManager;
 import com.intellij.openapi.util.Computable;
+import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.wm.ToolWindowAnchor;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentManager;
@@ -159,6 +160,7 @@ public class UsagesViewTool extends BaseProjectTool implements PersistentStateCo
         myUsageViewsData.remove(event.getIndex());
       }
     };
+    
     getContentManager().addContentManagerListener(myContentListener);
   }
 
@@ -171,7 +173,8 @@ public class UsagesViewTool extends BaseProjectTool implements PersistentStateCo
   }
 
   public void doUnregister() {
-    getContentManager().removeContentManagerListener(myContentListener);
+    //this is done automatically on content manager dispose, otherwise a dependency UVT->CM must be added
+    //getContentManager().removeContentManagerListener(myContentListener);
     KeymapManager.getInstance().getActiveKeymap().removeShortcut(PREV_COMMAND, getPrevShortcut());
     KeymapManager.getInstance().getActiveKeymap().removeShortcut(NEXT_COMMAND, getNextShortcut());
     ActionManager.getInstance().unregisterAction(PREV_COMMAND);
