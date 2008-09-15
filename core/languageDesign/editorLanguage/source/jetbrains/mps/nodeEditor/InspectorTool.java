b1350f67b74030d33ef2410f46f105df47d8eb74
Konstantin Solomatov <kostik@jetbrains.com>
2008-09-15 14:05:26 +0400

    revert

--- core/languageDesign/editorLanguage/source/jetbrains/mps/nodeEditor/InspectorTool.java
+++ core/languageDesign/editorLanguage/source/jetbrains/mps/nodeEditor/InspectorTool.java
@@ -1,16 +1,11 @@
 package jetbrains.mps.nodeEditor;
 
-import com.intellij.openapi.fileEditor.FileEditorManager;
-import com.intellij.openapi.fileEditor.FileEditorManagerAdapter;
-import com.intellij.openapi.fileEditor.FileEditorManagerListener;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.wm.ToolWindowAnchor;
 import jetbrains.mps.ide.projectPane.Icons;
 import jetbrains.mps.nodeEditor.inspector.InspectorEditorComponent;
 import jetbrains.mps.smodel.IOperationContext;
 import jetbrains.mps.smodel.SNode;
-import jetbrains.mps.workbench.nodesFs.MPSNodeVirtualFile;
 import jetbrains.mps.workbench.tools.BaseProjectTool;
 import org.jetbrains.annotations.Nullable;
 
@@ -24,27 +19,6 @@ public class InspectorTool extends BaseProjectTool {
 
   private JPanel myComponent;
   private InspectorEditorComponent myInspectorComponent;
-  private FileEditorManager myEditorManager;
-  private FileEditorManagerListener myListener = new FileEditorManagerAdapter() {
-    public void fileClosed(FileEditorManager source, VirtualFile file) {
-      boolean close = true;
-      for (VirtualFile vfile : myEditorManager.getOpenFiles()) {
-        if (vfile instanceof MPSNodeVirtualFile) {
-          close = false;
-          break;
-        }
-      }
-      if (close) {
-        makeUnavailableLater();
-      }
-    }
-
-    public void fileOpened(FileEditorManager source, VirtualFile file) {
-      if (file instanceof MPSNodeVirtualFile && !isAvailable()) {
-        makeAvailableLater();
-      }
-    }
-  };
 
   public InspectorTool(Project project) {
     super(project, ID, 5, Icons.INSPECTOR_ICON, ToolWindowAnchor.BOTTOM, true, false);
@@ -52,7 +26,6 @@ public class InspectorTool extends BaseProjectTool {
 
   public void initComponent() {
     super.initComponent();
-    myEditorManager = FileEditorManager.getInstance(getProject());
     SwingUtilities.invokeLater(new Runnable() {
       public void run() {
         myComponent = new JPanel(new BorderLayout());
@@ -60,11 +33,6 @@ public class InspectorTool extends BaseProjectTool {
         myComponent.add(myInspectorComponent.getExternalComponent(), BorderLayout.CENTER);
       }
     });
-    myEditorManager.addFileEditorManagerListener(myListener);
-  }
-
-  public void disposeComponent() {
-    myEditorManager.removeFileEditorManagerListener(myListener);
   }
 
   protected boolean isInitiallyAvailable() {
