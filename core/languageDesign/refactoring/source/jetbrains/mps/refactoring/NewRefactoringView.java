913275dacbfbbb35f871333d29339a36ae4cdfd7
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2008-09-17 20:41:18 +0400

    NewRefactoringView visuals

--- core/languageDesign/refactoring/source/jetbrains/mps/refactoring/NewRefactoringView.java
+++ core/languageDesign/refactoring/source/jetbrains/mps/refactoring/NewRefactoringView.java
@@ -1,26 +1,19 @@
 package jetbrains.mps.refactoring;
 
-import com.intellij.openapi.actionSystem.ActionManager;
-import com.intellij.openapi.actionSystem.ActionPlaces;
-import com.intellij.openapi.actionSystem.DefaultActionGroup;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.startup.StartupManager;
 import com.intellij.openapi.wm.ToolWindowAnchor;
+import com.intellij.ui.content.ContentManager;
 import jetbrains.mps.ide.findusages.model.SearchResults;
 import jetbrains.mps.ide.projectPane.Icons;
-import jetbrains.mps.logging.Logger;
 import jetbrains.mps.workbench.tools.BaseProjectTool;
 import org.jetbrains.annotations.NotNull;
 
-import javax.swing.JComponent;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
-import java.awt.BorderLayout;
 
 public class NewRefactoringView extends BaseProjectTool {
-  private static final Logger LOG = Logger.getLogger(NewRefactoringView.class);
   private RefactoringViewItem myRefactoringViewItem;
-  private JPanel myComponent = new JPanel(new BorderLayout());
   public JLabel myLabel;
 
   protected NewRefactoringView(Project project) {
@@ -29,16 +22,13 @@ public class NewRefactoringView extends BaseProjectTool {
 
   public void initComponent() {
     super.initComponent();
-    myLabel = new JLabel("no refactoring");
-
-    myComponent.add(myLabel, BorderLayout.CENTER);
-    SwingUtilities.invokeLater(new Runnable() {
+    StartupManager.getInstance(getProject()).registerPostStartupActivity(new Runnable() {
       public void run() {
-        DefaultActionGroup group = new DefaultActionGroup();
-        group.add(createCloseAction());
-
-        JComponent toolbar = ActionManager.getInstance().createActionToolbar(ActionPlaces.UNKNOWN, group, false).getComponent();
-        myComponent.add(toolbar, BorderLayout.WEST);
+        SwingUtilities.invokeLater(new Runnable() {
+          public void run() {
+            getToolWindow().installWatcher(getContentManager());
+          }
+        });
       }
     });
   }
@@ -46,25 +36,19 @@ public class NewRefactoringView extends BaseProjectTool {
   public void showRefactoringView(@NotNull RefactoringViewAction refactoringViewAction,
                                   SearchResults searchResults) {
     myRefactoringViewItem = new RefactoringViewItem(refactoringViewAction, searchResults, this);
-    myComponent.remove(myLabel);
-    myComponent.add(myRefactoringViewItem.getComponent(), BorderLayout.CENTER);
+    addContent(myRefactoringViewItem.getComponent(), "refactoring", null, false);
     myRefactoringViewItem.initUsagesView();
     openTool(true);
   }
 
-  /*package*/ void closeRefactoringView() {
+  void closeRefactoringView() {
     SwingUtilities.invokeLater(new Runnable() {
       public void run() {
-        myComponent.remove(myRefactoringViewItem.getComponent());
-        myLabel = new JLabel("no refactoring");
-        myComponent.add(myLabel, BorderLayout.CENTER);
+        ContentManager manager = getContentManager();
+        manager.removeContent(manager.getContent(myRefactoringViewItem.getComponent()), true);
         myRefactoringViewItem = null;
         close();
       }
     });
-  }
-
-  public JComponent getComponent() {
-    return myComponent;
   }
 }
