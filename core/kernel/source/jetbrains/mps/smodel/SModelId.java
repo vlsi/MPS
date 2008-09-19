b4014375ae4b024d018d58c51bf4075c1c645615
Konstantin Solomatov <kostik@jetbrains.com>
2008-09-19 14:58:08 +0400

    smodel ids

--- core/kernel/source/jetbrains/mps/smodel/SModelId.java
+++ core/kernel/source/jetbrains/mps/smodel/SModelId.java
@@ -3,6 +3,9 @@ package jetbrains.mps.smodel;
 import java.util.concurrent.atomic.AtomicLong;
 
 public abstract class SModelId {
+  private static final String REGULAR_PREFIX = "r:";
+  private static final String FOREIGN_PREFIX = "f:";
+
   private static final AtomicLong ourIds = new AtomicLong(System.currentTimeMillis());
 
   public static SModelId generate() {
@@ -15,6 +18,18 @@ public abstract class SModelId {
 
   public static SModelId foreign(String id) {
     return new ForeignSModelId(id);
+  }
+
+  public static SModelId fromString(String id) {
+    if (id.startsWith(REGULAR_PREFIX)) {
+      String suffix = id.substring(REGULAR_PREFIX.length());
+      return regular(Long.parseLong(suffix));
+    }
+    if (id.startsWith(FOREIGN_PREFIX)) {
+      String suffix = id.substring(FOREIGN_PREFIX.length());
+      return foreign(suffix);
+    }
+    throw new IllegalArgumentException();
   }
 
   private SModelId() {
@@ -35,6 +50,10 @@ public abstract class SModelId {
     public int hashCode() {
       return (int) myId;
     }
+
+    public String toString() {
+      return REGULAR_PREFIX + myId;
+    }
   }
 
   private static class ForeignSModelId extends SModelId {
@@ -51,6 +70,10 @@ public abstract class SModelId {
 
     public int hashCode() {
       return myId.hashCode();
+    }
+
+    public String toString() {
+      return FOREIGN_PREFIX + myId;
     }
   }
 }
