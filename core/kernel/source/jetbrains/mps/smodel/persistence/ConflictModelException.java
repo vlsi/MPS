d4002fbb55001c25b049e0765b37608223a52193
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2008-09-23 15:06:52 +0400

    better error message

--- core/kernel/source/jetbrains/mps/smodel/persistence/ConflictModelException.java
+++ core/kernel/source/jetbrains/mps/smodel/persistence/ConflictModelException.java
@@ -5,6 +5,6 @@ import jetbrains.mps.smodel.SModelDescriptor;
 public class ConflictModelException extends RuntimeException {
   public ConflictModelException(SModelDescriptor modelDescriptor) {
     super("Model " + modelDescriptor.getSModelReference() + " file " + modelDescriptor.getModelFile().getAbsolutePath() + " is in conflict." +
-      " If merge dialog does not appear automatically select action \"Resolve Text Conflict\" from menu.");
+      " If merge dialog does not appear automatically, select action \"Subversion\"->\"Resolve Text Conflict\" from model file popup menu.");
   }
 }
