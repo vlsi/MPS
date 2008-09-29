b37f2a6f6c7d9a5f09391427fdd1239c99f02f68
Konstantin Solomatov <kostik@jetbrains.com>
2008-09-29 19:41:12 +0400

    option to regenerate language after rename

--- core/kernel/source/jetbrains/mps/refactoring/renameLanguage/RenameLanguageDialog.java
+++ core/kernel/source/jetbrains/mps/refactoring/renameLanguage/RenameLanguageDialog.java
@@ -2,10 +2,16 @@ package jetbrains.mps.refactoring.renameLanguage;
 
 import jetbrains.mps.ide.dialogs.BaseDialog;
 import jetbrains.mps.ide.dialogs.DialogDimensionsSettings.DialogDimensions;
+import jetbrains.mps.ide.genconf.GeneratorConfigUtil;
+import jetbrains.mps.ide.genconf.GenParameters;
 import jetbrains.mps.smodel.*;
-import jetbrains.mps.project.SModelRoot;
+import jetbrains.mps.project.*;
 import jetbrains.mps.refactoring.renameModel.ModelRenamer;
 import jetbrains.mps.util.FileUtil;
+import jetbrains.mps.projectLanguage.structure.LanguageGeneratorConfiguration;
+import jetbrains.mps.MPSProjectHolder;
+import jetbrains.mps.generator.GeneratorManager;
+import jetbrains.mps.generator.IGenerationType;
 
 import javax.swing.*;
 import java.awt.Frame;
@@ -13,19 +19,24 @@ import java.awt.HeadlessException;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Computable;
+
 public class RenameLanguageDialog extends BaseDialog {
   private JPanel myMainPanel;
   private JTextField myLanguageNameField;
   private JCheckBox myRegenerateLanguage;
 
   private Language myLanguage;
+  private Project myProject;
 
-  public RenameLanguageDialog(Frame frame, Language language) throws HeadlessException {
+  public RenameLanguageDialog(Frame frame, Project project, Language language) throws HeadlessException {
     super(frame);
 
     setTitle("Rename Language");
 
     myLanguage = language;
+    myProject = project;
 
     myMainPanel = new JPanel(new GridBagLayout());
 
@@ -88,10 +99,27 @@ public class RenameLanguageDialog extends BaseDialog {
     });
 
     if (myRegenerateLanguage.getModel().isSelected()) {
-      FileUtil.clear(myLanguage.getSourceDir());
+      final MPSProject mpsProject = myProject.getComponent(MPSProjectHolder.class).getMPSProject();
+      GenParameters params = ModelAccess.instance().runReadAction(new Computable<GenParameters>() {
+        public GenParameters compute() {
+          FileUtil.clear(myLanguage.getSourceDir());
 
-      
+          SModel model = AuxilaryRuntimeModel.getDescriptor().getSModel();
 
+          LanguageGeneratorConfiguration languageConfig = LanguageGeneratorConfiguration.newInstance(model);
+          languageConfig.setLanguageNamespace(myLanguage.getNamespace());
+          languageConfig.setName("tmp");
+
+          return GeneratorConfigUtil.calculate(mpsProject, languageConfig, true);
+        }
+      });
+
+
+      myProject.getComponent(GeneratorManager.class)
+        .generateModelsFromDifferentModules(
+          new ModuleContext(myLanguage, mpsProject),
+          params.getModels(),
+          IGenerationType.FILES);
     }
 
     dispose();
