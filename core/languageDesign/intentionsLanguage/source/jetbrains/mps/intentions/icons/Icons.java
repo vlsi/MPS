166b19ba14b6499031dffe19957f86df46c0f9a6
Cyril Konopko <Cyril.Konopko@jetbrains.com>
2008-09-29 14:21:04 +0400

    icon for migration intentions

--- core/languageDesign/intentionsLanguage/source/jetbrains/mps/intentions/icons/Icons.java
+++ core/languageDesign/intentionsLanguage/source/jetbrains/mps/intentions/icons/Icons.java
@@ -7,6 +7,6 @@ public class Icons {
   public static final Icon INTENTION = new ImageIcon(jetbrains.mps.intentions.icons.Icons.class.getResource("intentionBulb.png"));
   public static final Icon ERROR_INTENTION = new ImageIcon(jetbrains.mps.intentions.icons.Icons.class.getResource("quickfixBulb.png"));
   public static final Icon INTENTION_OFF = new ImageIcon(jetbrains.mps.intentions.icons.Icons.class.getResource("intentionOffBulb.png"));
-  public static final Icon MIGRATION = INTENTION;
+  public static final Icon MIGRATION = new ImageIcon(jetbrains.mps.intentions.icons.Icons.class.getResource("migrationBulb.png"));
   public static final Icon QUICKFIX = INTENTION;
 }
