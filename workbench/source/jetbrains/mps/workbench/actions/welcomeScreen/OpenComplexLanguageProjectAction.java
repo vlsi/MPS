a524b856fa28800e27f1fe4f2c01a9c8865dfa97
Konstantin Solomatov <kostik@jetbrains.com>
2008-10-02 19:40:55 +0400

    actions for test project on the start page

---
+++ workbench/source/jetbrains/mps/workbench/actions/welcomeScreen/OpenComplexLanguageProjectAction.java
@@ -0,0 +1,17 @@
+package jetbrains.mps.workbench.actions.welcomeScreen;
+
+import jetbrains.mps.workbench.action.BaseAction;
+import jetbrains.mps.util.PathManager;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.ide.impl.ProjectUtil;
+
+import java.io.File;
+
+public class OpenComplexLanguageProjectAction extends AnAction {
+  public void actionPerformed(AnActionEvent e) {
+    String projectFile = PathManager.getSamplesPath() + File.separator + "complexLanguage" + File.separator + "Complex.ipr";
+    ProjectUtil.openProject(projectFile, e.getData(PlatformDataKeys.PROJECT), false);
+  }
+}
---
+++ workbench/source/jetbrains/mps/workbench/actions/welcomeScreen/OpenTheSimplestLanguageProjectAction.java
@@ -0,0 +1,17 @@
+package jetbrains.mps.workbench.actions.welcomeScreen;
+
+import jetbrains.mps.workbench.action.BaseAction;
+import jetbrains.mps.util.PathManager;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.ide.impl.ProjectUtil;
+
+import java.io.File;
+
+public class OpenTheSimplestLanguageProjectAction extends AnAction {
+  public void actionPerformed(AnActionEvent e) {
+    String projectFile = PathManager.getSamplesPath() + File.separator + "theSimplestLanguage" + File.separator + "theSimplestLanguage.ipr";
+    ProjectUtil.openProject(projectFile, e.getData(PlatformDataKeys.PROJECT), false);
+  }
+}
