4e2a2d1ee9b0ab069f1ed9af4588e5ba32ad368f
Konstantin Solomatov <kostik@jetbrains.com>
2008-10-02 17:39:24 +0400

    disable project import dialog

--- core/kernel/source/jetbrains/mps/MPSMainImpl.java
+++ core/kernel/source/jetbrains/mps/MPSMainImpl.java
@@ -2,6 +2,7 @@ package jetbrains.mps;
 
 import com.intellij.idea.MainImpl;
 import com.intellij.ide.license.LicenseManager;
+import com.intellij.openapi.application.PathManager;
 
 import java.lang.reflect.Method;
 
@@ -12,6 +13,9 @@ public class MPSMainImpl {
   protected static void start(final String[] args) {
     try {
       LicenseManager.setInstance(new MPSLicenseManager());
+
+      //we need this in order to disable import settings dialog
+      PathManager.ensureConfigFolderExists(true);
 
       Class mainImplCls = MainImpl.class;
       Method method = mainImplCls.getDeclaredMethod("start", String[].class);
