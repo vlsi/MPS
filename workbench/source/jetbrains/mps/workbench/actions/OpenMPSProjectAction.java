ea6aac1689acdba5624470137006532b33b78c76
Konstantin Solomatov <kostik@jetbrains.com>
2008-10-06 19:21:47 +0400

    minor

--- workbench/source/jetbrains/mps/workbench/actions/OpenMPSProjectAction.java
+++ workbench/source/jetbrains/mps/workbench/actions/OpenMPSProjectAction.java
@@ -24,8 +24,6 @@ public class OpenMPSProjectAction extends BaseAction {
   }
 
   public void doExecute(AnActionEvent e) {
-    final ProjectManagerEx projectManager = ProjectManagerEx.getInstanceEx();
-
     TreeFileChooser chooser = new TreeFileChooser();
 
     chooser.setFileFilter(new IFileFilter() {
