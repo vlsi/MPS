5d5c9b1f8b681d168fd27dda34a12e6ab7d558e0
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2008-10-07 17:43:21 +0400

    remove unused code and duplicates

--- core/kernel/source/jetbrains/mps/util/Pair.java
+++ core/kernel/source/jetbrains/mps/util/Pair.java
@@ -6,6 +6,8 @@
  */
 package jetbrains.mps.util;
 
+@Deprecated
+//use com.intellij.openapi.util.Pair
 @SuppressWarnings({"InstanceVariableNamingConvention"})
 public class Pair<T1, T2> {
   @SuppressWarnings({"ClassEscapesDefinedScope"})
