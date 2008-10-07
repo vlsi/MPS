ee3676d9aa4899381c92fdca7e1ef44fc247c07d
Konstantin Solomatov <kostik@jetbrains.com>
2008-10-07 11:34:07 +0400

    concept fq name in inspector header

--- core/languageDesign/editorLanguage/source/jetbrains/mps/nodeEditor/InspectorTool.java
+++ core/languageDesign/editorLanguage/source/jetbrains/mps/nodeEditor/InspectorTool.java
@@ -4,26 +4,29 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindowAnchor;
 import com.intellij.openapi.fileEditor.FileEditor;
 import com.intellij.openapi.actionSystem.DataProvider;
+import com.intellij.ui.LightColors;
 import jetbrains.mps.ide.projectPane.Icons;
 import jetbrains.mps.nodeEditor.inspector.InspectorEditorComponent;
 import jetbrains.mps.smodel.IOperationContext;
 import jetbrains.mps.smodel.SNode;
+import jetbrains.mps.smodel.ModelAccess;
 import jetbrains.mps.workbench.tools.BaseProjectTool;
 import jetbrains.mps.workbench.editors.MPSFileNodeEditor;
 import jetbrains.mps.workbench.MPSDataKeys;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.NonNls;
 
-import javax.swing.JComponent;
-import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
+import javax.swing.*;
 import java.awt.BorderLayout;
 
 public class InspectorTool extends BaseProjectTool {
   public static final String ID = "Inspector";
 
+  private static final String NO_CONCEPT_MESSAGE = "<no message>";
+
   private JPanel myComponent;
   private InspectorEditorComponent myInspectorComponent;
+  private MyMessagePanel myMessagePanel = new MyMessagePanel();
   private FileEditor myFileEditor;
 
   public InspectorTool(Project project) {
@@ -37,6 +40,8 @@ public class InspectorTool extends BaseProjectTool {
         myComponent = new MyPanel();
         myInspectorComponent = new InspectorEditorComponent();
         myComponent.add(myInspectorComponent.getExternalComponent(), BorderLayout.CENTER);
+        myMessagePanel.setText(NO_CONCEPT_MESSAGE);
+        myComponent.add(myMessagePanel, BorderLayout.NORTH);
       }
     });
   }
@@ -57,9 +62,19 @@ public class InspectorTool extends BaseProjectTool {
     return myComponent;
   }
 
-  public void inspect(SNode node, IOperationContext context, FileEditor fileEditor, @Nullable Runnable afterInspect) {
+  public void inspect(final SNode node, IOperationContext context, FileEditor fileEditor, @Nullable Runnable afterInspect) {
     myFileEditor = fileEditor;
     myInspectorComponent.inspectNode(node, context, afterInspect);
+
+    ModelAccess.instance().runReadInEDT(new Runnable() {
+      public void run() {
+        if (node == null) {
+          myMessagePanel.setText(NO_CONCEPT_MESSAGE);
+        } else {
+          myMessagePanel.setText(node.getConceptFqName());
+        }
+      }
+    });
   }
 
   private class MyPanel extends JPanel implements DataProvider {
@@ -73,6 +88,23 @@ public class InspectorTool extends BaseProjectTool {
         return myFileEditor;
       }
       return null;
+    }
+  }
+
+  private class MyMessagePanel extends JPanel {
+    private JLabel myLabel = new JLabel();
+
+    private MyMessagePanel() {
+      setLayout(new BorderLayout());
+
+      setBackground(LightColors.YELLOW);
+      setBorder(BorderFactory.createEmptyBorder(0, 4, 0, 4));
+
+      add(myLabel, BorderLayout.CENTER);
+    }
+
+    public void setText(String newText) {
+      myLabel.setText(newText);
     }
   }
 }
