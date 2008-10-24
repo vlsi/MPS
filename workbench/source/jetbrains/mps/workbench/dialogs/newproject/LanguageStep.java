12cc601cbaf99c3020e99272eaffeee1a06a5809
Konstantin Solomatov <kostik@jetbrains.com>
2008-10-24 12:58:25 +0400

    better definition of a language

--- workbench/source/jetbrains/mps/workbench/dialogs/newproject/LanguageStep.java
+++ workbench/source/jetbrains/mps/workbench/dialogs/newproject/LanguageStep.java
@@ -74,11 +74,10 @@ public class LanguageStep extends BaseStep {
 
   public String getCommentString() {
     return
-      "Language is a module that is more complex than a solution. <br>" +
-        "It consists of aspect models: structure, editor, actions, typesystem, etc." +
-        "Languages can extend other languages, which means they can use concepts from " +
-        "an extended language. They can derive concepts from an extended language, use" +
-        "them as a target of a reference, and store them as children.";
+      "Language is a set of concepts and their aspects: structure, editor, actions, typesystem, etc." +
+      "Languages can extend other languages, which means they can use concepts from " +
+      "an extended language. They can derive concepts from an extended language, use" +
+      "them as a target of a reference, and store them as children.";
   }
 
   @Nullable
