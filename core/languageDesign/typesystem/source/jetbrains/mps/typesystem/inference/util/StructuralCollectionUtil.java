8b7df5c6274236c96d52a84d03c18abee65b74db
Konstantin Solomatov <kostik@jetbrains.com>
2008-11-06 15:20:08 +0300

    fix but in hashCode

--- core/languageDesign/typesystem/source/jetbrains/mps/typesystem/inference/util/StructuralCollectionUtil.java
+++ core/languageDesign/typesystem/source/jetbrains/mps/typesystem/inference/util/StructuralCollectionUtil.java
@@ -10,24 +10,32 @@ public class StructuralCollectionUtil {
 
   public static int hashCode(SNode node) {
     StringBuilder sb = new StringBuilder();    
-    toString(sb, node);
+    toString(sb, node, node);
     return sb.toString().hashCode();
   }
 
-  private static void toString(StringBuilder result, SNode node) {
+  private static void toString(StringBuilder result, SNode root, SNode node) {
     result.append(node.getConceptFqName());
     result.append("(");
 
     Set<String> roles = new TreeSet<String>(node.getChildRoles(true));
     for (String role : roles) {
       for (SNode child : node.getChildren(role)) {
-        toString(result, child);
+        toString(result, root, child);
         result.append(";");
       }
     }
 
     for (SReference ref : node.getReferences()) {
-      result.append(ref.getTargetNodeId());
+
+      if (!ref.isExternal()) {
+        SNode target = ref.getTargetNode();
+        if (!target.isDescendantOf(root, true)) {
+          result.append(target);
+        }
+      } else {
+        result.append(ref.getTargetNodeId());
+      }
     }
 
     result.append(")");
