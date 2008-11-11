3b9b7280b08d6d988f15ceb2f6b9afecbafbf60f
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2008-11-11 18:09:02 +0300

    PSM subsequent checks improved

--- core/kernel/source/jetbrains/mps/plugin/PluginStateMonitor.java
+++ core/kernel/source/jetbrains/mps/plugin/PluginStateMonitor.java
@@ -10,16 +10,23 @@ import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import javax.swing.Icon;
 import javax.swing.JLabel;
 import javax.swing.Timer;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class PluginStateMonitor implements ProjectComponent {
-  private Timer myTimer;
-  private JLabel myLabel = new JLabel();
+  private static final int INITIAL_DELAY = 4000;
+  private static final int CRITICAL_DELAY = 16000;
+  private static final double DELAY_MUL = 2.0;
 
   private Project myProject;
+  private Timer myTimer;
+  private JLabel myLabel;
+  private State myState = new TryingToConnectState();
 
   public PluginStateMonitor(Project project) {
     myProject = project;
@@ -40,12 +47,36 @@ public class PluginStateMonitor implements ProjectComponent {
   public void projectOpened() {
     StatusBar bar = getStatusBar();
     if (bar != null) {
+      myLabel = new JLabel();
+      myLabel.addMouseListener(new MouseAdapter() {
+        @Override
+        public void mouseClicked(MouseEvent e) {
+          if (myState instanceof DisconnectedState) {
+            setNewState(new TryingToConnectState());
+          }
+        }
+      });
       bar.addCustomIndicationComponent(myLabel);
-      myTimer = new Timer(5000, new ActionListener() {
+
+      myTimer = new Timer(INITIAL_DELAY, new ActionListener() {
         public void actionPerformed(ActionEvent e) {
-          boolean connected = MPSPlugin.getInstance().isIDEAPresent();
-          myLabel.setIcon(connected ? Icons.CONNECTED : Icons.DISCONNECTED);
-          myLabel.setToolTipText(connected ? "Connected to IDEA" : "Not connected to IDEA");
+          if (myState instanceof ConnectedState){
+            if (isConnected()) return;
+            setNewState(new TryingToConnectState());
+          }
+
+          if (!(myState instanceof TryingToConnectState)) return;
+
+          if (isConnected()) {
+            setNewState(new ConnectedState());
+          } else {
+            int newDelay = (int) (myTimer.getDelay() * DELAY_MUL);
+            if (newDelay <= CRITICAL_DELAY) {
+              myTimer.setDelay(newDelay);
+            } else {
+              setNewState(new DisconnectedState());
+            }
+          }
         }
       });
       myTimer.start();
@@ -55,9 +86,24 @@ public class PluginStateMonitor implements ProjectComponent {
   public void projectClosed() {
     StatusBar bar = getStatusBar();
     if (bar != null) {
-      myTimer.stop();
+      if (myTimer.isRunning()) {
+        myTimer.stop();
+      }
       bar.removeCustomIndicationComponent(myLabel);
     }
+  }
+
+  private void setNewState(State state) {
+    assert myState.getClass()!=state.getClass();
+
+    myState = state;
+    myTimer.setDelay(INITIAL_DELAY);
+    myLabel.setIcon(myState.getIcon());
+    myLabel.setToolTipText(myState.getHelpText());
+  }
+
+  private boolean isConnected() {
+    return MPSPlugin.getInstance().isIDEAPresent();
   }
 
   @Nullable
@@ -65,5 +111,41 @@ public class PluginStateMonitor implements ProjectComponent {
     IdeFrame ideFrame = WindowManager.getInstance().getIdeFrame(myProject);
     if (ideFrame == null) return null;
     return ideFrame.getStatusBar();
+  }
+
+  private abstract class State {
+    public abstract Icon getIcon();
+
+    public abstract String getHelpText();
+  }
+
+  private class ConnectedState extends State {
+    public Icon getIcon() {
+      return Icons.CONNECTED;
+    }
+
+    public String getHelpText() {
+      return "Connected to IDEA";
+    }
+  }
+
+  private class TryingToConnectState extends State {
+    public Icon getIcon() {
+      return Icons.TRYING_TO_CONNECT;
+    }
+
+    public String getHelpText() {
+      return "Connecting to IDEA...";
+    }
+  }
+
+  private class DisconnectedState extends State {
+    public Icon getIcon() {
+      return Icons.DISCONNECTED;
+    }
+
+    public String getHelpText() {
+      return "Not connected to IDEA. Click to reconnect.";
+    }
   }
 }
--- core/kernel/source/jetbrains/mps/plugin/icons/Icons.java
+++ core/kernel/source/jetbrains/mps/plugin/icons/Icons.java
@@ -6,4 +6,5 @@ import javax.swing.ImageIcon;
 public class Icons {
   public static final Icon CONNECTED = new ImageIcon(Icons.class.getResource("connected.png"));
   public static final Icon DISCONNECTED = new ImageIcon(Icons.class.getResource("disconnected.png"));
+  public static final Icon TRYING_TO_CONNECT = new ImageIcon(Icons.class.getResource("disconnected.png"));
 }
