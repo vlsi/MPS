6e0c65260d2b574c9c5c1d54ea1853e33dfb684e
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2008-11-12 18:05:52 +0300

    fix

--- core/kernel/source/jetbrains/mps/project/structure/testconfigurations/GeneratorConfigurationException.java
+++
@@ -1,7 +0,0 @@
-package jetbrains.mps.project.structure.testconfigurations;
-
-public class GeneratorConfigurationException extends RuntimeException {
-  public GeneratorConfigurationException(String message) {
-    super(message);
-  }
-}
---
+++ core/languageDesign/generator/source/jetbrains/mps/generator/IllegalGeneratorConfigurationException.java
@@ -0,0 +1,7 @@
+package jetbrains.mps.generator;
+
+public class IllegalGeneratorConfigurationException extends Exception {
+  public IllegalGeneratorConfigurationException(String message) {
+    super(message);
+  }
+}
