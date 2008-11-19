2d3cd0f7b356f9ac8db4cce0f271f087a9ed36b0
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2008-11-19 21:06:18 +0300

    Revert: fix MPS-3209: If there's only one language in a model, all the roots concepts should be available on the first level

--- workbench/source/jetbrains/mps/workbench/actions/model/CreateRootNodeGroup.java
+++ workbench/source/jetbrains/mps/workbench/actions/model/CreateRootNodeGroup.java
@@ -73,14 +73,12 @@ public class CreateRootNodeGroup extends BaseGroup {
 
     Collections.sort(modelLanguages, new ToStringComparator());
 
-    boolean plain = myPlain || modelLanguages.size() == 1;
-
     for (final Language language : modelLanguages) {
       String name = language.getNamespace();
       Icon icon = IconManager.getIconFor(language.getNamespace());
       BaseGroup langRootsGroup;
 
-      if (!plain) {
+      if (!myPlain) {
         langRootsGroup = new BaseGroup(name, name, icon);
         langRootsGroup.setPopup(true);
       } else {
@@ -95,14 +93,13 @@ public class CreateRootNodeGroup extends BaseGroup {
         }
       }
       if (hasChildren) {
-        if (!plain) {
+        if (!myPlain) {
           this.add(langRootsGroup);
         } else {
           this.addSeparator();
         }
       }
     }
-
 
     Integer selectedItemsCount = MPSDataKeys.SELECTED_ITEMS_NUM.getData(event.getDataContext());
     boolean enabled = selectedItemsCount != null && selectedItemsCount == 1;
