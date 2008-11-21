81dfdbada069a91c55bd430ecb65b32c64ca6560
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2008-11-21 15:17:15 +0300

    regen packaging language

--- platform/buildlanguage/languages/packaging/source_gen/jetbrains/mps/build/packaging/plugin/Packaging_ProjectPlugin.java
+++ platform/buildlanguage/languages/packaging/source_gen/jetbrains/mps/build/packaging/plugin/Packaging_ProjectPlugin.java
@@ -7,7 +7,6 @@ import java.util.List;
 import jetbrains.mps.plugins.pluginparts.tool.GeneratedTool;
 import com.intellij.openapi.project.Project;
 import java.util.ArrayList;
-import jetbrains.mps.plugins.pluginparts.prefs.BaseProjectPrefsComponent;
 
 public class Packaging_ProjectPlugin extends BaseProjectPlugin {
 
@@ -15,12 +14,6 @@ public class Packaging_ProjectPlugin extends BaseProjectPlugin {
     List<GeneratedTool> tools = new ArrayList<GeneratedTool>();
     tools.add(new BuildTool_Tool(project));
     return tools;
-  }
-
-  public List<BaseProjectPrefsComponent> createPreferencesComponents(Project project) {
-    List<BaseProjectPrefsComponent> components = new ArrayList<BaseProjectPrefsComponent>();
-    components.add(new PackagingScript_PreferencesComponent(project));
-    return components;
   }
 
 }
