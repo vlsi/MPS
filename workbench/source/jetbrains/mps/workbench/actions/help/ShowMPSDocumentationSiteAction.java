2bfe26e55848bd0d2c497efd235b4558f1d5c231
Konstantin Solomatov <kostik@jetbrains.com>
2008-11-24 22:43:16 +0300

    fix show documentation action's icon

--- workbench/source/jetbrains/mps/workbench/actions/help/ShowMPSDocumentationSiteAction.java
+++ workbench/source/jetbrains/mps/workbench/actions/help/ShowMPSDocumentationSiteAction.java
@@ -1,12 +1,25 @@
 package jetbrains.mps.workbench.actions.help;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.ActionPlaces;
+import jetbrains.mps.workbench.ActionPlace;
 
 public class ShowMPSDocumentationSiteAction extends ShowSiteAction {
   public ShowMPSDocumentationSiteAction() {
     super("MPS Documentation");
 
     getTemplatePresentation().setIcon(Icons.READ_HELP_ICON);
+  }
+
+  @Override
+  protected void doUpdate(AnActionEvent e) {
+    super.doUpdate(e);
+
+    if (ActionPlaces.WELCOME_SCREEN.equals(e.getPlace())) {
+      e.getPresentation().setIcon(Icons.READ_HELP_ICON);
+    } else {
+      e.getPresentation().setIcon(null);
+    }
   }
 
   protected String getSiteURL() {
