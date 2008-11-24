49c2246b6d5b28e617a7071f2f622107048b9f76
Konstantin Solomatov <kostik@jetbrains.com>
2008-11-24 15:32:34 +0300

    reformat

--- core/languageDesign/plugin/source/jetbrains/mps/plugins/PluginSorter.java
+++ core/languageDesign/plugin/source/jetbrains/mps/plugins/PluginSorter.java
@@ -10,34 +10,34 @@ import java.util.*;
 public class PluginSorter {
   //todo for now, the cycle will be broken in unknown place
   public static <T> ArrayList<T> sortByDependencies(Map<IModule, T> plugins) {
-    List <Item> items = new ArrayList<Item>();
-    Map<IModule,Item> module2Item = new HashMap<IModule, Item>();
-    for (IModule module:plugins.keySet()){
+    List<Item> items = new ArrayList<Item>();
+    Map<IModule, Item> module2Item = new HashMap<IModule, Item>();
+    for (IModule module : plugins.keySet()) {
       Item item = new Item(module, -1);
       items.add(item);
-      module2Item.put(module,item);
+      module2Item.put(module, item);
     }
     int initIndex = 0;
-    for (Item item:items){
-      if (item.initIndex==-1){
-        initIndex = init(item,module2Item,initIndex);
+    for (Item item : items) {
+      if (item.initIndex == -1) {
+        initIndex = init(item, module2Item, initIndex);
       }
     }
 
     T[] result = (T[]) new Object[initIndex];
-    for (Item item:items){
-      result[item.initIndex]=plugins.get(item.module);
+    for (Item item : items) {
+      result[item.initIndex] = plugins.get(item.module);
     }
     return new ArrayList<T>(Arrays.asList(result));
   }
 
   private static int init(Item item, Map<IModule, Item> module2Item, int initIndex) {
-    item.initIndex=-2;
-    for (IModule dependency:item.module.getAllDependOnModules()){
+    item.initIndex = -2;
+    for (IModule dependency : item.module.getAllDependOnModules()) {
       Item depItem = module2Item.get(dependency);
-      if (depItem !=null) {
-        if (depItem.initIndex==-1){
-          initIndex = init(depItem,module2Item,initIndex);
+      if (depItem != null) {
+        if (depItem.initIndex == -1) {
+          initIndex = init(depItem, module2Item, initIndex);
         }
       }
     }
@@ -46,7 +46,7 @@ public class PluginSorter {
     return initIndex;
   }
 
-  private static class Item{
+  private static class Item {
     private Item(IModule module, int initIndex) {
       this.module = module;
       this.initIndex = initIndex;
