4a7ceaf1eef0e6d3dc434d088a86cc9b7c2b65ac
Igor Alshannikov <Igor.Alshannikov@jetbrains.com>
2008-12-03 04:40:41 +0300

    regen

--- core/baseLanguage/baseLanguage/source_gen/jetbrains/mps/baseLanguage/typesystem/check_parametersCount_NonTypesystemRule.java
+++ core/baseLanguage/baseLanguage/source_gen/jetbrains/mps/baseLanguage/typesystem/check_parametersCount_NonTypesystemRule.java
@@ -34,7 +34,7 @@ public class check_parametersCount_NonTypesystemRule extends AbstractNonTypesyst
     if (!(b)) {
       {
         BaseIntentionProvider intentionProvider = null;
-        intentionProvider = new BaseIntentionProvider("r:0703a47d-3a82-4572-8ecf-4469b872117d(jetbrains.mps.baseLanguage.typesystem@3_0).ChooseAppropriateMethodDeclaration_QuickFix", true);
+        intentionProvider = new BaseIntentionProvider("r:29128313-4779-49d3-863f-e3dd57e36475(jetbrains.mps.baseLanguage.typesystem@16_0).ChooseAppropriateMethodDeclaration_QuickFix", true);
         intentionProvider.putArgument("methodCall", iMethodCall);
         intentionProvider.putArgument("classifier", SNodeOperations.getAncestor(baseMethodDeclaration, "jetbrains.mps.baseLanguage.structure.Classifier", false, false));
         IErrorTarget errorTarget = new NodeErrorTarget();
--- core/baseLanguage/baseLanguage/source_gen/jetbrains/mps/baseLanguage/typesystem/subtypesOfClassifierTypeWWildcards_InequationReplacementRule.java
+++ core/baseLanguage/baseLanguage/source_gen/jetbrains/mps/baseLanguage/typesystem/subtypesOfClassifierTypeWWildcards_InequationReplacementRule.java
@@ -80,7 +80,7 @@ public class subtypesOfClassifierTypeWWildcards_InequationReplacementRule extend
                 SNode classifier = SNodeOperations.getAncestor(SLinkOperations.getTarget(methodCall, "baseMethodDeclaration", false), "jetbrains.mps.baseLanguage.structure.Classifier", false, false);
                 {
                   BaseIntentionProvider intentionProvider = null;
-                  intentionProvider = new BaseIntentionProvider("r:0703a47d-3a82-4572-8ecf-4469b872117d(jetbrains.mps.baseLanguage.typesystem@3_0).ChooseAppropriateMethodDeclaration_QuickFix", true);
+                  intentionProvider = new BaseIntentionProvider("r:29128313-4779-49d3-863f-e3dd57e36475(jetbrains.mps.baseLanguage.typesystem@16_0).ChooseAppropriateMethodDeclaration_QuickFix", true);
                   intentionProvider.putArgument("methodCall", methodCall);
                   intentionProvider.putArgument("classifier", classifier);
                   IErrorTarget errorTarget = new NodeErrorTarget();
