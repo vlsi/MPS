ee6db96b0e8c4352445babb30e44d09c78128b18
Darja Chembrovskaya <Darja.Chembrovskaya@jetbrains.com>
2008-12-10 16:57:41 +0300

    move search panel

--- core/languageDesign/editor/source/jetbrains/mps/nodeEditor/SearchPanel.java
+++ core/languageDesign/editor/source/jetbrains/mps/nodeEditor/SearchPanel.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package jetbrains.mps.nodeEditor.search;
+package jetbrains.mps.nodeEditor;
 
 import com.intellij.openapi.actionSystem.*;
 import jetbrains.mps.nodeEditor.DefaultEditorMessage;
