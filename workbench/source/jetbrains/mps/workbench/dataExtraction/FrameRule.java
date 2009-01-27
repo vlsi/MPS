61c1a077fdc9ccb7bfd75d129367c2d806efba25
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2009-01-27 17:51:55 +0300

    fix MPS-3679: Frame parameter in action may get wrong value, when two frames opened.

--- workbench/source/jetbrains/mps/workbench/dataExtraction/FrameRule.java
+++ workbench/source/jetbrains/mps/workbench/dataExtraction/FrameRule.java
@@ -16,6 +16,7 @@
 package jetbrains.mps.workbench.dataExtraction;
 
 import com.intellij.ide.impl.dataRules.GetDataRule;
+import com.intellij.ide.DataManager;
 import com.intellij.openapi.actionSystem.DataProvider;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.WindowManager;
@@ -26,7 +27,7 @@ import org.jetbrains.annotations.Nullable;
 public class FrameRule implements GetDataRule {
   @Nullable
   public Object getData(DataProvider dataProvider) {
-    Project project = (Project) dataProvider.getData(MPSDataKeys.PROJECT.getName());
+    Project project = (Project) MPSDataKeys.PROJECT.getData(DataManager.getInstance().getDataContext());
     if (project == null){
       IdeFrame[] frames = WindowManager.getInstance().getAllFrames();
       return frames.length==0?null:frames[0];
