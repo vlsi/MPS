dc38599c1db7fd117cb227226bef9a0b3c3cba56
Darja Chembrovskaya <Darja.Chembrovskaya@jetbrains.com>
2009-02-04 14:17:38 +0300

    simplify

--- workbench/source/jetbrains/mps/workbench/actions/goTo/index/EnumeratorSNodeDescriptor.java
+++ workbench/source/jetbrains/mps/workbench/actions/goTo/index/EnumeratorSNodeDescriptor.java
@@ -25,25 +25,25 @@ class EnumeratorSNodeDescriptor implements KeyDescriptor<SNodeDescriptor> {
     private final byte mySizeOfUUID = 16;
 
     private void putLong(byte[] b, int off, long val) {
-	b[off + 7] = (byte) (val >>> 0);
-	b[off + 6] = (byte) (val >>> 8);
-	b[off + 5] = (byte) (val >>> 16);
-	b[off + 4] = (byte) (val >>> 24);
-	b[off + 3] = (byte) (val >>> 32);
-	b[off + 2] = (byte) (val >>> 40);
-	b[off + 1] = (byte) (val >>> 48);
-	b[off + 0] = (byte) (val >>> 56);
+      b[off + 7] = (byte) (val);
+      b[off + 6] = (byte) (val >>> 8);
+      b[off + 5] = (byte) (val >>> 16);
+      b[off + 4] = (byte) (val >>> 24);
+      b[off + 3] = (byte) (val >>> 32);
+      b[off + 2] = (byte) (val >>> 40);
+      b[off + 1] = (byte) (val >>> 48);
+      b[off + 0] = (byte) (val >>> 56);
     }
 
     private long getLong(byte[] b, int off) {
-	return ((b[off + 7] & 0xFFL) << 0) +
-	       ((b[off + 6] & 0xFFL) << 8) +
-	       ((b[off + 5] & 0xFFL) << 16) +
-	       ((b[off + 4] & 0xFFL) << 24) +
-	       ((b[off + 3] & 0xFFL) << 32) +
-	       ((b[off + 2] & 0xFFL) << 40) +
-	       ((b[off + 1] & 0xFFL) << 48) +
-	       (((long) b[off + 0]) << 56);
+      return ((b[off + 7] & 0xFFL)) +
+        ((b[off + 6] & 0xFFL) << 8) +
+        ((b[off + 5] & 0xFFL) << 16) +
+        ((b[off + 4] & 0xFFL) << 24) +
+        ((b[off + 3] & 0xFFL) << 32) +
+        ((b[off + 2] & 0xFFL) << 40) +
+        ((b[off + 1] & 0xFFL) << 48) +
+        (((long) b[off + 0]) << 56);
     }
 
     private void putString(byte[] b, int off, String str) {
