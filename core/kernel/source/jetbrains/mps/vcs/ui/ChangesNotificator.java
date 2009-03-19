f4b5b4d650deaa768a339f689cd9d01e45e7c955
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2009-03-19 16:49:03 +0300

    MPS-4116 Add to vcs root dialog shouldn't be modal it should be shown as popup (like error popup) instead

--- core/kernel/source/jetbrains/mps/vcs/ui/ChangesNotificator.java
+++ core/kernel/source/jetbrains/mps/vcs/ui/ChangesNotificator.java
@@ -22,16 +22,27 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.wm.StatusBar;
+import com.intellij.openapi.wm.WindowManager;
+import com.intellij.ui.LightColors;
+import com.intellij.util.ui.UIUtil;
 import jetbrains.mps.smodel.SModelDescriptor;
 import jetbrains.mps.vcs.VcsRootsManager;
 import jetbrains.mps.vcs.VcsRootsManager.VcsRootsListener;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
+import javax.swing.JEditorPane;
+import javax.swing.text.html.HTMLEditorKit;
+import javax.swing.event.HyperlinkListener;
+import javax.swing.event.HyperlinkEvent;
+import java.awt.Color;
+
 public class ChangesNotificator implements ProjectComponent {
   private final VcsRootsManager myVcsRootsManager;
   private final Project myProject;
   private final ChangesNotificator.MyVcsRootsListener myListener = new MyVcsRootsListener();
+  private static final Color POPUP_COLOR = LightColors.RED;
 
   public ChangesNotificator(Project project, VcsRootsManager manager) {
     myProject = project;
@@ -60,12 +71,47 @@ public class ChangesNotificator implements ProjectComponent {
     myVcsRootsManager.removeListener(myListener);
   }
 
-  private boolean showAddVcsRootsDialog(Project project, VirtualFile vcsRoot, SModelDescriptor sm) {
+  private void showAddVcsRootsPopup(final Project project, final VirtualFile vcsRoot, final SModelDescriptor sm) {
+
+    StatusBar bar = WindowManager.getInstance().getStatusBar(project);
+
+    JEditorPane pane = createPopupComponent(sm);
+    pane.addHyperlinkListener(new HyperlinkListener() {
+      public void hyperlinkUpdate(final HyperlinkEvent e) {
+        if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+
+          boolean result = showAddVcsRootsDialog(sm, vcsRoot, project);
+
+          if (result) {
+            myVcsRootsManager.addNewVcsRoot(vcsRoot);
+          } else {
+            myVcsRootsManager.addExcludedRoot(vcsRoot);
+          }
+        }
+      }
+    });
+
+    bar.fireNotificationPopup(pane, POPUP_COLOR);
+  }
+
+  private boolean showAddVcsRootsDialog(SModelDescriptor sm, VirtualFile vcsRoot, Project project) {
     String message = "You have changed model " + sm.getSModelFqName() + ".\n" +
       "Do you want to add folder " + vcsRoot.getPath() + " to the list of vcs roots so you would be able to commit your changes?\n" +
       "You can always do it later choosing Settings -> Project Settings -> Version Control.";
     String title = "Add Folder " + vcsRoot.getPath() + " To The List Of Vcs Roots?";
+
     return Messages.showYesNoDialog(project, message, title, Messages.getQuestionIcon()) == DialogWrapper.OK_EXIT_CODE;
+  }
+
+  private JEditorPane createPopupComponent(SModelDescriptor sm) {
+    String popupMessage = "<html><body><font size=\"3\">Model <b>" + sm.getSModelFqName() + "</b> is not under version control. <a href=\"more\">Read More...</a></font></body></html>";
+    JEditorPane pane = new JEditorPane();
+    pane.setBackground(POPUP_COLOR);
+    pane.setEditorKit(new HTMLEditorKit());
+    pane.setContentType(UIUtil.HTML_MIME);
+    pane.setEditable(false);
+    pane.setText(popupMessage);
+    return pane;
   }
 
   private class MyVcsRootsListener implements VcsRootsListener {
@@ -75,12 +121,7 @@ public class ChangesNotificator implements ProjectComponent {
           if (myVcsRootsManager.isExcluded(vcsRoot)) {
             return;
           }
-          boolean result = showAddVcsRootsDialog(myProject, vcsRoot, modelDescriptor);
-          if (result) {
-            myVcsRootsManager.addNewVcsRoot(vcsRoot);
-          } else {
-            myVcsRootsManager.addExcludedRoot(vcsRoot);
-          }
+          showAddVcsRootsPopup(myProject, vcsRoot, modelDescriptor);
         }
       }, ModalityState.NON_MODAL);
     }
