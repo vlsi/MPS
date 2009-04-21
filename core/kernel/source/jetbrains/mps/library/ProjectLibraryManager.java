eba8620e67be8bc72e7bcd9199391b5560fb01cf
Konstantin Solomatov <kostik@jetbrains.com>
2009-04-21 21:39:07 +0400

    work on project library manager

---
+++ core/kernel/source/jetbrains/mps/library/ProjectLibraryManager.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2003-2009 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jetbrains.mps.library;
+
+import com.intellij.openapi.components.ProjectComponent;
+import com.intellij.openapi.components.State;
+import com.intellij.openapi.components.Storage;
+import jetbrains.mps.smodel.MPSModuleRepository;
+
+@State(
+  name = "ProjectLibraryManager",
+  storages = {
+    @Storage(
+      id = "other",
+      file = "$PROJECT_FILE$"
+    )
+  }
+)
+public class ProjectLibraryManager extends BaseLibraryManager implements ProjectComponent {
+  public ProjectLibraryManager(MPSModuleRepository repo) {
+    super(repo);
+  }
+
+  public void projectOpened() {
+
+  }
+
+  public void projectClosed() {
+
+  }
+}
