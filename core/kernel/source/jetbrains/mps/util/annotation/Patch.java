d5c4f4a1badf504f841bf09c201b6d4753714809
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2009-05-08 18:43:34 +0400

    fix MPS-4638 Excpetion on editing project settings

---
+++ core/kernel/source/jetbrains/mps/util/annotation/Patch.java
@@ -0,0 +1,11 @@
+package jetbrains.mps.util.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.METHOD)
+public @interface Patch {
+}
