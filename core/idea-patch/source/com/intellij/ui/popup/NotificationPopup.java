8caf28fa86f01fd215402d1d2c46176cd72c787d
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2009-05-21 16:13:41 +0400

    fix MPS-4663 If you generate closures language from webr_dnq's project, a lot of vcs warnings appear as pop-ups

---
+++ core/idea-patch/source/com/intellij/ui/popup/NotificationPopup.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2000-2006 JetBrains s.r.o. All Rights Reserved.
+ */
+
+package com.intellij.ui.popup;
+
+import com.intellij.openapi.ui.popup.Balloon;
+import com.intellij.openapi.ui.popup.JBPopup;
+import com.intellij.openapi.ui.popup.JBPopupFactory;
+import com.intellij.openapi.ui.popup.JBPopupListener;
+import com.intellij.openapi.wm.impl.IdeFrameImpl;
+import com.intellij.ui.components.panels.Wrapper;
+import com.intellij.ui.components.panels.NonOpaquePanel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+/**
+ * @author max
+ */
+public class NotificationPopup {
+
+  private Impl myImpl;
+
+  public NotificationPopup(final JComponent owner, final JComponent content, Color backgroud) {
+    this(owner, content, backgroud, true);
+  }
+
+  public NotificationPopup(final JComponent owner, final JComponent content, Color backgroud, boolean useDefaultPreferredSize) {
+    this(owner, content, backgroud, useDefaultPreferredSize, null, false);
+  }
+
+  public NotificationPopup(final JComponent owner, final JComponent content, Color backgroud, final boolean useDefaultPreferredSize, ActionListener clickHandler, boolean closeOnClick) {
+    final IdeFrameImpl frame = findFrame(owner);
+    if (frame == null) {
+      //todo kirillk
+      if (clickHandler != null) {
+        throw new UnsupportedOperationException("Click handler is not supported in frameless mode");
+      }
+      final FramelessNotificationPopup popup = new FramelessNotificationPopup(owner, content, backgroud, useDefaultPreferredSize);
+
+      myImpl = new Impl() {
+        public void addListener(JBPopupListener listener) {
+          popup.getPopup().addListener(listener);
+        }
+
+        public void hide() {
+          popup.getPopup().cancel();
+        }
+      };
+    } else {
+      final Wrapper wrapper = new NonOpaquePanel(content) {
+        @Override
+        public Dimension getPreferredSize() {
+          final Dimension size = super.getPreferredSize();
+          if (useDefaultPreferredSize) {
+            if (size.width > 400 || size.height > 200) {
+              size.width = 400;
+              size.height = 200;
+            }
+          }
+          return size;
+        }
+      };
+
+      final Balloon balloon = JBPopupFactory.getInstance().createBalloonBuilder(wrapper)
+        .setFadeoutTime(5000)
+        .setHideOnClickOutside(false)
+        .setHideOnFrameResize(false)
+        .setHideOnKeyOutside(false)
+        .setCloseButtonEnabled(true)
+        .setFillColor(backgroud)
+        .setShowCallout(false)
+        .setClickHandler(clickHandler, closeOnClick)
+        .createBalloon();
+
+      frame.getBalloonLayout().add(balloon);
+
+      myImpl = new Impl() {
+        public void addListener(JBPopupListener listener) {
+          balloon.addListener(listener);
+        }
+
+        public void hide() {
+          balloon.hide();
+        }
+      };
+    }
+  }
+
+  private IdeFrameImpl findFrame(JComponent owner) {
+    final Window frame = SwingUtilities.getWindowAncestor(owner);
+    if (frame instanceof IdeFrameImpl) {
+      return (IdeFrameImpl)frame;
+    }
+
+    return null;
+  }
+
+  public JBPopup getPopup() {
+    return null;
+  }
+
+
+  interface Impl {
+    void addListener(JBPopupListener listener);
+    void hide();
+  }
+
+  public void addListener(JBPopupListener listener) {
+    myImpl.addListener(listener);
+  }
+
+  public void hide() {
+    myImpl.hide();
+  }
+}
--- core/kernel/source/jetbrains/mps/vcs/ui/ChangesNotificator.java
+++ core/kernel/source/jetbrains/mps/vcs/ui/ChangesNotificator.java
@@ -21,28 +21,40 @@ import com.intellij.openapi.components.ProjectComponent;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.ui.popup.JBPopupAdapter;
+import com.intellij.openapi.ui.popup.LightweightWindowEvent;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.wm.StatusBar;
 import com.intellij.openapi.wm.WindowManager;
 import com.intellij.ui.LightColors;
+import com.intellij.ui.popup.NotificationPopup;
 import com.intellij.util.ui.UIUtil;
 import jetbrains.mps.smodel.SModelDescriptor;
 import jetbrains.mps.vcs.VcsRootsManager;
 import jetbrains.mps.vcs.VcsRootsManager.VcsRootsListener;
+import jetbrains.mps.util.misc.hash.HashMap;
+import jetbrains.mps.util.misc.hash.HashSet;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.JEditorPane;
+import javax.swing.JComponent;
 import javax.swing.text.html.HTMLEditorKit;
 import javax.swing.event.HyperlinkListener;
 import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ChangeEvent;
 import java.awt.Color;
+import java.awt.Component;
+import java.util.Map;
+import java.util.Set;
 
 public class ChangesNotificator implements ProjectComponent {
   private final VcsRootsManager myVcsRootsManager;
   private final Project myProject;
   private final ChangesNotificator.MyVcsRootsListener myListener = new MyVcsRootsListener();
   private static final Color POPUP_COLOR = LightColors.RED;
+  private final Map<VirtualFile, MyNotificationInfo> myRoots = new HashMap<VirtualFile, MyNotificationInfo>();
 
   public ChangesNotificator(Project project, VcsRootsManager manager) {
     myProject = project;
@@ -73,45 +85,119 @@ public class ChangesNotificator implements ProjectComponent {
 
   private void showAddVcsRootsPopup(final Project project, final VirtualFile vcsRoot, final SModelDescriptor sm) {
 
-    StatusBar bar = WindowManager.getInstance().getStatusBar(project);
+    final StatusBar bar = WindowManager.getInstance().getStatusBar(project);
 
-    JEditorPane pane = createPopupComponent(sm);
-    pane.addHyperlinkListener(new HyperlinkListener() {
-      public void hyperlinkUpdate(final HyperlinkEvent e) {
-        if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+    if (myRoots.get(vcsRoot) == null) {
 
-          boolean result = showAddVcsRootsDialog(sm, vcsRoot, project);
+      final MyNotificationInfo info = new MyNotificationInfo();
+      info.addSModelDescriptor(sm);
+      myRoots.put(vcsRoot, info);
 
-          if (result) {
-            myVcsRootsManager.addNewVcsRoot(vcsRoot);
-          } else {
-            myVcsRootsManager.addExcludedRoot(vcsRoot);
+      final JEditorPane pane = createPopupComponent();
+      pane.addHyperlinkListener(new HyperlinkListener() {
+        public void hyperlinkUpdate(final HyperlinkEvent e) {
+          if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+
+            boolean result = showAddVcsRootsDialog(info, vcsRoot, project);
+
+            if (result) {
+              myVcsRootsManager.addNewVcsRoot(vcsRoot);
+            } else {
+              myVcsRootsManager.addExcludedRoot(vcsRoot);
+            }
           }
         }
-      }
-    });
+      });
+      pane.setText(info.getPopupMessage());
 
-    bar.fireNotificationPopup(pane, POPUP_COLOR);
+      info.setListener(new ChangeListener() {
+        public void stateChanged(ChangeEvent e) {
+          pane.setText(info.getPopupMessage());
+        }
+      });
+
+      final NotificationPopup popup = new NotificationPopup((JComponent) bar, pane, POPUP_COLOR);
+      popup.addListener(new JBPopupAdapter() {
+        @Override
+        public void onClosed(LightweightWindowEvent event) {
+          myRoots.remove(vcsRoot);
+        }
+      });
+
+    } else {
+      MyNotificationInfo info = myRoots.get(vcsRoot);
+      info.addSModelDescriptor(sm);
+    }
   }
 
-  private boolean showAddVcsRootsDialog(SModelDescriptor sm, VirtualFile vcsRoot, Project project) {
-    String message = "You have changed model " + sm.getSModelFqName() + ".\n" +
-      "Do you want to add folder " + vcsRoot.getPath() + " to the list of vcs roots so you would be able to commit your changes?\n" +
-      "You can always do it later choosing Settings -> Project Settings -> Version Control.";
+  private boolean showAddVcsRootsDialog(MyNotificationInfo info, VirtualFile vcsRoot, Project project) {
+    String message = info.getDialogMessage(vcsRoot);
     String title = "Add Folder " + vcsRoot.getPath() + " To The List Of Vcs Roots?";
 
     return Messages.showYesNoDialog(project, message, title, Messages.getQuestionIcon()) == DialogWrapper.OK_EXIT_CODE;
   }
 
-  private JEditorPane createPopupComponent(SModelDescriptor sm) {
-    String popupMessage = "<html><body><font size=\"3\">Model <b>" + sm.getSModelFqName() + "</b> is not under version control. <a href=\"more\">Read More...</a></font></body></html>";
+  private JEditorPane createPopupComponent() {
     JEditorPane pane = new JEditorPane();
     pane.setBackground(POPUP_COLOR);
     pane.setEditorKit(new HTMLEditorKit());
     pane.setContentType(UIUtil.HTML_MIME);
     pane.setEditable(false);
-    pane.setText(popupMessage);
     return pane;
+  }
+
+  private static class MyNotificationInfo {
+    private final Set<SModelDescriptor> myDescriptors = new HashSet<SModelDescriptor>();
+    private ChangeListener myListener;
+
+    public void setListener(ChangeListener listener) {
+      myListener = listener;
+    }
+
+    public void addSModelDescriptor(SModelDescriptor sm) {
+      myDescriptors.add(sm);
+      if (myListener != null) myListener.stateChanged(new ChangeEvent(sm));
+    }
+
+    public String getPopupMessage() {
+      int size = myDescriptors.size();
+      assert size > 0;
+      if (size == 1) {
+        return getExactMessage("Model", myDescriptors.iterator().next().getSModelFqName().toString(), "is");
+      } else {
+        return getExactMessage("Models", getModelsList(true), "are");
+      }
+    }
+
+    private String getExactMessage(String modelTitle, String modelsList, String verb) {
+      return "<html><body><font size=\"3\">" + modelTitle + " <b>" + modelsList + "</b> " + verb + " not under version control." +
+        " <a href=\"more\">Read More...</a></font></body></html>";
+    }
+
+    public String getDialogMessage(VirtualFile vcsRoot) {
+      int size = myDescriptors.size();
+      assert size > 0;
+      if (size == 1) {
+        return getExactDialogMessage("Model", myDescriptors.iterator().next().getSModelFqName().toString(), vcsRoot);
+      } else {
+        return getExactDialogMessage("Models", getModelsList(false), vcsRoot);
+      }
+    }
+
+    private String getModelsList(boolean html) {
+      StringBuffer sb = new StringBuffer();
+      for (SModelDescriptor sm : myDescriptors) {
+        sb.append(sm.getSModelFqName().toString());
+        sb.append(html ? "<br>" : "\n");
+      }
+      return sb.toString();
+    }
+
+    private String getExactDialogMessage(String modelTitle, String modelsList, VirtualFile vcsRoot) {
+      return "You have changed " + modelTitle + " " + modelsList + ".\n" +
+        "Do you want to add folder " + vcsRoot.getPath() + " to the list of vcs roots so you would be able to commit your changes?\n" +
+        "You can always do it later choosing Settings -> Project Settings -> Version Control.";
+    }
   }
 
   private class MyVcsRootsListener implements VcsRootsListener {
