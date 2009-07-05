cb5f87447ef337a8ee83a55f4b9c3b6e4ed42571
Konstantin Solomatov <kostik@jetbrains.com>
2009-07-05 15:54:21 +0400

    test reload all action

--- workbench/source_gen/jetbrains/mps/ide/actions/ReloadAll_Action.java
+++ workbench/source_gen/jetbrains/mps/ide/actions/ReloadAll_Action.java
@@ -60,18 +60,22 @@ public class ReloadAll_Action extends GeneratedAction {
 
   public void doExecute(@NotNull() final AnActionEvent event) {
     try {
-      ProgressManager.getInstance().run(new Task.Modal(ReloadAll_Action.this.project, "Reloading Classes", false) {
+      int iteration = 0;
+      while (true) {
+        System.out.println("iteration = " + iteration++);        
+        ProgressManager.getInstance().run(new Task.Modal(ReloadAll_Action.this.project, "Reloading Classes", false) {
 
-        public void run(@NotNull() final ProgressIndicator indicator) {
-          indicator.setIndeterminate(true);
-          ModelAccess.instance().runReadAction(new Runnable() {
+          public void run(@NotNull() final ProgressIndicator indicator) {
+            indicator.setIndeterminate(true);
+            ModelAccess.instance().runReadAction(new Runnable() {
 
-            public void run() {
-              ClassLoaderManager.getInstance().reloadAll(indicator);
-            }
-          });
-        }
-      });
+              public void run() {
+                ClassLoaderManager.getInstance().reloadAll(indicator);
+              }
+            });
+          }
+        });
+      }
     } catch (Throwable t) {
       if (log.isErrorEnabled()) {
         log.error("User's action execute method failed. Action:" + "ReloadAll", t);
