3cd5c9e81cfb7fd8faadc5398dc874a980d8bbfb
Konstantin Solomatov <kostik@jetbrains.com>
2009-07-08 01:38:31 +0400

    missing file

--- platform/buildlanguage/languages/packaging/source_gen/jetbrains/mps/build/packaging/editor/CompositePathComponent_Editor.java
+++ platform/buildlanguage/languages/packaging/source_gen/jetbrains/mps/build/packaging/editor/CompositePathComponent_Editor.java
@@ -29,7 +29,7 @@ public class CompositePathComponent_Editor extends DefaultNodeEditor {
   }
 
   public EditorCell createCollection_6133_0(EditorContext context, SNode node) {
-    EditorCell_Collection editorCell = EditorCell_Collection.createHorizontal(context, node);
+    EditorCell_Collection editorCell = EditorCell_Collection.createIndent2(context, node);
     setupBasic_Collection_6133_0(editorCell, node, context);
     editorCell.setGridLayout(false);
     editorCell.setUsesBraces(false);
