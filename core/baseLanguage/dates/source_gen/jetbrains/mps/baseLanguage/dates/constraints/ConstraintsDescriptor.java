100f2d6422154cf939fc54ba11e41cc7a18847d5
Fedor Isakov <Fedor.Isakov@jetbrains.com>
2009-07-21 17:19:48 +0400

    Offset timezone

--- core/baseLanguage/dates/source_gen/jetbrains/mps/baseLanguage/dates/constraints/ConstraintsDescriptor.java
+++ core/baseLanguage/dates/source_gen/jetbrains/mps/baseLanguage/dates/constraints/ConstraintsDescriptor.java
@@ -14,6 +14,7 @@ public class ConstraintsDescriptor implements IModelConstraints {
 
   public ConstraintsDescriptor() {
     ListSequence.fromList(this.myConstraints).addElement(new DateFormat_isPublic_PropertyConstraint());
+    ListSequence.fromList(this.myConstraints).addElement(new TimeZoneIDExpression_timezone_id_PropertyConstraint());
     ListSequence.fromList(this.myConstraints).addElement(new TokenConditionalPair_format_ReferentConstraint());
     ListSequence.fromList(this.myConstraints).addElement(new FormatExpression_dateFormat_ReferentConstraint());
     ListSequence.fromList(this.myConstraints).addElement(new DateTimePropertyFormatToken_dateTimePropertyFormatConfiguration_ReferentConstraint());
