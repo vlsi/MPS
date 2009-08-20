b6bc0e92fb3b103916dfccc9fb0f40891a07e918
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2009-08-20 16:33:48 +0400

    add nested elements to generic task and generate fields and methods for them

--- platform/buildlanguage/languages/generictasks/source_gen/jetbrains/mps/build/generictasks/constraints/ConstraintsDescriptor.java
+++ platform/buildlanguage/languages/generictasks/source_gen/jetbrains/mps/build/generictasks/constraints/ConstraintsDescriptor.java
@@ -20,7 +20,6 @@ public class ConstraintsDescriptor implements IModelConstraints {
     ListSequence.fromList(this.myConstraints).addElement(new TaskCall_DefaultSearchScope());
   }
 
-
   public void unRegisterSelf(ModelConstraintsManager p0) {
     for (IModelConstraints c : this.myConstraints) {
       c.unRegisterSelf(p0);
