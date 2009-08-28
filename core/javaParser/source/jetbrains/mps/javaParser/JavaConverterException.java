c09f71a644b1a4b3bd4af6e6bb495b1b50dd2a83
Cyril Konopko <Cyril.Konopko@jetbrains.com>
2009-08-28 21:37:58 +0400

    expression parser in progress

---
+++ core/javaParser/source/jetbrains/mps/javaParser/JavaConverterException.java
@@ -0,0 +1,17 @@
+package jetbrains.mps.javaParser;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: User
+ * Date: 28.08.2009
+ * Time: 20:31:32
+ * To change this template use File | Settings | File Templates.
+ */
+public class JavaConverterException extends RuntimeException {
+  public JavaConverterException() {
+  }
+
+  public JavaConverterException(String message) {
+    super(message);
+  }
+}
