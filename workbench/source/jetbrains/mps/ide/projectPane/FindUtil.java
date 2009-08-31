0dd8e8816efe4e35e47485e6ee14fcde5942baa0
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2009-08-31 19:59:55 +0400

    partially fix MPS-5388 When I press Alt+F1 or Alt+F2 the place where node is opened should be prioritized - accessory models

--- workbench/source/jetbrains/mps/ide/projectPane/FindUtil.java
+++ workbench/source/jetbrains/mps/ide/projectPane/FindUtil.java
@@ -2,50 +2,56 @@ package jetbrains.mps.ide.projectPane;
 
 import jetbrains.mps.project.IModule;
 import jetbrains.mps.project.MPSProject;
-import jetbrains.mps.project.Solution;
-import jetbrains.mps.project.DevKit;
-import jetbrains.mps.smodel.SModelDescriptor;
+import jetbrains.mps.smodel.Generator;
 import jetbrains.mps.smodel.Language;
+import jetbrains.mps.smodel.SModelDescriptor;
 
 public class FindUtil {
-  public static IModule getModuleForModel(MPSProject project,SModelDescriptor model) {
-    //own models
-    for (IModule owner: model.getModules()){
-      if (project.isProjectModule(owner)) return owner;
+  public static IModule getModuleForModel(MPSProject project, SModelDescriptor model) {
+    //language's and solution's own models (+generator models in language)
+    for (IModule owner : model.getModules()) {
+      IModule mainModule = owner instanceof Generator ? ((Generator) owner).getSourceLanguage() : owner;
+      if (project.isProjectModule(mainModule)) return owner;
+    }
+
+    //accessories models in languages
+    for (Language l : project.getProjectLanguages()) {
+      if (l.isAccessoryModel(model.getSModelReference())) return l;
     }
 
 /*
-    //generators in languages
-    for (Language l: project.getProjectLanguages()){
-
-    }
-
-    //generators in devkits
-
-    //accessories models in languages
-    for (Language l: project.getProjectLanguages()){
-
-      if (l.isAccessoryModel(model.getSModelReference()))
-    }
-
-    //accessories models in devkits
-
     //runtime models in languages
-
-    //runtime models in devkits
-
-    for (IModule module: project.getModules()){
-      if (module instanceof Solution){
-
-      } else if (module instanceof Language){
-
-      } else if (module instanceof DevKit){
-
-      } else {
-        LOG.error("Unknown module type: "+module.getClass().getSimpleName());
+    for (Language l : project.getProjectLanguages()) {
+      for (IModule depModule : l.getRuntimeDependOnModules()){
+        for (IModule owner : model.getModules()) {
+          IModule mainModule = owner instanceof Generator ? ((Generator) owner).getSourceLanguage() : owner;
+          if (depModule.getModuleReference().equals(mainModule.getModuleReference())) return owner;
+        }
       }
     }
 */
+
+
+/*
+
+
+    //accessories models in devkits
+
+
+    //runtime models in devkits
+
+    for (IModule module : project.getModules()) {
+      if (module instanceof Solution) {
+
+      } else if (module instanceof Language) {
+
+      } else if (module instanceof DevKit) {
+
+      } else {
+        LOG.error("Unknown module type: " + module.getClass().getSimpleName());
+      }
+    }
+    */
 
     return model.getModule();
   }
