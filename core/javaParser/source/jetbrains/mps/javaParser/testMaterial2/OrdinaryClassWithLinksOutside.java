c97bbbfd9d522d5398fc6c9d290640cf3ff22621
Cyril Konopko <Cyril.Konopko@jetbrains.com>
2009-09-17 18:38:13 +0400

    converting classes which refer one another

---
+++ core/javaParser/source/jetbrains/mps/javaParser/testMaterial2/OrdinaryClassWithLinksOutside.java
@@ -0,0 +1,13 @@
+package jetbrains.mps.javaParser.testMaterial2;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: User
+ * Date: 17.09.2009
+ * Time: 18:17:53
+ * To change this template use File | Settings | File Templates.
+ */
+public class OrdinaryClassWithLinksOutside {
+  OrdinaryClassWithLinksOutside2 myFieldO2;
+  String myS;
+}
---
+++ core/javaParser/source/jetbrains/mps/javaParser/testMaterial2/OrdinaryClassWithLinksOutside2.java
@@ -0,0 +1,12 @@
+package jetbrains.mps.javaParser.testMaterial2;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: User
+ * Date: 17.09.2009
+ * Time: 18:18:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class OrdinaryClassWithLinksOutside2 {
+  OrdinaryClassWithLinksOutside myFieldO1;
+}
