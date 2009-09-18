49b457d7b3c6550d3ec666484276e3c2fabbfc8f
Konstantin Solomatov <kostik@jetbrains.com>
2009-09-18 12:33:22 +0400

    optimize non type system rule

--- core/baseLanguage/tuples/languages/tuples/source_gen/jetbrains/mps/baseLanguage/tuples/typesystem/check_NamedTupleComponent_final_NonTypesystemRule.java
+++ core/baseLanguage/tuples/languages/tuples/source_gen/jetbrains/mps/baseLanguage/tuples/typesystem/check_NamedTupleComponent_final_NonTypesystemRule.java
@@ -6,14 +6,14 @@ import jetbrains.mps.lang.typesystem.runtime.AbstractNonTypesystemRule_Runtime;
 import jetbrains.mps.lang.typesystem.runtime.NonTypesystemRule_Runtime;
 import jetbrains.mps.smodel.SNode;
 import jetbrains.mps.typesystem.inference.TypeCheckingContext;
+import jetbrains.mps.lang.smodel.generator.smodelAdapter.SLinkOperations;
+import jetbrains.mps.lang.smodel.generator.smodelAdapter.SNodeOperations;
 import jetbrains.mps.lang.pattern.IMatchingPattern;
 import jetbrains.mps.lang.typesystem.runtime.HUtil;
 import jetbrains.mps.typesystem.inference.TypeChecker;
 import jetbrains.mps.internal.collections.runtime.ListSequence;
-import jetbrains.mps.lang.smodel.generator.smodelAdapter.SLinkOperations;
 import jetbrains.mps.internal.collections.runtime.IWhereFilter;
 import jetbrains.mps.lang.smodel.generator.smodelAdapter.SPropertyOperations;
-import jetbrains.mps.lang.smodel.generator.smodelAdapter.SNodeOperations;
 import jetbrains.mps.intentions.BaseIntentionProvider;
 import jetbrains.mps.typesystem.inference.IErrorTarget;
 import jetbrains.mps.typesystem.inference.NodeErrorTarget;
@@ -24,24 +24,27 @@ public class check_NamedTupleComponent_final_NonTypesystemRule extends AbstractN
   }
 
   public void applyRule(final SNode expression, final TypeCheckingContext typeCheckingContext) {
-    {
-      IMatchingPattern pattern_0 = HUtil.createMatchingPatternByConceptFQName("jetbrains.mps.baseLanguage.tuples.structure.NamedTupleType");
-      SNode coercedNode_0 = TypeChecker.getInstance().getRuntimeSupport().coerce_(TypeChecker.getInstance().getTypeOf(expression), pattern_0, typeCheckingContext);
-      if (coercedNode_0 != null) {
-        if (ListSequence.fromList(SLinkOperations.getTargets(SLinkOperations.getTarget(coercedNode_0, "classifier", false), "component", true)).any(new IWhereFilter<SNode>() {
-          public boolean accept(SNode cmp) {
-            return SPropertyOperations.getBoolean(cmp, "final");
+    if (expression == SLinkOperations.getTarget(SNodeOperations.as(SNodeOperations.getParent(expression), "jetbrains.mps.baseLanguage.structure.BaseAssignmentExpression"), "lValue", true)) {
+      {
+        IMatchingPattern pattern_0 = HUtil.createMatchingPatternByConceptFQName("jetbrains.mps.baseLanguage.tuples.structure.NamedTupleType");
+        SNode coercedNode_0 = TypeChecker.getInstance().getRuntimeSupport().coerce_(TypeChecker.getInstance().getTypeOf(expression), pattern_0, typeCheckingContext);
+        if (coercedNode_0 != null) {
+          if (ListSequence.fromList(SLinkOperations.getTargets(SLinkOperations.getTarget(coercedNode_0, "classifier", false), "component", true)).any(new IWhereFilter<SNode>() {
+            public boolean accept(SNode cmp) {
+              return SPropertyOperations.getBoolean(cmp, "final");
+            }
+          })) {
+            {
+              BaseIntentionProvider intentionProvider = null;
+              IErrorTarget errorTarget = new NodeErrorTarget();
+              typeCheckingContext.reportTypeError(expression, "Cannot assign to final tuple component", "r:e119dbbd-3529-4067-8bad-6b9edd79d0b6(jetbrains.mps.baseLanguage.tuples.typesystem)", "4292022102258809122", intentionProvider, errorTarget);
+            }
           }
-        })) {
-          if (!(!(expression == SLinkOperations.getTarget(SNodeOperations.as(SNodeOperations.getParent(expression), "jetbrains.mps.baseLanguage.structure.BaseAssignmentExpression"), "lValue", true)))) {
-            BaseIntentionProvider intentionProvider = null;
-            IErrorTarget errorTarget = new NodeErrorTarget();
-            typeCheckingContext.reportTypeError(expression, "Cannot assign to final tuple component", "r:e119dbbd-3529-4067-8bad-6b9edd79d0b6(jetbrains.mps.baseLanguage.tuples.typesystem)", "1240403693265", intentionProvider, errorTarget);
-          }
+        } else {
         }
-      } else {
       }
     }
+
   }
 
   public String getApplicableConceptFQName() {
