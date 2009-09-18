99ba393f96facb6ede6bf47dab4b1f798ad75b05
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2009-09-18 05:15:06 +0400

    fix broken references test output

--- platform/buildlanguage/ant/sources/jetbrains/mps/build/ant/TestBrokenReferencesOnTeamcity.java
+++ platform/buildlanguage/ant/sources/jetbrains/mps/build/ant/TestBrokenReferencesOnTeamcity.java
@@ -4,4 +4,20 @@ public class TestBrokenReferencesOnTeamcity extends MpsLoadTask{
   protected Class<? extends MpsWorker> getWorkerClass() {
     return TestBrokenReferencesWorker.class;
   }
+
+  @Override
+  protected MyExecuteStreamHandler getExecuteStreamHandler() {
+    return new MyTeamcityAwareExecuteStreamHandler();
+  }
+
+  private class MyTeamcityAwareExecuteStreamHandler extends MyExecuteStreamHandler {
+    @Override
+    protected void logOutput(String line) {
+      if (line.matches("##teamcity\\[.*\\].*")){
+        System.out.println(line);
+      } else {
+        super.logOutput(line);
+      }
+    }
+  }
 }
