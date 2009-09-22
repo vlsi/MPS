70c6050ef748bcf6c03f5226f83cf7174c10cd43
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2009-09-22 19:30:43 +0400

    disable popups, when vcs is didabled

--- core/kernel/source/jetbrains/mps/vcs/VcsRootsManager.java
+++ core/kernel/source/jetbrains/mps/vcs/VcsRootsManager.java
@@ -56,7 +56,7 @@ public class VcsRootsManager implements ProjectComponent {
         VirtualFile file = VFileSystem.getFile(modelFile.getParent());
         if (file == null) return;
         AbstractVcs vcs = myVcsManager.findVersioningVcs(file);
-        if (vcs == null) return;
+        if (vcs == null || !Arrays.asList(myVcsManager.getAllActiveVcss()).contains(vcs)) return;
         if (myVcsManager.getVcsRootFor(file) != null) return;
         VirtualFile root = file;
         while ((root.getParent() != null) && vcs.isVersionedDirectory(root.getParent())) {
