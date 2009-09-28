39280efb71cb10888b39b44111d3e1ef993da8b9
Evgeny Kurbatsky <Evgeny.Kurbatsky@jetbrains.com>
2009-09-28 20:42:08 +0400

    Delete obsolete code.

--- core/languageDesign/editor/source/jetbrains/mps/nodeEditor/style/StyleAttributes.java
+++ core/languageDesign/editor/source/jetbrains/mps/nodeEditor/style/StyleAttributes.java
@@ -113,8 +113,5 @@ public class StyleAttributes {
   public static final StyleAttribute<Boolean> INDENT_LAYOUT_CHILDREN_NEWLINE = new SimpleStyleAttribute<Boolean>("indent-layout-children-new-line", false);
   public static final StyleAttribute<Boolean> INDENT_LAYOUT_NO_WRAP = new SimpleStyleAttribute<Boolean>("indent-layout-no-wrap", false);
 
-  public static final StyleAttribute<ParametersInformation> PARAMETERS_INFORMATION = new SimpleStyleAttribute<ParametersInformation>("parameters-information");
-
-  @Deprecated
-  public static final StyleAttribute<Boolean> GET_PARENT_SUBSTITUDE_INFO = new SimpleStyleAttribute<Boolean>("get-parent-substitude-info", false);
+  public static final StyleAttribute<ParametersInformation> PARAMETERS_INFORMATION = new SimpleStyleAttribute<ParametersInformation>("parameters-information");  
 }
