10bfdfc00b501f12153cfc8c3bb74df9eb7a9ab0
Konstantin Solomatov <kostik@jetbrains.com>
2009-10-24 18:44:46 +0400

    files from @tests models are compiled

--- core/kernel/source/jetbrains/mps/project/AbstractModule.java
+++ core/kernel/source/jetbrains/mps/project/AbstractModule.java
@@ -530,6 +530,7 @@ public abstract class AbstractModule implements IModule {
       }
     }
     result.add(getGeneratorOutputPath());
+    result.add(getTestsGeneratorOutputPath());
     return result;
   }
 
