a2688cb2c9e1d201fbf5529494acc6655e8e40f9
Darja Chembrovskaya <Darja.Chembrovskaya@jetbrains.com>
2009-10-30 17:42:10 +0300

    fix MPS-3371sort languages and solutions in project view by their FQName

--- workbench/source/jetbrains/mps/ide/projectPane/ModuleTreeNodeComparator.java
+++ workbench/source/jetbrains/mps/ide/projectPane/ModuleTreeNodeComparator.java
@@ -1,27 +1,38 @@
 package jetbrains.mps.ide.projectPane;
 
+import jetbrains.mps.ide.projectPane.fileSystem.nodes.ModuleTreeNode;
 import jetbrains.mps.ide.ui.MPSTreeNode;
-import jetbrains.mps.project.Solution;
 import jetbrains.mps.project.IModule;
+import jetbrains.mps.project.Solution;
 import jetbrains.mps.smodel.Language;
 
 import java.util.Comparator;
 
 public class ModuleTreeNodeComparator implements Comparator<MPSTreeNode> {
-  private boolean isModuleTreeNode(MPSTreeNode treeNode) {
-    return treeNode instanceof ProjectModuleTreeNode;
+  private IModule getModule(MPSTreeNode treeNode) {
+    if (treeNode instanceof ProjectModuleTreeNode) {
+      return ((ProjectModuleTreeNode) treeNode).getModule();
+    }
+    if (treeNode instanceof ModuleTreeNode) {
+      return ((ModuleTreeNode) treeNode).getModule();
+    }
+    return null;
   }
 
   private String getValueToCompare(MPSTreeNode treeNode) {
-    return ((ProjectModuleTreeNode) treeNode).getModulePresentation();
+    if (treeNode instanceof ProjectModuleTreeNode) {
+      return ((ProjectModuleTreeNode) treeNode).getModulePresentation();
+    }
+    if (treeNode instanceof ModuleTreeNode) {
+      return ((ModuleTreeNode) treeNode).getText();
+    }
+    return null;
   }
 
   public int compare(MPSTreeNode o1, MPSTreeNode o2) {
-    boolean isModule1 = isModuleTreeNode(o1);
-    boolean isModule2 = isModuleTreeNode(o2);
-    if (isModule1 && isModule2) {
-      IModule module1 = ((ProjectModuleTreeNode) o1).getModule();
-      IModule module2 = ((ProjectModuleTreeNode) o2).getModule();
+    IModule module1 = getModule(o1);
+    IModule module2 = getModule(o2);
+    if (module1 != null && module2 != null) {
       if (module1.getClass().equals(module2.getClass())) {
         return getValueToCompare(o1).compareTo(getValueToCompare(o2));
       } else if (module1 instanceof Solution) {
