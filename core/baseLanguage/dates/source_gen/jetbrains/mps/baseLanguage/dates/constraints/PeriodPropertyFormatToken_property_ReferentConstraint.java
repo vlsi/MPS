500c4a0abfd0aa04e0f237101c392133faf1a01b
Evgeny Gryaznov <Evgeny.Gryaznov@jetbrains.com>
2009-11-03 18:35:20 +0300

    period format editor: show only valid property and format references

--- core/baseLanguage/dates/source_gen/jetbrains/mps/baseLanguage/dates/constraints/PeriodPropertyFormatToken_property_ReferentConstraint.java
+++ core/baseLanguage/dates/source_gen/jetbrains/mps/baseLanguage/dates/constraints/PeriodPropertyFormatToken_property_ReferentConstraint.java
@@ -6,7 +6,9 @@ import jetbrains.mps.smodel.constraints.BaseNodeReferenceSearchScopeProvider;
 import jetbrains.mps.smodel.constraints.IModelConstraints;
 import jetbrains.mps.smodel.constraints.ModelConstraintsManager;
 import jetbrains.mps.smodel.IOperationContext;
+import jetbrains.mps.smodel.constraints.ReferentConstraintContext;
 import jetbrains.mps.smodel.constraints.PresentationReferentConstraintContext;
+import org.apache.commons.lang.StringUtils;
 import jetbrains.mps.lang.smodel.generator.smodelAdapter.SPropertyOperations;
 
 public class PeriodPropertyFormatToken_property_ReferentConstraint extends BaseNodeReferenceSearchScopeProvider implements IModelConstraints {
@@ -21,14 +23,22 @@ public class PeriodPropertyFormatToken_property_ReferentConstraint extends BaseN
     manager.unRegisterNodeReferentSearchScopeProvider("jetbrains.mps.baseLanguage.dates.structure.PeriodPropertyFormatToken", "property");
   }
 
+  public Object createSearchScopeOrListOfNodes(final IOperationContext operationContext, final ReferentConstraintContext _context) {
+    return DateFormatReferenceUtil.buildPeriodPropertySearchScope(_context.getEnclosingNode(), operationContext.getScope());
+  }
+
   public boolean hasPresentation() {
     return true;
   }
 
   public String getPresentation(final IOperationContext operationContext, final PresentationReferentConstraintContext _context) {
-    return (_context.getSmartReference() ?
-      "{" + SPropertyOperations.getString(_context.getParameterNode(), "name") + "}" :
+    String name = (StringUtils.isNotEmpty(SPropertyOperations.getString(_context.getParameterNode(), "pluralForm")) ?
+      SPropertyOperations.getString(_context.getParameterNode(), "pluralForm") :
       SPropertyOperations.getString(_context.getParameterNode(), "name")
+    );
+    return (_context.getSmartReference() ?
+      "{" + name + "}" :
+      name
     );
   }
 }
