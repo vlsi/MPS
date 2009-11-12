a0c5ab6f888cdd709f1abbdcbd94afc18043d326
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2009-11-12 22:43:32 +0300

    fix loading unnecessary projects

--- platform/buildlanguage/ant/sources/jetbrains/mps/build/ant/ProjectNested.java
+++ platform/buildlanguage/ant/sources/jetbrains/mps/build/ant/ProjectNested.java
@@ -1,26 +1,18 @@
 package jetbrains.mps.build.ant;
 
 import org.apache.tools.ant.types.FileSet;
+import org.apache.tools.ant.BuildException;
+
+import java.io.File;
 
 public class ProjectNested extends FileSet {
   private boolean myWholeProject = false;
   private static final String[] INCLUDES = new String[]{"**/*.mpr"};
 
-  public ProjectNested() {
-    init();
-  }
-
-  public ProjectNested(FileSet fileset) {
-    super(fileset);
-    init();
-  }
-
-  private void init() {
-    if (isReference()) {
-      ((FileSet) getRef(getProject())).appendExcludes(INCLUDES);
-    } else {
-      appendIncludes(INCLUDES);
-    }
+  @Override
+  public void setDir(File dir) throws BuildException {
+    super.setDir(dir);
+    appendIncludes(INCLUDES);
   }
 
   public boolean getWholeProject() {
