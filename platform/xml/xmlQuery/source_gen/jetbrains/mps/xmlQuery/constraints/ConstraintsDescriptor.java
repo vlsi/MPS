db03068ed7e61ab8e6196683f36aeb3dad6d368e
Evgeny Gerashchenko <Eugene.Gerashchenko@jetbrains.com>
2009-12-04 20:55:34 +0300

    xmlQuery: renamed AccessChildOperation and AccessArbitraryChildOperation concepts («child» was replaced by «children»

--- platform/xml/xmlQuery/source_gen/jetbrains/mps/xmlQuery/constraints/ConstraintsDescriptor.java
+++ platform/xml/xmlQuery/source_gen/jetbrains/mps/xmlQuery/constraints/ConstraintsDescriptor.java
@@ -15,7 +15,7 @@ public class ConstraintsDescriptor implements IModelConstraints {
     ListSequence.fromList(this.myConstraints).addElement(new ElementBuilder_elementDeclaration_ReferentConstraint());
     ListSequence.fromList(this.myConstraints).addElement(new XMLElementType_schema_ReferentConstraint());
     ListSequence.fromList(this.myConstraints).addElement(new XMLElementType_complexType_ReferentConstraint());
-    ListSequence.fromList(this.myConstraints).addElement(new AccessChildOperation_elementDeclaration_ReferentConstraint());
+    ListSequence.fromList(this.myConstraints).addElement(new AccessChildrenOperation_elementDeclaration_ReferentConstraint());
     ListSequence.fromList(this.myConstraints).addElement(new AccessAttributeOperation_attributeDeclaration_ReferentConstraint());
     ListSequence.fromList(this.myConstraints).addElement(new AttributeBuilder_attributeDeclaration_ReferentConstraint());
   }
