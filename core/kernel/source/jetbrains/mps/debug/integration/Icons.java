37baed0d8fb07a519ac273704aef6b0fcff3c694
Cyril Konopko <Cyril.Konopko@jetbrains.com>
2009-12-09 18:38:05 +0300

    UI for breakpoints

---
+++ core/kernel/source/jetbrains/mps/debug/integration/Icons.java
@@ -0,0 +1,15 @@
+package jetbrains.mps.debug.integration;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Cyril.Konopko
+ * Date: 09.12.2009
+ * Time: 18:22:38
+ * To change this template use File | Settings | File Templates.
+ */
+public class Icons {
+  public static final Icon BREAKPOINT = new ImageIcon(Icons.class.getResource("db_set_breakpoint.png"));
+}
