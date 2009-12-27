d4cb6e644a169e02e0bd6b2c648e8ea8f1065b58
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2009-12-27 18:30:23 +0300

    fix MPS-6332 alt​+​num doesn​'​t work on MAC

--- workbench/source_gen/jetbrains/mps/ide/actions/Mac_KeymapChanges.java
+++ workbench/source_gen/jetbrains/mps/ide/actions/Mac_KeymapChanges.java
@@ -10,6 +10,16 @@ public class Mac_KeymapChanges extends BaseKeymapChanges {
     this.addAction("jetbrains.mps.ide.actions.ShowClassInHierarchy_Action", "jetbrains.mps.ide", "ctrl H");
     this.addAction("jetbrains.mps.ide.actions.ShowNodeInfo_Action", "jetbrains.mps.ide", "ctrl Q");
     this.addAction("jetbrains.mps.ide.actions.GenerationIntentions_Action", "jetbrains.mps.ide", "ctrl N");
+    this.addAction("jetbrains.mps.ide.actions.GoToBookmark0_Action", "jetbrains.mps.ide", "ctrl 0");
+    this.addAction("jetbrains.mps.ide.actions.GoToBookmark1_Action", "jetbrains.mps.ide", "ctrl 1");
+    this.addAction("jetbrains.mps.ide.actions.GoToBookmark2_Action", "jetbrains.mps.ide", "ctrl 2");
+    this.addAction("jetbrains.mps.ide.actions.GoToBookmark3_Action", "jetbrains.mps.ide", "ctrl 3");
+    this.addAction("jetbrains.mps.ide.actions.GoToBookmark4_Action", "jetbrains.mps.ide", "ctrl 4");
+    this.addAction("jetbrains.mps.ide.actions.GoToBookmark5_Action", "jetbrains.mps.ide", "ctrl 5");
+    this.addAction("jetbrains.mps.ide.actions.GoToBookmark6_Action", "jetbrains.mps.ide", "ctrl 6");
+    this.addAction("jetbrains.mps.ide.actions.GoToBookmark7_Action", "jetbrains.mps.ide", "ctrl 7");
+    this.addAction("jetbrains.mps.ide.actions.GoToBookmark8_Action", "jetbrains.mps.ide", "ctrl 8");
+    this.addAction("jetbrains.mps.ide.actions.GoToBookmark9_Action", "jetbrains.mps.ide", "ctrl 9");
   }
 
   public String getScheme() {
