386ed36789c47995b321e8d96870851ab255b561
Cyril Konopko <Cyril.Konopko@jetbrains.com>
2009-12-28 16:55:35 +0300

    test classes

--- core/javaParser/source/jetbrains/mps/javaParser/testMaterial5/OrdinaryClassWithEnum.java
+++ core/javaParser/source/jetbrains/mps/javaParser/testMaterial5/OrdinaryClassWithEnum.java
@@ -1,20 +1,21 @@
 package jetbrains.mps.javaParser.testMaterial5;
 
-import java.util.List;
-
 /**
  * Created by IntelliJ IDEA.
  * User: Cyril.Konopko
  * Date: 28.12.2009
- * Time: 16:30:29
+ * Time: 16:51:25
  * To change this template use File | Settings | File Templates.
  */
-public class OrdinaryClassWithErasure2 {
-  public void foo(Object value) {
-    if (value instanceof List) {
-      List list = (List) value;
-      for (Object o : list) {
+public class OrdinaryClassWithEnum {
+  public static void foo() {
+    OrdinaryEnum2 enum2 = null;
+    switch (enum2) {
+      case A: {
         System.err.println("");
+      }
+      case B: {
+        System.out.println("");
       }
     }
   }
---
+++ core/javaParser/source/jetbrains/mps/javaParser/testMaterial5/OrdinaryEnum2.java
@@ -0,0 +1,12 @@
+package jetbrains.mps.javaParser.testMaterial5;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Cyril.Konopko
+ * Date: 28.12.2009
+ * Time: 16:46:34
+ * To change this template use File | Settings | File Templates.
+ */
+public enum OrdinaryEnum2 {
+  A,B,C,D
+}
