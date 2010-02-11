789e2201f0d9e672f9288978b83caa2557e6c60c
Evgeny Gryaznov <Evgeny.Gryaznov@jetbrains.com>
2010-02-11 16:37:48 +0300

    extract mappings into AbstractTemplateGenerator

--- core/languageDesign/generator/source/jetbrains/mps/generator/impl/AbstractTemplateGenerator.java
+++ core/languageDesign/generator/source/jetbrains/mps/generator/impl/AbstractTemplateGenerator.java
@@ -17,21 +17,48 @@ package jetbrains.mps.generator.impl;
 
 import com.intellij.openapi.progress.ProgressIndicator;
 import jetbrains.mps.generator.GenerationCanceledException;
+import jetbrains.mps.generator.GenerationFailureException;
 import jetbrains.mps.generator.template.ITemplateGenerator;
-import jetbrains.mps.smodel.IOperationContext;
-import jetbrains.mps.smodel.IScope;
-import jetbrains.mps.smodel.SNode;
+import jetbrains.mps.lang.generator.structure.Reduction_MappingRule;
+import jetbrains.mps.lang.generator.structure.RuleConsequence;
+import jetbrains.mps.lang.generator.structure.TemplateSwitch;
+import jetbrains.mps.lang.structure.structure.AbstractConceptDeclaration;
+import jetbrains.mps.logging.Logger;
+import jetbrains.mps.smodel.*;
+import jetbrains.mps.util.Pair;
+
+import java.util.*;
 
 public abstract class AbstractTemplateGenerator implements ITemplateGenerator {
+
   private IOperationContext myOperationContext;
   private ProgressIndicator myProgressMonitor;
   private GeneratorLogger myLogger;
 
+  protected final SModel myInputModel;
+  protected final SModel myOutputModel;
+
+  private final ArrayList<SNode> myRootsNotToCopy = new ArrayList<SNode>();
+  private final HashMap<Pair<SNode, SNode>, SNode> myTemplateNodeAndInputNodeToOutputNodeMap = new HashMap<Pair<SNode, SNode>, SNode>();
+  private final Set<SNode> myInputNodesWithNotUniqueCopiedOutputNode = new HashSet<SNode>();
+  private final HashMap<Pair<String, SNode>, Object> myMappingNameAndInputNodeToOutputNodeMap = new HashMap<Pair<String, SNode>, Object>();
+  private final HashMap<SNode, SNode> myOutputNodeToTemplateNodeMap = new HashMap<SNode, SNode>();
+  private final HashMap<SNode, Pair<SNode, Boolean>> myTemplateNodeToOutputNodeMap = new HashMap<SNode, Pair<SNode, Boolean>>();
+  private final DelayedChanges myDelayedChanges = new DelayedChanges();
+  private final Map<SNode, SNode> myNewToOldRoot = new HashMap<SNode, SNode>();
+
+  private TemplateSwitchGraph myTemplateSwitchGraph;
+  private Map<TemplateSwitch, List<TemplateSwitch>> myTemplateSwitchToListCache;
+  private Map<String, SNode> myCurrentPreviousInputNodesByMappingName;
+
   protected AbstractTemplateGenerator(IOperationContext operationContext,
-                                      ProgressIndicator progressMonitor, GeneratorLogger logger) {
+                                      ProgressIndicator progressMonitor, GeneratorLogger logger,
+                                      SModel inputModel, SModel outputModel) {
     myOperationContext = operationContext;
     myProgressMonitor = progressMonitor;
     myLogger = logger;
+    myInputModel = inputModel;
+    myOutputModel = outputModel;
   }
 
   public IOperationContext getOperationContext() {
@@ -79,5 +106,262 @@ public abstract class AbstractTemplateGenerator implements ITemplateGenerator {
 
   public int getWarningCount() {
     return myLogger.getWarningCount();
+  }
+
+  public SModel getInputModel() {
+    return myInputModel;
+  }
+
+  /**
+   * @deprecated
+   */
+  public SModel getSourceModel() {
+    return myInputModel;
+  }
+
+  public SModel getOutputModel() {
+    return myOutputModel;
+  }
+
+  /**
+   * @deprecated
+   */
+  public SModel getTargetModel() {
+    return myOutputModel;
+  }
+
+  public void addRootNotToCopy(SNode inputNode) {
+    myRootsNotToCopy.add(inputNode);
+  }
+
+  public Iterable<SNode> getRootsNotToCopy() {
+    return myRootsNotToCopy;
+  }
+
+  public SNode findTemplateNodeByOutputNode(SNode outputNode) {
+    return myOutputNodeToTemplateNodeMap.get(outputNode);
+  }
+
+  public void addTemplateNodeByOutputNode(SNode outputNode, SNode templateNode) {
+//    if(myOutputNodeToTemplateNodeMap.get(outputNode) != null) {
+//     showWarningMessage(templateNode, "The template node already exists, that was build by this output node");
+//    }
+    myOutputNodeToTemplateNodeMap.put(outputNode, templateNode);
+  }
+
+  public void addOutputNodeByTemplateNode(SNode templateNode, SNode outputNode) {
+    Pair<SNode, Boolean> pair = myTemplateNodeToOutputNodeMap.get(templateNode);
+    if (pair == null) {
+      myTemplateNodeToOutputNodeMap.put(templateNode, new Pair<SNode, Boolean>(outputNode, true));
+      return;
+    }
+
+    // that means that there were more than one output node for given template node
+    if (!pair.o2) {
+      return;
+    }
+    myTemplateNodeToOutputNodeMap.put(templateNode, new Pair<SNode, Boolean>(pair.o1, false));
+  }
+
+  public SNode findOutputNodeByTemplateNode(SNode templateNode, boolean unique) {
+    Pair<SNode, Boolean> pair = myTemplateNodeToOutputNodeMap.get(templateNode);
+    if (pair != null) {
+      if (pair.o2 || !unique) {
+        return pair.o1;
+      }
+    }
+    return null;
+  }
+
+  public DelayedChanges getDelayedChanges() {
+    return myDelayedChanges;
+  }
+
+  public SNode findOutputNodeByInputNodeAndMappingName(SNode inputNode, String mappingLabel) {
+    Object o = myMappingNameAndInputNodeToOutputNodeMap.get(new Pair(mappingLabel, inputNode));
+    if (o instanceof List) {
+      List<SNode> list = (List<SNode>) o;
+      showWarningMessage(inputNode, "" + list.size() + " output nodes found for mapping label '" + mappingLabel + "' and input " + inputNode.getDebugText());
+      for (int i = 0; i < list.size(); i++) {
+        Logger.getLogger(this.getClass()).warning("-- output [" + i + "] : " + list.get(i).getDebugText(), list.get(i));
+      }
+      return list.get(0);
+    }
+
+    return (SNode) o;
+  }
+
+  @Override
+  public SNode findOutputNodeByInputNodeAndOutputNodeAndMappingName(SNode inputNode, SNode outputNode, String mappingLabel) {
+    Object o = myMappingNameAndInputNodeToOutputNodeMap.get(new Pair(mappingLabel, inputNode));
+    if (o instanceof List) {
+      List<SNode> list = (List<SNode>) o;
+      showWarningMessage(inputNode, "" + list.size() + " output nodes found for mapping label '" + mappingLabel + "' and input " + inputNode.getDebugText());
+      for (int i = 0; i < list.size(); i++) {
+        Logger.getLogger(this.getClass()).warning("-- output [" + i + "] : " + list.get(i).getDebugText(), list.get(i));
+      }
+      //heuristics
+      List<SNode> ancestors = outputNode.getAncestors(true);
+      SNode[] candidates = new SNode[ancestors.size()];
+      for (int i = 0; i < list.size(); i++) {
+        SNode candidate = list.get(i);
+        for (SNode ancestor : candidate.getAncestors(true)) {
+          if (ancestors.contains(ancestor)) {
+            candidates[ancestors.indexOf(ancestor)] = candidate;
+            break;
+          }
+        }
+      }
+      for (SNode candidate : candidates) {
+        if (candidate != null) return candidate;
+      }
+      return list.get(0);
+    }
+
+    return (SNode) o;
+  }
+
+  public List<SNode> findAllOutputNodesByInputNodeAndMappingName(SNode inputNode, String mappingLabel) {
+    Object o = myMappingNameAndInputNodeToOutputNodeMap.get(new Pair(mappingLabel, inputNode));
+    if (o instanceof List) return ((List<SNode>) o);
+    List<SNode> list = new ArrayList<SNode>();
+    list.add((SNode) o);
+    return list;
+  }
+
+  /*package*/
+  void addOutputNodeByInputNodeAndMappingName(SNode inputNode, String mappingName, SNode outputNode) {
+    if (mappingName == null) return;
+    Pair key = new Pair(mappingName, inputNode);
+    Object o = myMappingNameAndInputNodeToOutputNodeMap.get(key);
+    if (o == null) {
+      myMappingNameAndInputNodeToOutputNodeMap.put(key, outputNode);
+    } else if (o instanceof List) {
+      ((List<SNode>) o).add(outputNode);
+    } else {
+      List<SNode> list = new ArrayList<SNode>();
+      list.add((SNode) o);
+      list.add(outputNode);
+      myMappingNameAndInputNodeToOutputNodeMap.put(key, list);
+    }
+  }
+
+
+  public SNode findCopiedOutputNodeForInputNode(SNode inputNode) {
+    return findOutputNodeByInputAndTemplateNode(inputNode, inputNode);
+  }
+
+  /*package*/
+  SNode findCopiedOutputNodeForInputNode_unique(SNode inputNode) {
+    SNode node = findOutputNodeByInputAndTemplateNode(inputNode, inputNode);
+    if (!myInputNodesWithNotUniqueCopiedOutputNode.contains(inputNode)) return node;
+    return null;
+  }
+
+  /*package*/
+  void addCopiedOutputNodeForInputNode(SNode inputNode, SNode outputNode) {
+    // todo: can be several copied output nodes for one input node
+    Pair key = new Pair(inputNode, inputNode);
+    if (!myTemplateNodeAndInputNodeToOutputNodeMap.containsKey(key)) {
+      myTemplateNodeAndInputNodeToOutputNodeMap.put(key, outputNode);
+    } else {
+      myInputNodesWithNotUniqueCopiedOutputNode.add(inputNode);
+    }
+  }
+
+  public SNode findOutputNodeByInputAndTemplateNode(SNode inputNode, SNode templateNode) {
+    SNode outputNode = myTemplateNodeAndInputNodeToOutputNodeMap.get(new Pair(templateNode, inputNode));
+    if (outputNode == null) {
+      // input node has been cloned?
+      if (inputNode == templateNode) {
+        outputNode = findOutputNodeById(inputNode.getSNodeId());
+      }
+    }
+    return outputNode;
+  }
+
+  /*package*/
+  void addOutputNodeByInputAndTemplateNode(SNode inputNode, SNode templateNode, SNode outputNode) {
+    // todo: combination of (templateN, inputN) -> outputN
+    // todo: is not unique
+    // todo: generator should repotr error on attempt to obtain not unique output-node
+    myTemplateNodeAndInputNodeToOutputNodeMap.put(new Pair(templateNode, inputNode), outputNode);
+  }
+
+  /*package*/
+  void addOutputNodeByIndirectInputAndTemplateNode(SNode inditectInputNode, SNode templateNode, SNode outputNode) {
+    // todo: combination of (templateN, inputN) -> outputN
+    // todo: is not unique
+    // todo: generator should repotr error on attempt to obtain not unique output-node
+    Pair key = new Pair(templateNode, inditectInputNode);
+    if (!myTemplateNodeAndInputNodeToOutputNodeMap.containsKey(key)) {
+      myTemplateNodeAndInputNodeToOutputNodeMap.put(key, outputNode);
+    }
+  }
+
+  public Map<String, SNode> setPreviousInputNodesByMappingName(Map<String, SNode> inputNodesByMappingName) {
+    Map<String, SNode> old = myCurrentPreviousInputNodesByMappingName;
+    myCurrentPreviousInputNodesByMappingName = inputNodesByMappingName;
+    return old;
+  }
+
+  public SNode getPreviousInputNodeByMappingName(String mappingName) {
+    if (myCurrentPreviousInputNodesByMappingName == null || mappingName == null) {
+      return null;
+    }
+    return myCurrentPreviousInputNodesByMappingName.get(mappingName);
+  }
+
+  public RuleConsequence getConsequenceForSwitchCase(SNode inputNode, TemplateSwitch templateSwitch) throws GenerationFailureException {
+    AbstractConceptDeclaration inputNodeConcept = inputNode.getConceptDeclarationAdapter();
+
+    if (myTemplateSwitchGraph == null) {
+      myTemplateSwitchGraph = new TemplateSwitchGraph(getGeneratorSessionContext().getTemplateModels());
+      myTemplateSwitchToListCache = new HashMap<TemplateSwitch, List<TemplateSwitch>>();
+    }
+
+    List<TemplateSwitch> switches = myTemplateSwitchToListCache.get(templateSwitch);
+    if (switches == null) {
+      switches = myTemplateSwitchGraph.getSubgraphAsList(templateSwitch);
+      myTemplateSwitchToListCache.put(templateSwitch, switches);
+    }
+
+    // for each template switch test conditions and choose template node
+    for (TemplateSwitch aSwitch : switches) {
+      List<Reduction_MappingRule> rules = aSwitch.getReductionMappingRules();
+      for (Reduction_MappingRule rule : rules) {
+        if (GeneratorUtil.checkPremiseForBaseMappingRule(inputNode, inputNodeConcept, rule, this)) {
+          RuleConsequence ruleConsequence = rule.getRuleConsequence();
+          if (ruleConsequence == null) {
+            showErrorMessage(inputNode, null, rule.getNode(), "couldn't apply reduction: no rule consequence");
+          }
+          return ruleConsequence;
+        }
+      }
+
+      // default
+      RuleConsequence ruleConsequence = aSwitch.getDefaultConsequence();
+      if (ruleConsequence != null) {
+        return ruleConsequence;
+      }
+    }
+
+    return null;
+  }
+
+  public SNode findOutputNodeById(SNodeId nodeId) {
+    return myOutputModel.getNodeById(nodeId);
+  }
+
+  public SNode findInputNodeById(SNodeId nodeId) {
+    return myInputModel.getNodeById(nodeId);
+  }
+
+  void registerRoot(SNode newroot, SNode old) {
+    myNewToOldRoot.put(newroot, old);
+  }
+
+  public SNode getInputRootForOutput(SNode node) {
+    return myNewToOldRoot.get(node);
   }
 }
--- core/languageDesign/generator/source/jetbrains/mps/generator/impl/TemplateGenerator.java
+++ core/languageDesign/generator/source/jetbrains/mps/generator/impl/TemplateGenerator.java
@@ -38,20 +38,6 @@ import java.util.*;
  */
 public class TemplateGenerator extends AbstractTemplateGenerator {
 
-  private SModel myInputModel;
-  private SModel myOutputModel;
-  private ArrayList<SNode> myRootsNotToCopy = new ArrayList<SNode>();
-  private HashMap<Pair<SNode, SNode>, SNode> myTemplateNodeAndInputNodeToOutputNodeMap = new HashMap<Pair<SNode, SNode>, SNode>();
-  private Set<SNode> myInputNodesWithNotUniqueCopiedOutputNode = new HashSet<SNode>();
-  private HashMap<Pair<String, SNode>, Object> myMappingNameAndInputNodeToOutputNodeMap = new HashMap<Pair<String, SNode>, Object>();
-  private HashMap<SNode, SNode> myOutputNodeToTemplateNodeMap = new HashMap<SNode, SNode>();
-  private HashMap<SNode, Pair<SNode, Boolean>> myTemplateNodeToOutputNodeMap = new HashMap<SNode, Pair<SNode, Boolean>>();
-  private DelayedChanges myDelayedChanges = new DelayedChanges();
-  private TemplateSwitchGraph myTemplateSwitchGraph;
-  private Map<TemplateSwitch, List<TemplateSwitch>> myTemplateSwitchToListCache;
-  private Map<String, SNode> myCurrentPreviousInputNodesByMappingName;
-  private Map<SNode, SNode> myNewToOldRoot = new HashMap<SNode, SNode>();
-
   private boolean myChanged = false;
   private final RuleProcessor myRuleProcessor;
 
@@ -61,10 +47,8 @@ public class TemplateGenerator extends AbstractTemplateGenerator {
                            RuleManager ruleManager,
                            SModel inputModel,
                            SModel outputModel) {
-    super(operationContext, progressMonitor, logger);
+    super(operationContext, progressMonitor, logger, inputModel, outputModel);
     myRuleProcessor = ruleManager.createProcessor(this);
-    myInputModel = inputModel;
-    myOutputModel = outputModel;
   }
 
   public GenerationSessionContext getGeneratorSessionContext() {
@@ -103,7 +87,7 @@ public class TemplateGenerator extends AbstractTemplateGenerator {
     myRuleProcessor.applyWeaving_MappingRules();
 
     // execute mapper in all $MAP_SRC$/$MAP_SRCL$
-    myDelayedChanges.doAllChanges();
+    getDelayedChanges().doAllChanges();
 
     // new unresolved references could appear after applying reduction rules (all delayed changes should be done before this, like replacing children)
     for (SNode copiedRoot : copiedOutputRoots) {
@@ -118,7 +102,7 @@ public class TemplateGenerator extends AbstractTemplateGenerator {
 
   private List<SNode> copyRootsFromInputModel() throws GenerationFailureException {
     List<SNode> rootsToCopy = new ArrayList<SNode>(myInputModel.getRoots());
-    for (SNode rootNode : myRootsNotToCopy) {
+    for (SNode rootNode : getRootsNotToCopy()) {
       rootsToCopy.remove(rootNode);
     }
 
@@ -201,264 +185,11 @@ public class TemplateGenerator extends AbstractTemplateGenerator {
     }
   }
 
-  public SModel getInputModel() {
-    return myInputModel;
-  }
-
-  /**
-   * @deprecated
-   */
-  public SModel getSourceModel() {
-    return myInputModel;
-  }
-
-  public SModel getOutputModel() {
-    return myOutputModel;
-  }
-
-  /**
-   * @deprecated
-   */
-  public SModel getTargetModel() {
-    return myOutputModel;
-  }
-
-  public void addRootNotToCopy(SNode inputNode) {
-    myRootsNotToCopy.add(inputNode);
-  }
-
-  public SNode findTemplateNodeByOutputNode(SNode outputNode) {
-    return myOutputNodeToTemplateNodeMap.get(outputNode);
-  }
-
-  public void addTemplateNodeByOutputNode(SNode outputNode, SNode templateNode) {
-//    if(myOutputNodeToTemplateNodeMap.get(outputNode) != null) {
-//     showWarningMessage(templateNode, "The template node already exists, that was build by this output node");
-//    }
-    myOutputNodeToTemplateNodeMap.put(outputNode, templateNode);
-  }
-
-  public void addOutputNodeByTemplateNode(SNode templateNode, SNode outputNode) {
-    Pair<SNode, Boolean> pair = myTemplateNodeToOutputNodeMap.get(templateNode);
-    if (pair == null) {
-      myTemplateNodeToOutputNodeMap.put(templateNode, new Pair<SNode, Boolean>(outputNode, true));
-      return;
-    }
-
-    // that means that there were more than one output node for given template node
-    if (!pair.o2) {
-      return;
-    }
-    myTemplateNodeToOutputNodeMap.put(templateNode, new Pair<SNode, Boolean>(pair.o1, false));
-  }
-
-  public SNode findOutputNodeByTemplateNode(SNode templateNode, boolean unique) {
-    Pair<SNode, Boolean> pair = myTemplateNodeToOutputNodeMap.get(templateNode);
-    if (pair != null) {
-      if (pair.o2 || !unique) {
-        return pair.o1;
-      }
-    }
-    return null;
-  }
-
-  public DelayedChanges getDelayedChanges() {
-    return myDelayedChanges;
-  }
-
-  public SNode findOutputNodeByInputNodeAndMappingName(SNode inputNode, String mappingLabel) {
-    Object o = myMappingNameAndInputNodeToOutputNodeMap.get(new Pair(mappingLabel, inputNode));
-    if (o instanceof List) {
-      List<SNode> list = (List<SNode>) o;
-      showWarningMessage(inputNode, "" + list.size() + " output nodes found for mapping label '" + mappingLabel + "' and input " + inputNode.getDebugText());
-      for (int i = 0; i < list.size(); i++) {
-        Logger.getLogger(this.getClass()).warning("-- output [" + i + "] : " + list.get(i).getDebugText(), list.get(i));
-      }
-      return list.get(0);
-    }
-
-    return (SNode) o;
-  }
-
-  @Override
-  public SNode findOutputNodeByInputNodeAndOutputNodeAndMappingName(SNode inputNode, SNode outputNode, String mappingLabel) {
-    Object o = myMappingNameAndInputNodeToOutputNodeMap.get(new Pair(mappingLabel, inputNode));
-    if (o instanceof List) {
-      List<SNode> list = (List<SNode>) o;
-      showWarningMessage(inputNode, "" + list.size() + " output nodes found for mapping label '" + mappingLabel + "' and input " + inputNode.getDebugText());
-      for (int i = 0; i < list.size(); i++) {
-        Logger.getLogger(this.getClass()).warning("-- output [" + i + "] : " + list.get(i).getDebugText(), list.get(i));
-      }
-      //heuristics
-      List<SNode> ancestors = outputNode.getAncestors(true);
-      SNode[] candidates = new SNode[ancestors.size()];
-      for (int i = 0; i < list.size(); i++) {
-        SNode candidate = list.get(i);
-        for (SNode ancestor : candidate.getAncestors(true)) {
-          if (ancestors.contains(ancestor)) {
-            candidates[ancestors.indexOf(ancestor)] = candidate;
-            break;
-          }
-        }
-      }
-      for (SNode candidate : candidates) {
-        if (candidate != null) return candidate;
-      }
-      return list.get(0);
-    }
-
-    return (SNode) o;
-  }
-
-  public List<SNode> findAllOutputNodesByInputNodeAndMappingName(SNode inputNode, String mappingLabel) {
-    Object o = myMappingNameAndInputNodeToOutputNodeMap.get(new Pair(mappingLabel, inputNode));
-    if (o instanceof List) return ((List<SNode>) o);
-    List<SNode> list = new ArrayList<SNode>();
-    list.add((SNode) o);
-    return list;
-  }
-
-  /*package*/
-  void addOutputNodeByInputNodeAndMappingName(SNode inputNode, String mappingName, SNode outputNode) {
-    if (mappingName == null) return;
-    Pair key = new Pair(mappingName, inputNode);
-    Object o = myMappingNameAndInputNodeToOutputNodeMap.get(key);
-    if (o == null) {
-      myMappingNameAndInputNodeToOutputNodeMap.put(key, outputNode);
-    } else if (o instanceof List) {
-      ((List<SNode>) o).add(outputNode);
-    } else {
-      List<SNode> list = new ArrayList<SNode>();
-      list.add((SNode) o);
-      list.add(outputNode);
-      myMappingNameAndInputNodeToOutputNodeMap.put(key, list);
-    }
-  }
-
-
-  public SNode findCopiedOutputNodeForInputNode(SNode inputNode) {
-    return findOutputNodeByInputAndTemplateNode(inputNode, inputNode);
-  }
-
-  /*package*/
-  SNode findCopiedOutputNodeForInputNode_unique(SNode inputNode) {
-    SNode node = findOutputNodeByInputAndTemplateNode(inputNode, inputNode);
-    if (!myInputNodesWithNotUniqueCopiedOutputNode.contains(inputNode)) return node;
-    return null;
-  }
-
-  /*package*/
-  void addCopiedOutputNodeForInputNode(SNode inputNode, SNode outputNode) {
-    // todo: can be several copied output nodes for one input node
-    Pair key = new Pair(inputNode, inputNode);
-    if (!myTemplateNodeAndInputNodeToOutputNodeMap.containsKey(key)) {
-      myTemplateNodeAndInputNodeToOutputNodeMap.put(key, outputNode);
-    } else {
-      myInputNodesWithNotUniqueCopiedOutputNode.add(inputNode);
-    }
-  }
-
-  public SNode findOutputNodeByInputAndTemplateNode(SNode inputNode, SNode templateNode) {
-    SNode outputNode = myTemplateNodeAndInputNodeToOutputNodeMap.get(new Pair(templateNode, inputNode));
-    if (outputNode == null) {
-      // input node has been cloned?
-      if (inputNode == templateNode) {
-        outputNode = findOutputNodeById(inputNode.getSNodeId());
-      }
-    }
-    return outputNode;
-  }
-
-  /*package*/
-  void addOutputNodeByInputAndTemplateNode(SNode inputNode, SNode templateNode, SNode outputNode) {
-    // todo: combination of (templateN, inputN) -> outputN
-    // todo: is not unique
-    // todo: generator should repotr error on attempt to obtain not unique output-node
-    myTemplateNodeAndInputNodeToOutputNodeMap.put(new Pair(templateNode, inputNode), outputNode);
-  }
-
-  /*package*/
-  void addOutputNodeByIndirectInputAndTemplateNode(SNode inditectInputNode, SNode templateNode, SNode outputNode) {
-    // todo: combination of (templateN, inputN) -> outputN
-    // todo: is not unique
-    // todo: generator should repotr error on attempt to obtain not unique output-node
-    Pair key = new Pair(templateNode, inditectInputNode);
-    if (!myTemplateNodeAndInputNodeToOutputNodeMap.containsKey(key)) {
-      myTemplateNodeAndInputNodeToOutputNodeMap.put(key, outputNode);
-    }
-  }
-
-  public Map<String, SNode> setPreviousInputNodesByMappingName(Map<String, SNode> inputNodesByMappingName) {
-    Map<String, SNode> old = myCurrentPreviousInputNodesByMappingName;
-    myCurrentPreviousInputNodesByMappingName = inputNodesByMappingName;
-    return old;
-  }
-
-  public SNode getPreviousInputNodeByMappingName(String mappingName) {
-    if (myCurrentPreviousInputNodesByMappingName == null || mappingName == null) {
-      return null;
-    }
-    return myCurrentPreviousInputNodesByMappingName.get(mappingName);
-  }
-
-  public RuleConsequence getConsequenceForSwitchCase(SNode inputNode, TemplateSwitch templateSwitch) throws GenerationFailureException {
-    AbstractConceptDeclaration inputNodeConcept = inputNode.getConceptDeclarationAdapter();
-
-    if (myTemplateSwitchGraph == null) {
-      myTemplateSwitchGraph = new TemplateSwitchGraph(getGeneratorSessionContext().getTemplateModels());
-      myTemplateSwitchToListCache = new HashMap<TemplateSwitch, List<TemplateSwitch>>();
-    }
-
-    List<TemplateSwitch> switches = myTemplateSwitchToListCache.get(templateSwitch);
-    if (switches == null) {
-      switches = myTemplateSwitchGraph.getSubgraphAsList(templateSwitch);
-      myTemplateSwitchToListCache.put(templateSwitch, switches);
-    }
-
-    // for each template switch test conditions and choose template node
-    for (TemplateSwitch aSwitch : switches) {
-      List<Reduction_MappingRule> rules = aSwitch.getReductionMappingRules();
-      for (Reduction_MappingRule rule : rules) {
-        if (GeneratorUtil.checkPremiseForBaseMappingRule(inputNode, inputNodeConcept, rule, this)) {
-          RuleConsequence ruleConsequence = rule.getRuleConsequence();
-          if (ruleConsequence == null) {
-            showErrorMessage(inputNode, null, rule.getNode(), "couldn't apply reduction: no rule consequence");
-          }
-          return ruleConsequence;
-        }
-      }
-
-      // default
-      RuleConsequence ruleConsequence = aSwitch.getDefaultConsequence();
-      if (ruleConsequence != null) {
-        return ruleConsequence;
-      }
-    }
-
-    return null;
-  }
-
-  public SNode findOutputNodeById(SNodeId nodeId) {
-    return myOutputModel.getNodeById(nodeId);
-  }
-
-  public SNode findInputNodeById(SNodeId nodeId) {
-    return myInputModel.getNodeById(nodeId);
-  }
-
   public boolean isChanged() {
     return myChanged;
   }
 
   public void setChanged(boolean b) {
     myChanged = b;
-  }
-
-  void registerRoot(SNode newroot, SNode old) {
-    myNewToOldRoot.put(newroot, old);
-  }
-
-  public SNode getInputRootForOutput(SNode node) {
-    return myNewToOldRoot.get(node);
   }
 }
