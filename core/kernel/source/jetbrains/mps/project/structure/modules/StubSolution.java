85a1e7d7772440e479c363f76333da6074a66d67
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2010-03-08 22:57:27 +0300

    stub solutions in language properties dialog (first variant)

--- core/kernel/source/jetbrains/mps/project/structure/modules/StubSolution.java
+++ core/kernel/source/jetbrains/mps/project/structure/modules/StubSolution.java
@@ -3,6 +3,9 @@ package jetbrains.mps.project.structure.modules;
 import jetbrains.mps.project.ModuleId;
 
 public class StubSolution {
+  public static final String ID = "id";
+  public static final String NAME = "name";
+
   private ModuleId myId;
   private String myName;
 
@@ -20,5 +23,12 @@ public class StubSolution {
 
   public void setName(String name) {
     myName = name;
+  }
+
+  public StubSolution getCopy() {
+    StubSolution result = new StubSolution();
+    result.myName = myName;
+    result.myId = myId;
+    return result;
   }
 }
