4cfb63a616a506a5f4e5fd1b8c61817bd013f0c5
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2010-04-03 13:11:25 +0400

    regenerate

--- platform/buildlanguage/languages/packaging/source_gen/jetbrains/mps/build/packaging/behavior/Variable_Behavior.java
+++ platform/buildlanguage/languages/packaging/source_gen/jetbrains/mps/build/packaging/behavior/Variable_Behavior.java
@@ -81,7 +81,7 @@ __switch__:
                     default:
                       break __loop__;
                   }
-                } while(true);
+                } while (true);
                 return false;
               }
             };
