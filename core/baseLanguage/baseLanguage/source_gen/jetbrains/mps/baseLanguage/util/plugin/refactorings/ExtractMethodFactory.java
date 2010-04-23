2f1123bcb2c60bda26a2075bbcbfd25af91aebb4
Gleb Leonov <Gleb.Leonov@jetbrains.com>
2010-04-23 12:39:32 +0400

    fix tests

--- core/baseLanguage/baseLanguage/source_gen/jetbrains/mps/baseLanguage/util/plugin/refactorings/ExtractMethodFactory.java
+++ core/baseLanguage/baseLanguage/source_gen/jetbrains/mps/baseLanguage/util/plugin/refactorings/ExtractMethodFactory.java
@@ -7,17 +7,6 @@ import jetbrains.mps.smodel.SNode;
 import jetbrains.mps.internal.collections.runtime.ListSequence;
 import jetbrains.mps.lang.smodel.generator.smodelAdapter.SNodeOperations;
 import java.util.ArrayList;
-import jetbrains.mps.lang.smodel.generator.smodelAdapter.SLinkOperations;
-import java.util.Set;
-import java.util.HashSet;
-import jetbrains.mps.smodel.SModelUtil_new;
-import jetbrains.mps.typesystem.inference.TypeChecker;
-import jetbrains.mps.project.GlobalScope;
-import jetbrains.mps.smodel.SReference;
-import jetbrains.mps.smodel.SModelReference;
-import jetbrains.mps.smodel.SNodeId;
-import jetbrains.mps.smodel.CopyUtil;
-import jetbrains.mps.lang.typesystem.runtime.HUtil;
 
 public class ExtractMethodFactory {
   public static ExtractMethodRefactoringParameters createParameters(List<SNode> nodes) {
@@ -72,44 +61,5 @@ public class ExtractMethodFactory {
       }
     }
     return null;
-  }
-
-  public static void temp(SNode node) {
-    List<SNode> a = SLinkOperations.getTargets(SNodeOperations.cast(SLinkOperations.getTarget(node, "creator", true), "jetbrains.mps.baseLanguage.structure.ClassCreator"), "actualArgument", true);
-    SLinkOperations.getTargets(SNodeOperations.cast(SLinkOperations.getTarget(node, "creator", true), "jetbrains.mps.baseLanguage.structure.ClassCreator"), "actualArgument", true);
-    SNodeOperations.replaceWithAnother(node, new ExtractMethodFactory.QuotationClass_v5nwg4_a0a0c0e().createNode(ListSequence.fromList(a).getElement(0)));
-  }
-
-  public static class QuotationClass_v5nwg4_a0a0c0e {
-    public QuotationClass_v5nwg4_a0a0c0e() {
-    }
-
-    public SNode createNode(Object parameter_5) {
-      SNode result = null;
-      Set<SNode> _parameterValues_129834374 = new HashSet<SNode>();
-      SNode quotedNode_1 = null;
-      SNode quotedNode_2 = null;
-      {
-        quotedNode_1 = SModelUtil_new.instantiateConceptDeclaration("jetbrains.mps.baseLanguage.structure.StaticMethodCall", TypeChecker.getInstance().getRuntimeTypesModel(), GlobalScope.getInstance(), false);
-        SNode quotedNode1_3 = quotedNode_1;
-        quotedNode1_3.addReference(SReference.create("baseMethodDeclaration", quotedNode1_3, SModelReference.fromString("r:6f239204-0a4e-4aeb-b636-582aa7e86328(jetbrains.mps.baseLanguage.util.plugin.refactorings@0_1)"), SNodeId.fromString("8155843501977598856")));
-        quotedNode1_3.addReference(SReference.create("classConcept", quotedNode1_3, SModelReference.fromString("r:6f239204-0a4e-4aeb-b636-582aa7e86328(jetbrains.mps.baseLanguage.util.plugin.refactorings@0_1)"), SNodeId.fromString("8492459591399162714")));
-        {
-          quotedNode_2 = (SNode) parameter_5;
-          SNode quotedNode1_4;
-          if (_parameterValues_129834374.contains(quotedNode_2)) {
-            quotedNode1_4 = CopyUtil.copy(quotedNode_2);
-          } else {
-            _parameterValues_129834374.add(quotedNode_2);
-            quotedNode1_4 = quotedNode_2;
-          }
-          if (quotedNode1_4 != null) {
-            quotedNode_1.addChild("actualArgument", HUtil.copyIfNecessary(quotedNode1_4));
-          }
-        }
-        result = quotedNode1_3;
-      }
-      return result;
-    }
   }
 }
