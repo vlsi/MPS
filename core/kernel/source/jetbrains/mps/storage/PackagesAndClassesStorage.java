622e77ad0ad32f4d098e4e345796e0f226ff1c6c
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2010-04-30 21:06:47 +0400

    working on storage

--- core/kernel/source/jetbrains/mps/storage/PackagesAndClassesStorage.java
+++ core/kernel/source/jetbrains/mps/storage/PackagesAndClassesStorage.java
@@ -20,6 +20,10 @@ public final class PackagesAndClassesStorage extends PreifxStringStorage {
     return obj.getChildStrings();
   }
 
+  public StringObject getParent(StringObject obj){
+    return obj.getParent();
+  }
+
   public void addClasses(String pack, Set<String> names){
     StringObject node = get(pack);
     for (String name:names){
