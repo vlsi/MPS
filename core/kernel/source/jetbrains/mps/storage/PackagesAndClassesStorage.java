4af424291529f2719ff05d36dbc8709d676ca7d9
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2010-04-30 20:34:22 +0400

    PackagesAndClassesStorage

---
+++ core/kernel/source/jetbrains/mps/storage/PackagesAndClassesStorage.java
@@ -0,0 +1,29 @@
+package jetbrains.mps.storage;
+
+import java.util.Collection;
+import java.util.Set;
+
+public final class PackagesAndClassesStorage extends PreifxStringStorage {
+  private static PackagesAndClassesStorage ourInstance = new PackagesAndClassesStorage();
+
+  public static PackagesAndClassesStorage getInstance() {
+    return ourInstance;
+  }
+
+  private PackagesAndClassesStorage() {
+    super('.');
+  }
+
+  //---------------------------
+
+  public Collection<String> getChildStrings(StringObject obj){
+    return obj.getChildStrings();
+  }
+
+  public void addClasses(String pack, Set<String> names){
+    StringObject node = get(pack);
+    for (String name:names){
+      node.getChild(name);
+    }
+  }
+}
