296fd5fed82466d9b4de4d3b6b0308b2e7ddd7ce
Cyril Konopko <Cyril.Konopko@jetbrains.com>
2010-05-01 16:51:09 +0400

    gdb breakpoints in progress

--- samples/nanoc/source/jetbrains/mps/nanoc/debug/breakpoints/GDBBreakpoint.java
+++ samples/nanoc/source/jetbrains/mps/nanoc/debug/breakpoints/GDBBreakpoint.java
@@ -6,9 +6,13 @@ import jetbrains.mps.debug.api.AbstractMPSBreakpoint;
 import jetbrains.mps.debug.api.DebugSessionManagerComponent;
 import jetbrains.mps.nanoc.debug.CppDebugSession;
 import jetbrains.mps.nanoc.debug.CppDebugSession.DebugSessionAction;
+import jetbrains.mps.nanoc.debug.answer.GDBValue;
+import jetbrains.mps.nanoc.debug.answer.RecordValue;
 import jetbrains.mps.nanoc.debug.answer.ResultAnswer;
+import jetbrains.mps.nanoc.debug.answer.StringValue;
 import jetbrains.mps.nanoc.debug.requests.BreakpointRequestor;
 import jetbrains.mps.nanoc.debug.requests.GDBRequestManager;
+import jetbrains.mps.nanoc.debug.requests.RemoveBreakpointRequestor;
 import jetbrains.mps.smodel.SNodePointer;
 
 import java.util.Set;
@@ -22,6 +26,10 @@ import java.util.Set;
 public class GDBBreakpoint extends AbstractMPSBreakpoint {
   BreakpointRequestor myBreakpointRequestor;
   boolean myAdded = false;
+  int myInternalGDBNumber = -1;
+
+  private static final String BKPT = "bkpt";
+  private static final String NUMBER = "number";
 
   protected GDBBreakpoint(SNodePointer nodePointer, Project project) {
     super(nodePointer, project);
@@ -52,16 +60,20 @@ public class GDBBreakpoint extends AbstractMPSBreakpoint {
       @Override
       public void onRequestFulfilled(ResultAnswer answer) {
         myAdded = true;
+        RecordValue bkptInfo = (RecordValue) answer.getResults().getPropertyValue(BKPT);
+        StringValue numberValue = (StringValue) bkptInfo.getPropertyValue(NUMBER);
+        int number = Integer.parseInt(numberValue.getString());
+        myInternalGDBNumber = number;
       }
     });
   }
 
   public void createRemoveBreakpointRequest(GDBRequestManager requestManager) {
-    //todo
-    requestManager.createRequest(new BreakpointRequestor(getFileName(), getLineIndexInFile()) {
+    requestManager.createRequest(new RemoveBreakpointRequestor(myInternalGDBNumber) {
       @Override
       public void onRequestFulfilled(ResultAnswer answer) {
         myAdded = false;
+        myInternalGDBNumber = -1;
       }
     });
   }
