a18920344b50c542099e3e72b553a36ceb971a75
Evgeny Gerashchenko <Eugene.Gerashchenko@jetbrains.com>
2010-05-06 20:38:33 +0400

    Fixed MPS-8634: "Surround with parenthesis" intention is not working (IllegalModelAccessError)

--- workbench/source_gen/jetbrains/mps/ide/actions/SurroundWithIntentions_Action.java
+++ workbench/source_gen/jetbrains/mps/ide/actions/SurroundWithIntentions_Action.java
@@ -143,7 +143,11 @@ public class SurroundWithIntentions_Action extends GeneratedAction {
     for (final Pair<Intention, SNode> pair : groupItems) {
       BaseAction action = new BaseAction(pair.getFirst().getDescription(pair.getSecond(), SurroundWithIntentions_Action.this.editorContext)) {
         protected void doExecute(AnActionEvent p0) {
-          pair.getFirst().execute(pair.getSecond(), SurroundWithIntentions_Action.this.editorContext);
+          ModelAccess.instance().runWriteActionInCommand(new Runnable() {
+            public void run() {
+              pair.getFirst().execute(pair.getSecond(), SurroundWithIntentions_Action.this.editorContext);
+            }
+          });
         }
       };
       action.setExecuteOutsideCommand(true);
