5879c7c31f9a92ac492987a3bdab780992090ebe
Evgeny Gerashchenko <Eugene.Gerashchenko@jetbrains.com>
2010-05-31 15:57:11 +0400

    Fixed: MPS-8984    NPE in Version Control Dialog

--- core/kernel/source/jetbrains/mps/vcs/ui/VcsGeneratedFilesConfigurable.java
+++ core/kernel/source/jetbrains/mps/vcs/ui/VcsGeneratedFilesConfigurable.java
@@ -6,23 +6,24 @@ import com.intellij.openapi.project.Project;
 import jetbrains.mps.vcs.MPSVcsProjectConfiguration;
 import org.jetbrains.annotations.Nls;
 
-import javax.swing.*;
+import javax.swing.Icon;
+import javax.swing.JCheckBox;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
 import javax.swing.border.TitledBorder;
 import java.awt.BorderLayout;
 
 public class VcsGeneratedFilesConfigurable extends JPanel implements Configurable {
-  private MPSVcsProjectConfiguration myConfiguration;
-
-  private JCheckBox myIgnoreGeneratedFilesCheckBox;
+  private JCheckBox myIgnoreGeneratedFilesCheckBox = new JCheckBox("Ignore generated files");
+  private Project myProject;
 
   public VcsGeneratedFilesConfigurable(Project project) {
-    myConfiguration = project.getComponent(MPSVcsProjectConfiguration.class);
+    myProject = project;
 
     setLayout(new BorderLayout());
 
     JPanel generatedFilesPanel = new JPanel(new BorderLayout());
     generatedFilesPanel.setBorder(new TitledBorder("Generated Files"));
-    myIgnoreGeneratedFilesCheckBox = new JCheckBox("Ignore generated files");
     generatedFilesPanel.add(myIgnoreGeneratedFilesCheckBox);
 
     add(generatedFilesPanel, BorderLayout.NORTH);
@@ -51,20 +52,24 @@ public class VcsGeneratedFilesConfigurable extends JPanel implements Configurabl
 
   @Override
   public boolean isModified() {
-    return myIgnoreGeneratedFilesCheckBox.isSelected() != myConfiguration.isIgnoreGeneratedFiles();
+    return myIgnoreGeneratedFilesCheckBox.isSelected() != getConfiguration().isIgnoreGeneratedFiles();
   }
 
   @Override
   public void apply() throws ConfigurationException {
-    myConfiguration.setIgnoreGeneratedFiles(myIgnoreGeneratedFilesCheckBox.isSelected());
+    getConfiguration().setIgnoreGeneratedFiles(myIgnoreGeneratedFilesCheckBox.isSelected());
   }
 
   @Override
   public void reset() {
-    myIgnoreGeneratedFilesCheckBox.setSelected(myConfiguration.isIgnoreGeneratedFiles());
+    myIgnoreGeneratedFilesCheckBox.setSelected(getConfiguration().isIgnoreGeneratedFiles());
   }
 
   @Override
   public void disposeUIResources() {
+  }
+
+  private MPSVcsProjectConfiguration getConfiguration() {
+    return myProject.getComponent(MPSVcsProjectConfiguration.class);
   }
 }
