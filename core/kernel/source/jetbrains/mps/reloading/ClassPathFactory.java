c6f2682d3c23efa75464dc2425cdcd062cca28f7
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2010-06-16 21:48:45 +0400

    code issues

--- core/kernel/source/jetbrains/mps/reloading/ClassPathFactory.java
+++ core/kernel/source/jetbrains/mps/reloading/ClassPathFactory.java
@@ -16,46 +16,9 @@ public class ClassPathFactory {
   private static final Logger LOG = Logger.getLogger(ClassPathFactory.class);
   private static final ClassPathFactory ourInstance = new ClassPathFactory();
 
-  @UseCarefully
-  //this is supposed to be used only on class reloading
-  public void update() {
-    for (RealClassPathItem p : myCache.values()) {
-      p.invalidate();
-    }
-    myCache.clear();
-  }
-
-  @UseCarefully
-  //this should be used only from modules to invalidate their classpaths
-  public void invalidate(Set<String> paths) {
-    for (String path : paths) {
-      if (myCache.containsKey(path)) {
-        myCache.remove(path).invalidate();
-      }
-    }
-  }
-
   public static ClassPathFactory getInstance() {
     return ourInstance;
   }
-
-  //--------------------------
-
-  private Map<String, RealClassPathItem> myCache = new HashMap<String, RealClassPathItem>();
-
-  private RealClassPathItem get(String path) {
-    if (!myCache.containsKey(path)) {
-      if (FileSystem.getFile(path).isDirectory()) {
-        myCache.put(path, new FileClassPathItem(path));
-      } else {
-        myCache.put(path, new JarFileClassPathItem(path));
-      }
-    }
-
-    return myCache.get(path);
-  }
-
-  //--------------------------
 
   public RealClassPathItem createFromPath(String path) {
     try {
@@ -76,5 +39,42 @@ public class ClassPathFactory {
     }
 
     return getInstance().get(path);
+  }
+
+  //--------------------------
+
+  @UseCarefully
+  //this is supposed to be used only on class reloading
+  public void update() {
+    for (RealClassPathItem p : myCache.values()) {
+      p.invalidate();
+    }
+    myCache.clear();
+  }
+
+  @UseCarefully
+  //this should be used only from modules to invalidate their classpaths
+  public void invalidate(Set<String> paths) {
+    for (String path : paths) {
+      if (myCache.containsKey(path)) {
+        myCache.remove(path).invalidate();
+      }
+    }
+  }
+
+  //--------------------------
+
+  private Map<String, RealClassPathItem> myCache = new HashMap<String, RealClassPathItem>();
+
+  private RealClassPathItem get(String path) {
+    if (!myCache.containsKey(path)) {
+      if (FileSystem.getFile(path).isDirectory()) {
+        myCache.put(path, new FileClassPathItem(path));
+      } else {
+        myCache.put(path, new JarFileClassPathItem(path));
+      }
+    }
+
+    return myCache.get(path);
   }
 }
