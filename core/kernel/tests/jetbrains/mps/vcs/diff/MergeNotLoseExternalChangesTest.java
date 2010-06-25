1e3c9759b15be4dd708c124fbab3f404abf1f8f9
Evgeny Gerashchenko <Eugene.Gerashchenko@jetbrains.com>
2010-06-25 16:27:51 +0400

    minor

--- core/kernel/tests/jetbrains/mps/vcs/diff/MergeNotLoseExternalChangesTest.java
+++ core/kernel/tests/jetbrains/mps/vcs/diff/MergeNotLoseExternalChangesTest.java
@@ -19,7 +19,7 @@ import jetbrains.mps.vcs.diff.ui.ModelDiffTool.ReadException;
 
 import java.io.IOException;
 
-public class MergeNotLooseExternalChangesTest extends MergeTestCase{
+public class MergeNotLoseExternalChangesTest extends MergeTestCase{
   public void testOneSimpleChangeFromRepo() throws IOException, ReadException {
     testZip("resources/repositoryChanges.mps.zip", false);
   }
