02ffe5344c2f06097641677d5cf083ffe58820e8
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2010-08-11 19:24:23 +0400

    fix MPS-9820: some classes are not compiled at startup

--- core/kernel/source/jetbrains/mps/compiler/JavaCompiler.java
+++ core/kernel/source/jetbrains/mps/compiler/JavaCompiler.java
@@ -35,8 +35,6 @@ import java.util.*;
 public class JavaCompiler {
   private static final Logger LOG = Logger.getLogger(JavaCompiler.class);
 
-  private static final int MAX_RESULTS = 100;
-
   private Map<String, CompilationUnit> myCompilationUnits = new HashMap<String, CompilationUnit>();
   private List<CompilationResult> myCompilationResults = new LinkedList<CompilationResult>();
   private Map<String, byte[]> myClasses = new HashMap<String, byte[]>();
@@ -182,9 +180,7 @@ public class JavaCompiler {
         myClasses.put(name, file.getBytes());
       }
 
-      if (myCompilationResults.size() <= MAX_RESULTS){
-        myCompilationResults.add(result);
-      }
+      myCompilationResults.add(result);
     }
   }
 }
