ef8905e75b39dc91a6a247a2db39f865067237cc
Evgeny Gryaznov <Evgeny.Gryaznov@jetbrains.com>
2010-09-09 19:57:13 +0200

    optimize imports

--- core/kernel/source/jetbrains/mps/smodel/DefaultModelAccess.java
+++ core/kernel/source/jetbrains/mps/smodel/DefaultModelAccess.java
@@ -15,17 +15,10 @@
  */
 package jetbrains.mps.smodel;
 
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.command.CommandProcessor;
 import com.intellij.openapi.command.UndoConfirmationPolicy;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Computable;
 import jetbrains.mps.InternalFlag;
-import jetbrains.mps.ide.ThreadUtils;
-
-import javax.swing.SwingUtilities;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Evgeny Gryaznov, Sep 3, 2010
--- core/kernel/source/jetbrains/mps/vcs/GlobalClassPathIndex.java
+++ core/kernel/source/jetbrains/mps/vcs/GlobalClassPathIndex.java
@@ -15,16 +15,11 @@
  */
 package jetbrains.mps.vcs;
 
-import com.intellij.ide.projectView.ProjectView;
-import com.intellij.ide.projectView.impl.AbstractProjectViewPane;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ApplicationComponent;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.vcs.FilePath;
 import com.intellij.openapi.vcs.actions.VcsContextFactory;
 import com.intellij.openapi.vfs.VirtualFile;
-import jetbrains.mps.ide.projectPane.fileSystem.FileViewProjectPane;
 import jetbrains.mps.logging.Logger;
 import jetbrains.mps.project.IModule;
 import jetbrains.mps.reloading.EachClassPathItemVisitor;
