6543f495a6103f031cb1277ef74ac9cfda54d838
Ilya Lintsbah <Ilya.Lintsbakh@jetbrains.com>
2010-09-10 19:04:45 +0400

    Started working on new typesystem

---
+++ core/typesystemEngine/source/jetbrains/mps/newTypesystem/NodesToTypes.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2003-2010 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jetbrains.mps.newTypesystem;
+
+import jetbrains.mps.smodel.SNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Ilya.Lintsbakh
+ * Date: Sep 10, 2010
+ * Time: 6:38:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class NodesToTypes {
+  private Map<SNode, SNode> myNodeToTypes = new HashMap<SNode, SNode>();
+
+  public void rollBack(Map<SNode, SNode> difference) {
+    for (SNode node :difference.keySet()) {
+      myNodeToTypes.remove(node);
+    }
+  }
+}
---
+++ core/typesystemEngine/source/jetbrains/mps/newTypesystem/State.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2003-2010 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jetbrains.mps.newTypesystem;
+
+
+import jetbrains.mps.smodel.SNode;
+import jetbrains.mps.typesystem.inference.EquationInfo;
+import jetbrains.mps.typesystem.inference.IWrapper;
+
+import java.util.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Ilya.Lintsbakh
+ * Date: Sep 10, 2010
+ * Time: 6:09:38 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class State {
+  private Equations myEquations = new Equations();
+  private Inequalities myInequalities = new Inequalities();
+  private NodesToTypes myNodesToTypes = new NodesToTypes();
+
+  public void rollBack(Difference difference) {
+    myEquations.rollBack();
+
+    myNodesToTypes.rollBack(difference.getNodeToTypes());
+  }
+
+  public Equations getEquations() {
+    return myEquations;
+  }
+
+  public Difference addEquation(IWrapper left, IWrapper right, EquationInfo info) {
+    return null;
+  }
+
+}
