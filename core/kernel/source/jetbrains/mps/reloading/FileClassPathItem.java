9298584db99f7256e31def6abcc6e29807c1959a
Evgeny Gerashchenko <Eugene.Gerashchenko@jetbrains.com>
2010-09-20 14:33:46 +0400

    minor

--- core/kernel/source/jetbrains/mps/reloading/FileClassPathItem.java
+++ core/kernel/source/jetbrains/mps/reloading/FileClassPathItem.java
@@ -15,13 +15,13 @@
  */
 package jetbrains.mps.reloading;
 
+import jetbrains.mps.project.MPSExtentions;
 import jetbrains.mps.stubs.javastub.classpath.ClassifierKind;
 import jetbrains.mps.util.InternUtil;
 import jetbrains.mps.util.NameUtil;
 import jetbrains.mps.util.ReadUtil;
 import jetbrains.mps.vfs.FileSystem;
 import jetbrains.mps.vfs.IFile;
-import jetbrains.mps.vfs.MPSExtentions;
 
 import java.io.File;
 import java.io.IOException;
@@ -63,7 +63,7 @@ public class FileClassPathItem extends RealClassPathItem {
     }
 
     String path = myClassPath + File.separatorChar + name.replace('.', File.separatorChar) + MPSExtentions.DOT_CLASSFILE;
-    IFile file = FileSystem.getFile(path);
+    IFile file = FileSystem.getInstance().getFileByPath(path);
     try {
       byte[] result = null;
       InputStream inp = null;
@@ -84,7 +84,7 @@ public class FileClassPathItem extends RealClassPathItem {
 
   public ClassifierKind getClassifierKind(String name) {
     String path = myClassPath + File.separatorChar + name.replace('.', File.separatorChar) + MPSExtentions.DOT_CLASSFILE;
-    IFile file = FileSystem.getFile(path);
+    IFile file = FileSystem.getInstance().getFileByPath(path);
     try {
       InputStream inp = null;
       try {
@@ -103,7 +103,7 @@ public class FileClassPathItem extends RealClassPathItem {
   public URL getResource(String name) {
     checkValidity();
     try {
-      IFile resourceFile = FileSystem.getFile(myClassPath + File.separator + name.replace('/', File.separatorChar));
+      IFile resourceFile = FileSystem.getInstance().getFileByPath(myClassPath + File.separator + name.replace('/', File.separatorChar));
       if (!resourceFile.exists()) return null;
       return resourceFile.toURL();
     } catch (MalformedURLException e) {
@@ -195,7 +195,7 @@ public class FileClassPathItem extends RealClassPathItem {
   public IFile getModelDir(String namespace) {
     checkValidity();
     if (namespace == null) namespace = "";
-    return FileSystem.getFile(myClassPath + File.separatorChar + namespace.replace('.', File.separatorChar));
+    return FileSystem.getInstance().getFileByPath(myClassPath + File.separatorChar + namespace.replace('.', File.separatorChar));
   }
 
 
