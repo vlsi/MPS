7577b3f7ce25f3d587db4fc73a21a67cf7ceab7a
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2010-09-29 17:11:02 +0400

    MPSMatcher rewritten

--- workbench/source/jetbrains/mps/workbench/actions/goTo/matcher/MPSMatcher.java
+++ workbench/source/jetbrains/mps/workbench/actions/goTo/matcher/MPSMatcher.java
@@ -16,79 +16,20 @@
 package jetbrains.mps.workbench.actions.goTo.matcher;
 
 import com.intellij.ide.util.gotoByName.ChooseByNameModel;
+import com.intellij.ide.util.gotoByName.matchers.DefaultMatcher;
 import com.intellij.ide.util.gotoByName.matchers.EntityMatcher;
-import com.intellij.openapi.progress.ProcessCanceledException;
 import com.intellij.openapi.util.Computable;
-import com.intellij.util.SmartList;
-import jetbrains.mps.ide.ChooseItemComponent;
-import jetbrains.mps.workbench.choose.base.BaseMPSChooseModel;
+import jetbrains.mps.workbench.choose.base.FakePsiContext;
 
-import java.util.*;
+import java.util.Set;
 
-public abstract class MPSMatcher implements EntityMatcher {
-  private ChooseByNameModel myModel;
-
+public abstract class MPSMatcher extends DefaultMatcher implements EntityMatcher {
   protected MPSMatcher(ChooseByNameModel model) {
-    myModel = model;
+    super(model, new FakePsiContext());
   }
 
   public boolean addElementsByPattern(Set<Object> result, String pattern, String[] names, boolean checkboxState, int maxCount, Computable<Boolean> isCancelled) {
-    if (pattern.length() == 0 && !canShowListForEmptyPattern()) return false;
-
-    List<String> namesList = new ArrayList<String>();
-    getNamesByPattern(namesList, pattern, names, isCancelled);
-    if (isCancelled.compute()) {
-      throw new ProcessCanceledException();
-    }
-    Collections.sort(namesList, new MatchesComparator(pattern));
-
-    boolean overflow = false;
-    All:
-    for (String name : namesList) {
-      if (isCancelled.compute()) {
-        throw new ProcessCanceledException();
-      }
-
-      final Object[] elements = myModel.getElementsByName(name, checkboxState, name);
-
-      List<Object> sameNameElements = new SmartList<Object>();
-      sameNameElements.addAll(Arrays.asList(elements));
-
-      //sortByProximity(sameNameElements);
-      for (Object element : sameNameElements) {
-        result.add(element);
-        if (result.size() >= maxCount) {
-          overflow = true;
-          break All;
-        }
-      }
-    }
-
-    return overflow;
-  }
-
-  protected abstract boolean canShowListForEmptyPattern();
-
-  public void getNamesByPattern(List<String> list, String pattern, String[] names, Computable<Boolean> isCancelled) {
-    boolean isMatchesSomething = false;
-    java.util.regex.Matcher itemMatcher = getItemPattern(pattern).matcher("");
-    for (String name : names) {
-      if (name == null) continue;
-      itemMatcher.reset(name);
-      if (itemMatcher.matches()) {
-        if (!isMatchesSomething) isMatchesSomething = true;
-        list.add(name);
-      }
-    }
-  }
-
-  private java.util.regex.Pattern getItemPattern(String pattern) {
-    StringBuilder b = getExactItemPatternBuilder(pattern);
-    b.append(".*");
-    return java.util.regex.Pattern.compile(b.toString());
-  }
-
-  private StringBuilder getExactItemPatternBuilder(String text) {
-    return ChooseItemComponent.getExactItemPatternBuilder(text, true);
+    String newPattern = pattern.endsWith(" ") ? pattern : pattern + ".*";
+    return super.addElementsByPattern(result, newPattern, names, checkboxState, maxCount, isCancelled);
   }
 }
