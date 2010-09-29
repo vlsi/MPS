9264739e1a3aee3ede330dac814fe9396bf71538
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2010-09-29 20:20:12 +0400

    re-patched files for GotoFileCellRenderer

--- core/idea-patch/source/com/intellij/ide/util/PsiElementListCellRenderer.java
+++ core/idea-patch/source/com/intellij/ide/util/PsiElementListCellRenderer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2010 JetBrains s.r.o.
+ * Copyright 2000-2009 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,27 +21,28 @@ import com.intellij.navigation.ItemPresentation;
 import com.intellij.navigation.NavigationItem;
 import com.intellij.openapi.editor.colors.EditorColorsManager;
 import com.intellij.openapi.editor.colors.TextAttributesKey;
+import com.intellij.openapi.editor.markup.EffectType;
 import com.intellij.openapi.editor.markup.TextAttributes;
 import com.intellij.openapi.ui.popup.PopupChooserBuilder;
+import com.intellij.openapi.vcs.FileStatus;
+import com.intellij.openapi.vcs.FileStatusManager;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.problems.WolfTheProblemSolver;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
 import com.intellij.ui.ColoredListCellRenderer;
+import com.intellij.ui.FileColorManager;
 import com.intellij.ui.ListSpeedSearch;
 import com.intellij.ui.SimpleTextAttributes;
 import com.intellij.util.Function;
 import com.intellij.util.IconUtil;
 import com.intellij.util.ui.UIUtil;
-import jetbrains.mps.util.annotation.Patch;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Font;
+import java.awt.*;
 import java.util.Comparator;
-
-import jetbrains.mps.ide.icons.IconManager;
-import jetbrains.mps.util.annotation.Patch;
 
 public abstract class PsiElementListCellRenderer<T extends PsiElement> extends JPanel implements ListCellRenderer {
   protected PsiElementListCellRenderer() {
@@ -55,68 +56,81 @@ public abstract class PsiElementListCellRenderer<T extends PsiElement> extends J
       myModuleName = moduleName;
     }
 
-    @Patch
-    protected void customizeCellRenderer(
-      JList list,
-      Object value,
-      int index,
-      boolean selected,
-      boolean hasFocus
-    ) {
+    protected void customizeCellRenderer(JList list, Object value, int index, boolean selected, boolean hasFocus) {
       Color bgColor = UIUtil.getListBackground();
       Color color = list.getForeground();
-      boolean isProblemFile = false;
+      if (value instanceof PsiElement) {
+        T element = (T)value;
+        String name = getElementText(element);
+        PsiFile psiFile = element.getContainingFile();
+        boolean isProblemFile = false;
 
-      if (!(value instanceof NavigationItem)) {
+        if (psiFile != null) {
+          VirtualFile vFile = psiFile.getVirtualFile();
+          if (vFile != null) {
+            if (WolfTheProblemSolver.getInstance(psiFile.getProject()).isProblemFile(vFile)) {
+              isProblemFile = true;
+            }
+            FileStatus status = FileStatusManager.getInstance(psiFile.getProject()).getStatus(vFile);
+            color = status.getColor();
+
+            final FileColorManager colorManager = FileColorManager.getInstance(psiFile.getProject());
+            if (colorManager.isEnabled()) {
+              final Color fileBgColor = colorManager.getRendererBackground(psiFile);
+              bgColor = fileBgColor == null ? bgColor : fileBgColor;
+            }
+          }
+        }
+
+        TextAttributes attributes = null;
+
+        if (value instanceof NavigationItem) {
+          TextAttributesKey attributesKey = null;
+          final ItemPresentation presentation = ((NavigationItem)value).getPresentation();
+          if (presentation != null) attributesKey = presentation.getTextAttributesKey();
+
+          if (attributesKey != null) {
+            attributes = EditorColorsManager.getInstance().getGlobalScheme().getAttributes(attributesKey);
+          }
+        }
+
+        if (isProblemFile) {
+          attributes = TextAttributes.merge(new TextAttributes(color, null, Color.red, EffectType.WAVE_UNDERSCORE, Font.PLAIN), attributes);
+        }
+
+        SimpleTextAttributes nameAttributes = attributes != null ? SimpleTextAttributes.fromTextAttributes(attributes) : null;
+
+        if (nameAttributes == null) nameAttributes = new SimpleTextAttributes(Font.PLAIN, color);
+
+        assert name != null : "Null name for PSI element " + element;
+        append(name, nameAttributes);
+        setIcon(PsiElementListCellRenderer.this.getIcon(element));
+
+        String containerText = getContainerText(element, name + (myModuleName != null ? myModuleName + "        " : ""));
+        if (containerText != null) {
+          append(" " + containerText, new SimpleTextAttributes(Font.PLAIN, Color.GRAY));
+        }
+      }
+      else if (!customizeLeftRenderer(this, list, value, index, selected, hasFocus)) {
         setIcon(IconUtil.getEmptyIcon(false));
         append(value == null ? "" : value.toString(), new SimpleTextAttributes(Font.PLAIN, list.getForeground()));
-        return;
       }
-
-      NavigationItem item = (NavigationItem) value;
-
-      TextAttributes attributes = null;
-
-      TextAttributesKey attributesKey = null;
-      final ItemPresentation presentation = ((NavigationItem) value).getPresentation();
-      if (presentation != null) attributesKey = presentation.getTextAttributesKey();
-
-      if (attributesKey != null) {
-        attributes = EditorColorsManager.getInstance().getGlobalScheme().getAttributes(attributesKey);
-      }
-
-      SimpleTextAttributes nameAttributes = attributes != null ? SimpleTextAttributes.fromTextAttributes(attributes) : null;
-
-      if (nameAttributes == null) nameAttributes = new SimpleTextAttributes(Font.PLAIN, color);
-
-      append(item + " ", nameAttributes);
-      ItemPresentation itemPresentation = item.getPresentation();
-      assert itemPresentation != null;
-      setIcon(itemPresentation.getIcon(true));
-
-      append(itemPresentation.getLocationString(), new SimpleTextAttributes(Font.PLAIN, Color.GRAY));
-
       setPaintFocusBorder(false);
       setBackground(selected ? UIUtil.getListSelectionBackground() : bgColor);
     }
+
   }
 
-  public Component getListCellRendererComponent(JList list,
-                                                Object value,
-                                                int index,
-                                                boolean isSelected,
-                                                boolean cellHasFocus) {
+  public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
     removeAll();
-    String moduleName = null;
     DefaultListCellRenderer rightRenderer = getRightCellRenderer();
-    final Component leftCellRendererComponent =
-      new LeftRenderer(moduleName).getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+    ListCellRenderer leftRenderer = new LeftRenderer(null);
+    final Component leftCellRendererComponent = leftRenderer.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
     if (rightRenderer != null) {
-      final Component rightCellRendererComponent =
-        rightRenderer.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
-      rightCellRendererComponent.setBackground(isSelected ? UIUtil.getListSelectionBackground() : leftCellRendererComponent.getBackground());
+      final Component rightCellRendererComponent = rightRenderer.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+      rightCellRendererComponent
+        .setBackground(isSelected ? UIUtil.getListSelectionBackground() : leftCellRendererComponent.getBackground());
       add(rightCellRendererComponent, BorderLayout.EAST);
-      moduleName = rightRenderer.getText();
       final JPanel spacer = new JPanel();
       spacer.setBorder(BorderFactory.createEmptyBorder(0, 2, 0, 2));
       spacer.setBackground(isSelected ? UIUtil.getListSelectionBackground() : leftCellRendererComponent.getBackground());
@@ -127,10 +141,24 @@ public abstract class PsiElementListCellRenderer<T extends PsiElement> extends J
     return this;
   }
 
+  protected boolean customizeLeftRenderer(ColoredListCellRenderer renderer,
+                                          JList list,
+                                          Object value,
+                                          int index,
+                                          boolean selected,
+                                          boolean hasFocus) {
+    return false;
+  }
+
   @Nullable
   protected DefaultListCellRenderer getRightCellRenderer() {
     if (UISettings.getInstance().SHOW_ICONS_IN_QUICK_NAVIGATION) {
-      return ModuleRendererFactory.getInstance().getModuleRenderer();
+      final DefaultListCellRenderer renderer = ModuleRendererFactory.getInstance().getModuleRenderer();
+      if (renderer instanceof PlatformModuleRendererFactory.PlatformModuleRenderer) {
+        // it won't display any new information
+        return null;
+      }
+      return renderer;
     }
     return null;
   }
@@ -149,23 +177,33 @@ public abstract class PsiElementListCellRenderer<T extends PsiElement> extends J
   public Comparator<T> getComparator() {
     return new Comparator<T>() {
       public int compare(T o1, T o2) {
-        return getText(o1).compareTo(getText(o2));
-      }
-
-      private String getText(T element) {
-        String elementText = getElementText(element);
-        String containerText = getContainerText(element, elementText);
-        return containerText != null ? elementText + " " + containerText : elementText;
+        return getComparingObject(o1).compareTo(getComparingObject(o2));
       }
     };
   }
 
+  @NotNull
+  public Comparable getComparingObject(T element) {
+    String elementText = getElementText(element);
+    String containerText = getContainerText(element, elementText);
+    return containerText != null ? elementText + " " + containerText : elementText;
+  }
+
   public void installSpeedSearch(PopupChooserBuilder builder) {
+    installSpeedSearch(builder, false);
+  }
+
+  public void installSpeedSearch(PopupChooserBuilder builder, final boolean includeContainerText) {
     builder.setFilteringEnabled(new Function<Object, String>() {
       public String fun(Object o) {
         if (o instanceof PsiElement) {
-          return PsiElementListCellRenderer.this.getElementText((T) o);
-        } else {
+          final String elementText = PsiElementListCellRenderer.this.getElementText((T)o);
+          if (includeContainerText) {
+            return elementText + " " + getContainerText((T)o, elementText);
+          }
+          return elementText;
+        }
+        else {
           return o.toString();
         }
       }
@@ -180,8 +218,10 @@ public abstract class PsiElementListCellRenderer<T extends PsiElement> extends J
     new ListSpeedSearch(list) {
       protected String getElementText(Object o) {
         if (o instanceof PsiElement) {
-          return PsiElementListCellRenderer.this.getElementText((T) o);
-        } else {
+          final String elementText = PsiElementListCellRenderer.this.getElementText((T)o);
+          return elementText + " " + getContainerText((T)o, elementText);
+        }
+        else {
           return o.toString();
         }
       }
