99c49b98339e1a8c6a10d866bb25e8bcbdfcd42f
Fedor Isakov <fisakov@gmail.com>
2010-10-18 10:44:27 +0200

    Revert "optimization: do not load all the model when only concept declaration is needed"
    This reverts commit 53b352e224fcaadf68d25dd03a0c353743b261fb.

--- core/kernel/source/jetbrains/mps/smodel/Language.java
+++ core/kernel/source/jetbrains/mps/smodel/Language.java
@@ -496,7 +496,7 @@ public class Language extends AbstractModule implements MPSModuleOwner {
       if (structureModelDescriptor == null) return null;
 
       SModel structureModel = structureModelDescriptor.getSModel();
-      for (SNode node : structureModel.roots()) {
+      for (SNode node:structureModel.nodes()){
         if (node.getAdapter() instanceof AbstractConceptDeclaration) {
           myNameToConceptCache.put(node.getName(), (AbstractConceptDeclaration) node.getAdapter());
         }
