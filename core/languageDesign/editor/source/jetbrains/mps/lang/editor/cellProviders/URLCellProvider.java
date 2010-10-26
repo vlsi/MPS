74cca4fdd62c548dde4f3fd5aa552982a69dadb5
Alexander Shatalin <alexander.shatalin@jetbrains.com>
2010-10-26 16:30:44 +0200

    Optimizing imports

--- core/languageDesign/editor/source/jetbrains/mps/lang/editor/cellProviders/URLCellProvider.java
+++ core/languageDesign/editor/source/jetbrains/mps/lang/editor/cellProviders/URLCellProvider.java
@@ -15,30 +15,13 @@
  */
 package jetbrains.mps.lang.editor.cellProviders;
 
-import com.intellij.openapi.util.Computable;
-import jetbrains.mps.lang.annotations.structure.PropertyAttributeConcept;
-import jetbrains.mps.lang.core.structure.BaseConcept;
-import jetbrains.mps.lang.structure.structure.DataTypeDeclaration;
-import jetbrains.mps.lang.structure.structure.EnumerationDataTypeDeclaration;
-import jetbrains.mps.lang.structure.structure.PropertyDeclaration;
-import jetbrains.mps.logging.Logger;
 import jetbrains.mps.nodeEditor.CellActionType;
 import jetbrains.mps.nodeEditor.EditorContext;
-import jetbrains.mps.nodeEditor.NodeReadAccessCasterInEditor;
 import jetbrains.mps.nodeEditor.cellActions.CellAction_DeleteEasily;
 import jetbrains.mps.nodeEditor.cellActions.CellAction_DeletePropertyOrNode;
-import jetbrains.mps.nodeEditor.cellMenu.BooleanPropertySubstituteInfo;
-import jetbrains.mps.nodeEditor.cellMenu.CellContext;
-import jetbrains.mps.nodeEditor.cellMenu.EnumPropertySubstituteInfo;
-import jetbrains.mps.nodeEditor.cellMenu.NodeSubstituteInfo;
-import jetbrains.mps.nodeEditor.cellProviders.CellProviderWithRole;
 import jetbrains.mps.nodeEditor.cells.EditorCell;
-import jetbrains.mps.nodeEditor.cells.EditorCell_Property;
 import jetbrains.mps.nodeEditor.cells.EditorCell_URL;
-import jetbrains.mps.nodeEditor.cells.PropertyAccessor;
-import jetbrains.mps.smodel.Primitives;
 import jetbrains.mps.smodel.SNode;
-import jetbrains.mps.util.InternUtil;
 
 public class URLCellProvider extends PropertyCellProvider {  
 
