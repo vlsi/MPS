e39a80ce7049cb247ce55ddd2e1fb47ed510deff
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2010-11-27 21:20:24 +0300

    minor: clarification for the todo

--- workbench/source/jetbrains/mps/debug/api/info/StacktraceUtil.java
+++ workbench/source/jetbrains/mps/debug/api/info/StacktraceUtil.java
@@ -7,7 +7,7 @@ import jetbrains.mps.ide.common.JavaConsoleCreator;
 @Deprecated
 public class StacktraceUtil {
   // is used in generated code, so can't remove now
-  // TODO remove later
+  // TODO remove later (was replaced by JavaConsoleCreator in september, after 1.5)
   @Deprecated
   public static ConsoleViewImpl createConsoleView(Project project) {
     return JavaConsoleCreator.createConsoleView(project);
