aff26e9a95c366c8e6c4ee077ed4c80c60028310
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2010-12-05 03:13:56 +0300

    ProjectPaneTreeGenStatusUpdater is ok

--- workbench/source/jetbrains/mps/ide/projectPane/logicalview/visitor/ProjectPaneTreeGenStatusUpdater.java
+++ workbench/source/jetbrains/mps/ide/projectPane/logicalview/visitor/ProjectPaneTreeGenStatusUpdater.java
@@ -21,21 +21,22 @@ import jetbrains.mps.generator.GeneratorManager;
 import jetbrains.mps.generator.ModelGenerationStatusManager;
 import jetbrains.mps.ide.projectPane.logicalview.nodes.AccessoriesModelTreeNode;
 import jetbrains.mps.ide.projectPane.logicalview.nodes.ProjectModuleTreeNode;
+import jetbrains.mps.ide.projectPane.logicalview.nodes.ProjectTreeNode;
 import jetbrains.mps.ide.ui.MPSTreeNode;
 import jetbrains.mps.ide.ui.smodel.SModelTreeNode;
 import jetbrains.mps.ide.ui.smodel.SModelTreeNode.GenerationStatus;
 import jetbrains.mps.ide.ui.smodel.SNodeTreeNode;
 import jetbrains.mps.project.ProjectOperationContext;
+import jetbrains.mps.smodel.SModelDescriptor;
 import jetbrains.mps.smodel.descriptor.EditableSModelDescriptor;
 
-public class ProjectPaneTreeGenStatusUpdater extends TreeNodeVisitor{
-  public void visit(MPSTreeNode node) {
-    if (node instanceof ProjectModuleTreeNode){
-      updateGenStatus(((ProjectModuleTreeNode) node));
-    }
+public class ProjectPaneTreeGenStatusUpdater extends TreeNodeVisitor {
+  protected void visitModelNode(SModelTreeNode node) {
+    GenerationStatus generationStatus = getGenerationStatus(node);
+    node.setAdditionalText(generationStatus.getMessage());
   }
 
-  private void updateGenStatus(ProjectModuleTreeNode node){
+  protected void visitModuleNode(ProjectModuleTreeNode node) {
     if (node.getModule().isPackaged()) {
       node.setAdditionalText("packaged");
     } else {
@@ -44,13 +45,17 @@ public class ProjectPaneTreeGenStatusUpdater extends TreeNodeVisitor{
     node.updateNodePresentationInTree();
   }
 
+  protected void visitProjectNode(ProjectTreeNode node) {
+
+  }
+
   private GenerationStatus generationRequired(MPSTreeNode node) {
     if (node instanceof SNodeTreeNode) return GenerationStatus.NOT_REQUIRED;
     if (node instanceof AccessoriesModelTreeNode) return GenerationStatus.NOT_REQUIRED;
 
     if (node instanceof SModelTreeNode) {
       SModelTreeNode smodelTreeNode = (SModelTreeNode) node;
-      GenerationStatus modelGenStatus = smodelTreeNode.getGenerationStatus();
+      GenerationStatus modelGenStatus = getGenerationStatus(smodelTreeNode);
       if (isInheritableGenStatus(modelGenStatus)) return modelGenStatus;
 
       for (SModelTreeNode child : smodelTreeNode.getSubfolderSModelTreeNodes()) {
@@ -79,34 +84,27 @@ public class ProjectPaneTreeGenStatusUpdater extends TreeNodeVisitor{
     return childGenStatus == GenerationStatus.REQUIRED || childGenStatus == GenerationStatus.UPDATING;
   }
 
-           //////////model
+  private GenerationStatus getGenerationStatus(SModelTreeNode node) {
+    if (node.getSModelDescriptor() == null) return GenerationStatus.NOT_REQUIRED;
+    if (isPackaged(node)) return GenerationStatus.PACKAGED;
+    if (isDoNotGenerate(node)) return GenerationStatus.DO_NOT_GENERATE;
 
-  private void updateGenerationStatus(SModelTreeNode node) {
-    GenerationStatus generationStatus = getGenerationStatus();
-    setAdditionalText(generationStatus.getMessage());
-  }
-
-  public GenerationStatus getGenerationStatus() {
-    if (getSModelDescriptor() == null) return GenerationStatus.NOT_REQUIRED;
-    if (isPackaged()) return GenerationStatus.PACKAGED;
-    if (isDoNotGenerate()) return GenerationStatus.DO_NOT_GENERATE;
-
-    Project project = getOperationContext().getProject();
+    Project project = node.getOperationContext().getProject();
     if (DumbService.getInstance(project).isDumb()) return GenerationStatus.UPDATING;
 
-    boolean required = ModelGenerationStatusManager.getInstance().generationRequired(getSModelDescriptor(), ProjectOperationContext.get(project), false, true);
+    boolean required = ModelGenerationStatusManager.getInstance().generationRequired(node.getSModelDescriptor(), ProjectOperationContext.get(project), false, true);
     return required ? GenerationStatus.REQUIRED : GenerationStatus.NOT_REQUIRED;
   }
 
-  private boolean isPackaged() {
-    if (!(myModelDescriptor instanceof EditableSModelDescriptor)) return false;
-    return ((EditableSModelDescriptor) myModelDescriptor).isPackaged();
+  private boolean isPackaged(SModelTreeNode node) {
+    SModelDescriptor md = node.getSModelDescriptor();
+    if (!(md instanceof EditableSModelDescriptor)) return false;
+    return ((EditableSModelDescriptor) md).isPackaged();
   }
 
-  private boolean isDoNotGenerate() {
-    if (getSModelDescriptor() == null) return false;
-    return GeneratorManager.isDoNotGenerate(getSModelDescriptor());
+  private boolean isDoNotGenerate(SModelTreeNode node) {
+    SModelDescriptor md = node.getSModelDescriptor();
+    if (md == null) return false;
+    return GeneratorManager.isDoNotGenerate(md);
   }
-
-
 }
