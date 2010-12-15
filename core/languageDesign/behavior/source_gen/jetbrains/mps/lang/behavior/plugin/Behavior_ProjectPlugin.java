4b4344dd0460263e35a92ee282f40c885fbb6bec
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2010-12-15 14:45:24 +0300

    fix diff tests

--- core/languageDesign/behavior/source_gen/jetbrains/mps/lang/behavior/plugin/Behavior_ProjectPlugin.java
+++ core/languageDesign/behavior/source_gen/jetbrains/mps/lang/behavior/plugin/Behavior_ProjectPlugin.java
@@ -5,4 +5,6 @@ package jetbrains.mps.lang.behavior.plugin;
 import jetbrains.mps.plugins.projectplugins.BaseProjectPlugin;
 
 public class Behavior_ProjectPlugin extends BaseProjectPlugin {
+  public Behavior_ProjectPlugin() {
+  }
 }
