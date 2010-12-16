126b6d8e6cd08bf62ce1239cd4c49fb14feff542
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2010-12-16 07:26:42 +0300

    revert Fedor's commit (got rid of synchronization), because the new interner consumes 35M more memory just after loading charisma project

--- core/runtime/source/jetbrains/mps/util/Interner.java
+++ core/runtime/source/jetbrains/mps/util/Interner.java
@@ -15,139 +15,28 @@
  */
 package jetbrains.mps.util;
 
-import java.lang.ref.ReferenceQueue;
-import java.lang.ref.SoftReference;
-import java.util.concurrent.ConcurrentHashMap;
+import jetbrains.mps.util.misc.ObjectCache;
 
 public class Interner {
+  private final ObjectCache<String, String> myCache;
 
-  private final DumbLRUCache<String> myCache;
 
   public Interner(int size) {
-    myCache = new DumbLRUCache<String>() {
-      @Override
-      public String canonic(String s) {
-        // Ensure we cache only what's necessary!
-        return new String(s);
-      }
-    };
+    myCache = new ObjectCache<String, String>(size);
   }
 
   public String intern(String s) {
-    if (s == null) {
-      return null;
+    if (s == null) return null;
+    synchronized (myCache) {
+      String result = myCache.tryKey(s);
+      if (result != null) {
+        assert result.equals(s);
+        return result;
+      }
+      // Ensure we cache only what's necessary!
+      s = new String (s);
+      myCache.cacheObject(s, s);
     }
-    return myCache.cacheObject(s);
+    return s;
   }
-
-
-  private static class DumbLRUCache<K> {
-
-    private ThreadLocal<SubstituteKeyHolder<K>> myThreadKey = new ThreadLocal<SubstituteKeyHolder<K>>() {
-      @Override
-      protected SubstituteKeyHolder<K> initialValue() {
-        return new SubstituteKeyHolder<K>();
-      }
-    };
-
-    private ConcurrentHashMap<KeyHolder<K>, KeyHolder<K>> myMap = new ConcurrentHashMap<KeyHolder<K>, KeyHolder<K>>();
-
-    private ReferenceQueue<K> myRefQueue = new ReferenceQueue<K>();
-
-    public K cacheObject(K k) {
-      purge();
-
-      SubstituteKeyHolder<K> substituteKeyHolder = myThreadKey.get();
-      substituteKeyHolder.myKey = k;
-
-      K res;
-      try {
-        KeyHolder<K> keyHolder = myMap.get(substituteKeyHolder);
-        if (keyHolder == null) {
-          keyHolder = new SoftKeyHolder<K>(canonic(k), myRefQueue);
-          KeyHolder<K> skh = myMap.putIfAbsent(keyHolder, keyHolder);
-          keyHolder = skh != null ? skh : keyHolder;
-        }
-        res = keyHolder.get();
-        assert res.equals(k);
-      }
-      finally {
-        substituteKeyHolder.myKey = null;
-      }
-
-      return res;
-    }
-
-    public K canonic(K k) {
-      return k;
-    }
-
-    private void purge() {
-      SoftKeyHolder<K> ref = null;
-      while ((ref = (SoftKeyHolder<K>) myRefQueue.poll()) != null) {
-        myMap.remove(ref);
-      }
-    }
-
-    private interface KeyHolder<T> {
-      T get();
-    }
-
-    private static class SubstituteKeyHolder<T> implements KeyHolder<T> {
-      T myKey;
-
-      public T get() {
-        return myKey;
-      }
-
-      @Override
-      public int hashCode() {
-        return myKey.hashCode();
-      }
-
-      @Override
-      public boolean equals(Object obj) {
-        if (obj == null) return false;
-        if (obj == this) return true;
-        if (obj instanceof KeyHolder) {
-          return eq(myKey, ((KeyHolder) obj).get());
-        }
-        return false;
-      }
-    }
-
-    private static class SoftKeyHolder<T> extends SoftReference<T> implements KeyHolder<T> {
-      int myHash;
-
-      private SoftKeyHolder(T ref, ReferenceQueue queue) {
-        super(ref, queue);
-        myHash = ref.hashCode();
-      }
-
-      @Override
-      public int hashCode() {
-        return myHash;
-      }
-
-      @Override
-      public T get() {
-        return super.get();
-      }
-
-      @Override
-      public boolean equals(Object obj) {
-        if (obj == null) return false;
-        if (obj == this) return true;
-        if (obj instanceof KeyHolder) {
-          return eq(get(), ((KeyHolder) obj).get());
-        }
-        return false;
-      }
-    }
-
-    private static boolean eq(Object a, Object b) {
-      return a == null ? b == null : a.equals(b);
-    }
-  }
-
 }
