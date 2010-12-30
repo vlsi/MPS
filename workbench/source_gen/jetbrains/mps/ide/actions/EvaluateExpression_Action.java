437514e0fd28a553246fe5e54bd9cad1d34dde6b
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2010-12-30 18:44:02 +0300

    redo changes in evaluate expression action, undone by bad merge

--- workbench/source_gen/jetbrains/mps/ide/actions/EvaluateExpression_Action.java
+++ workbench/source_gen/jetbrains/mps/ide/actions/EvaluateExpression_Action.java
@@ -8,7 +8,6 @@ import jetbrains.mps.ide.icons.IconManager;
 import jetbrains.mps.plugins.MacrosUtil;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import com.intellij.openapi.project.Project;
 import jetbrains.mps.smodel.IOperationContext;
 import org.jetbrains.annotations.NotNull;
 import com.intellij.openapi.actionSystem.AnActionEvent;
@@ -19,7 +18,6 @@ public class EvaluateExpression_Action extends GeneratedAction {
   private static final Icon ICON = IconManager.loadIcon(MacrosUtil.expandPath("${solution_descriptor}/icons/debug/evaluate.png", "jetbrains.mps.ide"), true);
   protected static Log log = LogFactory.getLog(EvaluateExpression_Action.class);
 
-  private Project project;
   private IOperationContext operationContext;
 
   public EvaluateExpression_Action() {
@@ -37,7 +35,7 @@ public class EvaluateExpression_Action extends GeneratedAction {
     try {
       {
         AbstractDebugSession debugSession = DebugActionsUtil.getDebugSession(event);
-        event.getPresentation().setEnabled(debugSession != null && debugSession.isStepEnabled() && debugSession.canShowEvaluationDialog());
+        event.getPresentation().setEnabled(debugSession != null && debugSession.isStepEnabled() && debugSession.getEvaluationProvider() != null);
       }
     } catch (Throwable t) {
       if (log.isErrorEnabled()) {
@@ -51,10 +49,6 @@ public class EvaluateExpression_Action extends GeneratedAction {
     if (!(super.collectActionData(event))) {
       return false;
     }
-    this.project = event.getData(MPSDataKeys.PROJECT);
-    if (this.project == null) {
-      return false;
-    }
     this.operationContext = event.getData(MPSDataKeys.OPERATION_CONTEXT);
     if (this.operationContext == null) {
       return false;
@@ -64,13 +58,12 @@ public class EvaluateExpression_Action extends GeneratedAction {
 
   protected void cleanup() {
     super.cleanup();
-    this.project = null;
     this.operationContext = null;
   }
 
   public void doExecute(@NotNull final AnActionEvent event) {
     try {
-      DebugActionsUtil.getDebugSession(event).showEvaluationDialog(EvaluateExpression_Action.this.operationContext);
+      DebugActionsUtil.getDebugSession(event).getEvaluationProvider().showEvaluationDialog(EvaluateExpression_Action.this.operationContext);
     } catch (Throwable t) {
       if (log.isErrorEnabled()) {
         log.error("User's action execute method failed. Action:" + "EvaluateExpression", t);
