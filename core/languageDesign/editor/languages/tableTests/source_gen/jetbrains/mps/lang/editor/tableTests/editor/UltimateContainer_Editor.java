7957d37038c913f4e8d55b5e6633b47e6bf9c0f5
Alexander Shatalin <Alexander.Shatalin@jetbrains.com>
2011-01-26 15:34:55 +0100

    adding XUCTableModel with unit-tests.

--- core/languageDesign/editor/languages/tableTests/source_gen/jetbrains/mps/lang/editor/tableTests/editor/UltimateContainer_Editor.java
+++ core/languageDesign/editor/languages/tableTests/source_gen/jetbrains/mps/lang/editor/tableTests/editor/UltimateContainer_Editor.java
@@ -44,6 +44,8 @@ public class UltimateContainer_Editor extends DefaultNodeEditor {
     editorCell.addEditorCell(this.createRefNodeList_hd4pfy_b1a(editorContext, node));
     editorCell.addEditorCell(this.createConstant_hd4pfy_c1a(editorContext, node));
     editorCell.addEditorCell(this.createRefNodeList_hd4pfy_d1a(editorContext, node));
+    editorCell.addEditorCell(this.createConstant_hd4pfy_e1a(editorContext, node));
+    editorCell.addEditorCell(this.createRefNodeList_hd4pfy_f1a(editorContext, node));
     return editorCell;
   }
 
@@ -80,6 +82,17 @@ public class UltimateContainer_Editor extends DefaultNodeEditor {
     return editorCell;
   }
 
+  private EditorCell createConstant_hd4pfy_e1a(EditorContext editorContext, SNode node) {
+    EditorCell_Constant editorCell = new EditorCell_Constant(editorContext, node, "matrixes:");
+    editorCell.setCellId("Constant_hd4pfy_e1a");
+    {
+      Style style = editorCell.getStyle();
+      style.set(StyleAttributes.INDENT_LAYOUT_NEW_LINE, true);
+    }
+    editorCell.setDefaultText("");
+    return editorCell;
+  }
+
   private EditorCell createConstant_hd4pfy_c0(EditorContext editorContext, SNode node) {
     EditorCell_Constant editorCell = new EditorCell_Constant(editorContext, node, "}");
     editorCell.setCellId("Constant_hd4pfy_c0");
@@ -108,6 +121,19 @@ public class UltimateContainer_Editor extends DefaultNodeEditor {
     AbstractCellListHandler handler = new UltimateContainer_Editor.tablesListHandler_hd4pfy_d1a(node, "tables", editorContext);
     EditorCell_Collection editorCell = handler.createCells(editorContext, new CellLayout_Indent(), false);
     editorCell.setCellId("refNodeList_tables");
+    {
+      Style style = editorCell.getStyle();
+      style.set(StyleAttributes.INDENT_LAYOUT_CHILDREN_NEWLINE, true);
+      style.set(StyleAttributes.INDENT_LAYOUT_INDENT, true);
+    }
+    editorCell.setRole(handler.getElementRole());
+    return editorCell;
+  }
+
+  private EditorCell createRefNodeList_hd4pfy_f1a(EditorContext editorContext, SNode node) {
+    AbstractCellListHandler handler = new UltimateContainer_Editor.matrixesListHandler_hd4pfy_f1a(node, "matrixes", editorContext);
+    EditorCell_Collection editorCell = handler.createCells(editorContext, new CellLayout_Indent(), false);
+    editorCell.setCellId("refNodeList_matrixes");
     {
       Style style = editorCell.getStyle();
       style.set(StyleAttributes.INDENT_LAYOUT_CHILDREN_NEWLINE, true);
@@ -157,6 +183,44 @@ public class UltimateContainer_Editor extends DefaultNodeEditor {
 
   private static class tablesListHandler_hd4pfy_d1a extends RefNodeListHandler {
     public tablesListHandler_hd4pfy_d1a(SNode ownerNode, String childRole, EditorContext context) {
+      super(ownerNode, childRole, context, false);
+    }
+
+    public SNode createNodeToInsert(EditorContext editorContext) {
+      SNode listOwner = super.getOwner();
+      return NodeFactoryManager.createNode(listOwner, editorContext, super.getElementRole());
+    }
+
+    public EditorCell createNodeCell(EditorContext editorContext, SNode elementNode) {
+      EditorCell elementCell = super.createNodeCell(editorContext, elementNode);
+      this.installElementCellActions(this.getOwner(), elementNode, elementCell, editorContext);
+      return elementCell;
+    }
+
+    public EditorCell createEmptyCell(EditorContext editorContext) {
+      EditorCell emptyCell = null;
+      emptyCell = super.createEmptyCell(editorContext);
+      this.installElementCellActions(super.getOwner(), null, emptyCell, editorContext);
+      return emptyCell;
+    }
+
+    public void installElementCellActions(SNode listOwner, SNode elementNode, EditorCell elementCell, EditorContext editorContext) {
+      if (elementCell.getUserObject(AbstractCellListHandler.ELEMENT_CELL_ACTIONS_SET) == null) {
+        elementCell.putUserObject(AbstractCellListHandler.ELEMENT_CELL_ACTIONS_SET, AbstractCellListHandler.ELEMENT_CELL_ACTIONS_SET);
+        SNode substituteInfoNode = listOwner;
+        if (elementNode != null) {
+          substituteInfoNode = elementNode;
+          elementCell.setAction(CellActionType.DELETE, new CellAction_DeleteNode(elementNode));
+        }
+        if (elementCell.getSubstituteInfo() == null || elementCell.getSubstituteInfo() instanceof DefaultReferenceSubstituteInfo) {
+          elementCell.setSubstituteInfo(new DefaultChildSubstituteInfo(listOwner, elementNode, super.getLinkDeclaration(), editorContext));
+        }
+      }
+    }
+  }
+
+  private static class matrixesListHandler_hd4pfy_f1a extends RefNodeListHandler {
+    public matrixesListHandler_hd4pfy_f1a(SNode ownerNode, String childRole, EditorContext context) {
       super(ownerNode, childRole, context, false);
     }
 
