dc71a98aa2922837964e493d069e06195095d760
Timur Abishev <Timur.Abishev@jetbrains.com>
2011-03-09 19:23:04 +0300

    _rebuild behavior/generator

--- core/languageDesign/behavior/source_gen/jetbrains/mps/lang/behavior/generator/template/main/QueriesGenerated.java
+++ core/languageDesign/behavior/source_gen/jetbrains/mps/lang/behavior/generator/template/main/QueriesGenerated.java
@@ -43,7 +43,8 @@ import jetbrains.mps.smodel.SNodeId;
 
 public class QueriesGenerated {
   public static boolean createRootRule_Condition_7923290658387314702(final IOperationContext operationContext, final CreateRootRuleContext _context) {
-    return LanguageAspect.BEHAVIOR.is(_context.getOriginalInputModel());
+    LanguageAspect.BEHAVIOR.is(_context.getOriginalInputModel());
+    return false;
   }
 
   public static Object propertyMacro_GetPropertyValue_1225195370259(final IOperationContext operationContext, final PropertyMacroContext _context) {
