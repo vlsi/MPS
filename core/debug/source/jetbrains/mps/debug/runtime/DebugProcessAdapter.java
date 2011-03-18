91ff3e80cbdbe89dda338975e9386574f4c716df
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2011-03-18 22:03:12 +0300

    minor: comments

--- core/debug/source/jetbrains/mps/debug/runtime/DebugProcessAdapter.java
+++ core/debug/source/jetbrains/mps/debug/runtime/DebugProcessAdapter.java
@@ -17,36 +17,24 @@ package jetbrains.mps.debug.runtime;
 
 import org.jetbrains.annotations.NotNull;
 
-/**
- * Created by IntelliJ IDEA.
- * User: Cyril.Konopko
- * Date: 18.12.2009
- * Time: 19:25:45
- * To change this template use File | Settings | File Templates.
- */
 public abstract class DebugProcessAdapter implements DebugProcessListener {
   @Override
   public void connectorIsReady() {
-    //To change body of implemented methods use File | Settings | File Templates.
   }
 
   @Override
   public void paused(@NotNull SuspendContext suspendContext) {
-    //To change body of implemented methods use File | Settings | File Templates.
   }
 
   @Override
   public void resumed(@NotNull SuspendContext suspendContext) {
-    //To change body of implemented methods use File | Settings | File Templates.
   }
 
   @Override
   public void processDetached(@NotNull DebugVMEventsProcessor process, boolean closedByUser) {
-    //To change body of implemented methods use File | Settings | File Templates.
   }
 
   @Override
   public void processAttached(@NotNull DebugVMEventsProcessor process) {
-    //To change body of implemented methods use File | Settings | File Templates.
   }
 }
