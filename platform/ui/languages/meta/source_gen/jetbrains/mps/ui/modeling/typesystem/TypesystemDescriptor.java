0ebf70289f29d34b166d7c50cc2fad6e9a8e6a70
Ilya Lintsbah <Ilya.Lintsbakh@jetbrains.com>
2011-05-11 12:57:30 +0400

    Regenerate

--- platform/ui/languages/meta/source_gen/jetbrains/mps/ui/modeling/typesystem/TypesystemDescriptor.java
+++ platform/ui/languages/meta/source_gen/jetbrains/mps/ui/modeling/typesystem/TypesystemDescriptor.java
@@ -4,7 +4,6 @@ package jetbrains.mps.ui.modeling.typesystem;
 
 import jetbrains.mps.lang.typesystem.runtime.BaseHelginsDescriptor;
 import jetbrains.mps.lang.typesystem.runtime.InferenceRule_Runtime;
-import jetbrains.mps.lang.typesystem.runtime.NonTypesystemRule_Runtime;
 
 public class TypesystemDescriptor extends BaseHelginsDescriptor {
   public TypesystemDescriptor() {
@@ -63,14 +62,6 @@ public class TypesystemDescriptor extends BaseHelginsDescriptor {
     {
       InferenceRule_Runtime inferenceRule = new typeof_ThisUIObject_InferenceRule();
       this.myInferenceRules.add(inferenceRule);
-    }
-    {
-      NonTypesystemRule_Runtime nonTypesystemRule = new check_IAspectParameterHolder_NonTypesystemRule();
-      this.myNonTypesystemRules.add(nonTypesystemRule);
-    }
-    {
-      NonTypesystemRule_Runtime nonTypesystemRule = new check_UIObject_consistent_belongsTo_NonTypesystemRule();
-      this.myNonTypesystemRules.add(nonTypesystemRule);
     }
   }
 }
