dcf2ad6ada687bec8b526929d979b5c0c8de8ee7
Alexander Shatalin <Alexander.Shatalin@jetbrains.com>
2011-05-17 11:40:51 +0200

    MPS-12424 exception on closing project with editor having a find panel

--- workbench/source/jetbrains/mps/workbench/editors/MPSEditorWarningsManager.java
+++ workbench/source/jetbrains/mps/workbench/editors/MPSEditorWarningsManager.java
@@ -67,7 +67,7 @@ public class MPSEditorWarningsManager implements ProjectComponent {
   }
 
   public void projectClosed() {
-
+    myFileEditorManager.removeFileEditorManagerListener(myFileEditorManagerListener);
   }
 
   @NonNls
