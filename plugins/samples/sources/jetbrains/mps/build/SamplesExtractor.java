a04d1ea48e775ae0697d82b9bb7fda9f60071568
Evgeny Gryaznov <Evgeny.Gryaznov@jetbrains.com>
2011-05-20 17:06:45 +0200

    samples extractor: replace Ok/Cancel dialog -> yes/no

--- plugins/samples/sources/jetbrains/mps/build/SamplesExtractor.java
+++ plugins/samples/sources/jetbrains/mps/build/SamplesExtractor.java
@@ -143,7 +143,7 @@ public class SamplesExtractor implements ApplicationComponent, PersistentStateCo
       File samplesDir = new File(getSamplesPathInUserHome());
 
       if (samplesDir.exists()) {
-        int answer = Messages.showOkCancelDialog("Do you want to replace directory\n" + samplesDir + "\n with version " + currentBuildNumberString() + " (old directory contents will be deleted)?", "Replace MPS Samples?", Messages.getQuestionIcon());
+        int answer = Messages.showYesNoDialog("Do you want to replace directory\n" + samplesDir + "\n with version " + currentBuildNumberString() + " (old directory contents will be deleted)?", "Replace MPS Samples?", Messages.getQuestionIcon());
         if (answer == 0) {
           FileUtil.delete(samplesDir);
           actuallyExtractSamples(samplesZipFile);
