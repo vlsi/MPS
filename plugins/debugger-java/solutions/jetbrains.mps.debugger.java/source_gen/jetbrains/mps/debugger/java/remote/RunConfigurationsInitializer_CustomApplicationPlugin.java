d651234b651f1712d5c006cd6d002e493decdb01
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2011-05-28 21:06:40 +0400

    regenerate debugger.java

--- plugins/debugger-java/solutions/jetbrains.mps.debugger.java/source_gen/jetbrains/mps/debugger/java/remote/RunConfigurationsInitializer_CustomApplicationPlugin.java
+++ plugins/debugger-java/solutions/jetbrains.mps.debugger.java/source_gen/jetbrains/mps/debugger/java/remote/RunConfigurationsInitializer_CustomApplicationPlugin.java
@@ -7,29 +7,43 @@ import java.util.List;
 import com.intellij.execution.configurations.ConfigurationType;
 import jetbrains.mps.internal.collections.runtime.ListSequence;
 import java.util.ArrayList;
+import com.intellij.execution.junit.RuntimeConfigurationProducer;
 import com.intellij.openapi.extensions.ExtensionPoint;
 import com.intellij.openapi.extensions.Extensions;
 
 public class RunConfigurationsInitializer_CustomApplicationPlugin extends BaseCustomApplicationPlugin {
   private List<ConfigurationType> myRegisteredKinds = ListSequence.fromList(new ArrayList<ConfigurationType>());
+  private List<RuntimeConfigurationProducer> myRegisteredProducers = ListSequence.fromList(new ArrayList<RuntimeConfigurationProducer>());
 
   public RunConfigurationsInitializer_CustomApplicationPlugin() {
   }
 
   public void doInit() {
-    ExtensionPoint<ConfigurationType> extensionPoint = Extensions.getArea(null).getExtensionPoint(ConfigurationType.CONFIGURATION_TYPE_EP);
+    // register kinds 
+    ExtensionPoint<ConfigurationType> configurationExtensionPoint = Extensions.getArea(null).getExtensionPoint(ConfigurationType.CONFIGURATION_TYPE_EP);
     {
       Remote_Kind runConfigurationKind = new Remote_Kind();
       ListSequence.fromList(RunConfigurationsInitializer_CustomApplicationPlugin.this.myRegisteredKinds).addElement(runConfigurationKind);
-      extensionPoint.registerExtension(runConfigurationKind);
+      configurationExtensionPoint.registerExtension(runConfigurationKind);
     }
+
+    // add foreign factories 
+
+    // register creators 
+    ExtensionPoint<RuntimeConfigurationProducer> producerExtensionPoint = Extensions.getArea(null).getExtensionPoint(RuntimeConfigurationProducer.RUNTIME_CONFIGURATION_PRODUCER);
   }
 
   public void doDispose() {
-    ExtensionPoint<ConfigurationType> extensionPoint = Extensions.getArea(null).getExtensionPoint(ConfigurationType.CONFIGURATION_TYPE_EP);
+    ExtensionPoint<ConfigurationType> configurationExtensionPoint = Extensions.getArea(null).getExtensionPoint(ConfigurationType.CONFIGURATION_TYPE_EP);
     for (ConfigurationType configurationKind : ListSequence.fromList(RunConfigurationsInitializer_CustomApplicationPlugin.this.myRegisteredKinds).reversedList()) {
-      extensionPoint.unregisterExtension(configurationKind);
+      configurationExtensionPoint.unregisterExtension(configurationKind);
     }
     ListSequence.fromList(RunConfigurationsInitializer_CustomApplicationPlugin.this.myRegisteredKinds).clear();
+
+    ExtensionPoint<RuntimeConfigurationProducer> producerExtensionPoint = Extensions.getArea(null).getExtensionPoint(RuntimeConfigurationProducer.RUNTIME_CONFIGURATION_PRODUCER);
+    for (RuntimeConfigurationProducer producer : ListSequence.fromList(RunConfigurationsInitializer_CustomApplicationPlugin.this.myRegisteredProducers)) {
+      producerExtensionPoint.unregisterExtension(producer);
+    }
+    ListSequence.fromList(RunConfigurationsInitializer_CustomApplicationPlugin.this.myRegisteredProducers).clear();
   }
 }
