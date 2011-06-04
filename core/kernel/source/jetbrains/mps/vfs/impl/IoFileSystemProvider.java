f6b9eb80146128dedc0b8459c95dfcded050a18d
Evgeny Gerashchenko <Evgeny.Gerashchenko@jetbrains.com>
2011-06-05 00:12:36 +0400

    Returned "!" in JarEntryFile.getPath().
    #MPS-12571 fixed

--- core/kernel/source/jetbrains/mps/vfs/impl/IoFileSystemProvider.java
+++ core/kernel/source/jetbrains/mps/vfs/impl/IoFileSystemProvider.java
@@ -32,8 +32,8 @@ public class IoFileSystemProvider implements FileSystemProvider {
       String jarPath = path.substring(0, index);
       String entryPath = path.substring(index + 1);
 
-      if (!entryPath.startsWith("/")) {
-        entryPath = "/" + entryPath;
+      if (entryPath.startsWith("/")) {
+        entryPath = entryPath.substring(1);
       }
 
       File jarFile = new File(jarPath);
