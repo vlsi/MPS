47a353858e14586353e5176302433b24be32a03a
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2011-07-11 15:33:23 +0400

    regenerate execution.configurations language

--- workbench/runConfigurations/languages/configurations/source_gen/jetbrains/mps/execution/configurations/generator/template/preprocess/QueriesGenerated.java
+++ workbench/runConfigurations/languages/configurations/source_gen/jetbrains/mps/execution/configurations/generator/template/preprocess/QueriesGenerated.java
@@ -65,7 +65,7 @@ public class QueriesGenerated {
         {
           quotedNode_2 = SModelUtil_new.instantiateConceptDeclaration("jetbrains.mps.baseLanguage.structure.ParameterReference", null, GlobalScope.getInstance(), false);
           SNode quotedNode1_6 = quotedNode_2;
-          quotedNode1_6.addReference(SReference.create("variableDeclaration", quotedNode1_6, SModelReference.fromString("r:9ab41d12-8c02-45be-a006-e05acdfb3a72(jetbrains.mps.execution.configurations.generator.template.preprocess@1_1)"), SNodeId.fromString("")));
+          quotedNode1_6.addReference(SReference.create("variableDeclaration", quotedNode1_6, SModelReference.fromString("r:ef62eed0-f24e-4ff0-94b8-b3d170d4cdcb(jetbrains.mps.execution.configurations.generator.template.preprocess@1_1)"), SNodeId.fromString("")));
           quotedNode_1.addChild("operand", quotedNode1_6);
         }
         {
