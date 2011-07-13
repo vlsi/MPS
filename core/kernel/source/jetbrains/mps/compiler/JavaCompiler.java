9c73a6ab342ee5072c4ed62d4130355a3361acfa
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2011-07-13 15:22:58 +0400

    fix exception on tests init

--- core/kernel/source/jetbrains/mps/compiler/JavaCompiler.java
+++ core/kernel/source/jetbrains/mps/compiler/JavaCompiler.java
@@ -146,9 +146,11 @@ public class JavaCompiler {
 
   private class MyCompilerRequestor implements ICompilerRequestor {
     public void acceptResult(CompilationResult result) {
-      for (CategorizedProblem e : result.getErrors()) {
-        char[] fname = e.getOriginatingFileName();
-        LOG.error((fname == null ? "" : new String(fname)) + ":::" + e.getMessage());
+      if (result != null) {
+        for (CategorizedProblem e : result.getErrors()) {
+          char[] fname = e.getOriginatingFileName();
+          LOG.error((fname == null ? "" : new String(fname)) + ":::" + e.getMessage());
+        }
       }
       for (ClassFile file : result.getClassFiles()) {
         onClass(file);
