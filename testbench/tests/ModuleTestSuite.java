a54876230e6561a9555a59676fdadda7ed822cdd
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2011-07-27 16:38:04 +0400

    run runconfiguration migrations tests on server

--- testbench/tests/ModuleTestSuite.java
+++ testbench/tests/ModuleTestSuite.java
@@ -35,6 +35,7 @@ import org.junit.runner.RunWith;
   "jetbrains.mps.tests.TestModule_jetbrains_mps_baseLanguage_tuples_test",
   "jetbrains.mps.tests.TestModule_jetbrains_mps_baseLanguage_unittest",
   "jetbrains.mps.tests.TestModule_jetbrains_mps_debug_tests",
+  "jetbrains.mps.tests.TestModule_jetbrains_mps_execution_impl",
   "jetbrains.mps.tests.TestModule_jetbrains_mps_graphLayout_test",
   "jetbrains.mps.tests.TestModule_jetbrains_mps_lang_editor_table_tests",
   "jetbrains.mps.tests.TestModule_jetbrains_mps_lang_editor_tests",
