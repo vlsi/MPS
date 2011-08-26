c5d2639868fe110a0e4f9ee5300bad18b5d7279c
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2011-08-26 04:36:32 +0400

    DependenciesTests started

---
+++ core/kernel/tests/jetbrains/mps/project/DependenciesTests.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2003-2011 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jetbrains.mps.project;
+
+import jetbrains.mps.TestMain;
+import jetbrains.mps.ide.IdeMain;
+import jetbrains.mps.ide.IdeMain.TestMode;
+import jetbrains.mps.project.structure.modules.SolutionDescriptor;
+import jetbrains.mps.smodel.MPSModuleOwner;
+import jetbrains.mps.smodel.MPSModuleRepository;
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.UUID;
+
+import static junit.framework.Assert.assertTrue;
+
+public class DependenciesTests {
+  private static final MPSModuleOwner OWNER = new MPSModuleOwner() {
+  };
+
+  @Rule
+  public WriteAction wa = new WriteAction();
+
+  @BeforeClass
+  public static void setUp() throws Exception {
+    IdeMain.setTestMode(TestMode.CORE_TEST);
+    TestMain.configureMPS();
+  }
+
+  @After
+  public void afterTest() {
+    MPSModuleRepository.getInstance().unRegisterModules(OWNER);
+  }
+
+  @Test
+  public void testDependencyInScope() {
+    Solution s1 = createSolution();
+    Solution s2 = createSolution();
+
+    s1.addDependency(s2.getModuleReference(), false);
+
+    assertTrue(s1.getDependenciesManager().getAllVisibleModules().contains(s2));
+  }
+
+  private Solution createSolution() {
+    SolutionDescriptor d = new SolutionDescriptor();
+    String uuid = UUID.randomUUID().toString();
+    d.setNamespace(uuid);
+    d.setUUID(uuid);
+    return Solution.newInstance(d, OWNER);
+  }
+}
