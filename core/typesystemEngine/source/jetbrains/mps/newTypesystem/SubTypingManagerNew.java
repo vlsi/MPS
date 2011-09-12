1a66e2f31c3482169a3fd6ec8b2923b901a8add8
Ilya Lintsbah <Ilya.Lintsbakh@jetbrains.com>
2011-09-12 18:37:32 +0400

    Fix MPS-13777

--- core/typesystemEngine/source/jetbrains/mps/newTypesystem/SubTypingManagerNew.java
+++ core/typesystemEngine/source/jetbrains/mps/newTypesystem/SubTypingManagerNew.java
@@ -303,6 +303,9 @@ public class SubTypingManagerNew extends SubtypingManager {
       if (types.size() == 1) {
         return types.remove(0);
       }
+      if (types.size() == 0) {
+        return null;
+      }
       SNode left = types.remove(0);
       SNode right = types.remove(0);
       List<SNode> newNodes = eliminateSubOrSuperTypes(leastCommonSuperTypes(left, right, context), true);
