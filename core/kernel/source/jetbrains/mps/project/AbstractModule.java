0162602ba940686a3011e1eab4b4be524d5fdd72
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2011-09-14 01:16:15 +0400

    remove non-existing own stub path from modules not compiled in MPS

--- core/kernel/source/jetbrains/mps/project/AbstractModule.java
+++ core/kernel/source/jetbrains/mps/project/AbstractModule.java
@@ -207,6 +207,8 @@ public abstract class AbstractModule implements IModule {
   }
 
   public List<StubPath> getOwnStubPaths() {
+    if (!isCompileInMPS()) return Collections.emptyList();
+
     IFile classFolder = getClassesGen();
     if (classFolder == null) return Collections.emptyList();
 
