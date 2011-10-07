b5893048f3f08f30a08d33a52ed004da4de5e68c
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2011-10-07 20:47:02 +0400

    MPS.Core and other stub modules - paths to load from

--- core/kernel/source/jetbrains/mps/reloading/CommonPaths.java
+++ core/kernel/source/jetbrains/mps/reloading/CommonPaths.java
@@ -36,16 +36,72 @@ public class CommonPaths {
 
   //--------paths-----------
 
-  public static List<String> getMPSCorePaths(){
-    return new ArrayList<String>();
+  public static List<String> getMPSCorePaths() {
+    CompositeClassPathItem result = new CompositeClassPathItem();
+    //addIfExists(result,"/classes");
+    addIfExists(result, "/core/kernel/classes");
+    addIfExists(result, "/core/findUsages-runtime/classes");
+    addIfExists(result, "/core/refactoring-runtime/classes");
+    addIfExists(result, "/core/typesystemEngine/classes");
+    addIfExists(result, "/core/typesystemIntegration/classes");
+    addIfExists(result, "/core/generator/classes");
+    addIfExists(result, "/core/make-runtime/classes");
+    addIfExists(result, "/core/baseLanguage/closures/runtime/classes");
+    addIfExists(result, "/core/baseLanguage/collections/runtime/classes");
+    addIfExists(result, "/core/baseLanguage/collections/languages/trove/runtime/classes");
+    addIfExists(result, "/core/baseLanguage/tuples/runtime/classes");
+    addIfExists(result, "/core/baseLanguage/closures/runtime/classes");
+    addIfExists(result, "/core/baseLanguage/collections/languages/trove/runtime/classes");
+    addIfExists(result, "/core/baseLanguage/collections/languages/trove/runtime/lib/trove-2.1.0.jar");
+    addIfExists(result, "/core/baseLanguage/unitTest/solutions/runtime/classes");
+    addIfExists(result, "/core/baseLanguage/unitTest/solutions/lib/lib/jmock-2.5.1/bsh-core-2.0b4.jar");
+    addIfExists(result, "/core/baseLanguage/unitTest/solutions/lib/lib/jmock-2.5.1/cglib-nodep-2.1_3.jar");
+    addIfExists(result, "/core/baseLanguage/unitTest/solutions/lib/lib/jmock-2.5.1/hamcrest-core-1.1.jar");
+    addIfExists(result, "/core/baseLanguage/unitTest/solutions/lib/lib/jmock-2.5.1/hamcrest-library-1.1.jar");
+    addIfExists(result, "/core/baseLanguage/unitTest/solutions/lib/lib/jmock-2.5.1/jmock-2.5.1.jar");
+    addIfExists(result, "/core/baseLanguage/unitTest/solutions/lib/lib/jmock-2.5.1/jmock-junit3-2.5.1.jar");
+    addIfExists(result, "/core/baseLanguage/unitTest/solutions/lib/lib/jmock-2.5.1/jmock-junit4-2.5.1.jar");
+    addIfExists(result, "/core/baseLanguage/unitTest/solutions/lib/lib/jmock-2.5.1/objenesis-1.0.jar");
+    addIfExists(result, "/core/baseLanguage/tuples/runtime/classes");
+    addIfExists(result, "/core/runtime/classes");
+    addIfExists(result, "");
+    return itemToPath(result);
   }
 
-  public static List<String> getMPSEditorPaths(){
-    return new ArrayList<String>();
+  public static List<String> getMPSEditorPaths() {
+    CompositeClassPathItem result = new CompositeClassPathItem();
+    addIfExists(result, "/core/editor-runtime/classes");
+    addIfExists(result, "/core/actions-runtime/classes");
+    addIfExists(result, "/core/intentions-runtime/classes");
+    return itemToPath(result);
   }
 
-  public static List<String> getMPSWorkbenchPaths(){
-    return new ArrayList<String>();
+  public static List<String> getMPSWorkbenchPaths() {
+    CompositeClassPathItem result = new CompositeClassPathItem();
+    addIfExists(result, "/core/idea-patch/classes");
+    addIfExists(result, "/core/plugin-runtime/classes");
+    addIfExists(result, "/core/baseLanguage/baseLanguage/solutions/jetbrains.mps.baseLanguage.index/classes_gen");
+    addIfExists(result, "/core/baseLanguage/baseLanguage/solutions/jetbrains.mps.baseLanguage.search/classes_gen");
+    addIfExists(result, "/core/baseLanguage/runConfigurations/runtime/classes");
+    addIfExists(result, "/core/languageDesign/test/classes");
+    addIfExists(result, "/workbench/classes");
+    addIfExists(result, "/languages/util/runConfigurations/classes");
+    addIfExists(result, "/workbench/typesystemUi/classes");
+    addIfExists(result, "/languages/util/uiLanguage/runtime/classes");
+    addIfExists(result, "/MPSPlugin/apiclasses");
+    addIfExists(result, "/core/baseLanguage/runConfigurations/runtime/classes");
+    addIfExists(result, "/core/analyzers/classes");
+    addIfExists(result, "/core/debug/classes");
+    addIfExists(result, "/core/debug-api/classes");
+    return itemToPath(result);
+  }
+
+  public static List<String> getTestbenchPaths() {
+    return itemToPath(getTestbenchClassPath());
+  }
+
+  public static List<String> getJDKPath() {
+    return itemToPath(getJDKClassPath());
   }
 
   public static String getBaseMPSPath() {
@@ -64,15 +120,6 @@ public class CommonPaths {
   public static List<String> getMPSPaths() {
     return itemToPath(getMPSClassPath());
   }
-
-  public static List<String> getTestbenchPaths() {
-    return itemToPath(getTestbenchClassPath());
-  }
-
-  public static List<String> getJDKPath() {
-    return itemToPath(getJDKClassPath());
-  }
-
 
   //------classpaths : JDK--------
 
