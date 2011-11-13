e77ee9a5e3e0ad64b9b8ffc5b9e08375aa204581
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2011-11-13 16:53:26 +0400

    rebuild testbench.make

--- testbench/tests_gen/jetbrains/mps/testbench/ProjectTestHelper.java
+++ testbench/tests_gen/jetbrains/mps/testbench/ProjectTestHelper.java
@@ -8,7 +8,6 @@ import java.util.List;
 import com.intellij.util.PathUtil;
 import java.io.File;
 import com.intellij.openapi.application.PathMacros;
-import jetbrains.mps.util.FileUtil;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 import org.apache.log4j.Level;
@@ -326,7 +325,7 @@ public class ProjectTestHelper {
       List<String> result = ListSequence.fromList(new ArrayList<String>());
       BufferedReader in = null;
       try {
-        in = new BufferedReader(new InputStreamReader(new FileInputStream(f), FileUtil.DEFAULT_CHARSET));
+        in = new BufferedReader(new InputStreamReader(new FileInputStream(f), "utf-8"));
         String line;
         while ((line = in.readLine()) != null) {
           ListSequence.fromList(result).addElement(line);
