d81241882d86c8356e1192e5701afa3683d791e5
Evgeny Gerashchenko <Evgeny.Gerashchenko@jetbrains.com>
2011-12-02 14:05:54 +0400

    Made blame dialog and runtime able to post issues with custom subsystem, attached files and hidden by default.
     #MPS-14901 in progress

--- workbench/mps-workbench/source/jetbrains/mps/ide/blame/command/Command.java
+++ workbench/mps-workbench/source/jetbrains/mps/ide/blame/command/Command.java
@@ -17,16 +17,31 @@ package jetbrains.mps.ide.blame.command;
 
 import jetbrains.mps.ide.blame.perform.Query;
 import jetbrains.mps.ide.blame.perform.Response;
+import jetbrains.mps.util.JDOMUtil;
 import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.NameValuePair;
 import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.multipart.FilePart;
+import org.apache.commons.httpclient.methods.multipart.MultipartRequestEntity;
+import org.apache.commons.httpclient.methods.multipart.Part;
+import org.apache.commons.httpclient.methods.multipart.StringPart;
+import org.jdom.Document;
+import org.jdom.JDOMException;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.xml.sax.InputSource;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Command {
   public static final String YOUTRACK_BASE_URL = "http://youtrack.jetbrains.net";
   public static final String LOGIN = "/rest/user/login";
   public static final String POST_ISSUE = "/rest/issue/";
+  public static final String ISSUE_COMMAND_FORMAT = "/rest/issue/%s/execute";
   public static final String ISSUE_BASE_URL = YOUTRACK_BASE_URL + "/issue/";
 
   private static final String PROJECT = "MPS";
@@ -38,6 +53,10 @@ public class Command {
   private static final String SUMMARY_PARAM_NAME = "summary";
   private static final String DESCRIPTION_PARAM_NAME = "description";
   private static final String TYPE_PARAM_NAME = "type";
+  private static final String PERMITTED_GROUP_PARAM_NAME = "permittedGroup";
+  private static final String MPS_GROUP_NAME = "mps-developers";
+  private static final String COMMAND_PARAM_NAME = "command";
+  private static final String SUBSYSTEM_COMMAND_FORMAT = "Subsystem %s";
 
   public static Response login(final HttpClient c, Query query) throws IOException {
     PostMethod p = new PostMethod(YOUTRACK_BASE_URL + LOGIN);
@@ -55,12 +74,25 @@ public class Command {
   }
 
   @NotNull
-  public static Response postIssue(HttpClient c, String summary, String description) throws IOException {
+  public static Response postIssue(HttpClient c, String summary, String description, boolean hidden,  File... files) throws IOException {
     PostMethod p = new PostMethod(YOUTRACK_BASE_URL + POST_ISSUE);
     p.addParameter(PROJECT_PARAM_NAME, PROJECT);
     p.addParameter(SUMMARY_PARAM_NAME, summary);
     p.addParameter(DESCRIPTION_PARAM_NAME, description);
     p.addParameter(TYPE_PARAM_NAME, EXCEPTION);
+    if (hidden) {
+      p.addParameter(PERMITTED_GROUP_PARAM_NAME, MPS_GROUP_NAME);
+    }
+    if (files.length != 0) {
+      List<Part> parts = new ArrayList<Part>();
+      for (NameValuePair nameValuePair : p.getParameters()) {
+        parts.add(new StringPart(nameValuePair.getName(), nameValuePair.getValue()));
+      }
+      for (File file : files) {
+        parts.add(new FilePart(file.getName(), file));
+      }
+      p.setRequestEntity(new MultipartRequestEntity(parts.toArray(new Part[parts.size()]), p.getParams()));
+    }
     c.executeMethod(p);
 
     int statusCode = p.getStatusCode();
@@ -71,4 +103,20 @@ public class Command {
       return new Response("Can't post issue", responseString, false, null);
     }
   }
+
+  @NotNull
+  public static Response setIssueSubsystem(HttpClient c, @NotNull String issueId, @NotNull String subsystem) throws IOException {
+    PostMethod p = new PostMethod(YOUTRACK_BASE_URL + String.format(ISSUE_COMMAND_FORMAT, issueId));
+    p.addParameter(COMMAND_PARAM_NAME, String.format(SUBSYSTEM_COMMAND_FORMAT, subsystem));
+    c.executeMethod(p);
+
+    int statusCode = p.getStatusCode();
+    String responseString = p.getResponseBodyAsString();
+    if (statusCode == 200) {
+      return new Response("Issue posted", responseString, true, null);
+    } else {
+      return new Response("Can't post issue", responseString, false, null);
+    }
+  }
+
 }
