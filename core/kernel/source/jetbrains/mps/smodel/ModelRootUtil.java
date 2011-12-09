e304b7b6df102f13348a6f186522da4e3890a0d8
Alexander Shatalin <Alexander.Shatalin@jetbrains.com>
2011-12-09 22:28:18 +0100

    correcting merge mistake

--- core/kernel/source/jetbrains/mps/smodel/ModelRootUtil.java
+++ core/kernel/source/jetbrains/mps/smodel/ModelRootUtil.java
@@ -41,13 +41,11 @@ public class ModelRootUtil {
     if (modelFile == null) return result;
 
     IModule module = model.getModule();
-    String modelRootManager = model.getModelRootManager().getClass().getName();
-    String modelCanonicalPath = IFileUtils.getCanonicalPath(modelFile);
     for (SModelRoot modelRoot : module.getSModelRoots()) {
-      String rootManager = modelRoot.getManager().getClass().getName();
+      String modelCanonicalPath = IFileUtils.getCanonicalPath(modelFile);
       String rootCanonicalPath = FileUtil.getCanonicalPath(modelRoot.getPath());
 
-      if (modelRootManager.equals(rootManager) && modelCanonicalPath.startsWith(rootCanonicalPath)) {
+      if (modelCanonicalPath.startsWith(rootCanonicalPath)) {
         result.add(modelRoot);
       }
     }
