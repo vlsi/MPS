ad683007b9dd47492bdfdb6dd473d1198d921735
Evgeny Gerashchenko <Evgeny.Gerashchenko@jetbrains.com>
2012-01-18 01:19:19 +0400

    Unmerged model now have original content.

--- plugins/vcs/source_gen/jetbrains/mps/vcs/mergedriver/SimpleMerger.java
+++ plugins/vcs/source_gen/jetbrains/mps/vcs/mergedriver/SimpleMerger.java
@@ -8,15 +8,8 @@ import java.io.OutputStream;
 import jetbrains.mps.util.ReadUtil;
 import jetbrains.mps.util.FileUtil;
 import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import jetbrains.mps.smodel.persistence.def.ModelPersistence;
-import jetbrains.mps.internal.collections.runtime.ListSequence;
-import java.util.Arrays;
 
 /*package*/ class SimpleMerger extends AbstractFileMerger {
-  private static byte[] LINE_SEPARATOR = System.getProperty("line.separator").getBytes();
-
   /*package*/ SimpleMerger() {
   }
 
@@ -49,27 +42,7 @@ import java.util.Arrays;
         out.write(localContent);
         return MERGED;
       }
-      // Make possible to load model id correctly when model is in conflicting state 
-      out.write(extractHeader(localContent));
-
-      out.write(myConflictStart);
-      out.write(LINE_SEPARATOR);
-
-      // current 
       out.write(localContent);
-      out.write(mySeparator);
-      out.write(LINE_SEPARATOR);
-
-      // base 
-      out.write(baseContent);
-      out.write(mySeparator);
-      out.write(LINE_SEPARATOR);
-
-      // other 
-      out.write(latestContent);
-      out.write(myConflictEnd);
-      out.write(LINE_SEPARATOR);
-
       return CONFLICTS;
     } catch (IOException e) {
       e.printStackTrace();
@@ -84,24 +57,5 @@ import java.util.Arrays;
 
   private static String contentAsString(byte[] bytes) {
     return new String(bytes, FileUtil.DEFAULT_CHARSET).replace("\r\n", "\n");
-  }
-
-  private static byte[] extractHeader(byte[] xmlContent) {
-    String header = new String(xmlContent, 0, 1024, FileUtil.DEFAULT_CHARSET);
-
-    // Pattern for finds text like _<persistence version="7"_ (without underscores) 
-    Matcher matcher = Pattern.compile("<" + ModelPersistence.PERSISTENCE + "\\s+" + ModelPersistence.PERSISTENCE_VERSION + "=\"\\d+\"").matcher(header);
-    if (matcher.find()) {
-      int end = matcher.end();
-      // Try to find _</persistence>_ or _/>_ (without underscores 
-      for (String possibleEnd : ListSequence.fromList(Arrays.asList("</" + ModelPersistence.PERSISTENCE + ">", "/>"))) {
-        int indexOf = header.indexOf(possibleEnd, end + 1);
-        if (indexOf != -1) {
-          header = header.substring(0, indexOf + possibleEnd.length()) + System.getProperty("line.separator");
-          return header.getBytes(FileUtil.DEFAULT_CHARSET);
-        }
-      }
-    }
-    return new byte[0];
   }
 }
