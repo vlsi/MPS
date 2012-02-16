679540e0e4779d81c0562de518da996cdf564027
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2012-02-16 15:50:17 +0400

    highlight current line in plugin prototype

---
+++ IdeaPlugin/mps-debugger/src/jetbrains/mps/idea/debugger/IdeaDebuggerPositionHighlighter.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2003-2011 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jetbrains.mps.idea.debugger;
+
+import com.intellij.debugger.DebuggerManagerEx;
+import com.intellij.debugger.SourcePosition;
+import com.intellij.debugger.impl.DebuggerContextImpl;
+import com.intellij.debugger.impl.DebuggerContextListener;
+import com.intellij.debugger.impl.DebuggerManagerListener;
+import com.intellij.debugger.impl.DebuggerSession;
+import com.intellij.openapi.components.ProjectComponent;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.project.Project;
+import jetbrains.mps.debugger.core.AbstractCurrentLinePositionComponent;
+import jetbrains.mps.smodel.SNode;
+import org.jetbrains.annotations.NotNull;
+
+public class IdeaDebuggerPositionHighlighter extends AbstractCurrentLinePositionComponent<DebuggerSession> implements ProjectComponent {
+    private DebuggerManagerEx myDebuggerManager;
+    private final DebuggerManagerListener myDebuggerManagerListener = new MyDebuggerManagerListener();
+    private final DebuggerContextListener mySessionListener = new MyDebuggerContextListener();
+
+    public IdeaDebuggerPositionHighlighter(Project project, FileEditorManager fileEditorManager) {
+        super(project, fileEditorManager);
+    }
+
+    @Override
+    protected SNode getNode(DebuggerSession session) {
+        SourcePosition sourcePosition = session.getContextManager().getContext().getSourcePosition();
+        if (sourcePosition instanceof MpsSourcePosition) {
+            return ((MpsSourcePosition) sourcePosition).getNode();
+        }
+        return null;
+    }
+
+    @Override
+    public void initComponent() {
+        myDebuggerManager = DebuggerManagerEx.getInstanceEx(myProject);
+        myDebuggerManager.addDebuggerManagerListener(myDebuggerManagerListener);
+    }
+
+    @Override
+    public void disposeComponent() {
+        myDebuggerManager.removeDebuggerManagerListener(myDebuggerManagerListener);
+    }
+
+    @NotNull
+    @Override
+    public String getComponentName() {
+        return "Idea Debugger Position Highlighter";
+    }
+
+    @Override
+    public void projectOpened() {
+    }
+
+    @Override
+    public void projectClosed() {
+    }
+
+    private class MyDebuggerManagerListener implements DebuggerManagerListener {
+        @Override
+        public void sessionCreated(DebuggerSession session) {
+            session.getContextManager().addListener(mySessionListener);
+        }
+
+        @Override
+        public void sessionRemoved(DebuggerSession session) {
+            detachPainter(session);
+            session.getContextManager().removeListener(mySessionListener);
+        }
+    }
+
+    private class MyDebuggerContextListener implements DebuggerContextListener {
+        @Override
+        public void changeEvent(DebuggerContextImpl newContext, int event) {
+            reAttachPainter(newContext.getDebuggerSession());
+        }
+    }
+}
---
+++ IdeaPlugin/mps-debugger/src/jetbrains/mps/idea/debugger/MpsSourcePosition.java
@@ -0,0 +1,141 @@
+/*
+ * Copyright 2003-2011 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jetbrains.mps.idea.debugger;
+
+import com.intellij.debugger.SourcePosition;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.PathManager;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiManager;
+import jetbrains.mps.ide.navigation.NodeNavigatable;
+import jetbrains.mps.ide.project.ProjectHelper;
+import jetbrains.mps.smodel.*;
+import jetbrains.mps.smodel.descriptor.source.ModelDataSource;
+import jetbrains.mps.smodel.descriptor.source.RegularModelDataSource;
+import jetbrains.mps.util.Computable;
+import jetbrains.mps.workbench.nodesFs.MPSNodeVirtualFile;
+import jetbrains.mps.workbench.nodesFs.MPSNodesVirtualFileSystem;
+import org.jetbrains.annotations.NotNull;
+
+public class MpsSourcePosition extends SourcePosition {
+    private final SNodePointer myNodePointer;
+    private final Project myProject;
+    private final NodeNavigatable myNavigatable;
+    private final MpsPositionManager myPositionManager;
+
+    public MpsSourcePosition(MpsPositionManager manager, SNodePointer node, Project project) {
+        myNodePointer = node;
+        myProject = project;
+        myPositionManager = manager;
+
+        myNavigatable = new NodeNavigatable(ProjectHelper.toMPSProject(myProject), myNodePointer);
+    }
+
+    public MPSNodeVirtualFile getVirtualFile() {
+        return MPSNodesVirtualFileSystem.getInstance().getFileFor(myNodePointer);
+    }
+
+    public MPSNodeVirtualFile getRootVirtualFile() {
+        SNodePointer rootPointer = ModelAccess.instance().runReadAction(new Computable<SNodePointer>() {
+            @Override
+            public SNodePointer compute() {
+                return new SNodePointer(myNodePointer.getNode().getContainingRoot());
+            }
+        });
+        return MPSNodesVirtualFileSystem.getInstance().getFileFor(rootPointer);
+    }
+
+    @NotNull
+    @Override
+    public PsiFile getFile() {
+        final String path = ModelAccess.instance().runReadAction(new Computable<String>() {
+            @Override
+            public String compute() {
+                SModelDescriptor modelDescriptor = myNodePointer.getNode().getModel().getModelDescriptor();
+                if (modelDescriptor instanceof BaseSModelDescriptorWithSource) {
+                    ModelDataSource source = ((BaseSModelDescriptorWithSource) modelDescriptor).getSource();
+                    if (source instanceof RegularModelDataSource) {
+                        return ((RegularModelDataSource) source).getFilesToListen().iterator().next();
+                    }
+                }
+                return null;
+            }
+        });
+        return ApplicationManager.getApplication().runReadAction(new com.intellij.openapi.util.Computable<PsiFile>() {
+            @Override
+            public PsiFile compute() {
+                if (path == null) {
+                    // we should return something %|
+                    return PsiManager.getInstance(myProject).findFile(LocalFileSystem.getInstance().findFileByPath(PathManager.getBinPath()));
+                }
+                return PsiManager.getInstance(myProject).findFile(LocalFileSystem.getInstance().findFileByPath(path));
+            }
+        });
+    }
+
+    @Override
+    public PsiElement getElementAt() {
+        return null;
+    }
+
+    @Override
+    public int getLine() {
+        return 0;
+    }
+
+    @Override
+    public int getOffset() {
+        return 0;
+    }
+
+    @Override
+    public Editor openEditor(boolean requestFocus) {
+        FileEditorManager.getInstance(myProject).openFile(getRootVirtualFile(), requestFocus);
+        // do we neccessary need to highlight when opening
+        myPositionManager.highlight(myNodePointer);
+        return null;
+    }
+
+    @Override
+    public void navigate(boolean requestFocus) {
+        myNavigatable.navigate(requestFocus);
+    }
+
+    @Override
+    public boolean canNavigate() {
+        return myNavigatable.canNavigate();
+    }
+
+    @Override
+    public boolean canNavigateToSource() {
+        return myNavigatable.canNavigateToSource();
+    }
+
+    public SNode getNode() {
+        return ModelAccess.instance().runReadAction(new Computable<SNode>() {
+            @Override
+            public SNode compute() {
+                return myNodePointer.getNode();
+            }
+        });
+    }
+}
