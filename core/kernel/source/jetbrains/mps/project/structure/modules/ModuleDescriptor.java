00e573d973b595c53fc8cd605a42a415d98ea330
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2012-02-20 17:36:14 +0300

    remove unused code

--- core/kernel/source/jetbrains/mps/project/structure/modules/ModuleDescriptor.java
+++ core/kernel/source/jetbrains/mps/project/structure/modules/ModuleDescriptor.java
@@ -26,8 +26,6 @@ public class ModuleDescriptor {
   private String myTimestamp;
   private boolean myCompileInMPS = true;
 
-  private boolean myEnableJavaStubs;
-
   private List<jetbrains.mps.project.structure.model.ModelRoot> myModelRoots;
   private List<Dependency> myDependencies;
   private List<ModuleReference> myUsedLanguages;
@@ -81,14 +79,6 @@ public class ModuleDescriptor {
 
   public void setCompileInMPS(boolean compileInMPS) {
     myCompileInMPS = compileInMPS;
-  }
-
-  public boolean getEnableJavaStubs() {
-    return myEnableJavaStubs;
-  }
-
-  public void setEnableJavaStubs(boolean enableJavaStubs) {
-    myEnableJavaStubs = enableJavaStubs;
   }
 
   public List<ModelRoot> getModelRoots() {
