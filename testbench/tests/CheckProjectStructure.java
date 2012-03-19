c4e5d715400e191a58885a72be51e3bed5aed138
Evgeny Gryaznov <Evgeny.Gryaznov@jetbrains.com>
2012-03-19 01:18:54 +0100

    rewrite CheckProjectStructure: get rid of TOKEN & ModelsExtractor

--- testbench/tests/CheckProjectStructure.java
+++ testbench/tests/CheckProjectStructure.java
@@ -16,11 +16,9 @@
 import jetbrains.mps.library.ModulesMiner;
 import jetbrains.mps.library.ModulesMiner.ModuleHandle;
 import jetbrains.mps.testbench.CheckProjectStructureHelper;
-import jetbrains.mps.testbench.CheckProjectStructureHelper.Token;
 import jetbrains.mps.testbench.junit.Order;
 import jetbrains.mps.testbench.junit.runners.WatchingParameterizedWithMake;
 import jetbrains.mps.vfs.FileSystem;
-import jetbrains.mps.vfs.IFile;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -42,7 +40,6 @@ import java.util.List;
 public class CheckProjectStructure {
 
   private static CheckProjectStructureHelper HELPER;
-  private static Token TOKEN;
 
   private static String getDescription(ModuleHandle handle) {
     if (handle.getFile().getName().endsWith(".mpl")) {
@@ -56,13 +53,18 @@ public class CheckProjectStructure {
   @Parameters
   public static List<Object[]> filePaths() {
     HELPER = new CheckProjectStructureHelper();
-    TOKEN = HELPER.init(new String[][]{{"samples_home", System.getProperty("user.dir") + "/samples"}});
+    HELPER.init(new String[][]{{"samples_home", System.getProperty("user.dir") + "/samples"}});
 
     List<ModuleHandle> moduleHandles = ModulesMiner.getInstance().collectModules(FileSystem.getInstance().getFileByPath(System.getProperty("user.dir")), false);
 
     ArrayList<Object[]> res = new ArrayList<Object[]>();
     for (ModuleHandle moduleHandle : moduleHandles) {
-      res.add(new Object[]{getDescription(moduleHandle), moduleHandle.getFile()});
+      if (moduleHandle.getFile().getName().endsWith(".iml")) {
+        // temporary ignore .iml files
+        continue;
+      }
+
+      res.add(new Object[]{getDescription(moduleHandle), moduleHandle});
     }
 
     Collections.sort(res, new Comparator<Object[]>() {
@@ -76,52 +78,47 @@ public class CheckProjectStructure {
 
   @BeforeClass
   public static void init() {
-    List<ModuleHandle> moduleHandles = ModulesMiner.getInstance().collectModules(FileSystem.getInstance().getFileByPath(System.getProperty("user.dir")), false);
-    List<IFile> files = new ArrayList<IFile>();
-    for (ModuleHandle moduleHandle : moduleHandles) {
-      files.add(moduleHandle.getFile());
-    }
-
-    HELPER.load(files);
+    CheckProjectStructureHelper.loadModules(ModulesMiner.getInstance().collectModules(FileSystem.getInstance().getFileByPath(System.getProperty("user.dir")), false));
   }
 
   @AfterClass
   public static void cleanUp() {
-    HELPER.cleanUp(TOKEN);
+    HELPER.cleanUp();
     HELPER.dispose();
   }
 
-  private IFile file;
+  /* module handle */
+  private ModuleHandle handle;
 
-  public CheckProjectStructure(String testName, IFile file) {
-    this.file = file;
+  public CheckProjectStructure(String testName, ModuleHandle handle) {
+    this.handle = handle;
   }
 
   @Test
   @Order(1)
   public void checkReferences() {
-    List<String> errors = HELPER.check(TOKEN, Collections.singletonList(file));
+    List<String> errors = HELPER.check(handle);
     Assert.assertTrue("Reference errors:\n" + HELPER.formatErrors(errors), errors.isEmpty());
   }
 
   @Test
   @Order(2)
   public void checkStructure() {
-    List<String> errors = HELPER.checkStructure(TOKEN, Collections.singletonList(file));
+    List<String> errors = HELPER.checkStructure(handle);
     Assert.assertTrue("Structure errors:\n" + HELPER.formatErrors(errors), errors.isEmpty());
   }
 
   @Test
   @Order(3)
   public void checkGenerationStatus() {
-    List<String> errors = HELPER.checkGenerationStatus(TOKEN, Collections.singletonList(file));
+    List<String> errors = HELPER.checkGenerationStatus(handle);
     Assert.assertTrue("Try to regenerate models:\n" + HELPER.formatErrors(errors), errors.isEmpty());
   }
 
   @Test
   @Order(4)
   public void checkModuleProperties() {
-    List<String> errors = HELPER.checkModule(TOKEN, Collections.singletonList(file));
+    List<String> errors = HELPER.checkModule(handle);
     Assert.assertTrue("Module property or dependency errors:\n" + HELPER.formatErrors(errors), errors.isEmpty());
   }
 }
