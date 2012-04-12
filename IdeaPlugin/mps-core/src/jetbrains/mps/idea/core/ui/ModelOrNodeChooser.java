fcdfe00f9b445b4c3c73ec3de7667912f5e8108a
Semen Alperovich <Semen.Alperovich@jetbrains.com>
2012-04-12 17:23:20 +0400

    fix errors: assertion failed if some Module hasn't MPSFacet, directory isn't shown if it is a parent of a model root

--- IdeaPlugin/mps-core/src/jetbrains/mps/idea/core/ui/ModelOrNodeChooser.java
+++ IdeaPlugin/mps-core/src/jetbrains/mps/idea/core/ui/ModelOrNodeChooser.java
@@ -1,19 +1,18 @@
-
 /*
- * Copyright 2003-2012 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* Copyright 2003-2012 JetBrains s.r.o.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 package jetbrains.mps.idea.core.ui;
 
@@ -26,6 +25,7 @@ import com.intellij.ide.projectView.impl.nodes.PsiFileNode;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleUtil;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.VirtualFileManager;
 import com.intellij.psi.impl.source.PsiPlainTextFileImpl;
@@ -40,61 +40,44 @@ import jetbrains.mps.smodel.SModelDescriptor;
 import jetbrains.mps.smodel.SModelRepository;
 import jetbrains.mps.smodel.SNode;
 import jetbrains.mps.vfs.IFile;
+import org.jetbrains.annotations.Nullable;
 
 import javax.swing.JComponent;
 import java.util.ArrayList;
 import java.util.Set;
 
-public class ModelOrNodeChooser extends ProjectViewPane implements ModelElementTargetChooser{
+public class ModelOrNodeChooser extends ProjectViewPane implements ModelElementTargetChooser {
   private Object myInitialValue;
   private Project myProject;
+
   public ModelOrNodeChooser(Project project, Object initialValue) {
     super(project);
     myInitialValue = initialValue;
     myProject = project;
-
-    
-  //  this.setTreeBuilder(new AbstractTreeBuilder());
   }
 
   @Override
   public JComponent getComponent() {
-    //JTree tree
     return createComponent();
   }
 
 
-
   protected ProjectAbstractTreeStructureBase createStructure() {
-    return new ProjectTreeStructure(myProject, ID){
+    return new ProjectTreeStructure(myProject, ID) {
+      private VirtualFile baseDir = myProject.getBaseDir();
+
       public Object[] getChildElements(Object element) {
-
         ArrayList<Object> result = new ArrayList<Object>();
-        for (Object o : super.getChildElements(element)){
-
-          if (o instanceof PsiFileNode ){
-            if ((((PsiFileNode) o).getValue() instanceof PsiPlainTextFileImpl)&&(((PsiFileNode) o).getValue().getFileType() instanceof MPSFileType)){
-              result.add(o);
-            }
-          }else if (o instanceof MPSProjectViewNode){
+        for (Object o : super.getChildElements(element)) {
+          if (o instanceof PsiFileNode && ((PsiFileNode) o).getValue().getFileType() instanceof MPSFileType) {
             result.add(o);
-          }else if (o instanceof PsiDirectoryNode){
+          } else if (o instanceof MPSProjectViewNode) {
+            result.add(o);
+          } else if (o instanceof PsiDirectoryNode) {
             VirtualFile virtualFile = ((PsiDirectoryNode) o).getVirtualFile();
-            assert virtualFile != null;
-            if (myProject.getBaseDir().equals(virtualFile)){
+            if (virtualFile == null) continue;
+            if (virtualFile.equals(baseDir) || isModelRootOrParent(virtualFile)) {
               result.add(o);
-              continue;
-            }
-            Module module = ModuleUtil.findModuleForFile(virtualFile,myProject);
-            if (module == null){
-              continue;
-            }
-            MPSFacet mpsFacet = FacetManager.getInstance(module).getFacetByType(MPSFacetType.ID);
-            assert mpsFacet != null;
-
-            ModelRoot modelRoot = new ModelRoot(VirtualFileManager.extractPath(virtualFile.getUrl()));
-            if (mpsFacet.getConfiguration().getState().getModelRoots().contains(modelRoot)){
-              result.add(o);  
             }
           }
         }
@@ -103,13 +86,31 @@ public class ModelOrNodeChooser extends ProjectViewPane implements ModelElementT
     };
 
   }
+
+  private boolean isModelRootOrParent(VirtualFile virtualFile) {
+    Module module = ModuleUtil.findModuleForFile(virtualFile, myProject);
+    if (module == null) return false;
+    MPSFacet mpsFacet = FacetManager.getInstance(module).getFacetByType(MPSFacetType.ID);
+    if (mpsFacet == null || !(mpsFacet.wasInitialized())) return false;
+    if (!(virtualFile.isDirectory())) return false;
+
+    String url = virtualFile.getUrl();
+    if (!LocalFileSystem.PROTOCOL.equals(VirtualFileManager.extractProtocol(url))) return false;
+
+    String path = VirtualFileManager.extractPath(url);
+    for (ModelRoot mr : mpsFacet.getConfiguration().getState().getModelRoots()) {
+      if (mr.getPath().startsWith(path)) return true;
+    }
+    return false;
+  }
+
   @Override
   public Object getSelectedObject() {
-    if ((getSelectedNode() != null) && (getSelectedNode().getUserObject() instanceof MPSProjectViewNode)){
-      return ((MPSProjectViewNode)(getSelectedNode().getUserObject())).getValue().getNode();
-    }else{
+    if ((getSelectedNode() != null) && (getSelectedNode().getUserObject() instanceof MPSProjectViewNode)) {
+      return ((MPSProjectViewNode) (getSelectedNode().getUserObject())).getValue().getNode();
+    } else {
       Set<IFile> models = MPSDataKeys.MODEL_FILES.getData(this);
-      if(models != null && models.size() == 1){
+      if (models != null && models.size() == 1) {
         return SModelRepository.getInstance().findModel(models.iterator().next());
       }
     }
