85e56024d7e5bdb8b74c52265407c3c14210d5fd
Ilya Lintsbakh <ilya.lintsbakh@jetbrains.com>
2012-04-16 13:21:27 +0400

    Don't report root error

--- core/typesystemEngine/source/jetbrains/mps/newTypesystem/TypeSystemComponent.java
+++ core/typesystemEngine/source/jetbrains/mps/newTypesystem/TypeSystemComponent.java
@@ -257,7 +257,7 @@ class TypeSystemComponent extends CheckingComponent {
       if (type == null) {
         if (node.isRoot()) {
           myNodeTypesComponent.getTypeCheckingContext().setSingleTypeComputation(false);
-          System.out.println("Root: " + initialNode.getDebugText());
+          //System.out.println("Root: " + initialNode.getDebugText());
           computeTypes(node, true, true, Collections.<SNode>emptyList(), true, initialNode);
           type = getType(initialNode);
           if (type == null && node != initialNode && myTypeChecker.isGenerationMode()) {
