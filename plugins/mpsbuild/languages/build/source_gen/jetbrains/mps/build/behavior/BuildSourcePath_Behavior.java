1d033ebaadfde16d1cc98956fa64adc0531f955f
Evgeny Gryaznov <Evgeny.Gryaznov@jetbrains.com>
2012-05-01 19:36:12 +0200

    build: regen

--- plugins/mpsbuild/languages/build/source_gen/jetbrains/mps/build/behavior/BuildSourcePath_Behavior.java
+++ plugins/mpsbuild/languages/build/source_gen/jetbrains/mps/build/behavior/BuildSourcePath_Behavior.java
@@ -6,8 +6,8 @@ import jetbrains.mps.smodel.SNode;
 import jetbrains.mps.build.util.Context;
 import jetbrains.mps.build.util.MacroHelper;
 import org.jetbrains.annotations.Nullable;
-import jetbrains.mps.smodel.structure.BehaviorDescriptor;
-import jetbrains.mps.smodel.structure.ConceptRegistry;
+import jetbrains.mps.smodel.runtime.BehaviorDescriptor;
+import jetbrains.mps.smodel.language.ConceptRegistry;
 import jetbrains.mps.lang.smodel.generator.smodelAdapter.SNodeOperations;
 import jetbrains.mps.smodel.behaviour.BehaviorManager;
 
