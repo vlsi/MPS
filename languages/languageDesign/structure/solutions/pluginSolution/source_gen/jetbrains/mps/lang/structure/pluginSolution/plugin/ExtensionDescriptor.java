9d0c6bf9b3819e673e3984a40fb6c6386a6b860a
Fedor Isakov <fisakov@gmail.com>
2012-05-17 16:58:52 +0200

    Rebuilt instances of ExtensionDeclaration

--- languages/languageDesign/structure/solutions/pluginSolution/source_gen/jetbrains/mps/lang/structure/pluginSolution/plugin/ExtensionDescriptor.java
+++ languages/languageDesign/structure/solutions/pluginSolution/source_gen/jetbrains/mps/lang/structure/pluginSolution/plugin/ExtensionDescriptor.java
@@ -7,7 +7,7 @@ import jetbrains.mps.smodel.structure.Extension;
 import java.util.Arrays;
 
 public class ExtensionDescriptor extends DefaultExtensionDescriptor {
-  private Extension[] extensions = new Extension[]{new Extension_NodeRenamer_()};
+  private Extension[] extensions = new Extension[]{new Extension_NodeRenamer7972144475523791621_()};
 
   public ExtensionDescriptor() {
   }
