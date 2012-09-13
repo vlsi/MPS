b6588a3e5d1eac57c857840b40e80a660ba2ed0f
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2012-09-13 18:43:27 +0400

    fix MPS-13613 Path variables don't applies on project load: show path macro configurable on clicking "fix..."

--- workbench/mps-platform/source/jetbrains/mps/ide/project/WorkbenchPathMacros.java
+++ workbench/mps-platform/source/jetbrains/mps/ide/project/WorkbenchPathMacros.java
@@ -15,17 +15,25 @@
  */
 package jetbrains.mps.ide.project;
 
+import com.intellij.application.options.pathMacros.PathMacroConfigurable;
+import com.intellij.ide.DataManager;
 import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationListener;
 import com.intellij.notification.NotificationType;
 import com.intellij.notification.Notifications;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.application.PathMacros;
 import com.intellij.openapi.components.ApplicationComponent;
+import com.intellij.openapi.options.ShowSettingsUtil;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.AsyncResult.Handler;
 import jetbrains.mps.ide.MPSCoreComponents;
 import jetbrains.mps.project.PathMacrosProvider;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkEvent.EventType;
 import java.util.Set;
 
 public class WorkbenchPathMacros implements ApplicationComponent, PathMacrosProvider {
@@ -65,11 +73,15 @@ public class WorkbenchPathMacros implements ApplicationComponent, PathMacrosProv
 
   @Override
   public void report(String message, String macro) {
-    Notifications.Bus.notify(new Notification("Path manager", "Undefined macro", macro+" <html><a href=\"\">fix...</a></html>", NotificationType.ERROR, new NotificationListener() {
-      @Override
+    Notifications.Bus.notify(new Notification(Notifications.SYSTEM_MESSAGES_GROUP_ID, "Undefined macro", macro + " <html><a href=''>fix...</a></html>", NotificationType.ERROR, new NotificationListener() {
       public void hyperlinkUpdate(@NotNull Notification notification, @NotNull HyperlinkEvent event) {
-        notification.expire();
-        System.out.print(123);
+        if (event.getEventType()!= EventType.ACTIVATED) return;
+        DataManager.getInstance().getDataContextFromFocus().doWhenDone(new Handler<DataContext>() {
+          public void run(DataContext dataContext) {
+            Project project = PlatformDataKeys.PROJECT.getData(dataContext);
+            ShowSettingsUtil.getInstance().showSettingsDialog(project, new PathMacroConfigurable().getDisplayName());
+          }
+        });
       }
     }));
   }
