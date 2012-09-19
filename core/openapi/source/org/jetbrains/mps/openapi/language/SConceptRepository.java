c2c5b2306e61616074d4b0a9e30a308f10fece48
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2012-09-19 18:49:47 +0400

    SConceptRepository implemented

--- core/openapi/source/org/jetbrains/mps/openapi/language/SConceptRepository.java
+++ core/openapi/source/org/jetbrains/mps/openapi/language/SConceptRepository.java
@@ -15,9 +15,25 @@
  */
 package org.jetbrains.mps.openapi.language;
 
+import jetbrains.mps.kernel.model.SModelUtil;
+import jetbrains.mps.project.GlobalScope;
+import jetbrains.mps.smodel.SNode;
+import jetbrains.mps.smodel.apiadapter.SConceptNodeAdapter;
+
 /**
  * This interface provides a per-concept access to SLanguageRepository
  */
-public interface SConceptRegistry {
-  SConcept getConcept(String id);
+public abstract class SConceptRepository {
+  private static SConceptRepository ourInstance = new SConceptRepository() {
+    public SConcept getConcept(String id) {
+      SNode cnode = SModelUtil.findConceptDeclaration(id, GlobalScope.getInstance());
+      return new SConceptNodeAdapter(cnode);
+    }
+  };
+
+  public static SConceptRepository getInstance(){
+    return ourInstance;
+  }
+
+  public abstract SConcept getConcept(String id);
 }
