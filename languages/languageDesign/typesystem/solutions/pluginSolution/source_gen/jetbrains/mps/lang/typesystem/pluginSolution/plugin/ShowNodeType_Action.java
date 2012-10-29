5ac136bc8d1d41d2c291e8bfe666c5b49104704a
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2012-10-29 18:38:49 +0300

    ShowNodeType action creates its own model to place type node into

--- languages/languageDesign/typesystem/solutions/pluginSolution/source_gen/jetbrains/mps/lang/typesystem/pluginSolution/plugin/ShowNodeType_Action.java
+++ languages/languageDesign/typesystem/solutions/pluginSolution/source_gen/jetbrains/mps/lang/typesystem/pluginSolution/plugin/ShowNodeType_Action.java
@@ -19,8 +19,6 @@ import jetbrains.mps.errors.IErrorReporter;
 import jetbrains.mps.smodel.SNode;
 import jetbrains.mps.smodel.ModelAccess;
 import jetbrains.mps.nodeEditor.EditorComponent;
-import jetbrains.mps.smodel.SModel;
-import jetbrains.mps.project.AuxilaryRuntimeModel;
 import javax.swing.JOptionPane;
 import java.awt.Frame;
 import jetbrains.mps.typesystem.uiActions.MyBaseNodeDialog;
@@ -80,31 +78,32 @@ public class ShowNodeType_Action extends BaseAction {
       final Wrappers._T<TypeCheckingContext> typeCheckingContext = new Wrappers._T<TypeCheckingContext>();
       final Wrappers._T<IErrorReporter> reporter = new Wrappers._T<IErrorReporter>();
       final Wrappers._T<SNode> type = new Wrappers._T<SNode>();
+
+      ModelAccess.instance().runWriteAction(new Runnable() {
+        public void run() {
+          typeCheckingContext.value = ((EditorComponent) MapSequence.fromMap(_params).get("editorComponent")).getTypeCheckingContext();
+          type.value = typeCheckingContext.value.getTypeDontCheck(((SNode) MapSequence.fromMap(_params).get("node")));
+          reporter.value = typeCheckingContext.value.getTypeMessageDontCheck(((SNode) MapSequence.fromMap(_params).get("node")));
+        }
+      });
+
+      if (type.value == null) {
+        JOptionPane.showMessageDialog(((Frame) MapSequence.fromMap(_params).get("frame")), "no type");
+        return;
+      }
+
+      final AuxilaryRuntimeModel tmpModel = new AuxilaryRuntimeModel();
       try {
         ModelAccess.instance().runWriteAction(new Runnable() {
           public void run() {
-            typeCheckingContext.value = ((EditorComponent) MapSequence.fromMap(_params).get("editorComponent")).getTypeCheckingContext();
-            type.value = typeCheckingContext.value.getTypeDontCheck(((SNode) MapSequence.fromMap(_params).get("node")));
-            reporter.value = typeCheckingContext.value.getTypeMessageDontCheck(((SNode) MapSequence.fromMap(_params).get("node")));
-
-            if (type.value != null) {
-              SModel auxModel = AuxilaryRuntimeModel.getDescriptor().getSModel();
-              auxModel.addRoot(type.value);
-            }
+            tmpModel.register();
+            tmpModel.getSModel().addRoot(type.value);
           }
         });
-
-        if (type.value == null) {
-          JOptionPane.showMessageDialog(((Frame) MapSequence.fromMap(_params).get("frame")), "no type");
-          return;
-        }
-
         new MyBaseNodeDialog(((IOperationContext) MapSequence.fromMap(_params).get("context")), ((SNode) MapSequence.fromMap(_params).get("node")), type.value, reporter.value).showDialog();
       } finally {
-        if (type.value != null) {
-          SModel auxModel = AuxilaryRuntimeModel.getDescriptor().getSModel();
-          auxModel.removeRoot(type.value);
-        }
+        tmpModel.getSModel().removeRoot(type.value);
+        tmpModel.unregister();
       }
     } catch (Throwable t) {
       if (log.isErrorEnabled()) {
