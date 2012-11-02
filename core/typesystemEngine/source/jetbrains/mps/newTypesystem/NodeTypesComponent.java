a1d78456d2268d65ffae49fe96cd2f786c8b38e6
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2012-11-03 02:02:55 +0400

    got rid of some more getContainingRoot usages

--- core/typesystemEngine/source/jetbrains/mps/newTypesystem/NodeTypesComponent.java
+++ core/typesystemEngine/source/jetbrains/mps/newTypesystem/NodeTypesComponent.java
@@ -259,16 +259,15 @@ public class NodeTypesComponent {
     keySet.addAll(nodesToErrorsMapNT.keySet());
     for (SNode key : keySet) {
       List<IErrorReporter> reporters = getErrors(key);
-      if (!reporters.isEmpty()) {
-        if (key.getContainingRoot() == null) {
-          /*  LOG.warning("Type system reports error for node without containing root. Node: " + key);
-                    for (IErrorReporter reporter : reporters) {
-                      LOG.warning("This error was reported from: model: " + reporter.getRuleModel() + " id: " + reporter.getRuleId());
-                    }     */
-          continue;
-        }
-        result.add(new Pair<SNode, List<IErrorReporter>>(key, reporters));
+      if (reporters.isEmpty()) continue;
+      if (key.getContainingRoot() == null) {
+        /*  LOG.warning("Type system reports error for node without containing root. Node: " + key);
+                  for (IErrorReporter reporter : reporters) {
+                    LOG.warning("This error was reported from: model: " + reporter.getRuleModel() + " id: " + reporter.getRuleId());
+                  }     */
+        continue;
       }
+      result.add(new Pair<SNode, List<IErrorReporter>>(key, reporters));
     }
     return result;
   }
