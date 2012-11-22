2b6241c2d97086523fa0e940aeff13753bd5b6bb
Timur Abishev <timur.abishev@jetbrains.com>
2012-11-22 17:15:17 +0400

    Made smodelLanguage-samples compile in MPS

--- languages/languageDesign/smodel/solutions/samples/source_gen/smodelLanguage/samples/LinkRef.java
+++ languages/languageDesign/smodel/solutions/samples/source_gen/smodelLanguage/samples/LinkRef.java
@@ -4,11 +4,11 @@ package smodelLanguage.samples;
 
 import jetbrains.mps.smodel.SNode;
 import jetbrains.mps.lang.smodel.generator.smodelAdapter.SLinkOperations;
-import jetbrains.mps.smodel.behaviour.BehaviorReflection;
+import jetbrains.mps.lang.structure.behavior.LinkDeclaration_Behavior;
 
 public class LinkRef {
   public void test1() {
     SNode link = SLinkOperations.findLinkDeclaration("jetbrains.mps.baseLanguage.structure.DotExpression", "operation");
-    boolean singular = BehaviorReflection.invokeNonVirtual(Boolean.TYPE, link, "jetbrains.mps.lang.structure.structure.LinkDeclaration", "call_isSingular_1213877254557", new Object[]{});
+    boolean singular = LinkDeclaration_Behavior.call_isSingular_1213877254557(link);
   }
 }
