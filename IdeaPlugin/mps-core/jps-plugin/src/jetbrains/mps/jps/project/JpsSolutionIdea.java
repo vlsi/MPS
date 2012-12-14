92d1f6393595165358e062ed1674fc1d67ab6e2f
Evgeny Gryaznov <Evgeny.Gryaznov@jetbrains.com>
2012-12-14 15:04:49 +0100

    [IdeaPlugin] fix assertion

--- IdeaPlugin/mps-core/jps-plugin/src/jetbrains/mps/jps/project/JpsSolutionIdea.java
+++ IdeaPlugin/mps-core/jps-plugin/src/jetbrains/mps/jps/project/JpsSolutionIdea.java
@@ -1,17 +1,13 @@
 package jetbrains.mps.jps.project;
 
 import jetbrains.mps.idea.core.project.JpsModelRootContributor;
-import jetbrains.mps.project.ModuleId;
 import jetbrains.mps.project.Solution;
-import jetbrains.mps.project.structure.modules.ModuleReference;
 import jetbrains.mps.project.structure.modules.SolutionDescriptor;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.jps.model.module.JpsModule;
 import org.jetbrains.jps.service.JpsServiceManager;
-import org.jetbrains.mps.openapi.module.SModuleId;
 import org.jetbrains.mps.openapi.persistence.ModelRoot;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -23,35 +19,33 @@ import java.util.Set;
 
 public class JpsSolutionIdea extends Solution {
 
-  private JpsModule myModule;
-  private Set<ModelRoot> myContributedModelRoots;
+    private JpsModule myModule;
+    private Set<ModelRoot> myContributedModelRoots;
 
-  public JpsSolutionIdea(@NotNull JpsModule module, SolutionDescriptor descriptor) {
-    super(descriptor, null);
-    myModule = module;
-    String name = module.getName();
-    setModuleReference(new ModuleReference(name, ModuleId.foreign(name)));
-  }
+    public JpsSolutionIdea(@NotNull JpsModule module, SolutionDescriptor descriptor) {
+        super(descriptor, null);
+        myModule = module;
+    }
 
-  @Override
-  protected Iterable<ModelRoot> loadRoots() {
-    if (myContributedModelRoots == null) {
-      myContributedModelRoots = new HashSet<ModelRoot>();
-      for (JpsModelRootContributor c : JpsServiceManager.getInstance().getExtensions(JpsModelRootContributor.class)) {
-        for (ModelRoot root : c.getModelRoots(myModule)) {
-          myContributedModelRoots.add(root);
+    @Override
+    protected Iterable<ModelRoot> loadRoots() {
+        if (myContributedModelRoots == null) {
+            myContributedModelRoots = new HashSet<ModelRoot>();
+            for (JpsModelRootContributor c : JpsServiceManager.getInstance().getExtensions(JpsModelRootContributor.class)) {
+                for (ModelRoot root : c.getModelRoots(myModule)) {
+                    myContributedModelRoots.add(root);
+                }
+            }
         }
-      }
+
+        List<ModelRoot> sum = new ArrayList<ModelRoot>();
+        for (ModelRoot mr : super.loadRoots()) {
+            sum.add(mr);
+        }
+
+        sum.addAll(myContributedModelRoots);
+
+        return sum;
     }
-
-    List<ModelRoot> sum = new ArrayList<ModelRoot>();
-    for (ModelRoot mr : super.loadRoots()) {
-      sum.add(mr);
-    }
-
-    sum.addAll(myContributedModelRoots);
-
-    return sum;
-  }
 
 }
