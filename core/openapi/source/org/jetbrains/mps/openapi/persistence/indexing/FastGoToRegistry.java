3247c057f4b803a9270f7956b7c35c4f470ce4fa
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2012-12-17 20:30:53 +0300

    registries for new ctrl-n and find usages

--- core/openapi/source/org/jetbrains/mps/openapi/persistence/indexing/FastGoToRegistry.java
+++ core/openapi/source/org/jetbrains/mps/openapi/persistence/indexing/FastGoToRegistry.java
@@ -1,4 +1,42 @@
 package org.jetbrains.mps.openapi.persistence.indexing;
 
-public class FastGoToRegistry {
+import jetbrains.mps.components.CoreComponent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FastGoToRegistry implements CoreComponent {
+  private static FastGoToRegistry INSTANCE;
+
+  public static FastGoToRegistry getInstance() {
+    return INSTANCE;
+  }
+
+  public void init() {
+    if (INSTANCE != null) {
+      throw new IllegalStateException("double initialization");
+    }
+
+    INSTANCE = this;
+  }
+
+  public void dispose() {
+    INSTANCE = null;
+  }
+
+  //------------CoreComponent stuff end----------------
+
+  private Map<String, NodeNavigationContributor> myRootType2Contributor = new HashMap<String, NodeNavigationContributor>();
+
+  public void addNavigationContributor(String id, NodeNavigationContributor contributor) {
+    myRootType2Contributor.put(id, contributor);
+  }
+
+  public void removeNavigationContributor(String id) {
+    myRootType2Contributor.remove(id);
+  }
+
+  public NodeNavigationContributor getNavigationContributor(String id) {
+    return myRootType2Contributor.get(id);
+  }
 }
