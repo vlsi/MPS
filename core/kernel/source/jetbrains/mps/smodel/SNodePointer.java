3c8f3d27a22f728934b7671e789d87b3145bbaab
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2012-12-18 21:25:57 +0300

    SNodeReference interface extended

--- core/kernel/source/jetbrains/mps/smodel/SNodePointer.java
+++ core/kernel/source/jetbrains/mps/smodel/SNodePointer.java
@@ -17,6 +17,7 @@ package jetbrains.mps.smodel;
 
 import jetbrains.mps.util.EqualUtil;
 import org.jetbrains.mps.openapi.model.SNodeReference;
+import org.jetbrains.mps.openapi.module.SRepository;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -66,6 +67,11 @@ public class SNodePointer implements SNodeReference {
     SModelReference modelReference = getCurrentSModelReference(myModelReference, myTimestamp);
     if (modelReference == null) return null;
     return getSModelRepository().getModelDescriptor(modelReference);
+  }
+
+  @Override
+  public org.jetbrains.mps.openapi.model.SNode resolve(SRepository repo) {
+    return getNode();
   }
 
   public SModelReference getModelReference() {
