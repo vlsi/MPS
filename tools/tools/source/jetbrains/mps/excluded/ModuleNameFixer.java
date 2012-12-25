4596ad3699d43b7a7a16bb07d24bc0c21901ce03
Timur Abishev <timur.abishev@jetbrains.com>
2012-12-25 16:53:49 +0400

    Implement renaming in ModuleNameFixer

--- tools/tools/source/jetbrains/mps/excluded/ModuleNameFixer.java
+++ tools/tools/source/jetbrains/mps/excluded/ModuleNameFixer.java
@@ -18,32 +18,87 @@ package jetbrains.mps.excluded;
 import jetbrains.mps.util.FileUtil;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
+import static jetbrains.mps.excluded.Utils.files;
 import static jetbrains.mps.excluded.Utils.root;
 import static jetbrains.mps.excluded.Utils.withExtension;
 import static jetbrains.mps.util.CollectionUtil.union;
 
 public class ModuleNameFixer {
-  public static void main(String[] args) {
-    List<File> files = union(withExtension(".mpl", Utils.files(root())), withExtension(".msd", Utils.files(root())));
-    int count = 0;
-    for (File file : files) {
+  private static final boolean DO_RENAMING = false;
+
+  public static void main(String[] args) throws IOException {
+    List<File> mprFiles = withExtension(".mpr", files(root()));
+//    System.out.println("mpr files count: " + mprFiles.size());
+//    for (File mprFile : mprFiles) {
+//      System.out.println(mprFile.getCanonicalPath());
+//    }
+//    System.out.println();
+
+    List<File> moduleFiles = union(withExtension(".mpl", files(root())), withExtension(".msd", files(root())));
+//    int count = 0;
+    for (File file : moduleFiles) {
       String fileName = file.getName();
       fileName = fileName.substring(0, fileName.length() - 4);
       String fqName = moduleFqName(file);
 
       if (!fileName.equals(fqName)) {
-        if (!fqName.endsWith(fileName)) {
-          if (!fileName.equals("languageDescriptor")) {
-            System.out.println(file.getName() + " " + moduleFqName(file));
-            count++;
+//        if (!fqName.endsWith(fileName)) {
+//          if (!fileName.equals("languageDescriptor")) {
+//            System.out.println(file.getName() + " " + moduleFqName(file));
+//            count++;
+//          }
+//        }
+        refactorModuleName(file, mprFiles);
+      }
+    }
+
+//    System.out.println("Count: " + count);
+  }
+
+  private static void refactorModuleName(File moduleFile, List<File> mprFiles) throws IOException {
+    String extension = moduleFile.getName().substring(moduleFile.getName().length() - 3);
+    File oldFile = moduleFile.getCanonicalFile();
+    File newFile = new File(moduleFile.getParentFile(), moduleFqName(moduleFile) + "." + extension).getCanonicalFile();
+
+    System.out.println("Rename " + oldFile.getAbsolutePath() + " to " + newFile.getAbsolutePath());
+
+    if (DO_RENAMING) {
+      oldFile.renameTo(newFile);
+    }
+
+    int okProjectsCount = 0;
+    for (File project : mprFiles) {
+      String projectDir = project.getParentFile().getCanonicalPath();
+      if (oldFile.getAbsolutePath().startsWith(projectDir)) {
+        String oldFilePath = "${project}" + oldFile.getAbsolutePath().substring(projectDir.length());
+        String content = FileUtil.read(project);
+        if (content.contains(oldFilePath)) {
+          System.out.println("Rename project in " + project.getName());
+          okProjectsCount++;
+          if (DO_RENAMING) {
+            content = content.replace(oldFilePath, "${project}" + newFile.getAbsolutePath().substring(projectDir.length()));
+            FileUtil.writeFile(project, content);
+          }
+        } else {
+          if (!project.getName().equals("mpsdevkit.mpr")) {
+            System.out.println("!warn: " + project.getName() + " doesn't contains " + oldFile.getAbsolutePath() + " module");
           }
         }
       }
     }
 
-    System.out.println("Count: " + count);
+    if (okProjectsCount == 0) {
+      System.out.println("!warn: " + oldFile.getAbsolutePath() + " doesn't contains in any project");
+    }
+
+    System.out.println();
+  }
+
+  private static String getModuleFingerprint(File module) {
+    return module.getParentFile().getName() + "/" + module.getName();
   }
 
   private static String moduleFqName(File file) {
