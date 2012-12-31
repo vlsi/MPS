569175716763950d943a697c7e324d94340d5e24
Fedor Isakov <fisakov@gmail.com>
2012-12-31 10:59:48 +0100

    Integration of MPS make into JPS

---
+++ IdeaPlugin/mps-core/jps-plugin/src/jetbrains/mps/jps/build/MPSMakeMediator.java
@@ -0,0 +1,280 @@
+/*
+ * Copyright 2003-2012 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jetbrains.mps.jps.build;
+
+import jetbrains.mps.generator.DefaultModifiableGenerationSettings;
+import jetbrains.mps.generator.GenerationFacade;
+import jetbrains.mps.generator.GenerationSettingsProvider;
+import jetbrains.mps.generator.impl.dependencies.GenerationDependenciesCache;
+import jetbrains.mps.generator.info.ForeignPathsProvider;
+import jetbrains.mps.generator.info.GeneratorPathsComponent;
+import jetbrains.mps.idea.core.make.MPSMakeConstants;
+import jetbrains.mps.internal.collections.runtime.ISelector;
+import jetbrains.mps.internal.collections.runtime.ISequence;
+import jetbrains.mps.internal.collections.runtime.IWhereFilter;
+import jetbrains.mps.internal.collections.runtime.Sequence;
+import jetbrains.mps.internal.make.runtime.util.DirUtil;
+import jetbrains.mps.jps.model.JpsMPSExtensionService;
+import jetbrains.mps.jps.model.JpsMPSModuleExtension;
+import jetbrains.mps.jps.project.JpsMPSProject;
+import jetbrains.mps.jps.project.JpsSolutionIdea;
+import jetbrains.mps.make.MakeSession;
+import jetbrains.mps.make.facet.IFacet;
+import jetbrains.mps.make.resources.IResource;
+import jetbrains.mps.make.script.IResult;
+import jetbrains.mps.make.script.IScript;
+import jetbrains.mps.make.script.IScriptController;
+import jetbrains.mps.make.script.ScriptBuilder;
+import jetbrains.mps.messages.IMessage;
+import jetbrains.mps.messages.IMessageHandler;
+import jetbrains.mps.project.IModule;
+import jetbrains.mps.project.ProjectOperationContext;
+import jetbrains.mps.smodel.IOperationContext;
+import jetbrains.mps.smodel.resources.IMResource;
+import jetbrains.mps.smodel.resources.ModelsToResources;
+import jetbrains.mps.tool.builder.MpsWorker;
+import jetbrains.mps.tool.builder.make.BuildMakeService;
+import jetbrains.mps.tool.builder.make.GeneratorWorker;
+import jetbrains.mps.tool.builder.make.ReducedMakeFacetConfiguration;
+import jetbrains.mps.tool.builder.paths.IRedirects;
+import jetbrains.mps.tool.builder.paths.ModuleOutputPaths;
+import jetbrains.mps.tool.builder.paths.OutputPathRedirects;
+import jetbrains.mps.vfs.IFile;
+import org.jetbrains.jps.builders.storage.BuildDataPaths;
+import org.jetbrains.jps.incremental.ModuleBuildTarget;
+import org.jetbrains.jps.incremental.messages.BuildMessage;
+import org.jetbrains.jps.incremental.messages.BuildMessage.Kind;
+import org.jetbrains.jps.incremental.messages.CompilerMessage;
+import org.jetbrains.jps.incremental.storage.BuildDataManager;
+import org.jetbrains.jps.model.module.JpsModule;
+import org.jetbrains.mps.openapi.model.SModel;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: fyodor
+ * Date: 12/19/12
+ * Time: 2:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MPSMakeMediator {
+
+  private final JpsMPSProject myProject;
+  private Map<SModel, ModuleBuildTarget> myToMake;
+  private final MPSCompilerContext myContext;
+
+  protected final List<String> myErrors = new ArrayList<String>();
+  private MyMessageHandler myMessageHandler = new MyMessageHandler();
+
+  private MyRedirects myRedirects;
+  private MyForeignRootPaths myForeignRootPaths;
+
+  public MPSMakeMediator(JpsMPSProject project, Map<SModel, ModuleBuildTarget> toMake, MPSCompilerContext context) {
+    myProject = project;
+    myToMake = toMake;
+    myContext = context;
+  }
+
+  public boolean build() {
+    ProjectOperationContext context = new ProjectOperationContext(myProject);
+    GenerationSettingsProvider.getInstance().setGenerationSettings(new DefaultModifiableGenerationSettings());
+
+    final Iterable<IMResource> resources = Sequence.fromIterable(collectResources(context, myToMake.keySet())).toListSequence();
+    ISequence<IModule> mpsModules = Sequence.fromIterable(resources).select(new ISelector<IMResource, IModule>() {
+      public IModule select(IMResource r) {
+        return r.module();
+      }
+    });
+    ModuleOutputPaths outputPaths = new ModuleOutputPaths(mpsModules);
+    myForeignRootPaths = new MyForeignRootPaths(outputPaths.getOutputPaths());
+
+    myRedirects = new MyRedirects();
+    Set<ModuleBuildTarget> processed = new HashSet<ModuleBuildTarget>();
+    for (ModuleBuildTarget target : myToMake.values()) {
+      if (processed.contains(target)) continue;
+      processed.add(target);
+
+      JpsMPSModuleExtension mpsModule = JpsMPSExtensionService.getInstance().getExtension(target.getModule());
+      if (mpsModule == null) continue;
+
+      File outputRoot = getOutputRoot(mpsModule.getModule(), myContext.getCompileContext().getProjectDescriptor().dataManager);
+      File cachesOutputRoot = getCachesOutputRoot(mpsModule.getModule(), myContext.getCompileContext().getProjectDescriptor().dataManager);
+      myRedirects.addRedirects(outputPaths, outputRoot.getAbsolutePath(), cachesOutputRoot.getAbsolutePath(), mpsModule.getConfiguration().isUseTransientOutputFolder());
+    }
+
+
+    GenerationDependenciesCache.getInstance().registerCachePathRedirect(new GenerationDependenciesCache.CachePathRedirect() {
+      public IFile redirectTo(IFile outputPath) {
+        return myRedirects.getRedirect(outputPath.getPath());
+      }
+    });
+    GeneratorPathsComponent.getInstance().registerForeignPathsProvider(new ForeignPathsProvider() {
+      public String belongsToForeignPath(IFile path) {
+        return (myForeignRootPaths != null ?
+          myForeignRootPaths.findForeignPrefix(path.getPath()) :
+          null
+        );
+      }
+    });
+
+    Future<IResult> res;
+
+    BuildMakeService bms = new BuildMakeService();
+    MakeSession ms = new MakeSession(context, myMessageHandler, true) {
+      @Override
+      public IScript toScript(ScriptBuilder scriptBuilder) {
+        scriptBuilder.withFacetNames(
+          new IFacet.Name("jetbrains.mps.build.reduced.ReportFiles"),
+          new IFacet.Name("jetbrains.mps.build.reduced.CollectHashes"));
+        return scriptBuilder.toScript();
+      }
+    };
+
+    ReducedMakeFacetConfiguration makeFacetConfiguration = new ReducedMakeFacetConfiguration(myRedirects);
+    IScriptController scriptCtl = makeFacetConfiguration.configureFacets();
+
+    try {
+      res = bms.make(ms, resources, null, scriptCtl);
+      if (!(res.get().isSucessful())) {
+        myContext.getCompileContext().processMessage(
+          new CompilerMessage(MPSMakeConstants.BUILDER_ID,
+            Kind.ERROR,
+            "Make was not successful"));
+      }
+      final List<String> writtenFiles = makeFacetConfiguration.getWrittenFiles();
+      final List<String> deletedFiles = makeFacetConfiguration.getDeletedFiles();
+      final Map<String, String> fileHashes = makeFacetConfiguration.getFileHashes();
+      // TODO do something with these
+
+    } catch (InterruptedException e) {
+      myErrors.add(e.toString());
+    } catch (ExecutionException e) {
+      myErrors.add(e.toString());
+    }
+
+    return myErrors.isEmpty();
+  }
+
+  private File getOutputRoot (JpsModule module, final BuildDataManager buildDataManager) {
+    File moduleDataRoot = getModuleDataRoot(module, buildDataManager.getDataPaths());
+    return new File(moduleDataRoot, MPSMakePaths.SOURCE_GEN);
+  }
+
+  private File getCachesOutputRoot (JpsModule module, final BuildDataManager buildDataManager) {
+    File moduleDataRoot = getModuleDataRoot(module, buildDataManager.getDataPaths());
+    return new File(moduleDataRoot, MPSMakePaths.SOURCE_GEN_CACHES);
+  }
+
+  private File getModuleDataRoot(JpsModule module, BuildDataPaths dataPaths) {
+    File dataRoot = new File(dataPaths.getDataStorageRoot(), MPSMakePaths.MPS_MAKE);
+    return new File(dataRoot, module.getName());
+  }
+
+
+  private Iterable<IMResource> collectResources(IOperationContext context, Collection<SModel> models) {
+    return Sequence.fromIterable(new ModelsToResources(context, Sequence.fromIterable(models).where(new IWhereFilter<SModel>() {
+      public boolean accept(SModel smd) {
+        return GenerationFacade.canGenerate(smd);
+      }
+    })).resources(false)).select(new ISelector<IResource, IMResource>() {
+      public IMResource select(IResource r) {
+        return (IMResource) r;
+      }
+    });
+  }
+
+  private class MyMessageHandler implements IMessageHandler {
+
+    public void handle(IMessage msg) {
+      switch (msg.getKind()) {
+        case ERROR:
+          myContext.getCompileContext().processMessage(
+            new CompilerMessage(MPSMakeConstants.BUILDER_ID,
+              Kind.ERROR,
+              msg.getText()));
+          break;
+        case WARNING:
+          myContext.getCompileContext().processMessage(
+            new CompilerMessage(MPSMakeConstants.BUILDER_ID,
+              Kind.WARNING,
+              msg.getText()));
+          break;
+        case INFORMATION:
+          myContext.getCompileContext().processMessage(
+            new CompilerMessage(MPSMakeConstants.BUILDER_ID,
+              Kind.INFO,
+              msg.getText()));
+          break;
+        default:
+      }
+    }
+
+    public void clear() {
+    }
+  }
+
+  private static class MyForeignRootPaths {
+    private String[] rootPaths;
+
+    public MyForeignRootPaths(Iterable<String> foreignRoots) {
+      this.rootPaths = Sequence.fromIterable(foreignRoots).select(new ISelector<String, String>() {
+        public String select(String dir) {
+          return DirUtil.normalizeAsDir(dir);
+        }
+      }).sort(new ISelector<String, String>() {
+        public String select(String dir) {
+          return dir;
+        }
+      }, true).toGenericArray(String.class);
+    }
+
+    public String findForeignPrefix(String path) {
+      int idx = DirUtil.findPrefixAsDir(path, rootPaths);
+      return (idx >= 0 ?
+        rootPaths[idx] :
+        null
+      );
+    }
+  }
+
+  public static class MyRedirects implements IRedirects {
+    private List<OutputPathRedirects> myOutputRedirects = new ArrayList<OutputPathRedirects>();
+
+    public void addRedirects (ModuleOutputPaths moduleOutputPaths, String outputRoot, String cachesOutputRoot, boolean useTransientOutputFolder) {
+      myOutputRedirects.add(new OutputPathRedirects(moduleOutputPaths, outputRoot, cachesOutputRoot, useTransientOutputFolder));
+    }
+
+    public IFile getRedirect(String path) {
+      for (OutputPathRedirects redirects : myOutputRedirects) {
+        IFile file = redirects.getRedirect(path);
+        if (file != null) {
+          return file;
+        }
+      }
+      return null;
+    }
+  }
+}
---
+++ IdeaPlugin/mps-core/jps-plugin/src/jetbrains/mps/jps/build/MPSMakePaths.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2003-2012 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jetbrains.mps.jps.build;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: fyodor
+ * Date: 12/21/12
+ * Time: 2:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MPSMakePaths {
+
+  public static final String MPS_MAKE = "mps-make";
+  public static final String SOURCE_GEN = "source_gen";
+  public static final String SOURCE_GEN_CACHES = "source_gen.caches";
+  public static final String TEST_GEN = "test_gen";
+  public static final String TEST_GEN_CACHES = "test_gen.caches";
+
+}
