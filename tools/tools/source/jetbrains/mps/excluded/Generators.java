b831162b87a93d937f41f5be4cebbcb623308987
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2013-01-11 00:00:34 +0300

    new gensources generation algorithm allowing no intersections betwen module roots

--- tools/tools/source/jetbrains/mps/excluded/Generators.java
+++ tools/tools/source/jetbrains/mps/excluded/Generators.java
@@ -68,61 +68,90 @@ public class Generators {
   }
 
   public static void updateGenSourcesIml(File genSourcesIml, File... sourceDirs) throws JDOMException, IOException {
+    System.out.println("Analyzing existing imls...");
+    Set<String> modelRoots = new HashSet<String>();
+    for (File imlFile : Utils.withExtension(".iml", Utils.files(new File(".")))) {
+      if (imlFile.getCanonicalPath().equals(genSourcesIml.getCanonicalPath())) continue;
+      Document doc = JDOMUtil.loadDocument(imlFile);
+      Element rootManager = Utils.getComponentWithName(doc, MODULE_ROOT_MANAGER);
+      for (Element e : (List<Element>) rootManager.getChildren(CONTENT)) {
+        String imlFormattedRoot = e.getAttributeValue(URL);
+        modelRoots.add(new File(imlFormattedRoot.replace("file://$MODULE_DIR$", imlFile.getParent())).getCanonicalPath());
+      }
+    }
+
+    System.out.println("Analyzing MPS modules...");
     Document doc = JDOMUtil.loadDocument(genSourcesIml);
     Element rootManager = Utils.getComponentWithName(doc, MODULE_ROOT_MANAGER);
-
-    Set<String> contentRootUrls = new HashSet<String>();
+    List<String> sourceGen = new ArrayList<String>();
+    List<String> classesGen = new ArrayList<String>();
     for (File dir : sourceDirs) {
-      contentRootUrls.add(PATH_START_MODULE + dir);
+      for (Pair<String, String> moduleWithSourceGen : Utils.collectMPSCompiledModulesInfo(dir)) {
+        classesGen.add(moduleWithSourceGen.o1 + "/" + AbstractModule.CLASSES_GEN);
+        sourceGen.add(moduleWithSourceGen.o2);
+      }
+    }
 
-      // try to modify existing content roots
-      Element contentRoot = Utils.getChildByAttribute(rootManager, CONTENT, URL, PATH_START_MODULE + dir);
-      if (contentRoot == null) {
-        contentRoot = new Element(CONTENT);
-        contentRoot.setAttribute(URL, PATH_START_MODULE + dir);
+    System.out.println("Building model roots for gensources module...");
+    Collections.sort(sourceGen);
+    Collections.sort(classesGen);
+    rootManager.removeChildren(CONTENT);
+
+    Set<String> newRoots = new HashSet<String>();
+    for (String sGen : sourceGen) {
+      String root = null;
+
+      // find existing
+      for (String newRoot : newRoots) {
+        if (sGen.startsWith(newRoot)) {
+          root = newRoot;
+        }
+      }
+
+      //find outermost directory not intersecting with other model roots
+      if (root == null) {
+        root = sGen;
+        String parent = new File(root).getParent();
+        while (!intersects(modelRoots, parent)) {
+          root = parent;
+          parent = new File(root).getParent();
+        }
+        newRoots.add(root);
+
+        Element contentRoot = new Element(CONTENT);
+        contentRoot.setAttribute(URL, PATH_START_MODULE + Utils.getRelativeProjectPath(root));
         rootManager.addContent(contentRoot);
       }
-      contentRoot.removeContent();
 
-      // generate lists of source gen and classes gen folders and add as source and excluded to content root
-      List<String> sourceGenFolders = new ArrayList<String>();
-      List<String> classesGenFolders = new ArrayList<String>();
-      for (Pair<String, String> module : Utils.collectMPSCompiledModulesInfo(dir)) {
-        if (new File(module.o2).exists()) {
-          String sFolder = PATH_START_MODULE + Utils.getRelativeProjectPath(module.o2);
-          sourceGenFolders.add(sFolder);
-        }
-        String cgFolder = PATH_START_MODULE + Utils.getRelativeProjectPath(module.o1) + "/" + AbstractModule.CLASSES_GEN;
-        classesGenFolders.add(cgFolder);
-      }
-      Collections.sort(sourceGenFolders);
-      Collections.sort(classesGenFolders);
+      String rootInImlFormat = PATH_START_MODULE + Utils.getRelativeProjectPath(root);
+      Element contentRoot = Utils.getChildByAttribute(rootManager, CONTENT, URL, rootInImlFormat);
 
-      for (String sourceGenFolder : sourceGenFolders) {
-        Element sourceFolder = new Element(SOURCE_FOLDER);
-        sourceFolder.setAttribute(URL, sourceGenFolder);
-        sourceFolder.setAttribute("isTestSource", "false");
-        contentRoot.addContent(sourceFolder);
-      }
-      for (String classesGenFolder : classesGenFolders) {
-        Element excludeFolder = new Element(EXCLUDE_FOLDER);
-        excludeFolder.setAttribute(URL, classesGenFolder);
-        contentRoot.addContent(excludeFolder);
-      }
+      Element sourceFolder = new Element(SOURCE_FOLDER);
+      sourceFolder.setAttribute(URL, sGen);
+      sourceFolder.setAttribute("isTestSource", "false");
+      contentRoot.addContent(sourceFolder);
     }
 
-    // remove unnecessary content roots
-    List<Element> toRemove = new ArrayList<Element>();
-    for (Object _contentRoot : rootManager.getChildren(CONTENT)) {
-      Element contentRoot = (Element) _contentRoot;
-      if (!contentRootUrls.contains(contentRoot.getAttributeValue(URL))) {
-        toRemove.add(contentRoot);
-      }
-    }
-    for (Element element : toRemove) {
-      element.detach();
+    for (String cGen : classesGen) {
+      String rootInImlFormat = PATH_START_MODULE + Utils.getRelativeProjectPath(cGen);
+      Element contentRoot = Utils.getChildByAttribute(rootManager, CONTENT, URL, rootInImlFormat);
+      assert contentRoot != null : "Classes gen folder which has no corresponding content root: " + cGen;
+
+      Element excludeFolder = new Element(EXCLUDE_FOLDER);
+      excludeFolder.setAttribute(URL, rootInImlFormat);
+      contentRoot.addContent(excludeFolder);
     }
 
+    System.out.println("Saving...");
     JDOMUtil.writeDocument(doc, genSourcesIml);
+
+    System.out.println("Done.");
+  }
+
+  private static boolean intersects(Set<String> existingRoots, String parent) {
+    for (String root : existingRoots) {
+      if (root.startsWith(parent)) return true;
+    }
+    return false;
   }
 }
