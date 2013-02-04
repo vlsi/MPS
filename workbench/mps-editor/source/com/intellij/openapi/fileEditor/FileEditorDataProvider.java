df9e00445a80a50884fad98ddc0f76e3fa9a32d3
Fedor Isakov <fisakov@gmail.com>
2013-02-04 10:05:12 +0100

    Missing data provider interface for FileEditor

---
+++ workbench/mps-editor/source/com/intellij/openapi/fileEditor/FileEditorDataProvider.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2003-2013 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intellij.openapi.fileEditor;
+
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.fileEditor.FileEditor;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: fyodor
+ * Date: 2/1/13
+ * Time: 2:23 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface FileEditorDataProvider {
+  @Nullable
+  Object getData(String dataId, FileEditor e, final VirtualFile file);
+}
---
+++ workbench/mps-editor/source/com/intellij/openapi/fileEditor/FileEditorDataProviderManager.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2003-2013 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intellij.openapi.fileEditor;
+
+import com.intellij.openapi.Disposable;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Disposer;
+import com.intellij.openapi.vfs.VirtualFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: fyodor
+ * Date: 2/1/13
+ * Time: 2:25 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FileEditorDataProviderManager {
+
+  private List<FileEditorDataProvider> myDataProviders = new ArrayList<FileEditorDataProvider>();
+
+  public static FileEditorDataProviderManager getInstance(Project project) {
+    return project.getComponent(FileEditorDataProviderManager.class);
+  }
+
+  public void registerDataProvider(final FileEditorDataProvider dataProvider, Disposable parentDisposable) {
+    myDataProviders.add(dataProvider);
+    if (parentDisposable != null) {
+      Disposer.register(parentDisposable, new Disposable() {
+        public void dispose() {
+          myDataProviders.remove(dataProvider);
+        }
+      });
+    }
+  }
+
+  public Object getData(String dataId, FileEditor fileEditor, final VirtualFile file) {
+    for (FileEditorDataProvider dataProvider : myDataProviders) {
+      final Object data = dataProvider.getData(dataId, fileEditor, file);
+      if (data != null) return data;
+    }
+    return null;
+  }
+}
