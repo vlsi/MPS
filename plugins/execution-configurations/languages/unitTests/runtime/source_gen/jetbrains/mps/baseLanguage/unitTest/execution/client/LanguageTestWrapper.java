6d98029d5f9ecabf22fec0e32d86b1526ae579c4
Timur Abishev <timur.abishev@jetbrains.com>
2013-02-05 20:54:26 +0400

    Revert changes in LanguageTestWrapper

--- plugins/execution-configurations/languages/unitTests/runtime/source_gen/jetbrains/mps/baseLanguage/unitTest/execution/client/LanguageTestWrapper.java
+++ plugins/execution-configurations/languages/unitTests/runtime/source_gen/jetbrains/mps/baseLanguage/unitTest/execution/client/LanguageTestWrapper.java
@@ -135,9 +135,9 @@ public class LanguageTestWrapper extends AbstractTestWrapper<SNode> {
     Language testsLanguage = (Language) SNodeOperations.getModel(SConceptOperations.findConceptDeclaration("jetbrains.mps.lang.test.structure.NodesTestCase")).getModelDescriptor().getModule();
     for (ModuleReference dep : CollectionSequence.fromCollection(testsLanguage.getRuntimeModulesReferences())) {
       AbstractModule module = (AbstractModule) MPSModuleRepository.getInstance().getModule(dep);
-//      for (String path : CollectionSequence.fromCollection(module.getAdditionalClassPath())) {
-//        ListSequence.fromList(result).addElement(path);
-//      }
+      for (String path : CollectionSequence.fromCollection(module.getAdditionalClassPath())) {
+        ListSequence.fromList(result).addElement(path);
+      }
     }
     ListSequence.fromList(result).addSequence(ListSequence.fromList(getPluginClasspath()));
     return result;
