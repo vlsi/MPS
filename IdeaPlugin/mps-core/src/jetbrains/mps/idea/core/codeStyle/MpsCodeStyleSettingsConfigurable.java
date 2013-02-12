58e38c0951e0d9d260cdac2569f6c5133dc8adec
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2013-02-13 03:39:08 +0400

    rename code style configurable into plugin #MPS-16806 fixed

---
+++ IdeaPlugin/mps-core/src/jetbrains/mps/idea/core/codeStyle/MpsCodeStyleSettingsConfigurable.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2003-2013 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jetbrains.mps.idea.core.codeStyle;
+
+import com.intellij.openapi.project.Project;
+import jetbrains.mps.ide.codeStyle.CodeStyleSettingsConfigurable;
+import jetbrains.mps.idea.core.MPSBundle;
+import org.jetbrains.annotations.Nls;
+
+public class MpsCodeStyleSettingsConfigurable extends CodeStyleSettingsConfigurable {
+
+  public MpsCodeStyleSettingsConfigurable(Project project) {
+    super(project);
+  }
+
+  @Nls
+  @Override
+  public String getDisplayName() {
+    return MPSBundle.message("mps.code.style");
+  }
+}
