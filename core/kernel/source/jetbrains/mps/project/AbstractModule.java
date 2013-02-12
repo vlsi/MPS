7d6eb59982476b361c46e64b2ee4fb37ba77caa3
Timur Abishev <timur.abishev@jetbrains.com>
2013-02-12 19:37:50 +0400

    Fix model resolving in module

--- core/kernel/source/jetbrains/mps/project/AbstractModule.java
+++ core/kernel/source/jetbrains/mps/project/AbstractModule.java
@@ -42,6 +42,7 @@ import jetbrains.mps.smodel.MPSModuleRepository;
 import jetbrains.mps.smodel.ModelAccess;
 import jetbrains.mps.smodel.ModuleRepositoryFacade;
 import jetbrains.mps.smodel.SModelDescriptor;
+import jetbrains.mps.smodel.SModelReference;
 import jetbrains.mps.smodel.SModelRepository;
 import jetbrains.mps.smodel.SModelStereotype;
 import jetbrains.mps.smodel.SuspiciousModelHandler;
@@ -194,8 +195,8 @@ public abstract class AbstractModule implements IModule, FileSystemListener {
 
   @Override
   public SModel resolveInDependencies(org.jetbrains.mps.openapi.model.SModelId ref) {
-    // TODO API (implement)
-    return null;
+    // TODO: implement something more meaningful
+    return SModelRepository.getInstance().getModelDescriptor(ref);
   }
 
   @Override
