dc71a9266891c83cd5bfc14bd15a8d9d3db1f9fa
Daniil Elovkov <daniil.elovkov@jetbrains.com>
2013-02-15 20:31:05 +0400

    psiSourceFinder ext.point; source finders for class stubs and psi stubs

---
+++ IdeaPlugin/mps-core/src/jetbrains/mps/idea/core/psi/impl/ClassStubPsiSourceFinder.java
@@ -0,0 +1,58 @@
+package jetbrains.mps.idea.core.psi.impl;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.JavaPsiFacade;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.search.GlobalSearchScope;
+import jetbrains.mps.idea.core.psi.MPSNodePsiSourceFinder;
+import jetbrains.mps.persistence.java.library.JavaClassStubModelDescriptor;
+import jetbrains.mps.smodel.MPSModuleRepository;
+import jetbrains.mps.smodel.SModel;
+import jetbrains.mps.smodel.SModelDescriptor;
+import jetbrains.mps.smodel.SModelRepository;
+import jetbrains.mps.smodel.language.ConceptRegistry;
+import jetbrains.mps.smodel.runtime.ConceptDescriptor;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.mps.openapi.model.SNode;
+import org.jetbrains.mps.openapi.module.SModule;
+
+/**
+ * danilla 2/15/13
+ */
+
+public class ClassStubPsiSourceFinder implements MPSNodePsiSourceFinder {
+  @Nullable
+  @Override
+  public PsiElement getPsiSource(SNode node, Project project) {
+    // for now only classifiers
+    // maybe should be done specifically for a few concepts which correspond to java entities
+
+    // FIXME there must a better way to ask if node isSubConceptOf
+    String superConcept = node.getConcept().getQualifiedName();
+    while (superConcept != null) {
+      if ("jetbrains.mps.baseLanguage.structure.Classifier".equals(superConcept)) {
+        break;
+      }
+      // TODO Could use SConcept.getSuperConcept instead?
+      ConceptDescriptor desc = ConceptRegistry.getInstance().getConceptDescriptor(superConcept);
+      assert desc != null;
+      superConcept = desc.getSuperConcept();
+    }
+
+    if (superConcept == null) {
+      // not a classifier
+      return null;
+    }
+
+    SModel model = node.getModel();
+    if (model == null) return null;
+
+    SModelDescriptor modelDesc = model.getModelDescriptor();
+
+    // class file stubs
+    if (!(modelDesc instanceof JavaClassStubModelDescriptor)) return null;
+
+    String classFQName = model.getLongName() + "." + node.getName();
+    return JavaPsiFacade.getInstance(project).findClass(classFQName, GlobalSearchScope.allScope(project));
+  }
+}
--- IdeaPlugin/mps-core/src/jetbrains/mps/idea/core/psi/impl/MPSPsiProvider.java
+++ IdeaPlugin/mps-core/src/jetbrains/mps/idea/core/psi/impl/MPSPsiProvider.java
@@ -20,8 +20,8 @@ import com.intellij.openapi.components.AbstractProjectComponent;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiManager;
+import jetbrains.mps.idea.core.psi.MPSNodePsiSourceFinder;
 import jetbrains.mps.idea.core.psi.MPSPsiNodeFactory;
-import jetbrains.mps.idea.core.psi.MPSPsiNodeFactoryStubAware;
 import jetbrains.mps.smodel.GlobalSModelEventsManager;
 import jetbrains.mps.smodel.MPSModuleRepository;
 import jetbrains.mps.smodel.event.SModelCommandListener;
@@ -73,10 +73,9 @@ public class MPSPsiProvider extends AbstractProjectComponent implements MPSPsiNo
     final SNode node = nodeRef.resolve(MPSModuleRepository.getInstance());
     if (node == null) return null;
 
-    // give chance to node factories to tell us what the PSI element is
-    for (MPSPsiNodeFactory factory : MPSPsiNodeFactory.EP_NAME.getExtensions()) {
-      if (!(factory instanceof MPSPsiNodeFactoryStubAware)) continue;
-      PsiElement psiElement = ((MPSPsiNodeFactoryStubAware) factory).getPsiSource(node);
+    // give chance to other to tell us what the PSI element is
+    for (MPSNodePsiSourceFinder finder : MPSNodePsiSourceFinder.EP_NAME.getExtensions()) {
+      PsiElement psiElement = finder.getPsiSource(node, myProject);
       if (psiElement != null) {
         return psiElement;
       }
