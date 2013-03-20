8738f7d28a1ad942a8dc36f37315f134ef2f5afd
Fedor Isakov <fisakov@gmail.com>
2013-03-20 11:46:35 +0100

    Rebuilt project MPS

--- plugins/execution-configurations/languages/unitTests/runtime/source_gen/jetbrains/mps/baseLanguage/unitTest/execution/client/LanguageTestWrapper.java
+++ plugins/execution-configurations/languages/unitTests/runtime/source_gen/jetbrains/mps/baseLanguage/unitTest/execution/client/LanguageTestWrapper.java
@@ -15,6 +15,7 @@ import jetbrains.mps.internal.collections.runtime.ISelector;
 import jetbrains.mps.baseLanguage.tuples.runtime.Tuples;
 import jetbrains.mps.baseLanguage.tuples.runtime.MultiTuple;
 import java.util.ArrayList;
+import jetbrains.mps.internal.collections.runtime.CollectionSequence;
 import com.intellij.openapi.application.PathMacros;
 import jetbrains.mps.baseLanguage.closures.runtime._FunctionTypes;
 import com.intellij.openapi.application.PathManager;
@@ -25,7 +26,6 @@ import java.net.URI;
 import jetbrains.mps.smodel.Language;
 import jetbrains.mps.lang.smodel.generator.smodelAdapter.SConceptOperations;
 import org.jetbrains.mps.openapi.module.SModuleReference;
-import jetbrains.mps.internal.collections.runtime.CollectionSequence;
 import org.jetbrains.mps.openapi.module.SModule;
 import jetbrains.mps.smodel.MPSModuleRepository;
 import jetbrains.mps.project.facets.JavaModuleFacet;
@@ -83,7 +83,7 @@ public class LanguageTestWrapper extends AbstractTestWrapper<SNode> {
       return super.getTestRunParameters();
     }
     if (BehaviorReflection.invokeVirtual(Boolean.TYPE, node, "virtual_isMpsStartRequired_3310779261129403089", new Object[]{})) {
-      return MultiTuple.<String,List<String>,List<String>>from("jetbrains.mps.baseLanguage.unitTest.execution.server.TransformationTestRunner", ListSequence.fromList(ListSequence.fromListAndArray(new ArrayList<String>(), "-Xmx1024m")).union(Sequence.fromIterable(ListSequence.fromCollection(PathMacros.getInstance().getUserMacroNames()).select(new _FunctionTypes._return_P1_E0<String, String>() {
+      return MultiTuple.<String,List<String>,List<String>>from("jetbrains.mps.baseLanguage.unitTest.execution.server.TransformationTestRunner", ListSequence.fromList(ListSequence.fromListAndArray(new ArrayList<String>(), "-Xmx1024m")).union(Sequence.fromIterable(CollectionSequence.fromCollection(PathMacros.getInstance().getUserMacroNames()).select(new _FunctionTypes._return_P1_E0<String, String>() {
         public String invoke(String key) {
           return "-D" + "path.macro." + key + "=" + PathMacros.getInstance().getValue(key);
         }
