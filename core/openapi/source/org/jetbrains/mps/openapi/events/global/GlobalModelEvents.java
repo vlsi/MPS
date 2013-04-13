4432983392f4b31b808cd5f3c1614d1c3a371dc1
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2013-04-14 03:30:55 +0400

    GlobalModelEvents - strategy extracted

--- core/openapi/source/org/jetbrains/mps/openapi/events/global/GlobalModelEvents.java
+++ core/openapi/source/org/jetbrains/mps/openapi/events/global/GlobalModelEvents.java
@@ -37,10 +37,7 @@ public class GlobalModelEvents {
   private final SRepository myRepository;
   private final GlobalModuleEvents myModuleEvents;
   private final GlobalSModuleListener myModuleListener = new MyGlobalSModuleListener();
-
-  private final MultiMap<SModel, GlobalSModelAccessListener> myRegisteredListeners = new MultiMap<SModel, GlobalSModelAccessListener>();
-
-  private final Map<GlobalSModelAccessListener, Condition<SModel>> myListenerDescriptors = new HashMap<GlobalSModelAccessListener, Condition<SModel>>();
+  private final MyAccessNotifier myAccessNotifier = new MyAccessNotifier();
 
   public GlobalModelEvents(SRepository repository) {
     myRepository = repository;
@@ -51,47 +48,12 @@ public class GlobalModelEvents {
   public void dispose() {
     myRepository.getModelAccess().checkWriteAccess();
 
-    assert myRegisteredListeners.isEmpty() : "Should be optimized";
+    myAccessNotifier.dispose();
 
     myModuleEvents.removeModuleListener(myModuleListener);
     myModuleEvents.dispose();
-    myListenerDescriptors.clear();
   }
 
-  public void addModelAccessListener(Condition<SModel> modelCondition, GlobalSModelAccessListener listener) {
-    myRepository.getModelAccess().checkWriteAccess();
-    assert !myListenerDescriptors.containsKey(listener) : "This listener is already added";
-
-    myListenerDescriptors.put(listener, modelCondition);
-    for (SModule m : myRepository.getModules()) {
-      for (SModel model : m.getModels()) {
-        if (!modelCondition.met(model)) continue;
-        addListener(listener, model);
-      }
-    }
-  }
-
-  public void removeModelAccessListener(GlobalSModelAccessListener listener) {
-    myRepository.getModelAccess().checkWriteAccess();
-
-    myListenerDescriptors.remove(listener);
-    for (Entry<SModel, Collection<GlobalSModelAccessListener>> rl : myRegisteredListeners.entrySet()) {
-      if (!rl.getValue().contains(listener)) continue;
-      removeListener(listener, rl.getKey());
-    }
-  }
-
-  private void removeListener(GlobalSModelAccessListener listener, SModel rl) {
-    myRegisteredListeners.removeValue(rl, listener);
-    listener.detached(rl);
-    rl.removeAccessListener(listener);
-  }
-
-  private void addListener(GlobalSModelAccessListener listener, SModel m) {
-    m.addAccessListener(listener);
-    listener.attached(m);
-    myRegisteredListeners.putValue(m, listener);
-  }
 
   private class MyGlobalSModuleListener extends SModuleAdapter implements GlobalSModuleListener {
     @Override
@@ -108,19 +70,72 @@ public class GlobalModelEvents {
     public void modelAdded(SModel model) {
       myRepository.getModelAccess().checkWriteAccess();
 
+      myAccessNotifier.onModelAdded(model);
+    }
+
+    @Override
+    public void beforeModelRemoved(SModel model) {
+      myRepository.getModelAccess().checkWriteAccess();
+      myAccessNotifier.onModelRemoved(model);
+    }
+  }
+
+  private class MyAccessNotifier{
+    private final MultiMap<SModel, GlobalSModelAccessListener> myRegisteredListeners = new MultiMap<SModel, GlobalSModelAccessListener>();
+
+    private final Map<GlobalSModelAccessListener, Condition<SModel>> myListenerDescriptors = new HashMap<GlobalSModelAccessListener, Condition<SModel>>();
+
+    public void addListener(Condition<SModel> modelCondition, GlobalSModelAccessListener listener) {
+      myRepository.getModelAccess().checkWriteAccess();
+      assert !myListenerDescriptors.containsKey(listener) : "This listener is already added";
+
+      myListenerDescriptors.put(listener, modelCondition);
+      for (SModule m : myRepository.getModules()) {
+        for (SModel model : m.getModels()) {
+          if (!modelCondition.met(model)) continue;
+          addListener(listener, model);
+        }
+      }
+    }
+
+    public void removeListener(GlobalSModelAccessListener listener) {
+      myRepository.getModelAccess().checkWriteAccess();
+
+      myListenerDescriptors.remove(listener);
+      for (Entry<SModel, Collection<GlobalSModelAccessListener>> rl : myRegisteredListeners.entrySet()) {
+        if (!rl.getValue().contains(listener)) continue;
+        removeListener(listener, rl.getKey());
+      }
+    }
+
+    public void onModelAdded(SModel model) {
       for (Entry<GlobalSModelAccessListener, Condition<SModel>> rl : myListenerDescriptors.entrySet()) {
         if (!rl.getValue().met(model)) continue;
         addListener(rl.getKey(), model);
       }
     }
 
-    @Override
-    public void beforeModelRemoved(SModel model) {
-      myRepository.getModelAccess().checkWriteAccess();
-
+    public void onModelRemoved(SModel model){
       for (GlobalSModelAccessListener l : myRegisteredListeners.get(model)) {
         removeListener(l, model);
       }
+    }
+
+    public void dispose(){
+      assert myRegisteredListeners.isEmpty() : "Should be optimized";
+      myListenerDescriptors.clear();
+    }
+
+    private void removeListener(GlobalSModelAccessListener listener, SModel rl) {
+      myRegisteredListeners.removeValue(rl, listener);
+      listener.detached(rl);
+      rl.removeAccessListener(listener);
+    }
+
+    private void addListener(GlobalSModelAccessListener listener, SModel m) {
+      m.addAccessListener(listener);
+      listener.attached(m);
+      myRegisteredListeners.putValue(m, listener);
     }
   }
 }
