8ef5012cd3f24512a9bef4821556e84229104061
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2013-04-14 03:47:33 +0400

    GlobalModelEvents finished

--- core/openapi/source/org/jetbrains/mps/openapi/events/global/GlobalModelEvents.java
+++ core/openapi/source/org/jetbrains/mps/openapi/events/global/GlobalModelEvents.java
@@ -37,7 +37,39 @@ public class GlobalModelEvents {
   private final SRepository myRepository;
   private final GlobalModuleEvents myModuleEvents;
   private final GlobalSModuleListener myModuleListener = new MyGlobalSModuleListener();
-  private final MyAccessNotifier myAccessNotifier = new MyAccessNotifier();
+  private final MyNotifier<GlobalSModelAccessListener> myAccessNotifier = new MyNotifier<GlobalSModelAccessListener>() {
+    @Override
+    protected void detachFromModel(GlobalSModelAccessListener listener, SModel rl) {
+      rl.removeAccessListener(listener);
+    }
+
+    @Override
+    protected void attachToModel(GlobalSModelAccessListener listener, SModel m) {
+      m.addAccessListener(listener);
+    }
+  };
+  private final MyNotifier<GlobalSModelChangeListener> myChangeNotifier = new MyNotifier<GlobalSModelChangeListener>() {
+    @Override
+    protected void detachFromModel(GlobalSModelChangeListener listener, SModel rl) {
+      rl.removeChangeListener(listener);
+    }
+
+    @Override
+    protected void attachToModel(GlobalSModelChangeListener listener, SModel m) {
+      m.addChangeListener(listener);
+    }
+  };
+  private final MyNotifier<GlobalSModelStateListener> myStateNotifier = new MyNotifier<GlobalSModelStateListener>() {
+    @Override
+    protected void detachFromModel(GlobalSModelStateListener listener, SModel rl) {
+      rl.removeStateListener(listener);
+    }
+
+    @Override
+    protected void attachToModel(GlobalSModelStateListener listener, SModel m) {
+      m.addStateListener(listener);
+    }
+  };
 
   public GlobalModelEvents(SRepository repository) {
     myRepository = repository;
@@ -45,15 +77,40 @@ public class GlobalModelEvents {
     myModuleEvents.addModuleListener(Condition.TRUE_CONDITION, myModuleListener);
   }
 
+  public void addStateListener(Condition<SModel> modelCondition, GlobalSModelStateListener listener) {
+    myStateNotifier.registerListener(modelCondition, listener);
+  }
+
+  public void removeStateListener(Condition<SModel> modelCondition, GlobalSModelStateListener listener) {
+    myStateNotifier.unregisterListener(listener);
+  }
+
+  public void addChangeListener(Condition<SModel> modelCondition, GlobalSModelChangeListener listener) {
+    myChangeNotifier.registerListener(modelCondition, listener);
+  }
+
+  public void removeChangeListener(Condition<SModel> modelCondition, GlobalSModelChangeListener listener) {
+    myChangeNotifier.unregisterListener(listener);
+  }
+
+  public void addAccessListener(Condition<SModel> modelCondition, GlobalSModelAccessListener listener) {
+    myAccessNotifier.registerListener(modelCondition, listener);
+  }
+
+  public void removeAccessListener(Condition<SModel> modelCondition, GlobalSModelAccessListener listener) {
+    myAccessNotifier.unregisterListener(listener);
+  }
+
   public void dispose() {
     myRepository.getModelAccess().checkWriteAccess();
 
     myAccessNotifier.dispose();
+    myStateNotifier.dispose();
+    myChangeNotifier.dispose();
 
     myModuleEvents.removeModuleListener(myModuleListener);
     myModuleEvents.dispose();
   }
-
 
   private class MyGlobalSModuleListener extends SModuleAdapter implements GlobalSModuleListener {
     @Override
@@ -69,23 +126,26 @@ public class GlobalModelEvents {
     @Override
     public void modelAdded(SModel model) {
       myRepository.getModelAccess().checkWriteAccess();
-
       myAccessNotifier.onModelAdded(model);
+      myStateNotifier.onModelAdded(model);
+      myChangeNotifier.onModelAdded(model);
     }
 
     @Override
     public void beforeModelRemoved(SModel model) {
       myRepository.getModelAccess().checkWriteAccess();
       myAccessNotifier.onModelRemoved(model);
+      myStateNotifier.onModelRemoved(model);
+      myChangeNotifier.onModelRemoved(model);
     }
   }
 
-  private class MyAccessNotifier{
-    private final MultiMap<SModel, GlobalSModelAccessListener> myRegisteredListeners = new MultiMap<SModel, GlobalSModelAccessListener>();
+  private abstract class MyNotifier<T extends GlobalRepositoryListener<SModel>> {
+    private final MultiMap<SModel, T> myRegisteredListeners = new MultiMap<SModel, T>();
 
-    private final Map<GlobalSModelAccessListener, Condition<SModel>> myListenerDescriptors = new HashMap<GlobalSModelAccessListener, Condition<SModel>>();
+    private final Map<T, Condition<SModel>> myListenerDescriptors = new HashMap<T, Condition<SModel>>();
 
-    public void addListener(Condition<SModel> modelCondition, GlobalSModelAccessListener listener) {
+    public void registerListener(Condition<SModel> modelCondition, T listener) {
       myRepository.getModelAccess().checkWriteAccess();
       assert !myListenerDescriptors.containsKey(listener) : "This listener is already added";
 
@@ -98,44 +158,48 @@ public class GlobalModelEvents {
       }
     }
 
-    public void removeListener(GlobalSModelAccessListener listener) {
+    public void unregisterListener(T listener) {
       myRepository.getModelAccess().checkWriteAccess();
 
       myListenerDescriptors.remove(listener);
-      for (Entry<SModel, Collection<GlobalSModelAccessListener>> rl : myRegisteredListeners.entrySet()) {
+      for (Entry<SModel, Collection<T>> rl : myRegisteredListeners.entrySet()) {
         if (!rl.getValue().contains(listener)) continue;
         removeListener(listener, rl.getKey());
       }
     }
 
     public void onModelAdded(SModel model) {
-      for (Entry<GlobalSModelAccessListener, Condition<SModel>> rl : myListenerDescriptors.entrySet()) {
+      for (Entry<T, Condition<SModel>> rl : myListenerDescriptors.entrySet()) {
         if (!rl.getValue().met(model)) continue;
         addListener(rl.getKey(), model);
       }
     }
 
-    public void onModelRemoved(SModel model){
-      for (GlobalSModelAccessListener l : myRegisteredListeners.get(model)) {
+    public void onModelRemoved(SModel model) {
+      for (T l : myRegisteredListeners.get(model)) {
         removeListener(l, model);
       }
     }
 
-    public void dispose(){
+    public void dispose() {
       assert myRegisteredListeners.isEmpty() : "Should be optimized";
       myListenerDescriptors.clear();
     }
 
-    private void removeListener(GlobalSModelAccessListener listener, SModel rl) {
+    private void removeListener(T listener, SModel rl) {
       myRegisteredListeners.removeValue(rl, listener);
       listener.detached(rl);
-      rl.removeAccessListener(listener);
+      detachFromModel(listener, rl);
     }
 
-    private void addListener(GlobalSModelAccessListener listener, SModel m) {
-      m.addAccessListener(listener);
+    private void addListener(T listener, SModel m) {
+      attachToModel(listener, m);
       listener.attached(m);
       myRegisteredListeners.putValue(m, listener);
     }
+
+    protected abstract void detachFromModel(T listener, SModel rl);
+
+    protected abstract void attachToModel(T listener, SModel m);
   }
 }
