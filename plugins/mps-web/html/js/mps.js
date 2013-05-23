0cec6732439c00ff24e07f3fbed8498c885a7f82
Evgeny Gryaznov <Evgeny.Gryaznov@jetbrains.com>
2013-05-23 11:03:26 +0200

    bootstrap 2.3.2, jquery 2.0, main page

---
+++ plugins/mps-web/html/js/mps.js
@@ -0,0 +1,8 @@
+/*
+$('.typeahead').typeahead({
+    source: function (query, process) {
+        return $.get('/typeahead', { query: query }, function (data) {
+            return process(data.options);
+        });
+    }
+});*/
