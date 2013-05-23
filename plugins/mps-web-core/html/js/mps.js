25bb4cdeead1a2300af075c677843b91c1bf4f41
Evgeny Gryaznov <Evgeny.Gryaznov@jetbrains.com>
2013-05-23 15:07:27 +0200

    list of projects on the main page; PathJS navigation; roots completion

--- plugins/mps-web-core/html/js/mps.js
+++ plugins/mps-web-core/html/js/mps.js
@@ -1,8 +1,59 @@
-/*
-$('.typeahead').typeahead({
+$('.roots-search').typeahead({
     source: function (query, process) {
-        return $.get('/typeahead', { query: query }, function (data) {
-            return process(data.options);
+        return $.ajax({
+            url: $(this)[0].$element[0].dataset.link,
+            type: 'get',
+            data: {query: query},
+            dataType: 'json',
+            success: function (json) {
+                return typeof json.options == 'undefined' ? false : process(json.options);
+            }
         });
+    },
+    updater: function (selection) {
+        $('#go-to-root input').val(selection);
+        $('#go-to-root').submit();
+        return "";
     }
-});*/
+});
+
+$('#go-to-root').submit(function () {
+    window.location = "#nodes/" + $('#go-to-root input').val();
+    return false;
+});
+
+Path.map("#nodes/:nodeId").to(function () {
+    $("#content").text("Node: " + this.params['nodeId']);
+});
+
+Path.map("#:projectId/view").to(function () {
+    $("#content").text("View for: " + this.params['projectId']);
+});
+
+Path.map("#projects").to(function () {
+    $("#content").html("<h1>Projects</h1> ");
+
+    $("#content").append("<div id='projects-list'></div>");
+
+    $.ajax({
+        url: "fake/projects.json",
+        type: 'get',
+        dataType: 'json',
+        success: function (json) {
+            return typeof json.projects == 'undefined' ? false : (function() {
+                $.each(json.projects, function(index, value){
+                    var pp = $('<a/>').attr("href", "#" + value.id + "/view").text(value.name);
+                    $("#projects-list").append(pp);
+                });
+            }());
+        }
+    });
+});
+
+Path.rescue(function () {
+    $("#content").text("Something went wrong!");
+});
+
+Path.root("#projects");
+
+Path.listen();
