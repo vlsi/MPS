8cae426539b5c58cb64acbb8cc9b3546cb44d606
Daniil Elovkov <daniil.elovkov@jetbrains.com>
2013-05-24 15:19:28 +0400

    In BL unknown concepts have red color

--- languages/baseLanguage/baseLanguage/source_gen/jetbrains/mps/baseLanguage/editor/UnknownDotCall_Editor.java
+++ languages/baseLanguage/baseLanguage/source_gen/jetbrains/mps/baseLanguage/editor/UnknownDotCall_Editor.java
@@ -14,6 +14,8 @@ import jetbrains.mps.openapi.editor.style.Style;
 import jetbrains.mps.editor.runtime.style.StyleImpl;
 import jetbrains.mps.editor.runtime.style.StyleAttributes;
 import jetbrains.mps.nodeEditor.MPSFonts;
+import jetbrains.mps.openapi.editor.style.StyleRegistry;
+import jetbrains.mps.nodeEditor.MPSColors;
 import jetbrains.mps.smodel.IOperationContext;
 import jetbrains.mps.nodeEditor.EditorManager;
 
@@ -54,6 +56,7 @@ public class UnknownDotCall_Editor extends DefaultNodeEditor {
     editorCell.setCellId("property_callee");
     Style style = new StyleImpl();
     style.set(StyleAttributes.FONT_STYLE, MPSFonts.ITALIC);
+    style.set(StyleAttributes.TEXT_COLOR, StyleRegistry.getInstance().getSimpleColor(MPSColors.red));
     editorCell.getStyle().putAll(style);
     editorCell.setSubstituteInfo(provider.createDefaultSubstituteInfo());
     SNode attributeConcept = provider.getRoleAttribute();
