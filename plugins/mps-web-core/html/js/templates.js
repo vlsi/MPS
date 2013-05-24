8189233dbd7763dcb62b95ab554c952f49131bbc
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2013-05-24 06:06:41 +0400

    provide icons from json

--- plugins/mps-web-core/html/js/templates.js
+++ plugins/mps-web-core/html/js/templates.js
@@ -2,6 +2,11 @@ function completionItemTemplate(completion_item, query) {
     var name = completion_item[completion_item["type"] + "-name"];
     var regex = new RegExp('(' + query + ')', 'gi');
     var name_with_selection = name.replace(regex, "<strong>$1</strong>");
-    var css_class = "goto " + completion_item["type"] + " " + (completion_item["hints"] || "");
-    return "<span class=\"" + css_class + "\"><span class=\"icon\"/>" + name_with_selection + "</span>";
+
+    var span = $('<span/>').addClass("goto").addClass(completion_item["type"]);
+    var icon = $('<span/>').addClass("icon").css("background-image", 'url(\'' + completion_item.icon + '\')');
+    span.append(icon);
+    span.append(name_with_selection);
+
+    return span;
 }
