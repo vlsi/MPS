2b723a7402b375eafd2e11bad5a288dc8e78dece
Evgeny Gryaznov <Evgeny.Gryaznov@jetbrains.com>
2013-05-24 18:40:17 +0200

    cursor; breadcrumb tooltip

--- plugins/mps-web-core/html/js/mps.js
+++ plugins/mps-web-core/html/js/mps.js
@@ -66,8 +66,11 @@ $(function () {
             }
             var index = 1;
             var li = $('.mbreadcrumb li');
-            while ($('.mbreadcrumb').width() > $('.mbreadcrumb-container').width() && index < li.size() - 1) {
-                $(li.get(index)).children("a").text("...");
+            while ($('.mbreadcrumb').width() > $('.mbreadcrumb-container').width() && index < li.length - 1) {
+                var children = $(li.get(index)).children("a");
+                children.tooltip({
+                    title: children.text(), placement: 'bottom'
+                }).text("...");
                 index++;
             }
         }
@@ -165,36 +168,76 @@ $(function () {
     }
 
     $('#go-to-root').submit(function () {
-        var type = $('#go-to-root input[name="search-type"]').val();
+        var gotof = $('#go-to-root');
+        var type = gotof.find('input[name="search-type"]').val();
         if (type == "node") {
             window.location = "#" + currentProject + "/node" +
-                "/" + $('#go-to-root input[name="search-module-id"]').val() +
-                "/" + $('#go-to-root input[name="search-model-id"]').val() +
-                "/" + $('#go-to-root input[name="search-node-id"]').val();
+                "/" + gotof.find('input[name="search-module-id"]').val() +
+                "/" + gotof.find('input[name="search-model-id"]').val() +
+                "/" + gotof.find('input[name="search-node-id"]').val();
         } else if (type == "model") {
             window.location = "#" + currentProject + "/model" +
-                "/" + $('#go-to-root input[name="search-module-id"]').val() +
-                "/" + $('#go-to-root input[name="search-model-id"]').val();
+                "/" + gotof.find('input[name="search-module-id"]').val() +
+                "/" + gotof.find('input[name="search-model-id"]').val();
         } else if (type == "module") {
             window.location = "#" + currentProject + "/module" +
-                "/" + $('#go-to-root input[name="search-module-id"]').val();
+                "/" + gotof.find('input[name="search-module-id"]').val();
         }
         return false;
     });
 
+    function nextLeaf(node, back) {
+        var all = $('.nodes .n-leaf');
+        if (all.length == 0) {
+            return null;
+        }
+        var index = node == null ? (back ? all.length - 1 : 0) : all.index(node);
+        if (index == -1) {
+            return null;
+        }
+
+        if (back) {
+            return index > 0 ? $(all.get(index - 1)) : null;
+        } else {
+            return index + 1 < all.length ? $(all.get(index + 1)) : null;
+        }
+    }
+
+    function setCursor() {
+        var c = $('.n-cursor');
+        var curr = nextLeaf(null, false);
+        c.data("cursor-anchor", curr);
+        if (curr == null) {
+            alert("null");
+        } else {
+            c.offset(curr.offset());
+        }
+    }
+
+    function updateCursor(back) {
+        var c = $('.n-cursor');
+        var curr = nextLeaf(c.data("cursor-anchor"), back);
+        c.data("cursor-anchor", curr);
+        c.offset(curr.offset());
+    }
+
     Path.map("#:projectId/node/:moduleId/:modelId/:rootId").to(function () {
         setContext(this.params['projectId'], this.params['moduleId'], this.params['modelId'], this.params['rootId']);
         $("#content").text("Content of node " + this.params['rootId'] + " is loading...");
-        fetchNodeAsHtml(currentProject, this.params["modelId"], this.params['rootId'], nameFetcher, function (data) {
-            $('#content').empty();
-            $('#content').append($('<div/>').attr('class', 'nodes').append(data));
-            $('#content div[target-node-id]').click(function (e) {
-                window.location = "#" + currentProject + "/node" +
-                    "/" + $(e.target).attr("target-module-id") +
-                    "/" + $(e.target).attr("target-model-id") +
-                    "/" + $(e.target).attr("target-node-id");
-            })
-        });
+        $.get("/rest/p/" + currentProject + "/view/" + escape(this.params["modelId"]) + "(" + nameFetcher.getModelName(this.params["modelId"]) + ")" + "/" + this.params['rootId'],
+            function (data) {
+                var content = $('#content');
+                content.empty();
+                content.append($('<div/>').toggleClass('n-cursor').html("&nbsp;"));
+                content.append($('<div/>').attr('class', 'nodes').append(data));
+                setCursor();
+                content.find('div[target-node-id]').click(function (e) {
+                    window.location = "#" + currentProject + "/node" +
+                        "/" + $(e.target).attr("target-module-id") +
+                        "/" + $(e.target).attr("target-model-id") +
+                        "/" + $(e.target).attr("target-node-id");
+                });
+            }, "html");
     });
 
     function treeToggle(event) {
@@ -318,6 +361,68 @@ $(function () {
     Path.root("#projects");
 
     Path.listen();
+
+    var keyNames = {27: "Esc", 33: "PageUp", 34: "PageDown", 35: "End", 36: "Home", 37: "Left", 38: "Up", 39: "Right", 40: "Down",
+        63276: "PageUp", 63277: "PageDown", 63275: "End", 63273: "Home", 63234: "Left", 63232: "Up", 63235: "Right", 63233: "Down"};
+    (function () {
+        for (var i = 0; i < 10; i++) keyNames[i + 48] = String(i);
+        for (var i = 65; i <= 90; i++) keyNames[i] = String.fromCharCode(i);
+    })();
+    var opera = /Opera\//.test(navigator.userAgent);
+    var ios = /AppleWebKit/.test(navigator.userAgent) && /Mobile\/\w+/.test(navigator.userAgent);
+    var webkit = /WebKit\//.test(navigator.userAgent);
+    var qtwebkit = webkit && /Qt\/\d+\.\d+/.test(navigator.userAgent);
+    var mac = ios || /Mac/.test(navigator.platform);
+    var opera_version = opera && navigator.userAgent.match(/Version\/(\d*\.\d*)/);
+    if (opera_version) opera_version = Number(opera_version[1]);
+    var flipCtrlCmd = mac && (qtwebkit || opera && (opera_version == null || opera_version < 12.11));
+
+
+    $('body').keydown(function (event) {
+        if ($(event.target).closest('.navbar').length != 0) {
+            return;
+        }
+        var name = keyNames[event.keyCode];
+        if (name == null || event.altGraphKey) return;
+
+        var oneChar = name.length == 1;
+
+        if (event.altKey) name = "Alt-" + name;
+        if (flipCtrlCmd ? event.metaKey : event.ctrlKey) name = "Ctrl-" + name;
+        if (flipCtrlCmd ? event.ctrlKey : event.metaKey) name = "Cmd-" + name;
+        if (event.shiftKey) name = "Shift-" + name;
+
+        if (oneChar && name.length > 1) {
+            var ch = String.fromCharCode(event.keyCode);
+            if (ch !== "B" && ch !== "N") {
+                return;
+            }
+        }
+
+        if (name === "Esc" || name === "Ctrl-N" || name === "Cmd-N") {
+            $('#go-to-root').find('input[name="search"]').focus();
+            return;
+        }
+
+        if (name === "Ctrl-B" || name === "Cmd-B") {
+            alert("Go to link!");
+            return;
+        }
+
+        if (name === "Down" || name === "Up") {
+            updateCursor(name === "Up");
+            return;
+        }
+
+        //alert(name);
+    });
+
+    $('#go-to-root').find('input[name="search"]').keydown(function (event) {
+        if (event.keyCode === 27 || event.keyCode == 9) {
+            $(this).blur();
+            event.preventDefault();
+        }
+    });
 });
 
 function createIconSpan(icon_json) {
--- plugins/mps-web-core/html/js/rest.js
+++ plugins/mps-web-core/html/js/rest.js
@@ -1,10 +1,5 @@
 // TODO: get rid of this file
 
-function fetchNodeAsHtml(project, modelId, rootId, nameFetcher, onSuccess) {
-    $.get("/rest/p/" + project + "/view/" + escape(modelId) + "(" + nameFetcher.getModelName(modelId) + ")" + "/" + rootId,
-        onSuccess, "html");
-}
-
 function getStructureUrl(project, item) {
     var type = item["type"];
     var id = escape(item[type + "-id"]);
