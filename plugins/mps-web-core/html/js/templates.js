81b8265ae6e3951fc812630e93ae38230e22dfbe
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2013-05-25 02:21:26 +0400

    some fiddling with styles

--- plugins/mps-web-core/html/js/templates.js
+++ plugins/mps-web-core/html/js/templates.js
@@ -2,12 +2,14 @@ function completionItemTemplate(completion_item, query) {
     var name = completion_item[completion_item["type"] + "-name"];
     var regex = new RegExp('(' + query + ')', 'gi');
     var name_with_selection = name.replace(regex, "<strong>$1</strong>");
+    var presentation = $('<span/>').addClass("nodes").addClass("goto-presentation").append(name_with_selection);
 
     var span = $('<span/>').addClass("goto").addClass(completion_item["type"]);
     if (completion_item["type"].indexOf("fetch") == -1) {
         span.append(createIconSpan(completion_item.icon));
     }
-    span.append(name_with_selection);
+
+    span.append(presentation);
 
     if (completion_item["type"] != "module") {
         // origin
