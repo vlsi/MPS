e9ed8b9b1263c34fa98a64ee112976d8c390db54
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2013-05-25 04:40:13 +0400

    extract tree.js

---
+++ plugins/mps-web-core/html/js/common.js
@@ -0,0 +1,92 @@
+var currentProject = null;
+
+var nameFetcher = {
+    getModuleName: function (moduleId) {
+        // todo: if doesn't contains
+        return nameFetcher.moduleNames[moduleId] ? nameFetcher.moduleNames[moduleId] : moduleId;
+    },
+
+    getModelName: function (modelId) {
+        // todo: if doesn't contains
+        return nameFetcher.modelNames[modelId] ? nameFetcher.modelNames[modelId] : modelId;
+    },
+
+    getNodeName: function (nodeId) {
+        // todo: if doesn't contains
+        return nameFetcher.nodeNames[nodeId] ? nameFetcher.nodeNames[nodeId] : nodeId;
+    },
+
+    updateWithCompletionResult: function (completionResult) {
+        completionResult.forEach(function (item) {
+            ["module", "model", "node"].forEach(function (type) {
+                if (typeof item[type + "-id"] != 'undefined') {
+                    nameFetcher[type + "Names"][item[type + "-id"]] = item[type + "-name"];
+                }
+            });
+        });
+    },
+
+    moduleNames: {},
+    modelNames: {},
+    nodeNames: {}
+};
+
+function addBreadcrumb(title, link) {
+    var li = $('<li/>');
+    if (link === null) {
+        li.attr("class", "active").text(title);
+    } else {
+        li.append($('<a/>').attr('href', link).text(title));
+        li.append(" ");
+        li.append($('<span class="divider">/</span>'));
+    }
+    $('.mbreadcrumb').append(li);
+}
+
+function showBreadCrumb(project, module_id, model_id, root_id) {
+    var bc = $('.mbreadcrumb');
+    bc.css("visibility", "visible");
+    bc.show();
+    bc.empty();
+    addBreadcrumb(project, module_id != null ? "#" + project + "/view" : null);
+    if (module_id != null) {
+        addBreadcrumb(nameFetcher.getModuleName(module_id), model_id !== null ? "#" + project + "/module/" + module_id : null);
+        if (model_id != null) {
+            addBreadcrumb(nameFetcher.getModelName(model_id), root_id !== null ? "#" + project + "/model/" + module_id + "/" + model_id : null);
+            if (root_id != null) {
+                addBreadcrumb(nameFetcher.getNodeName(root_id), null);
+            }
+        }
+    }
+    var index = 1;
+    var li = $('.mbreadcrumb li');
+    while ($('.mbreadcrumb').width() > $('.mbreadcrumb-container').width() && index < li.length - 1) {
+        var children = $(li.get(index)).children("a");
+        children.tooltip({
+            title: children.text(), placement: 'bottom'
+        }).text("...");
+        index++;
+    }
+}
+
+function setContext(project, module_id, model_id, root_id) {
+    var bc = $('.mbreadcrumb');
+    if (project === null) {
+        bc.hide()
+        $('#go-to-root').hide();
+    } else {
+        $('#go-to-root').show();
+        showBreadCrumb(project, module_id, model_id, root_id);
+    }
+    currentProject = project;
+}
+
+function createIconSpan(icon_json) {
+    icon_json = JSON.parse(icon_json);
+    var element = $("<span/>");
+    element.addClass("icon_any");
+    element.css('background-image', 'url(\'data:image/png;base64,' + icon_json["base64"] + '\')');
+    element.css('width', icon_json.width);
+    element.css('height', icon_json.height);
+    return element;
+}
---
+++ plugins/mps-web-core/html/js/tree.js
@@ -0,0 +1,80 @@
+function treeToggle(event) {
+    var li = $(event.delegateTarget).parent();
+    var node = li.children("ul");
+
+    if (node.attr("data-source")) {
+        loadTree(node);
+    } else {
+        li.attr("data-collapsed", li.attr("data-collapsed") !== "true");
+    }
+    event.preventDefault();
+}
+
+function applyTree(node, children, collapsed) {
+    node.removeAttr("data-source");
+    node.empty();
+    if (children.length) {
+        node.parent().attr('data-collapsed', 'false');
+    } else {
+        node.parent().removeAttr('data-collapsed');
+    }
+    $.each(children, function (index, child) {
+        var span = $('<span/>').css("class", "treeitem");
+        var li = $('<li/>').append(span);
+        span.append(createIconSpan(child.icon));
+        nameFetcher.updateWithCompletionResult([child]);
+        if ($.isArray(child.children)) {
+            span.append($('<a/>').text(child[child["type"] + "-name"]));
+            var childNode = $('<ul/>');
+            childNode.attr('class', 'tree-content');
+            li.append(childNode);
+            applyTree(childNode, child.children, true);
+            span.on("click", treeToggle);
+            li.attr("data-collapsed", collapsed);
+        } else if (child["isStructure"] == "true") {
+            span.append($('<a/>').text(child[child["type"] + "-name"]));
+            var childNode = $('<ul/>').attr("data-source", getStructureUrl(currentProject, child));
+            childNode.attr('class', 'tree-content');
+            li.append(childNode);
+            span.on("click", treeToggle);
+            li.attr("data-collapsed", collapsed);
+        } else {
+            var url = "/#" + currentProject + "/node/" + child["module-id"] + "/" + child["model-id"] + "/" + child["node-id"];
+            span.append($('<a/>').attr("href", url).text(child[child["type"] + "-name"]));
+        }
+        node.append(li);
+    });
+}
+
+function loadTree(node) {
+    node.empty();
+    node.append("<li>Loading....</li>");
+    $.ajax({
+        url: node.data("source"),
+        type: 'get',
+        dataType: 'json',
+        success: function (json) {
+            return typeof json.elements == 'undefined' ? false : applyTree(node, json.elements, false);
+        }
+    });
+}
+
+function showTree(project_id, module_id, model_id) {
+    setContext(project_id, module_id, model_id, null);
+    var content = $("#content");
+    content.html('<h2>Code</h2>');
+    var link = '/rest/p/' + currentProject + '/structure.json';
+    link = module_id === null ? link : link + '/' + module_id;
+    link = model_id === null ? link : link + '/' + model_id;
+    content.append('<ul class="tree-root" data-source="' + link + '"></ul>');
+    loadTree($('.tree-root'));
+}
+
+function getStructureUrl(project, item) {
+    var type = item["type"];
+    var id = escape(item[type + "-id"]);
+    if (type == "model") {
+        id = id + "()";
+    }
+    return "/rest/p/" + project + "/structure.json/" + type + "/" + id;
+}
