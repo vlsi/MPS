6ac5d60dab0840fb577b409edc7086b89696c9f7
Daniil Elovkov <daniil.elovkov@jetbrains.com>
2013-06-17 18:10:56 +0400

    Removed wrong obsolete imports

--- IdeaPlugin/mps-java/src/jetbrains/mps/idea/java/refactoring/MoveStaticFieldContributor.java
+++ IdeaPlugin/mps-java/src/jetbrains/mps/idea/java/refactoring/MoveStaticFieldContributor.java
@@ -1,15 +1,7 @@
 package jetbrains.mps.idea.java.refactoring;
 
-import jetbrains.mps.ide.java.actions.MoveRefactoringRunnable;
 import jetbrains.mps.ide.java.actions.MoveStaticFieldExecutable;
-import jetbrains.mps.ide.java.actions.MoveStaticFieldExecute.ExecuteRefactoring;
 import jetbrains.mps.ide.java.actions.MoveStaticMemberExecutable;
-import jetbrains.mps.ide.platform.refactoring.RefactoringAccess;
-import jetbrains.mps.project.MPSProject;
-import jetbrains.mps.refactoring.framework.RefactoringContext;
-import org.jetbrains.mps.openapi.model.SNode;
-
-import java.util.Arrays;
 
 /**
  * danilla 6/15/13
