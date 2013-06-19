3a901bfa8802c6faf83625e7a3707b4ba25aecd6
Fedor Isakov <fisakov@gmail.com>
2013-06-19 16:47:37 +0200

    IfInstanceOfVariable's type depends on the IfInstanceOfStatement

--- languages/languageDesign/smodel/source_gen/jetbrains/mps/lang/smodel/typesystem/typeof_IfInstanceOfVariable_InferenceRule.java
+++ languages/languageDesign/smodel/source_gen/jetbrains/mps/lang/smodel/typesystem/typeof_IfInstanceOfVariable_InferenceRule.java
@@ -7,7 +7,7 @@ import jetbrains.mps.lang.typesystem.runtime.InferenceRule_Runtime;
 import org.jetbrains.mps.openapi.model.SNode;
 import jetbrains.mps.typesystem.inference.TypeCheckingContext;
 import jetbrains.mps.lang.typesystem.runtime.IsApplicableStatus;
-import jetbrains.mps.typesystem.inference.EquationInfo;
+import jetbrains.mps.lang.smodel.generator.smodelAdapter.SNodeOperations;
 import jetbrains.mps.smodel.SModelUtil_new;
 
 public class typeof_IfInstanceOfVariable_InferenceRule extends AbstractInferenceRule_Runtime implements InferenceRule_Runtime {
@@ -15,11 +15,10 @@ public class typeof_IfInstanceOfVariable_InferenceRule extends AbstractInference
   }
 
   public void applyRule(final SNode variable, final TypeCheckingContext typeCheckingContext, IsApplicableStatus status) {
-    final SNode V_typevar_1883223317721107041 = typeCheckingContext.createNewRuntimeTypesVariable();
-    {
-      SNode _nodeToCheck_1029348928467 = variable;
-      EquationInfo _info_12389875345 = new EquationInfo(_nodeToCheck_1029348928467, null, "r:00000000-0000-4000-0000-011c895902fe(jetbrains.mps.lang.smodel.typesystem)", "1883223317721107053", 0, null);
-      typeCheckingContext.createEquation((SNode) typeCheckingContext.typeOf(_nodeToCheck_1029348928467, "r:00000000-0000-4000-0000-011c895902fe(jetbrains.mps.lang.smodel.typesystem)", "1883223317721107044", true), (SNode) typeCheckingContext.getRepresentative(V_typevar_1883223317721107041), _info_12389875345);
+    SNode iiostmt = SNodeOperations.as(SNodeOperations.getParent(variable), "jetbrains.mps.lang.smodel.structure.IfInstanceOfStatement");
+    if ((iiostmt != null)) {
+      SNode ignore = typeCheckingContext.typeOf(iiostmt, "r:00000000-0000-4000-0000-011c895902fe(jetbrains.mps.lang.smodel.typesystem)", "8658296822737562953", true);
+      typeCheckingContext.typeOf(iiostmt, "r:00000000-0000-4000-0000-011c895902fe(jetbrains.mps.lang.smodel.typesystem)", "8658296822738406899", true);
     }
   }
 
