4bf3fcba6553c4761cbe01c46ffe3f6971414a5f
Daniil Elovkov <daniil.elovkov@jetbrains.com>
2013-06-21 17:36:47 +0400

    Regenerated

--- plugins/mpsjava/basePlatform/source_gen/jetbrains/mps/ide/java/newparser/MultipleFilesParser.java
+++ plugins/mpsjava/basePlatform/source_gen/jetbrains/mps/ide/java/newparser/MultipleFilesParser.java
@@ -121,7 +121,6 @@ public class MultipleFilesParser {
     runCommand("model creation pass", new Runnable() {
       public void run() {
         ((AbstractModule) myModule).addDependency(PersistenceFacade.getInstance().createModuleReference("6354ebe7-c22a-4a0f-ac54-50b52ab9b065(JDK)"), false);
-        ((AbstractModule) myModule).addUsedLanguage(PersistenceFacade.getInstance().createModuleReference("f3061a53-9226-4cc5-a443-f952ceaf5816(jetbrains.mps.baseLanguage)"));
 
         for (String pakage : SetSequence.fromSet(MapSequence.fromMap(classesPerPackage).keySet())) {
           final SModel model = registerModelForPackage(pakage);
