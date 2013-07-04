4e86e66b12b7e4a23aa0f4363e63cc7b462a1bd4
Timur Abishev <timur.abishev@jetbrains.com>
2013-07-04 06:21:14 +0400

    support directory based format for mps projects in FileMPSProject

--- core/tool/builder/source_gen/jetbrains/mps/tool/builder/FileMPSProject.java
+++ core/tool/builder/source_gen/jetbrains/mps/tool/builder/FileMPSProject.java
@@ -147,17 +147,29 @@ public class FileMPSProject extends Project {
     private String name;
     private List<Path> myModulePaths = new ArrayList<Path>();
 
-    public ProjectDescriptor(File file) {
-      load(file);
+    public ProjectDescriptor(File project) {
+      load(project);
     }
 
-    private void load(File projFile) {
-      if (projFile == null) {
+    private void load(File project) {
+      if (project == null) {
+        return;
+      }
+      if (project.isDirectory()) {
+        load(project, new File(project, ".mps/modules.xml"));
+      } else {
+        // <node> 
+        load(project, project);
+      }
+    }
+
+    private void load(File project, File modulesFile) {
+      if (modulesFile == null) {
         return;
       }
       Document document = null;
       try {
-        document = JDOMUtil.loadDocument(projFile);
+        document = JDOMUtil.loadDocument(modulesFile);
       } catch (JDOMException ex) {
       } catch (IOException ex) {
       }
@@ -174,32 +186,33 @@ public class FileMPSProject extends Project {
         }
       }
       if (projectElement != null) {
-        load(projFile, projectElement);
+        load(project, projectElement);
       }
     }
 
-    private void load(File file, Element root) {
-      FileMPSProject.ProjectDescriptor result_dkknya_a0a4o = this;
-      final String result_dkknya_a0a0a4o = file.getName();
-      result_dkknya_a0a4o.setName(result_dkknya_a0a0a4o);
+    private void load(File project, Element modulesXml) {
+      FileMPSProject.ProjectDescriptor result_dkknya_a0a5o = this;
+      final String result_dkknya_a0a0a5o = project.getName();
+      result_dkknya_a0a5o.setName(result_dkknya_a0a0a5o);
 
-      if (root == null) {
+      if (modulesXml == null) {
         return;
       }
 
       List<Element> moduleList = ListSequence.fromList(new ArrayList<Element>());
-      ListSequence.fromList(moduleList).addSequence(Sequence.fromIterable(XmlUtil.children(XmlUtil.first(root, "projectSolutions"), "solutionPath")));
-      ListSequence.fromList(moduleList).addSequence(Sequence.fromIterable(XmlUtil.children(XmlUtil.first(root, "projectLanguages"), "languagePath")));
-      ListSequence.fromList(moduleList).addSequence(Sequence.fromIterable(XmlUtil.children(XmlUtil.first(root, "projectDevkits"), "devkitPath")));
-      ListSequence.fromList(moduleList).addSequence(Sequence.fromIterable(XmlUtil.children(XmlUtil.first(root, "projectModules"), "modulePath")));
+      ListSequence.fromList(moduleList).addSequence(Sequence.fromIterable(XmlUtil.children(XmlUtil.first(modulesXml, "projectSolutions"), "solutionPath")));
+      ListSequence.fromList(moduleList).addSequence(Sequence.fromIterable(XmlUtil.children(XmlUtil.first(modulesXml, "projectLanguages"), "languagePath")));
+      ListSequence.fromList(moduleList).addSequence(Sequence.fromIterable(XmlUtil.children(XmlUtil.first(modulesXml, "projectDevkits"), "devkitPath")));
+      ListSequence.fromList(moduleList).addSequence(Sequence.fromIterable(XmlUtil.children(XmlUtil.first(modulesXml, "projectModules"), "modulePath")));
       for (Element moduleElement : ListSequence.fromList(moduleList)) {
         Path modulePath = new Path();
-        Path result_dkknya_a1a9a0a4o = modulePath;
-        final String result_dkknya_a0a1a9a0a4o = MacrosFactory.forProjectFile(FileSystem.getInstance().getFileByPath(file.getPath())).expandPath(moduleElement.getAttributeValue("path"));
-        result_dkknya_a1a9a0a4o.setPath(result_dkknya_a0a1a9a0a4o);
-        final String result_dkknya_a1a1a9a0a4o = moduleElement.getAttributeValue("folder");
-        result_dkknya_a1a9a0a4o.setMPSFolder(result_dkknya_a1a1a9a0a4o);
-        result_dkknya_a0a4o.addModule(modulePath);
+        Path result_dkknya_a1a9a0a5o = modulePath;
+        // todo: replace - wtf? @see ProjectDescriptorPersistence#saveProjectDescriptorToElement 
+        final String result_dkknya_a1a1a9a0a5o = MacrosFactory.forProjectFile(FileSystem.getInstance().getFileByPath(project.getPath())).expandPath(moduleElement.getAttributeValue("path").replace("$PROJECT_DIR$", "${project}"));
+        result_dkknya_a1a9a0a5o.setPath(result_dkknya_a1a1a9a0a5o);
+        final String result_dkknya_a2a1a9a0a5o = moduleElement.getAttributeValue("folder");
+        result_dkknya_a1a9a0a5o.setMPSFolder(result_dkknya_a2a1a9a0a5o);
+        result_dkknya_a0a5o.addModule(modulePath);
       }
     }
 
