faf7cf0bd9beadcc7d5ca60bfce31dbee9d8c3c1
Semen Alperovich <semen.alperovich@jetbrains.com>
2013-07-23 03:29:23 +0400

    add test concepts

--- testbench/modules/testHybridEditor/source_gen/jetbrains/mps/testHybridEditor/behavior/BehaviorAspectDescriptor.java
+++ testbench/modules/testHybridEditor/source_gen/jetbrains/mps/testHybridEditor/behavior/BehaviorAspectDescriptor.java
@@ -12,10 +12,14 @@ public class BehaviorAspectDescriptor implements jetbrains.mps.smodel.runtime.Be
 
   public BehaviorDescriptor getDescriptor(String fqName) {
     switch (Arrays.binarySearch(stringSwitchCases_846f5o_a0a0b, fqName)) {
+      case 0:
+        return new RootConcept_BehaviorDescriptor();
+      case 1:
+        return new SimpleConcept_BehaviorDescriptor();
       default:
         return BehaviorAspectInterpreted.getInstance().getDescriptor(fqName);
     }
   }
 
-  private static String[] stringSwitchCases_846f5o_a0a0b = new String[]{};
+  private static String[] stringSwitchCases_846f5o_a0a0b = new String[]{"jetbrains.mps.testHybridEditor.structure.RootConcept", "jetbrains.mps.testHybridEditor.structure.SimpleConcept"};
 }
