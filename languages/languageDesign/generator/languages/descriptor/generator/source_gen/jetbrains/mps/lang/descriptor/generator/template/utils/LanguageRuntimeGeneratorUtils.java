a0def1034d4092033a2273a70041cf67af71f529
Alexander Shatalin <Alexander.Shatalin@jetbrains.com>
2013-07-23 17:21:10 +0200

    modifying descriptor generator to allow manual editor specification by implementing BaseLaguage classes

--- languages/languageDesign/generator/languages/descriptor/generator/source_gen/jetbrains/mps/lang/descriptor/generator/template/utils/LanguageRuntimeGeneratorUtils.java
+++ languages/languageDesign/generator/languages/descriptor/generator/source_gen/jetbrains/mps/lang/descriptor/generator/template/utils/LanguageRuntimeGeneratorUtils.java
@@ -5,36 +5,25 @@ package jetbrains.mps.lang.descriptor.generator.template.utils;
 import org.jetbrains.mps.openapi.model.SNode;
 import jetbrains.mps.smodel.LanguageAspect;
 import jetbrains.mps.generator.template.TemplateQueryContext;
-import jetbrains.mps.lang.smodel.generator.smodelAdapter.SNodeOperations;
-import jetbrains.mps.lang.smodel.generator.smodelAdapter.SPropertyOperations;
-import org.jetbrains.mps.openapi.module.SModuleReference;
-import org.jetbrains.mps.openapi.persistence.PersistenceFacade;
-import jetbrains.mps.smodel.behaviour.BehaviorReflection;
-import jetbrains.mps.smodel.Language;
-import jetbrains.mps.smodel.ModuleRepositoryFacade;
 import org.jetbrains.mps.openapi.model.SModel;
 import java.util.List;
 import jetbrains.mps.lang.smodel.generator.smodelAdapter.SModelOperations;
 import jetbrains.mps.internal.collections.runtime.ListSequence;
 import jetbrains.mps.internal.collections.runtime.IWhereFilter;
 import jetbrains.mps.lang.smodel.generator.smodelAdapter.SConceptOperations;
+import jetbrains.mps.lang.smodel.generator.smodelAdapter.SNodeOperations;
 import jetbrains.mps.util.NameUtil;
+import jetbrains.mps.lang.smodel.generator.smodelAdapter.SPropertyOperations;
+import org.jetbrains.mps.openapi.module.SModuleReference;
+import org.jetbrains.mps.openapi.persistence.PersistenceFacade;
+import jetbrains.mps.smodel.behaviour.BehaviorReflection;
+import jetbrains.mps.smodel.Language;
+import jetbrains.mps.smodel.ModuleRepositoryFacade;
 
 public class LanguageRuntimeGeneratorUtils {
   public static boolean isAspectOfLanguage(SNode modelReference, LanguageAspect aspect, TemplateQueryContext genContext, final SNode rootConcept) {
-    SNode language = SNodeOperations.cast(SNodeOperations.getParent(modelReference), "jetbrains.mps.lang.project.structure.Language");
-    if (language == null) {
-      genContext.showErrorMessage(modelReference, "Parent of ModelReference is not a Language: " + SPropertyOperations.getString(modelReference, "qualifiedName"));
-      return false;
-    }
-    SModuleReference moduleReference = PersistenceFacade.getInstance().createModuleReference(BehaviorReflection.invokeVirtual(String.class, language, "virtual_getModuleReference_9020561928507315628", new Object[]{}));
-    Language languageModule = ModuleRepositoryFacade.getInstance().getModule(moduleReference, Language.class);
-    if (languageModule == null) {
-      genContext.showErrorMessage(language, "No language in repository: " + SPropertyOperations.getString(language, "namespace"));
-      return false;
-    }
-    SModel aspectModel = aspect.get(languageModule);
-    if (aspectModel == null || !(aspectModel.getModelId().toString().equals(SPropertyOperations.getString(modelReference, "uuid")))) {
+    SModel aspectModel = getAspectModel(modelReference, aspect, genContext);
+    if (aspectModel == null) {
       return false;
     }
     List<SNode> roots = SModelOperations.getRoots(((SModel) aspectModel), null);
@@ -45,6 +34,38 @@ public class LanguageRuntimeGeneratorUtils {
           return SConceptOperations.isSubConceptOf(SNodeOperations.getConceptDeclaration(it), NameUtil.nodeFQName(rootConcept));
         }
       }).isNotEmpty()
+    );
+  }
+
+  public static boolean hasManualEditorAspectDescriptor(SNode modelReference, TemplateQueryContext genContext) {
+    SModel editorModel = getAspectModel(modelReference, LanguageAspect.EDITOR, genContext);
+    if (editorModel == null) {
+      return false;
+    }
+    List<SNode> roots = SModelOperations.getRoots(((SModel) editorModel), "jetbrains.mps.baseLanguage.structure.ClassConcept");
+    return ListSequence.fromList(roots).any(new IWhereFilter<SNode>() {
+      public boolean accept(SNode it) {
+        return "EditorAspectDescriptorImpl".equals(SPropertyOperations.getString(it, "name"));
+      }
+    });
+  }
+
+  private static SModel getAspectModel(SNode modelReference, LanguageAspect aspect, TemplateQueryContext genContext) {
+    SNode language = SNodeOperations.cast(SNodeOperations.getParent(modelReference), "jetbrains.mps.lang.project.structure.Language");
+    if (language == null) {
+      genContext.showErrorMessage(modelReference, "Parent of ModelReference is not a Language: " + SPropertyOperations.getString(modelReference, "qualifiedName"));
+      return null;
+    }
+    SModuleReference moduleReference = PersistenceFacade.getInstance().createModuleReference(BehaviorReflection.invokeVirtual(String.class, language, "virtual_getModuleReference_9020561928507315628", new Object[]{}));
+    Language languageModule = ModuleRepositoryFacade.getInstance().getModule(moduleReference, Language.class);
+    if (languageModule == null) {
+      genContext.showErrorMessage(language, "No language in repository: " + SPropertyOperations.getString(language, "namespace"));
+      return null;
+    }
+    SModel aspectModel = aspect.get(languageModule);
+    return (aspectModel == null || !(aspectModel.getModelId().toString().equals(SPropertyOperations.getString(modelReference, "uuid"))) ?
+      null :
+      aspectModel
     );
   }
 }
