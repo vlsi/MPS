1d4eaf6c404051005759550bb64c703cc26fcf9d
Evgeny Gryaznov <Evgeny.Gryaznov@jetbrains.com>
2013-07-29 15:18:36 +0200

    MPS-18405 NPE in MPS 3.0 EAP when using traces

--- core/openapi/source/org/jetbrains/mps/openapi/model/SNodeUtil.java
+++ core/openapi/source/org/jetbrains/mps/openapi/model/SNodeUtil.java
@@ -36,7 +36,7 @@ public class SNodeUtil {
   /**
    * Returns whether the given node belongs to the repository (or to one of its parent repositories).
    */
-  public static boolean isAccessible(SNode node, SRepository inRepository) {
+  public static boolean isAccessible(@NotNull SNode node, SRepository inRepository) {
     inRepository.getModelAccess().checkReadAccess();
 
     SModel model = node.getModel();
