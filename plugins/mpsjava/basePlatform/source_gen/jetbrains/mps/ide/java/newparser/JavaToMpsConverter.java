163953b6220a52cac88471626c0774427a879d30
Daniil Elovkov <daniil.elovkov@jetbrains.com>
2013-08-28 21:42:14 +0400

    Adding baselanguage.javadoc to models after java import

--- plugins/mpsjava/basePlatform/source_gen/jetbrains/mps/ide/java/newparser/JavaToMpsConverter.java
+++ plugins/mpsjava/basePlatform/source_gen/jetbrains/mps/ide/java/newparser/JavaToMpsConverter.java
@@ -160,6 +160,7 @@ public class JavaToMpsConverter {
             }
 
             ((SModelBase) model).addLanguage(PersistenceFacade.getInstance().createModuleReference("f3061a53-9226-4cc5-a443-f952ceaf5816(jetbrains.mps.baseLanguage)"));
+            ((SModelBase) model).addLanguage(PersistenceFacade.getInstance().createModuleReference("f2801650-65d5-424e-bb1b-463a8781b786(jetbrains.mps.baseLanguage.javadoc)"));
 
             Set<SNode> roots = MapSequence.fromMap(classesPerPackage).get(pakage);
             SetSequence.fromSet(roots).visitAll(new IVisitor<SNode>() {
