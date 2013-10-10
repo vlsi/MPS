76bbc43ace5d500d44c8021d6218b847592d6523
Vaclav <vaclav.pech@gmail.com>
2013-10-10 11:10:53 +0200

    MPS-12853 - Static context detection, new refactoring and updated action plus dialog

--- languages/baseLanguage/baseLanguage/solutions/jetbrains.mps.baseLanguage.util/source_gen/jetbrains/mps/baseLanguage/util/plugin/refactorings/FieldInitializationPlace.java
+++ languages/baseLanguage/baseLanguage/solutions/jetbrains.mps.baseLanguage.util/source_gen/jetbrains/mps/baseLanguage/util/plugin/refactorings/FieldInitializationPlace.java
@@ -6,7 +6,8 @@ package jetbrains.mps.baseLanguage.util.plugin.refactorings;
 public enum FieldInitializationPlace {
   FIELD("Field declaration"),
   CONSTRUCTOR("Class constructor(s)"),
-  METHOD("Current method");
+  METHOD("Current method"),
+  STATICINIT("Static initializer");
 
   private String myName;
 
