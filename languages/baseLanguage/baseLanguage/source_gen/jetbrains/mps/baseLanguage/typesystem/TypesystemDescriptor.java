c12d6acd2cf5fed49d4eb58fd77e4a2ecc45a58f
Vaclav <vaclav.pech@gmail.com>
2013-10-24 11:58:25 +0200

    MPS-11917 - updated checking rules for array creators to correctly detect gereric array creation

--- languages/baseLanguage/baseLanguage/source_gen/jetbrains/mps/baseLanguage/typesystem/TypesystemDescriptor.java
+++ languages/baseLanguage/baseLanguage/source_gen/jetbrains/mps/baseLanguage/typesystem/TypesystemDescriptor.java
@@ -870,6 +870,10 @@ public class TypesystemDescriptor extends BaseHelginsDescriptor {
       this.myNonTypesystemRules.add(nonTypesystemRule);
     }
     {
+      NonTypesystemRule_Runtime nonTypesystemRule = new genericArrayCreatorsWithInitializerAreIllegal_NonTypesystemRule();
+      this.myNonTypesystemRules.add(nonTypesystemRule);
+    }
+    {
       NonTypesystemRule_Runtime nonTypesystemRule = new typeof_ClassConcept_NonTypesystemRule();
       this.myNonTypesystemRules.add(nonTypesystemRule);
     }
@@ -1107,7 +1111,7 @@ public class TypesystemDescriptor extends BaseHelginsDescriptor {
         }
 
         public SNode convert(SNode contextNode, String role, SNode variable, boolean isAggregation) {
-          return _quotation_createNode_3ist9o_a0a1a0a0a0ik0a();
+          return _quotation_createNode_3ist9o_a0a1a0a0a0jk0a();
         }
       };
       this.myVariableConverters.add(converter);
@@ -1140,14 +1144,14 @@ public class TypesystemDescriptor extends BaseHelginsDescriptor {
     {
       OverloadedOpsProvider_OneTypeSpecified provider = new OverloadedOpsProvider_OneTypeSpecified() {
         {
-          this.myOperandType = _quotation_createNode_3ist9o_a0a0a0a0a0a0a672a0();
+          this.myOperandType = _quotation_createNode_3ist9o_a0a0a0a0a0a0a772a0();
           this.myOperationConceptFQName = "jetbrains.mps.baseLanguage.structure.PlusExpression";
           this.myTypeIsExact = false;
           this.myIsStrong = false;
         }
 
         public SNode getOperationType(SNode operation, SNode leftOperandType, SNode rightOperandType) {
-          return _quotation_createNode_3ist9o_a0a1a0a0a0a0qk0a();
+          return _quotation_createNode_3ist9o_a0a1a0a0a0a0rk0a();
         }
       };
       this.myOverloadedOperationsTypesProviders.add(provider);
@@ -1403,7 +1407,7 @@ public class TypesystemDescriptor extends BaseHelginsDescriptor {
     }
   }
 
-  private static SNode _quotation_createNode_3ist9o_a0a1a0a0a0ik0a() {
+  private static SNode _quotation_createNode_3ist9o_a0a1a0a0a0jk0a() {
     PersistenceFacade facade = PersistenceFacade.getInstance();
     SNode quotedNode_1 = null;
     quotedNode_1 = SModelUtil_new.instantiateConceptDeclaration("jetbrains.mps.baseLanguage.structure.ClassifierType", null, null, GlobalScope.getInstance(), false);
@@ -1411,14 +1415,14 @@ public class TypesystemDescriptor extends BaseHelginsDescriptor {
     return quotedNode_1;
   }
 
-  private static SNode _quotation_createNode_3ist9o_a0a0a0a0a0a0a672a0() {
+  private static SNode _quotation_createNode_3ist9o_a0a0a0a0a0a0a772a0() {
     PersistenceFacade facade = PersistenceFacade.getInstance();
     SNode quotedNode_1 = null;
     quotedNode_1 = SModelUtil_new.instantiateConceptDeclaration("jetbrains.mps.baseLanguage.structure.StringType", null, null, GlobalScope.getInstance(), false);
     return quotedNode_1;
   }
 
-  private static SNode _quotation_createNode_3ist9o_a0a1a0a0a0a0qk0a() {
+  private static SNode _quotation_createNode_3ist9o_a0a1a0a0a0a0rk0a() {
     PersistenceFacade facade = PersistenceFacade.getInstance();
     SNode quotedNode_1 = null;
     quotedNode_1 = SModelUtil_new.instantiateConceptDeclaration("jetbrains.mps.baseLanguage.structure.StringType", null, null, GlobalScope.getInstance(), false);
