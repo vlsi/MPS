51977d712bda1428c785c5c39635081398042874
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2014-01-18 23:46:37 +0400

    VisibilityUtil created

---
+++ core/kernel/source/jetbrains/mps/project/dependency/VisibilityUtil.java
@@ -0,0 +1,23 @@
+package jetbrains.mps.project.dependency;
+
+import jetbrains.mps.project.dependency.GlobalModuleDependenciesManager.Deptype;
+import org.jetbrains.mps.openapi.model.SModel;
+import org.jetbrains.mps.openapi.module.SModule;
+
+public class VisibilityUtil {
+  public static boolean isVisible(SModule from, SModule what) {
+    return new GlobalModuleDependenciesManager(from).getModules(Deptype.VISIBLE).contains(what);
+  }
+
+  public static boolean isVisible(SModule from, SModel what) {
+    SModule module = what.getModule();
+    if (module == null) return false;
+    return isVisible(from, module);
+  }
+
+  public static boolean isVisible(SModel from, SModel what) {
+    SModule fromModule = from.getModule();
+    if (fromModule == null) return false;
+    return isVisible(fromModule, what);
+  }
+}
