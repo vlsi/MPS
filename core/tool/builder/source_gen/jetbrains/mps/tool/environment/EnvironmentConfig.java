fb2f6e693bc473e019ea254c97f6731f1719a574
Artem Tikhomirov <tikhomirov.artem@gmail.com>
2014-02-06 19:49:58 +0100

    Check that test in the build are failing due to this change

--- core/tool/builder/source_gen/jetbrains/mps/tool/environment/EnvironmentConfig.java
+++ core/tool/builder/source_gen/jetbrains/mps/tool/environment/EnvironmentConfig.java
@@ -73,7 +73,8 @@ public class EnvironmentConfig {
     // running JUnit tests from an MPS instance requires "JUnit Tests" configuration, which is registered 
     // from execution.configuration (@see JUnitTests_Kind in RunConfigurationsInitializer_CustomApplicationPlugin), 
     // hence we add all dependencies of execution.configuration plugin. 
-    return addPlugin("jetbrains.mps.core").addPlugin("jetbrains.mps.vcs").addPlugin("jetbrains.mps.ide.make").addPlugin("jetbrains.mps.testing").addPlugin("jetbrains.mps.execution.api").addPlugin("jetbrains.mps.debugger.api").addPlugin("jetbrains.mps.debugger.java").addPlugin("jetbrains.mps.execution.languages").addPlugin("jetbrains.mps.execution.configurations");
+//    return addPlugin("jetbrains.mps.core").addPlugin("jetbrains.mps.vcs").addPlugin("jetbrains.mps.ide.make").addPlugin("jetbrains.mps.testing").addPlugin("jetbrains.mps.execution.api").addPlugin("jetbrains.mps.debugger.api").addPlugin("jetbrains.mps.debugger.java").addPlugin("jetbrains.mps.execution.languages").addPlugin("jetbrains.mps.execution.configurations");
+    return addPlugin("jetbrains.mps.vcs").addPlugin("jetbrains.mps.ide.editor").addPlugin("jetbrains.mps.ide.make");
   }
 
   public EnvironmentConfig withBootstrapLibraries() {
