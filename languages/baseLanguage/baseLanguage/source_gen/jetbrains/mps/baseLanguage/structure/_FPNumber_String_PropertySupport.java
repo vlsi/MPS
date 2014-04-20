a980be9a3df421ca3e999a5abee24520f336328b
Vaclav <vaclav.pech@gmail.com>
2014-04-20 19:00:13 +0200

    MPS-19523, MPS-19522, MPS-19501 - Float literal regular expression updated, inference rule updated to distinguish float and double constants

--- languages/baseLanguage/baseLanguage/source_gen/jetbrains/mps/baseLanguage/structure/_FPNumber_String_PropertySupport.java
+++ languages/baseLanguage/baseLanguage/source_gen/jetbrains/mps/baseLanguage/structure/_FPNumber_String_PropertySupport.java
@@ -12,6 +12,6 @@ public class _FPNumber_String_PropertySupport extends PropertySupport {
     } else {
       testValue = value;
     }
-    return testValue.matches("-?[0-9]+\\.[0-9]*");
+    return testValue.matches("-?[0-9]+\\.[0-9]*([Ee][\\+\\-]?[0-9]+)?[fFdD]?");
   }
 }
