ac25d8b0fe26e0283fc7109c92eeec5630bc8dbe
Mihail Buryakov <Mihail.Buryakov@jetbrains.com>
2014-05-06 13:33:07 +0400

    changed label (dominates over <all>)

--- languages/languageDesign/editor/source_gen/jetbrains/mps/lang/editor/editor/DominatesRecord_Editor.java
+++ languages/languageDesign/editor/source_gen/jetbrains/mps/lang/editor/editor/DominatesRecord_Editor.java
@@ -43,7 +43,7 @@ public class DominatesRecord_Editor extends DefaultNodeEditor {
   private EditorCell createRefNode_yrqgnt_b0(EditorContext editorContext, SNode node) {
     CellProviderWithRole provider = new RefNodeCellProvider(node, editorContext);
     provider.setRole("dominatesStyleClassList");
-    provider.setNoTargetText("<default>");
+    provider.setNoTargetText("<all>");
     EditorCell editorCell;
     editorCell = provider.createEditorCell(editorContext);
     if (editorCell.getRole() == null) {
