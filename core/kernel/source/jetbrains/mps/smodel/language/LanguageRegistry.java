a7538738b8eafa2d1f0ab0d9c0626f843a31fbde
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2014-06-17 00:23:36 +0400

    migrating to SConceptId:LanguageRegistry: id support (we suppose this to work only before model load, just to play refactorings on first startup)

--- core/kernel/source/jetbrains/mps/smodel/language/LanguageRegistry.java
+++ core/kernel/source/jetbrains/mps/smodel/language/LanguageRegistry.java
@@ -19,10 +19,12 @@ import jetbrains.mps.classloading.ClassLoaderManager;
 import jetbrains.mps.classloading.MPSClassesListener;
 import jetbrains.mps.components.CoreComponent;
 import jetbrains.mps.smodel.Language;
+import jetbrains.mps.smodel.MPSModuleRepository;
 import jetbrains.mps.smodel.ModelAccess;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.jetbrains.annotations.Nullable;
+import org.jetbrains.mps.openapi.language.SLanguageId;
 import org.jetbrains.mps.openapi.module.SModule;
 
 import java.util.Collection;
@@ -127,6 +129,11 @@ public class LanguageRegistry implements CoreComponent, MPSClassesListener {
   public Collection<LanguageRuntime> getAvailableLanguages() {
     ModelAccess.assertLegalRead();
     return myLanguages.values();
+  }
+
+  @Nullable
+  public LanguageRuntime getLanguage(SLanguageId id) {
+    return getLanguage(MPSModuleRepository.getInstance().getDebugRegistry().getLanguageName(id));
   }
 
   @Nullable
