f044964c1d0888720ed4d7940a9e85b8d9725984
Alexander Shatalin <Alexander.Shatalin@jetbrains.com>
2014-07-11 10:01:04 +0200

    MPS-17777 Better distinguish between the DELETE and BACKSPACE action events - in the code written in MPS using same actions for DELETE and BACKSPACE handling

--- languages/baseLanguage/constructors/source_gen/jetbrains/mps/baseLanguage/constructors/editor/CustomConstructorUsage_Editor.java
+++ languages/baseLanguage/constructors/source_gen/jetbrains/mps/baseLanguage/constructors/editor/CustomConstructorUsage_Editor.java
@@ -99,6 +99,7 @@ public class CustomConstructorUsage_Editor extends DefaultNodeEditor {
                   elementCell.putUserObject(AbstractCellListHandler.ELEMENT_CELL_ACTIONS_SET, AbstractCellListHandler.ELEMENT_CELL_ACTIONS_SET);
                   if (elementNode != null) {
                     elementCell.setAction(CellActionType.DELETE, new CellAction_DeleteNode(elementNode));
+                    elementCell.setAction(CellActionType.BACKSPACE, new CellAction_DeleteNode(elementNode));
                     elementCell.addKeyMap(new RefNodeListHandlerElementKeyMap(this, SPropertyOperations.getString(SLinkOperations.getTarget(node, "customConstructor", false), "separator")));
                   }
                   if (elementCell.getSubstituteInfo() == null || elementCell.getSubstituteInfo() instanceof DefaultReferenceSubstituteInfo) {
