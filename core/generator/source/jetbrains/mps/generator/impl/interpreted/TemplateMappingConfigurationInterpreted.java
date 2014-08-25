78e76b4e44beaeb71db1d824d85d1ec57be081bd
Artem Tikhomirov <tikhomirov.artem@gmail.com>
2014-08-25 15:08:30 +0200

    MPS-20164 IAE due to invalid generator model. Postpone model initialization up to actual use, otherwise mere consideration of a generator for make purposes triggers template model loading and failures to read a model unrelated to actual generation process.

--- core/generator/source/jetbrains/mps/generator/impl/interpreted/TemplateMappingConfigurationInterpreted.java
+++ core/generator/source/jetbrains/mps/generator/impl/interpreted/TemplateMappingConfigurationInterpreted.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2011 JetBrains s.r.o.
+ * Copyright 2003-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,11 +15,9 @@
  */
 package jetbrains.mps.generator.impl.interpreted;
 
-import jetbrains.mps.generator.impl.DefaultTemplateContext;
 import jetbrains.mps.generator.impl.GenerationFailureException;
 import jetbrains.mps.generator.impl.RuleUtil;
 import jetbrains.mps.generator.impl.query.MapConfigurationCondition;
-import jetbrains.mps.generator.impl.query.QueryProviderBase;
 import jetbrains.mps.generator.runtime.TemplateCreateRootRule;
 import jetbrains.mps.generator.runtime.TemplateDropRootRule;
 import jetbrains.mps.generator.runtime.TemplateMappingConfiguration;
@@ -43,49 +41,72 @@ import java.util.List;
 public class TemplateMappingConfigurationInterpreted implements TemplateMappingConfiguration {
 
   private final SNode myMappingConfiguration;
+  private final TemplateModel myModel;
 
-  private List<TemplateReductionRule> myReductionRules = new ArrayList<TemplateReductionRule>();
-  private List<TemplateCreateRootRule> myCreateRootRules = new ArrayList<TemplateCreateRootRule>();
-  private List<TemplateRootMappingRule> myRootMappingRules = new ArrayList<TemplateRootMappingRule>();
-  private List<TemplateWeavingRule> myWeaving_MappingRules = new ArrayList<TemplateWeavingRule>();
-  private List<TemplateDropRootRule> myDropRootRules = new ArrayList<TemplateDropRootRule>();
+  private List<TemplateReductionRule> myReductionRules;
+  private List<TemplateCreateRootRule> myCreateRootRules;
+  private List<TemplateRootMappingRule> myRootMappingRules;
+  private List<TemplateWeavingRule> myWeavingRules;
+  private List<TemplateDropRootRule> myDropRootRules;
+  private List<TemplateMappingScript> myPreScripts;
+  private List<TemplateMappingScript> myPostScripts;
 
-  private List<TemplateMappingScript> myPreScripts = new ArrayList<TemplateMappingScript>();
-  private List<TemplateMappingScript> myPostScripts = new ArrayList<TemplateMappingScript>();
-  private TemplateModel myModel;
   private MapConfigurationCondition myCondition;
+  private volatile boolean myInitialized = false;
 
   public TemplateMappingConfigurationInterpreted(TemplateModel model, SNode mappingConfiguration) {
     myModel = model;
     myMappingConfiguration = mappingConfiguration;
-    int patternRulesCount = 0;
+  }
 
-    for (SNode child : mappingConfiguration.getChildren()) {
-      String conceptName = child.getConcept().getQualifiedName();
-      if (conceptName.equals(RuleUtil.concept_Reduction_MappingRule)) {
-        myReductionRules.add(new TemplateReductionRuleInterpreted(child));
-      } else if (conceptName.equals(RuleUtil.concept_Root_MappingRule)) {
-        myRootMappingRules.add(new TemplateRootMappingRuleInterpreted(child));
-      } else if (conceptName.equals(RuleUtil.concept_PatternReduction_MappingRule)) {
-        // TODO make more effective
-        myReductionRules.add(patternRulesCount++, new TemplateReductionPatternRuleInterpreted(child));
-      } else if (conceptName.equals(RuleUtil.concept_CreateRootRule)) {
-        myCreateRootRules.add(new TemplateCreateRootRuleInterpreted(child));
-      } else if (conceptName.equals(RuleUtil.concept_Weaving_MappingRule)) {
-        myWeaving_MappingRules.add(new TemplateWeavingRuleInterpreted(child));
-      } else if (conceptName.equals(RuleUtil.concept_DropRootRule)) {
-        myDropRootRules.add(new TemplateDropRuleInterpreted(child));
-      } else if (conceptName.equals(RuleUtil.concept_MappingScriptReference)) {
-        SNode mappingScript = RuleUtil.getMappingScriptReference_Script(child);
-        if (mappingScript == null) {
-          continue;
-        }
-        if (child.getRoleInParent().equals(RuleUtil.link_MappingConfiguration_preMappingScript)) {
-          myPreScripts.add(new TemplateMappingScriptInterpreted(mappingScript));
-        } else {
-          myPostScripts.add(new TemplateMappingScriptInterpreted(mappingScript));
+  private void init() {
+    if (myInitialized) {
+      return;
+    }
+    synchronized (this) {
+      myCreateRootRules = new ArrayList<TemplateCreateRootRule>(5);
+      myRootMappingRules = new ArrayList<TemplateRootMappingRule>(5);
+      myWeavingRules = new ArrayList<TemplateWeavingRule>(5);
+      myDropRootRules = new ArrayList<TemplateDropRootRule>(5);
+      myPreScripts = new ArrayList<TemplateMappingScript>(5);
+      myPostScripts = new ArrayList<TemplateMappingScript>(5);
+      ArrayList<TemplateReductionRule> reductionRules = new ArrayList<TemplateReductionRule>(20);
+      ArrayList<TemplateReductionRule> patternRules = new ArrayList<TemplateReductionRule>(5);
+
+      for (SNode child : myMappingConfiguration.getChildren()) {
+        String conceptName = child.getConcept().getQualifiedName();
+        if (conceptName.equals(RuleUtil.concept_Reduction_MappingRule)) {
+          reductionRules.add(new TemplateReductionRuleInterpreted(child));
+        } else if (conceptName.equals(RuleUtil.concept_Root_MappingRule)) {
+          myRootMappingRules.add(new TemplateRootMappingRuleInterpreted(child));
+        } else if (conceptName.equals(RuleUtil.concept_PatternReduction_MappingRule)) {
+          patternRules.add(new TemplateReductionPatternRuleInterpreted(child));
+        } else if (conceptName.equals(RuleUtil.concept_CreateRootRule)) {
+          myCreateRootRules.add(new TemplateCreateRootRuleInterpreted(child));
+        } else if (conceptName.equals(RuleUtil.concept_Weaving_MappingRule)) {
+          myWeavingRules.add(new TemplateWeavingRuleInterpreted(child));
+        } else if (conceptName.equals(RuleUtil.concept_DropRootRule)) {
+          myDropRootRules.add(new TemplateDropRuleInterpreted(child));
+        } else if (conceptName.equals(RuleUtil.concept_MappingScriptReference)) {
+          SNode mappingScript = RuleUtil.getMappingScriptReference_Script(child);
+          if (mappingScript == null) {
+            continue;
+          }
+          if (child.getRoleInParent().equals(RuleUtil.link_MappingConfiguration_preMappingScript)) {
+            myPreScripts.add(new TemplateMappingScriptInterpreted(mappingScript));
+          } else {
+            myPostScripts.add(new TemplateMappingScriptInterpreted(mappingScript));
+          }
         }
       }
+      if (patternRules.isEmpty()) {
+        myReductionRules = reductionRules;
+      } else {
+        myReductionRules = new ArrayList<TemplateReductionRule>(patternRules.size() + reductionRules.size());
+        myReductionRules.addAll(patternRules);
+        myReductionRules.addAll(reductionRules);
+      }
+      myInitialized = true;
     }
   }
 
@@ -121,36 +142,43 @@ public class TemplateMappingConfigurationInterpreted implements TemplateMappingC
 
   @Override
   public Collection<TemplateRootMappingRule> getRootRules() {
+    init();
     return myRootMappingRules;
   }
 
   @Override
   public Collection<TemplateCreateRootRule> getCreateRules() {
+    init();
     return myCreateRootRules;
   }
 
   @Override
   public Collection<TemplateDropRootRule> getDropRules() {
+    init();
     return myDropRootRules;
   }
 
   @Override
   public Collection<TemplateReductionRule> getReductionRules() {
+    init();
     return myReductionRules;
   }
 
   @Override
   public Collection<TemplateWeavingRule> getWeavingRules() {
-    return myWeaving_MappingRules;
+    init();
+    return myWeavingRules;
   }
 
   @Override
   public Collection<TemplateMappingScript> getPreScripts() {
+    init();
     return myPreScripts;
   }
 
   @Override
   public Collection<TemplateMappingScript> getPostScripts() {
+    init();
     return myPostScripts;
   }
 
