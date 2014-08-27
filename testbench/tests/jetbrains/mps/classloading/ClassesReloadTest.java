fd099949a106306548a2a4347e54cff98c36c74e
AlexeyP <lexpyh@gmail.com>
2014-08-27 19:13:05 +0400

    Test became harder

--- testbench/tests/jetbrains/mps/classloading/ClassesReloadTest.java
+++ testbench/tests/jetbrains/mps/classloading/ClassesReloadTest.java
@@ -16,40 +16,138 @@
 package jetbrains.mps.classloading;
 
 import jetbrains.mps.WorkbenchMpsTest;
-import jetbrains.mps.internal.collections.runtime.backports.LinkedList;
+import jetbrains.mps.lang.typesystem.runtime.IHelginsDescriptor;
+import jetbrains.mps.openapi.editor.descriptor.EditorAspectDescriptor;
 import jetbrains.mps.project.Project;
+import jetbrains.mps.smodel.Language;
 import jetbrains.mps.smodel.MPSModuleRepository;
 import jetbrains.mps.smodel.ModelAccess;
+import jetbrains.mps.smodel.language.LanguageRegistry;
+import jetbrains.mps.smodel.language.LanguageRuntime;
+import jetbrains.mps.smodel.runtime.BehaviorAspectDescriptor;
+import jetbrains.mps.smodel.runtime.ConstraintsAspectDescriptor;
+import jetbrains.mps.smodel.runtime.LanguageAspectDescriptor;
+import jetbrains.mps.smodel.runtime.MakeAspectDescriptor;
+import jetbrains.mps.smodel.runtime.StructureAspectDescriptor;
 import jetbrains.mps.tool.builder.util.PathManager;
 import org.junit.Assert;
 import org.jetbrains.mps.openapi.module.SModule;
 import org.junit.Test;
 
 import java.io.File;
-import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import static org.junit.Assert.fail;
 
 public class ClassesReloadTest extends WorkbenchMpsTest {
   private static final String PROJECT_PATH = PathManager.getHomePath();
 
+  private Map<String, String> myModuleNamesToErrors = new TreeMap<String, String>();
+
   @Test
   public void ClassesAreLoaded() {
     Project project = openProject(new File(PROJECT_PATH));
-    ModelAccess.instance().runReadAction(new Runnable() {
-      @Override
-      public void run() {
-        List<SModule> wrongReloadableModules = new LinkedList<SModule>();
-        ClassLoaderManager classLoaderManager = ClassLoaderManager.getInstance();
-        for (SModule module : MPSModuleRepository.getInstance().getModules()) {
-          if (classLoaderManager.canLoad(module))
-            if (classLoaderManager.getClassLoader(module) == null)
-              wrongReloadableModules.add(module);
+    try {
+      ModelAccess.instance().runReadAction(new Runnable() {
+        @Override
+        public void run() {
+          for (SModule module : MPSModuleRepository.getInstance().getModules()) {
+            checkModule(module);
+          }
+          if (!myModuleNamesToErrors.isEmpty()) {
+            System.err.println("Some class loading problems for modules:");
+            for (String moduleName : myModuleNamesToErrors.keySet())
+              System.err.println(moduleName + " ::: " + myModuleNamesToErrors.get(moduleName));
+            fail();
+          }
         }
-        Assert.assertTrue("No class loader for modules:", wrongReloadableModules.isEmpty());
-        for (SModule module : wrongReloadableModules)
-          System.err.println(module.getModuleName());
-      }
-    });
-    disposeProject(project);
+      });
+    } finally {
+      disposeProject(project);
+    }
   }
 
+  private boolean checkModule(SModule module) {
+    ClassLoaderManager classLoaderManager = ClassLoaderManager.getInstance();
+    if (classLoaderManager.canLoad(module))
+      if (classLoaderManager.getClassLoader(module) == null) {
+        myModuleNamesToErrors.put(module.toString(), "No class loader for the module");
+        return false;
+      }
+
+    if (module instanceof Language)
+      try {
+        Language language = (Language) module;
+        checkIsRegistered(language);
+        tryLoadDescriptors(language);
+      } catch (AssertionFailedException e) {
+        myModuleNamesToErrors.put(module.toString(), e.getMsg());
+        return false;
+      } catch (Throwable t) {
+        return false;
+      }
+    return true;
+  }
+
+  private void checkIsRegistered(Language language) throws AssertionFailedException {
+    if (null == LanguageRegistry.getInstance().getLanguage(language))
+      throw new AssertionFailedException("The language is not registered in the LanguageRegistry");
+  }
+
+  private void tryLoadDescriptors(Language language) throws AssertionFailedException {
+    LanguageRuntime languageRuntime = LanguageRegistry.getInstance().getLanguage(language);
+    assert languageRuntime != null;
+    reachStructure(languageRuntime);
+    reachBehaviour(languageRuntime);
+    reachMake(languageRuntime);
+    reachTypeSystem(languageRuntime);
+    reachConstraints(languageRuntime);
+    reachEditor(languageRuntime);
+    languageRuntime.getAspect(ConstraintsAspectDescriptor.class);
+  }
+
+  private void reachStructure(LanguageRuntime languageRuntime) throws AssertionFailedException {
+    reachAspect(languageRuntime, StructureAspectDescriptor.class);
+  }
+
+  private void reachBehaviour(LanguageRuntime languageRuntime) throws AssertionFailedException {
+    reachAspect(languageRuntime, BehaviorAspectDescriptor.class);
+  }
+
+  private void reachMake(LanguageRuntime languageRuntime) throws AssertionFailedException {
+    reachAspect(languageRuntime, MakeAspectDescriptor.class);
+  }
+
+  private void reachTypeSystem(LanguageRuntime languageRuntime) throws AssertionFailedException {
+    reachAspect(languageRuntime, IHelginsDescriptor.class);
+  }
+
+  private void reachConstraints(LanguageRuntime languageRuntime) throws AssertionFailedException {
+    reachAspect(languageRuntime, ConstraintsAspectDescriptor.class);
+  }
+
+  private void reachEditor(LanguageRuntime languageRuntime) throws AssertionFailedException {
+    reachAspect(languageRuntime, EditorAspectDescriptor.class);
+  }
+
+  private void reachAspect(LanguageRuntime languageRuntime, Class<? extends LanguageAspectDescriptor> aspect) throws AssertionFailedException {
+    try {
+      languageRuntime.getAspect(aspect);
+    } catch (Throwable t) {
+      throw new AssertionFailedException("Exception while getting the aspect descriptor : " + aspect);
+    }
+  }
+
+  private static class AssertionFailedException extends Exception {
+    private final String myMsg;
+    public AssertionFailedException(String msg) {
+      super(msg);
+      myMsg = msg;
+    }
+
+    public String getMsg() {
+      return myMsg;
+    }
+  }
 }
