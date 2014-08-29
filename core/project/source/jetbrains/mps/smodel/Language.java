eefc80fb6cc7b072bca567ab0a4780fa26cdabb2
AlexeyP <lexpyh@gmail.com>
2014-08-29 18:11:25 +0400

    getDeclaredDependencies acts with coreLanguage the same way it acts with usual dependencies

--- core/project/source/jetbrains/mps/smodel/Language.java
+++ core/project/source/jetbrains/mps/smodel/Language.java
@@ -115,9 +115,8 @@ public class Language extends AbstractModule implements MPSModuleOwner {
       }
     }
 
-    if (BootstrapLanguages.coreLanguage() == null)
-      throw new IllegalStateException("Repository is not in a valid state: core language is not present");
-    dependencies.add(new SDependencyImpl(BootstrapLanguages.coreLanguage(), SDependencyScope.DEFAULT, true));
+    if (BootstrapLanguages.coreLanguage() != null)
+      dependencies.add(new SDependencyImpl(BootstrapLanguages.coreLanguage(), SDependencyScope.DEFAULT, true));
 
     return dependencies;
   }
