c49d175984526f4d31297e3467eac43dd6a09450
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2014-09-16 16:37:28 +0200

    migrating id->entity: SAbstractLink: class moved to impl package

--- core/kernel/source/jetbrains/mps/smodel/ids/SAbstractLinkId.java
+++ core/kernel/source/jetbrains/mps/smodel/ids/SAbstractLinkId.java
@@ -13,7 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jetbrains.mps.openapi.language;
+package jetbrains.mps.smodel.ids;
+
+import org.jetbrains.mps.openapi.language.SConceptId;
 
 public abstract class SAbstractLinkId {
   protected final SConceptId myConceptId;
