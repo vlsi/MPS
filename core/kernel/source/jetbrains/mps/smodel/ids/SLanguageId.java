336701eccc2699a4f4bdfcbf0def47344a114418
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2014-09-16 16:45:58 +0200

    migrating id->entity: SLanguageId: move id class to impl package

--- core/kernel/source/jetbrains/mps/smodel/ids/SLanguageId.java
+++ core/kernel/source/jetbrains/mps/smodel/ids/SLanguageId.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jetbrains.mps.openapi.language;
+package jetbrains.mps.smodel.ids;
 
 import org.jetbrains.annotations.NotNull;
 
