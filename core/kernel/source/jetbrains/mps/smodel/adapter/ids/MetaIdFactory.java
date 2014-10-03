3067799274607d874c6814315cb4778534260c27
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2014-10-03 17:32:03 +0200

    MetaIdFactory for simplier generated code

---
+++ core/kernel/source/jetbrains/mps/smodel/adapter/ids/MetaIdFactory.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2003-2014 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jetbrains.mps.smodel.adapter.ids;
+
+import org.jetbrains.mps.openapi.language.SContainmentLinkId;
+
+import java.util.UUID;
+
+public abstract class MetaIdFactory {
+  public static  org.jetbrains.mps.openapi.language.SConceptId conceptId(UUID lang, long concept){
+    return new SConceptId(new SLanguageId(lang), concept);
+  }
+
+  public static  org.jetbrains.mps.openapi.language.SPropertyId propId(UUID lang, long concept, long prop){
+    return new SPropertyId(conceptId(lang, concept),prop);
+  }
+
+  public static  org.jetbrains.mps.openapi.language.SReferenceLinkId refId(UUID lang, long concept, long ref){
+    return new SReferenceLinkId(conceptId(lang, concept),ref);
+  }
+
+  public static  SContainmentLinkId linkId(UUID lang, long concept, long link){
+    return new jetbrains.mps.smodel.adapter.ids.SContainmentLinkId(conceptId(lang, concept),link);
+  }
+}
