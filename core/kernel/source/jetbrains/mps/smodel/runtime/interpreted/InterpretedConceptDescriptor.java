96ef8f851ea64d9920676f019c3fb14999909444
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2014-10-08 17:32:35 +0200

    filling InterpretedConceptDescriptor: ref/link/prop, interpreted descriptors not yet done

--- core/kernel/source/jetbrains/mps/smodel/runtime/interpreted/InterpretedConceptDescriptor.java
+++ core/kernel/source/jetbrains/mps/smodel/runtime/interpreted/InterpretedConceptDescriptor.java
@@ -26,6 +26,9 @@ import jetbrains.mps.smodel.adapter.ids.SContainmentLinkId;
 import jetbrains.mps.smodel.adapter.ids.SPropertyId;
 import jetbrains.mps.smodel.adapter.ids.SReferenceLinkId;
 import jetbrains.mps.smodel.language.ConceptRegistry;
+import jetbrains.mps.smodel.runtime.BaseLinkDescriptor;
+import jetbrains.mps.smodel.runtime.BasePropertyDescriptor;
+import jetbrains.mps.smodel.runtime.BaseReferenceDescriptor;
 import jetbrains.mps.smodel.runtime.ConceptDescriptor;
 import jetbrains.mps.smodel.runtime.LinkDescriptor;
 import jetbrains.mps.smodel.runtime.PropertyDescriptor;
@@ -57,12 +60,12 @@ class InterpretedConceptDescriptor extends BaseConceptDescriptor {
   private List<SConceptId> parentsIds;
 
   private Set<String> ancestors;
-  private Map<SPropertyId,PropertyDescriptor> myProperties;
-  private Map<String,PropertyDescriptor> myPropertiesByName;
-  private Map<SReferenceLinkId,ReferenceDescriptor> myReferences;
-  private Map<String,ReferenceDescriptor> myReferencesByName;
-  private Map<SContainmentLinkId,LinkDescriptor> myLinks;
-  private Map<String,LinkDescriptor> myLinksByName;
+  private Map<SPropertyId, PropertyDescriptor> myProperties;
+  private Map<String, PropertyDescriptor> myPropertiesByName;
+  private Map<SReferenceLinkId, ReferenceDescriptor> myReferences;
+  private Map<String, ReferenceDescriptor> myReferencesByName;
+  private Map<SContainmentLinkId, LinkDescriptor> myLinks;
+  private Map<String, LinkDescriptor> myLinksByName;
   private Set<String> propertyNames;
   private Set<String> referenceNames;
   private Set<String> childrenNames;
@@ -81,6 +84,14 @@ class InterpretedConceptDescriptor extends BaseConceptDescriptor {
 
     final List<String> directProperties = new ArrayList<String>();
     final List<String> directReferences = new ArrayList<String>();
+
+    final Map<SPropertyId, PropertyDescriptor> directPropertiesByIds = new HashMap<SPropertyId, PropertyDescriptor>();
+    final Map<SReferenceLinkId, ReferenceDescriptor> directReferencesByIds = new HashMap<SReferenceLinkId, ReferenceDescriptor>();
+    final Map<SContainmentLinkId, LinkDescriptor> directLinksByIds = new HashMap<SContainmentLinkId, LinkDescriptor>();
+
+    final Map<String, PropertyDescriptor> directPropertiesByName = new HashMap<String, PropertyDescriptor>();
+    final Map<String, ReferenceDescriptor> directReferencesByName = new HashMap<String, ReferenceDescriptor>();
+    final Map<String, LinkDescriptor> directLinksByName = new HashMap<String, LinkDescriptor>();
 
     NodeReadAccessCasterInEditor.runReadTransparentAction(new Runnable() {
       @Override
@@ -153,6 +164,12 @@ class InterpretedConceptDescriptor extends BaseConceptDescriptor {
           String name = property.getName();
           if (name != null) {
             directProperties.add(name);
+
+            SPropertyId propId = MetaIdByDeclaration.getPropId(((jetbrains.mps.smodel.SNode) property));
+            BasePropertyDescriptor pd = new BasePropertyDescriptor(propId, name);
+
+            directPropertiesByIds.put(propId, pd);
+            directPropertiesByName.put(name, pd);
           }
         }
 
@@ -166,8 +183,20 @@ class InterpretedConceptDescriptor extends BaseConceptDescriptor {
             }
             if (SNodeUtil.getLinkDeclaration_IsReference(link)) {
               directReferences.add(role);
+
+              SReferenceLinkId refId = MetaIdByDeclaration.getRefRoleId(((jetbrains.mps.smodel.SNode) link));
+              BaseReferenceDescriptor pd = new BaseReferenceDescriptor(refId, name);
+
+              directReferencesByIds.put(refId, pd);
+              directReferencesByName.put(name, pd);
             } else {
               childrenMap.put(role, !SNodeUtil.getLinkDeclaration_IsSingular(link));
+
+              SContainmentLinkId linkId = MetaIdByDeclaration.getLinkId(((jetbrains.mps.smodel.SNode) link));
+              BaseLinkDescriptor pd = new BaseLinkDescriptor(linkId, name);
+
+              directLinksByIds.put(linkId, pd);
+              directLinksByName.put(name, pd);
             }
           }
         }
@@ -193,36 +222,75 @@ class InterpretedConceptDescriptor extends BaseConceptDescriptor {
 
       // properties
       LinkedHashSet<String> properties = new LinkedHashSet<String>();
+      Map<SPropertyId, PropertyDescriptor> propertiesByIds = new HashMap<SPropertyId, PropertyDescriptor>();
+      Map<String, PropertyDescriptor> propertiesByName = new HashMap<String, PropertyDescriptor>();
+
       properties.addAll(directProperties);
+      propertiesByIds.putAll(directPropertiesByIds);
+      propertiesByName.putAll(directPropertiesByName);
 
       for (ConceptDescriptor parentDescriptor : parentDescriptors) {
         properties.addAll(parentDescriptor.getPropertyNames());
+        for (SPropertyId pid : parentDescriptor.getPropertyIds()) {
+          propertiesByIds.put(pid, parentDescriptor.getPropertyDescriptor(pid));
+        }
+        for (String pname : parentDescriptor.getPropertyNames()) {
+          propertiesByName.put(pname, parentDescriptor.getPropertyDescriptor(pname));
+        }
       }
 
       propertyNames = Collections.unmodifiableSet(properties);
+      myProperties = Collections.unmodifiableMap(propertiesByIds);
+      myPropertiesByName = Collections.unmodifiableMap(propertiesByName);
 
       // references
       LinkedHashSet<String> references = new LinkedHashSet<String>();
+      Map<SReferenceLinkId, ReferenceDescriptor> referencesByIds = new HashMap<SReferenceLinkId, ReferenceDescriptor>();
+      Map<String, ReferenceDescriptor> referencesByName = new HashMap<String, ReferenceDescriptor>();
+
       references.addAll(directReferences);
+      referencesByIds.putAll(directReferencesByIds);
+      referencesByName.putAll(directReferencesByName);
 
       for (ConceptDescriptor parentDescriptor : parentDescriptors) {
         references.addAll(parentDescriptor.getReferenceNames());
+        for (SReferenceLinkId rid : parentDescriptor.getReferenceIds()) {
+          referencesByIds.put(rid, parentDescriptor.getRefDescriptor(rid));
+        }
+        for (String rname : parentDescriptor.getReferenceNames()) {
+          referencesByName.put(rname, parentDescriptor.getRefDescriptor(rname));
+        }
       }
 
       referenceNames = Collections.unmodifiableSet(references);
+      myReferences = Collections.unmodifiableMap(referencesByIds);
+      myReferencesByName  = Collections.unmodifiableMap(referencesByName);
 
       // children
+      Map<SContainmentLinkId, LinkDescriptor> linksByIds = new HashMap<SContainmentLinkId, LinkDescriptor>();
+      Map<String, LinkDescriptor> linksByName = new HashMap<String, LinkDescriptor>();
 
+      linksByIds.putAll(directLinksByIds);
+      linksByName.putAll(directLinksByName);
 
       for (ConceptDescriptor parentDescriptor : parentDescriptors) {
         for (String child : parentDescriptor.getChildrenNames()) {
           childrenMap.put(child, parentDescriptor.isMultipleChild(child));
         }
         unorderedChildren.addAll(parentDescriptor.getUnorderedChildrenNames());
+
+        for (SContainmentLinkId lid : parentDescriptor.getLinkIds()) {
+          linksByIds.put(lid, parentDescriptor.getLinkDescriptor(lid));
+        }
+        for (String lname : parentDescriptor.getChildrenNames()) {
+          linksByName.put(lname, parentDescriptor.getLinkDescriptor(lname));
+        }
       }
       unorderedChildren = Collections.unmodifiableSet(unorderedChildren);
-
       childrenNames = Collections.unmodifiableSet(childrenMap.keySet());
+
+      myLinks = Collections.unmodifiableMap(linksByIds);
+      myLinksByName  = Collections.unmodifiableMap(linksByName);
     }
   }
 
