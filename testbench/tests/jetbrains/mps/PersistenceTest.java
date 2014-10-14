e8e537dd154a6c5d5fb0a52049343a21141ca5ab
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2014-10-14 15:48:23 +0200

    fixing PersistenceTest

--- testbench/tests/jetbrains/mps/PersistenceTest.java
+++ testbench/tests/jetbrains/mps/PersistenceTest.java
@@ -15,15 +15,14 @@
  */
 package jetbrains.mps;
 
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.ModalityState;
 import jetbrains.mps.extapi.model.SModelBase;
 import jetbrains.mps.extapi.persistence.FileDataSource;
 import jetbrains.mps.ide.ThreadUtils;
-import jetbrains.mps.testbench.junit.runners.ProjectTestsSupport;
-import jetbrains.mps.testbench.junit.runners.ProjectTestsSupport.ProjectRunnable;
-import org.apache.log4j.Logger;
-import org.apache.log4j.LogManager;
 import jetbrains.mps.project.MPSExtentions;
 import jetbrains.mps.project.Project;
+import jetbrains.mps.smodel.CopyUtil;
 import jetbrains.mps.smodel.DefaultSModelDescriptor;
 import jetbrains.mps.smodel.ModelAccess;
 import jetbrains.mps.smodel.SModelHeader;
@@ -32,21 +31,24 @@ import jetbrains.mps.smodel.loading.ModelLoadingState;
 import jetbrains.mps.smodel.persistence.def.ModelPersistence;
 import jetbrains.mps.smodel.persistence.def.ModelReadException;
 import jetbrains.mps.testbench.TestOutputFilter;
+import jetbrains.mps.testbench.junit.runners.ProjectTestsSupport;
+import jetbrains.mps.testbench.junit.runners.ProjectTestsSupport.ProjectRunnable;
 import jetbrains.mps.util.Computable;
 import jetbrains.mps.util.PathManager;
 import jetbrains.mps.vfs.FileSystem;
 import jetbrains.mps.vfs.IFile;
 import junit.framework.AssertionFailedError;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.jetbrains.mps.openapi.model.SModel;
 import org.jetbrains.mps.openapi.persistence.StreamDataSource;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
@@ -68,6 +70,8 @@ public class PersistenceTest extends WorkbenchMpsTest {
     }
   };
 
+  private jetbrains.mps.smodel.SModel myModelBackup;
+
   @Test
   public void testPersistenceWriteRead() {
     boolean result = ProjectTestsSupport.testOnProjectCopy(sourceZip, tempDir, TEST_PERSISTENCE_PROJECT,
@@ -79,7 +83,7 @@ public class PersistenceTest extends WorkbenchMpsTest {
             ModelAccess.instance().runWriteInEDT(new Runnable() {
               public void run() {
                 try {
-                  DefaultSModelDescriptor testModel = (DefaultSModelDescriptor) ProjectTestsSupport.getModel(project, TEST_MODEL);
+                  DefaultSModelDescriptor testModel = getModel(project);
                   assertEquals(START_PERSISTENCE_TEST_VERSION, testModel.getPersistenceVersion());
                   SModel model = testModel;
                   for (int i = START_PERSISTENCE_TEST_VERSION; i <= ModelPersistence.LAST_VERSION; ++i) {
@@ -108,7 +112,7 @@ public class PersistenceTest extends WorkbenchMpsTest {
                 }
               }
             });
-            ModelAccess.instance().flushEventQueue();
+            waitEDT();
             return success[0];
           }
         });
@@ -119,122 +123,187 @@ public class PersistenceTest extends WorkbenchMpsTest {
   public void testPersistenceUpgrade() {
     try { // errors about not found attributes are expected for old models
       filter.start();
-
-      final int version[] = {START_PERSISTENCE_TEST_VERSION, START_PERSISTENCE_TEST_VERSION};
-      for (; version[0] < ModelPersistence.LAST_VERSION; ++version[0])
-        for (version[1] = version[0] + 1; version[1] <= ModelPersistence.LAST_VERSION; ++version[1]) {
-          boolean result = ProjectTestsSupport.testOnProjectCopy(sourceZip, tempDir, TEST_PERSISTENCE_PROJECT,
-              new ProjectRunnable() {
-                public boolean execute(final Project project) {
-
-                  final DefaultSModelDescriptor testModel = ModelAccess.instance().runWriteAction(new Computable<DefaultSModelDescriptor>() {
-                    public DefaultSModelDescriptor compute() {
-                      DefaultSModelDescriptor modelDescr = (DefaultSModelDescriptor) ProjectTestsSupport.getModel(project, TEST_MODEL);
-                      modelDescr.reloadFromSource();   // no way to remove model from repository, so reloading
-                      assertEquals(START_PERSISTENCE_TEST_VERSION, modelDescr.getPersistenceVersion());
-                      return modelDescr;
-                    }
-                  });
-
-                  final List<DefaultSModelDescriptor> list = new ArrayList<DefaultSModelDescriptor>(1);
-                  list.add(testModel);
-
-                  if (version[0] > START_PERSISTENCE_TEST_VERSION) {
-                    ModelAccess.instance().runWriteInEDT(new Runnable() {
-                      public void run() {
-                        upgradePersistence(list, version[0]);
-                      }
-                    });
-                  }
-                  ModelAccess.instance().flushEventQueue();
-                  assertTrue(testModel.getPersistenceVersion() == version[0]);
-
-                  final ModelLoadResult resultFrom = ModelAccess.instance().runReadAction(new Computable<ModelLoadResult>() {
-                    public ModelLoadResult compute() {
-                      try {
-                        ModelLoadResult result = ModelPersistence.readModel(SModelHeader.create(version[0]), (StreamDataSource) testModel.getSource(),
-                            ModelLoadingState.FULLY_LOADED);
-                        assertTrue(result.getState() == ModelLoadingState.FULLY_LOADED);
-                        return result;
-                      } catch (ModelReadException e) {
-                        return null;
-                      }
-                    }
-                  });
-                  assertNotNull(resultFrom);
-
-                  ModelAccess.instance().runWriteInEDT(new Runnable() {
-                    public void run() {
-                      upgradePersistence(list, version[1]);
-                    }
-                  });
-                  ModelAccess.instance().flushEventQueue();
-                  assertTrue(testModel.getPersistenceVersion() == version[1]);
-
-                  final ModelLoadResult resultTo = ModelAccess.instance().runReadAction(new Computable<ModelLoadResult>() {
-                    public ModelLoadResult compute() {
-                      try {
-                        ModelLoadResult result = ModelPersistence.readModel(SModelHeader.create(version[1]), (StreamDataSource) testModel.getSource(),
-                            ModelLoadingState.FULLY_LOADED);
-                        assertTrue(result.getState() == ModelLoadingState.FULLY_LOADED);
-                        ModelAssert.assertDeepModelEquals(resultFrom.getModel(),
-                            result.getModel());
-                        return result;
-                      } catch (ModelReadException e) {
-                        return null;
-                      }
-                    }
-                  });
-                  assertNotNull(resultTo);
-
-                  ModelAccess.instance().runWriteAction(new Runnable() {
-                    public void run() {
-                      resultFrom.getModel().dispose();
-                      resultTo.getModel().dispose();
-                    }
-                  });
-                  ModelAccess.instance().flushEventQueue();
-
-                  return true;
+      ProjectTestsSupport.testOnProjectCopy(sourceZip, tempDir, TEST_PERSISTENCE_PROJECT,
+          new ProjectRunnable() {
+            public boolean execute(final Project project) {
+              ModelAccess.instance().runReadAction(new Runnable() {
+                public void run() {
+                  myModelBackup = CopyUtil.copyModel(getModel(project).getSModel());
                 }
               });
-          assertTrue(result);
-        }
 
+              for (int fromVersion = START_PERSISTENCE_TEST_VERSION; fromVersion < ModelPersistence.LAST_VERSION; fromVersion++) {
+                for (int toVersion = fromVersion + 1; toVersion <= ModelPersistence.LAST_VERSION; toVersion++) {
+                  ModelLoadResult resultFrom = setInitialVersion(getModel(project), fromVersion);
+                  checkUpgrade(getModel(project), toVersion, resultFrom);
+                  restoreOriginalState(project);
+                }
+              }
+              return true;
+            }
+          });
     } finally {
       filter.stop();
     }
   }
 
-  public void upgradePersistence(final List<DefaultSModelDescriptor> modelDescriptors, final int toVersion) {
-    for (final DefaultSModelDescriptor modelDescriptor : modelDescriptors) {
-      assert ThreadUtils.isEventDispatchThread() : "you must be in EDT to write files";
+  private ModelLoadResult setInitialVersion(final DefaultSModelDescriptor testModel, final int fromVersion) {
+    //check result of the operation by independent load from the same file
+    final boolean[] correctPersistenceVersion = new boolean[1];
+    final ModelLoadResult[] result = new ModelLoadResult[1];
+    final boolean[] wasException = {false};
 
-      StreamDataSource source = (StreamDataSource) modelDescriptor.getSource();
-      if (source.isReadOnly()) continue;
+    ApplicationManager.getApplication().invokeAndWait(new Runnable() {
+      @Override
+      public void run() {
+        ModelAccess.instance().runWriteAction(new Runnable() {
+          @Override
+          public void run() {
+            try {
+              //upgrade to initial persistence version
+              if (fromVersion > START_PERSISTENCE_TEST_VERSION) {
+                upgradePersistence(testModel, fromVersion);
+                correctPersistenceVersion[0] = testModel.getPersistenceVersion() == fromVersion;
+              }
 
-      boolean wasInitialized = modelDescriptor.isLoaded();
-      if (wasInitialized) {
-        modelDescriptor.save();
+              result[0] = ModelPersistence.readModel(SModelHeader.create(fromVersion), (StreamDataSource) testModel.getSource(),
+                  ModelLoadingState.FULLY_LOADED);
+            } catch (ModelReadException e) {
+              wasException[0] = true;
+            }
+          }
+        });
       }
+    }, ModalityState.NON_MODAL);
 
-      int fromVersion = modelDescriptor.getPersistenceVersion();
-      if (fromVersion >= toVersion) continue;
+    //all asserts must reside in original thread
+    assertFalse(wasException[0]);
+    assertTrue(correctPersistenceVersion[0]);
+    assertNotNull(result[0]);
+    assertTrue(result[0].getState() == ModelLoadingState.FULLY_LOADED);
 
-      assert source != null;
+    waitEDT();
+    return result[0];
+  }
 
-      try {
-        jetbrains.mps.smodel.SModel model = wasInitialized ? modelDescriptor.getSModelInternal() : ModelPersistence.readModel(source, false);
-        ModelPersistence.saveModel(model, source, toVersion);
-        modelDescriptor.reloadFromSource();
-      } catch (ModelReadException e) {
-        // This hardly can happen, unreadable model should be already filtered out
-        LOG.error(null, e);
-        fail();
-      } catch (IOException e) {
-        LOG.error(null, e);
-        fail();
+  private void checkUpgrade(final DefaultSModelDescriptor testModel, final int finalToVersion, final ModelLoadResult resultFrom) {
+    final ModelLoadResult[] result = new ModelLoadResult[1];
+    final boolean[] wasException = {false};
+    final boolean[] correctVersion = {true};
+
+    ApplicationManager.getApplication().invokeAndWait(new Runnable() {
+      @Override
+      public void run() {
+        ModelAccess.instance().runWriteAction(new Runnable() {
+          @Override
+          public void run() {
+            upgradePersistence(testModel, finalToVersion);
+            correctVersion[0] = testModel.getPersistenceVersion() == finalToVersion;
+            try {
+              result[0] = ModelPersistence.readModel(SModelHeader.create(finalToVersion), (StreamDataSource) testModel.getSource(),
+                  ModelLoadingState.FULLY_LOADED);
+            } catch (ModelReadException e) {
+              wasException[0] = true;
+            }
+          }
+        });
       }
+    }, ModalityState.NON_MODAL);
+
+    //all asserts must reside in original thread
+    assertTrue(correctVersion[0]);
+    assertFalse(wasException[0]);
+    assertNotNull(result[0]);
+    assertTrue(result[0].getState() == ModelLoadingState.FULLY_LOADED);
+    ModelAccess.instance().runReadAction(new Runnable() {
+      @Override
+      public void run() {
+        ModelAssert.assertDeepModelEquals(resultFrom.getModel(), result[0].getModel());
+      }
+    });
+    waitEDT();
+  }
+
+  private void restoreOriginalState(final Project project) {
+    final boolean[] wasException = {false};
+    final boolean[] correctVersion = {true};
+
+    ApplicationManager.getApplication().invokeAndWait(new Runnable() {
+      @Override
+      public void run() {
+        ModelAccess.instance().runWriteAction(new Runnable() {
+          @Override
+          public void run() {
+            try {
+              DefaultSModelDescriptor model = getModel(project);
+              ModelPersistence.saveModel(myModelBackup, ((StreamDataSource) model.getSource()), START_PERSISTENCE_TEST_VERSION);
+              model.reloadFromSource();   // no way to remove model from repository, so reloading
+              correctVersion[0] = (START_PERSISTENCE_TEST_VERSION == model.getPersistenceVersion());
+            } catch (IOException e) {
+              wasException[0] = true;
+            }
+          }
+        });
+      }
+    }, ModalityState.NON_MODAL);
+
+    //all asserts must reside in original thread
+    assertTrue(correctVersion[0]);
+    assertFalse(wasException[0]);
+
+    waitEDT();
+  }
+
+  private DefaultSModelDescriptor getModel(final Project project) {
+    return ModelAccess.instance().runReadAction(new Computable<DefaultSModelDescriptor>() {
+      @Override
+      public DefaultSModelDescriptor compute() {
+        return (DefaultSModelDescriptor) ProjectTestsSupport.getModel(project, PersistenceTest.TEST_MODEL);
+      }
+    });
+  }
+
+  private static void waitEDT() {
+    ModelAccess.instance().flushEventQueue();
+    ApplicationManager.getApplication().invokeAndWait(new Runnable() {
+      public void run() {
+
+      }
+    }, ModalityState.NON_MODAL);
+    ThreadUtils.runInUIThreadAndWait(new Runnable() {
+      public void run() {
+
+      }
+    });
+  }
+
+  public void upgradePersistence(DefaultSModelDescriptor modelDescriptor, final int toVersion) {
+    assert ThreadUtils.isEventDispatchThread() : "you must be in EDT to write files";
+
+    StreamDataSource source = (StreamDataSource) modelDescriptor.getSource();
+    if (source.isReadOnly()) return;
+
+    boolean wasInitialized = modelDescriptor.isLoaded();
+    if (wasInitialized) {
+      modelDescriptor.save();
+    }
+
+    int fromVersion = modelDescriptor.getPersistenceVersion();
+    if (fromVersion >= toVersion) return;
+
+    assert source != null;
+
+    try {
+      jetbrains.mps.smodel.SModel model = wasInitialized ? modelDescriptor.getSModelInternal() : ModelPersistence.readModel(source, false);
+      ModelPersistence.saveModel(model, source, toVersion);
+      modelDescriptor.reloadFromSource();
+    } catch (ModelReadException e) {
+      // This hardly can happen, unreadable model should be already filtered out
+      LOG.error(null, e);
+      fail();
+    } catch (IOException e) {
+      LOG.error(null, e);
+      fail();
     }
   }
 }
