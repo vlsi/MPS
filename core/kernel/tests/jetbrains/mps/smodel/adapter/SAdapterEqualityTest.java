de286e7dbafaf7d4057a9b28b30440bb6b011d0f
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2014-10-15 15:12:06 +0200

    add SModelRefTest to GlobalTestSuite, start concept equality tests

---
+++ core/kernel/tests/jetbrains/mps/smodel/adapter/SAdapterEqualityTest.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2003-2013 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jetbrains.mps.smodel.adapter;
+
+import jetbrains.mps.persistence.PersistenceRegistry;
+import jetbrains.mps.smodel.SModelReference;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Needed only while we have by-name and by-id adapters
+ */
+public class SAdapterEqualityTest {
+  @Test
+  public void testConceptEquality(){
+  }
+}
