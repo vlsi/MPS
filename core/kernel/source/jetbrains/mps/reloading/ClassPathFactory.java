6c1465840a6c28505e48d55e30328401ce4b03bd
AlexeyP <lexpyh@gmail.com>
2014-10-23 22:57:03 +0400

    MPS-20645 Do not log class loader warning messages on MPS startup

--- core/kernel/source/jetbrains/mps/reloading/ClassPathFactory.java
+++ core/kernel/source/jetbrains/mps/reloading/ClassPathFactory.java
@@ -15,9 +15,9 @@
  */
 package jetbrains.mps.reloading;
 
+import jetbrains.mps.vfs.IFile;
 import org.apache.log4j.Logger;
 import org.apache.log4j.LogManager;
-import jetbrains.mps.util.annotation.UseCarefully;
 import jetbrains.mps.vfs.FileSystem;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -44,47 +44,29 @@ public class ClassPathFactory {
   public RealClassPathItem createFromPath(String path, @Nullable String requestor) throws IOException {
     synchronized (LOCK) {
       if (myCache.containsKey(path)) return myCache.get(path);
-
-      boolean jared = path.contains("!/");
-      boolean exists = jared ? FileSystem.getInstance().getFileByPath(path).exists() : new File(path).exists();
+      IFile file = FileSystem.getInstance().getFileByPath(path);
+      path = file.getPath();
+      boolean jared = FileSystem.getInstance().isPackaged(file) || path.endsWith(".jar");
+      boolean isDirectory = file.isDirectory();
+      boolean exists = file.exists() || new File(path).exists();
+      RealClassPathItem item;
       if (!exists) {
-        String moduleString = requestor == null ? "" : (" in " + requestor.toString());
-        String message = "Can't load class path item " + path + moduleString + "." + (new File(path).isDirectory() ? " Execute make in IDEA." : "");
-        LOG.warn(message);
-        NonExistingClassPathItem item = new NonExistingClassPathItem(path);
-        myCache.put(path, item);
-        return item;
-      }
-
-      if (!jared && new File(path).isDirectory()) {
-        myCache.put(path, new FileClassPathItem(path));
+        String moduleString = requestor == null ? "" : (" in " + requestor);
+        LOG.debug("Can't load class path item " + path + moduleString + "." + (isDirectory ? " Execute make in IDEA." : ""));
+        item = new NonExistingClassPathItem(path);
+      } else if (jared) {
+        item = new JarFileClassPathItem(path);
       } else {
-        myCache.put(path, new JarFileClassPathItem(path));
+        if (!isDirectory) throw new IllegalArgumentException("Path variable must point to a directory or to a jar location");
+        item = new FileClassPathItem(path);
       }
 
-      return myCache.get(path);
+      myCache.put(path, item);
+      return item;
     }
   }
 
   //--------------------------
-
-  @UseCarefully
-  //this is supposed to be used only on class reloading
-  public void invalidateAll() {
-    List<AbstractClassPathItem> invalidate = new ArrayList<AbstractClassPathItem>();
-
-    synchronized (LOCK) {
-      invalidate.addAll(myCache.values());
-      invalidate.addAll(myCompositeClassPathItems);
-
-      myCache = new HashMap<String, RealClassPathItem>();
-      myCompositeClassPathItems = new ArrayList<CompositeClassPathItem>();
-    }
-
-    for (AbstractClassPathItem p : invalidate) {
-      p.invalidate();
-    }
-  }
 
   public void invalidate(Set<String> paths) {
     List<AbstractClassPathItem> invalidate = new ArrayList<AbstractClassPathItem>();
