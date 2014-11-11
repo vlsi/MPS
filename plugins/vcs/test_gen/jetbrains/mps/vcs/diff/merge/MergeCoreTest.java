09ac130e2d9d6cfde818e2de30bb8dc39152a8d5
AlexeyP <lexpyh@gmail.com>
2014-11-11 11:54:22 +0300

    Removed excessing logging in mergecoretest

--- plugins/vcs/test_gen/jetbrains/mps/vcs/diff/merge/MergeCoreTest.java
+++ plugins/vcs/test_gen/jetbrains/mps/vcs/diff/merge/MergeCoreTest.java
@@ -61,14 +61,6 @@ public class MergeCoreTest extends WorkbenchMpsTest {
 
     RuntimeFlags.setPlayRefactoringsMode(false);
     RuntimeFlags.setMergeDriverMode(true);
-
-    Properties p = new Properties();
-    p.setProperty("log4j.rootLogger", "info, console");
-    p.setProperty("log4j.appender.console", "org.apache.log4j.ConsoleAppender");
-    p.setProperty("log4j.appender.console.layout", "org.apache.log4j.PatternLayout");
-    p.setProperty("log4j.appender.console.layout.ConversionPattern", "%-4r [%t] %-5p %c %x - %m%n");
-    p.setProperty("log4j.appender.console.target", "System.err");
-    PropertyConfigurator.configure(p);
   }
   @AfterClass
   public static void tearDownClass() {
