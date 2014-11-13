01d9cca22fe71f9d902d5b6e767f0c5e869adf19
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2014-11-13 20:05:57 +0300

    introduce project migrations

---
+++ core/migration-runtime/source_gen/jetbrains/mps/migration/global/ProjectsMigrationsRegistry.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2003-2014 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jetbrains.mps.migration.global;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ProjectsMigrationsRegistry {
+  private static ProjectsMigrationsRegistry ourInstance = new ProjectsMigrationsRegistry();
+
+  public static ProjectsMigrationsRegistry getInstance() {
+    return ourInstance;
+  }
+
+  private ProjectsMigrationsRegistry() {
+  }
+
+  private final List<ProjectMigration> myMigrations = new ArrayList<ProjectMigration>();
+
+  public void addProjectMigration(ProjectMigration migration){
+    myMigrations.add(migration);
+  }
+
+  public List<ProjectMigration> getMigrations() {
+    return Collections.unmodifiableList(myMigrations);
+  }
+}
