645ebe4059d3a80bd70fc0e1f9d4ba4808c7dea0
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2014-11-14 17:25:37 +0300

    ProjectMigrationProperties (base class only)

---
+++ core/kernel/source/jetbrains/mps/migration/global/ProjectMigrationProperties.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2003-2014 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jetbrains.mps.migration.global;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A storage for properties, which define the persistent state of project migrations
+ * An inheritor should set the INSTANCE field and define how to persist the properties
+ * Default value is null. Null value is not distinguishable from default value
+ */
+public abstract class ProjectMigrationProperties {
+  protected static ProjectMigrationProperties INSTANCE;
+
+  public static ProjectMigrationProperties getInstance() {
+    return INSTANCE;
+  }
+
+  //-------------
+
+  protected Map<String, String> myProperties = new HashMap<String, String>();
+
+  public void setProperty(@NotNull String key, String value) {
+    if (value == null) {
+      myProperties.remove(key);
+      return;
+    }
+
+    myProperties.put(key, value);
+  }
+
+  @Nullable
+  public String getProperty(String key) {
+    return myProperties.get(key);
+  }
+}
