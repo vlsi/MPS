bdf259269f1d59e9001ece081639663cf8ca26c1
Fedor Isakov <fisakov@gmail.com>
2014-12-16 13:33:07 +0100

    Rebuilt test languages

--- testbench/modules/testSubtyping/languages/testSubtypingB/source_gen/testSubtypingB/editor/CustomType_Editor.java
+++ testbench/modules/testSubtyping/languages/testSubtypingB/source_gen/testSubtypingB/editor/CustomType_Editor.java
@@ -13,7 +13,6 @@ import jetbrains.mps.editor.runtime.style.StyleAttributes;
 import jetbrains.mps.nodeEditor.cellProviders.CellProviderWithRole;
 import jetbrains.mps.lang.editor.cellProviders.PropertyCellProvider;
 import jetbrains.mps.baseLanguage.editor.BaseLanguageStyle_StyleSheet;
-import jetbrains.mps.smodel.IOperationContext;
 import jetbrains.mps.nodeEditor.EditorManager;
 
 public class CustomType_Editor extends DefaultNodeEditor {
@@ -50,8 +49,7 @@ public class CustomType_Editor extends DefaultNodeEditor {
     SNode attributeConcept = provider.getRoleAttribute();
     Class attributeKind = provider.getRoleAttributeClass();
     if (attributeConcept != null) {
-      IOperationContext opContext = editorContext.getOperationContext();
-      EditorManager manager = EditorManager.getInstanceFromContext(opContext);
+      EditorManager manager = EditorManager.getInstanceFromContext(editorContext);
       return manager.createNodeRoleAttributeCell(editorContext, attributeConcept, attributeKind, editorCell);
     } else
     return editorCell;
