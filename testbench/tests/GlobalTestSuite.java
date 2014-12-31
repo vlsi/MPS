7ef7e7626151418ca664a78922108ee90f064e9c
Simon Alperovich <semen.alperovich@jetbrains.com>
2014-12-31 14:32:39 +0400

    fix tests

--- testbench/tests/GlobalTestSuite.java
+++ testbench/tests/GlobalTestSuite.java
@@ -53,8 +53,8 @@ import org.junit.runners.Suite;
     jetbrains.mps.smodel.TreeIteratorTest.class,
     jetbrains.mps.smodel.SModelReferenceTest.class,
     jetbrains.mps.smodel.adapter.SAdapterEqualityTest.class,
-    jetbrains.mps.classloading.ProjectMPSDependenciesTest.class,
-    jetbrains.mps.compile.JavaCompilerTest.class
+    jetbrains.mps.compile.JavaCompilerTest.class,
+    jetbrains.mps.classloading.ProjectMPSDependenciesTest.class
 })
 public class GlobalTestSuite {
 }
