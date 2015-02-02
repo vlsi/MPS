10cdb137ce4bb7815e70b4d923cb539ee26c4585
Alexander Shatalin <Alexander.Shatalin@jetbrains.com>
2015-02-02 14:49:49 +0100

    generating NodeFactoryContainer class even if name of corresponding NodeFactory was not specified

--- languages/languageDesign/actions/generator/source_gen/jetbrains/mps/lang/actions/generator/baseLanguage/template/main/QueriesGenerated.java
+++ languages/languageDesign/actions/generator/source_gen/jetbrains/mps/lang/actions/generator/baseLanguage/template/main/QueriesGenerated.java
@@ -169,6 +169,9 @@ public class QueriesGenerated {
     return SPropertyOperations.getString(_context.getNode(), MetaAdapterFactory.getProperty(0xceab519525ea4f22L, 0x9b92103b95ca8c0cL, 0x110396eaaa4L, 0x110396ec041L, "name"));
   }
   public static Object propertyMacro_GetPropertyValue_5664333325054462317(final PropertyMacroContext _context) {
+    if (SPropertyOperations.getString(_context.getNode(), MetaAdapterFactory.getProperty(0xceab519525ea4f22L, 0x9b92103b95ca8c0cL, 0x110396eaaa4L, 0x110396ec041L, "name")) == null) {
+      return _context.createUniqueName("DefaultNodeFactory", null);
+    }
     return SPropertyOperations.getString(_context.getNode(), MetaAdapterFactory.getProperty(0xceab519525ea4f22L, 0x9b92103b95ca8c0cL, 0x110396eaaa4L, 0x110396ec041L, "name"));
   }
   public static Object propertyMacro_GetPropertyValue_5664333325054992550(final PropertyMacroContext _context) {
