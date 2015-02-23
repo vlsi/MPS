3f56c0e826a6572c9adebd2a13e1662e728560c6
Alexander Shatalin <Alexander.Shatalin@jetbrains.com>
2015-02-23 13:25:34 +0100

    MPS-21524 Editor performance: LeftEditorHighlighter will be rebuild several times if brace cell is selected/deselected - correcting SModelModificationCollector logic to process all nodes reported in model events

--- editor/editor-runtime/source/jetbrains/mps/nodeEditor/SModelModificationsCollector.java
+++ editor/editor-runtime/source/jetbrains/mps/nodeEditor/SModelModificationsCollector.java
@@ -60,32 +60,28 @@ public class SModelModificationsCollector extends SModelEventVisitorAdapter {
 
   @Override
   public void visitRootEvent(SModelRootEvent event) {
-    if (event.isAdded()) {
-      return;
-    }
-    addModification(event.getRoot(), event);
-    Queue<SNode> removedNodes = new LinkedList<SNode>();
-    removedNodes.add(event.getRoot());
-    while (!removedNodes.isEmpty()) {
-      SNode removedNode = removedNodes.remove();
-      for (SNode removedChild : removedNode.getChildren()) {
-        removedNodes.add(removedChild);
+    Queue<SNode> nodeQueue = new LinkedList<SNode>();
+    nodeQueue.add(event.getRoot());
+    while (!nodeQueue.isEmpty()) {
+      SNode nextNode = nodeQueue.remove();
+      for (SNode child : nextNode.getChildren()) {
+        nodeQueue.add(child);
       }
-      addModification(removedNode, event);
+      addModification(nextNode, event);
     }
   }
 
   @Override
   public void visitChildEvent(SModelChildEvent event) {
     addModification(event.getParent(), event);
-    Queue<SNode> removedNodes = new LinkedList<SNode>();
-    removedNodes.add(event.getChild());
-    while (!removedNodes.isEmpty()) {
-      SNode removedNode = removedNodes.remove();
-      for (SNode removedChild : removedNode.getChildren()) {
-        removedNodes.add(removedChild);
+    Queue<SNode> nodeQueue = new LinkedList<SNode>();
+    nodeQueue.add(event.getChild());
+    while (!nodeQueue.isEmpty()) {
+      SNode nextNode = nodeQueue.remove();
+      for (SNode child : nextNode.getChildren()) {
+        nodeQueue.add(child);
       }
-      addModification(removedNode, event);
+      addModification(nextNode, event);
     }
   }
 
