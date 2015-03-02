35138cad132a6f448bbd0b85fb246d9902363e68
Fedor Isakov <fisakov@gmail.com>
2015-03-02 16:29:54 +0100

    Deleted a rogue non-typechecking rule MPS-21576

--- languages/baseLanguage/tuples/languages/tuples/source_gen/jetbrains/mps/baseLanguage/tuples/typesystem/TypesystemDescriptor.java
+++ languages/baseLanguage/tuples/languages/tuples/source_gen/jetbrains/mps/baseLanguage/tuples/typesystem/TypesystemDescriptor.java
@@ -28,10 +28,6 @@ public class TypesystemDescriptor extends BaseHelginsDescriptor {
       this.myInferenceRules.add(inferenceRule);
     }
     {
-      NonTypesystemRule_Runtime nonTypesystemRule = new check_NamedTupleComponent_final_NonTypesystemRule();
-      this.myNonTypesystemRules.add(nonTypesystemRule);
-    }
-    {
       NonTypesystemRule_Runtime nonTypesystemRule = new check_NamedTupleDeclaration_extends_NonTypesystemRule();
       this.myNonTypesystemRules.add(nonTypesystemRule);
     }
