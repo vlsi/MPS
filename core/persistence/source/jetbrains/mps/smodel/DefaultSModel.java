7fec1b50f85bc904a761c7079a53a09fe2f8108c
Artem Tikhomirov <tikhomirov.artem@gmail.com>
2015-03-30 20:31:18 +0200

    DefaultSModel: drop deprecated code

--- core/persistence/source/jetbrains/mps/smodel/DefaultSModel.java
+++ core/persistence/source/jetbrains/mps/smodel/DefaultSModel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2014 JetBrains s.r.o.
+ * Copyright 2003-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,17 +15,14 @@
  */
 package jetbrains.mps.smodel;
 
-import jetbrains.mps.util.annotation.ToRemove;
-import org.jetbrains.mps.openapi.model.SModel.Problem.Kind;
-import org.jetbrains.mps.openapi.model.SModelReference;
-
 import jetbrains.mps.extapi.model.PersistenceProblem;
-import jetbrains.mps.smodel.nodeidmap.INodeIdToNodeMap;
 import jetbrains.mps.smodel.persistence.def.ModelReadException;
 import org.jdom.Element;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.mps.openapi.model.SModel.Problem;
+import org.jetbrains.mps.openapi.model.SModel.Problem.Kind;
+import org.jetbrains.mps.openapi.model.SModelReference;
 
 import java.util.Collections;
 
@@ -41,42 +38,11 @@ public class DefaultSModel extends LazySModel {
     this(modelReference, new SModelHeader());
   }
 
-  /**
-   * @deprecated there seems to be no reason to instantiate model with specific node id map as this implicitly restricts
-   * openapi clients on nodes they could put in the model.
-   */
-  @Deprecated
-  @ToRemove(version = 3.2)
-  public DefaultSModel(@NotNull SModelReference modelReference, INodeIdToNodeMap map) {
-    super(modelReference, map);
-    myHeader = new SModelHeader();
-    myHeader.setModelReference(modelReference);
-  }
-
   public DefaultSModel(@NotNull SModelReference modelReference, @Nullable SModelHeader modelHeader) {
     super(modelReference);
     myHeader = modelHeader == null ? new SModelHeader() : modelHeader;
     // make sure header's model reference, if any, is identical to what clients see for the model
     myHeader.setModelReference(modelReference);
-  }
-
-  /**
-   * @deprecated  persistence version is not attribute of SModelData
-   */
-  @Deprecated
-  @ToRemove(version = 3.2)
-  public void setPersistenceVersion(int persistenceVersion) {
-    myHeader.setPersistenceVersion(persistenceVersion);
-  }
-
-  /**
-   * @deprecated  persistence version is not attribute of SModelData, use PersistenceVersionAware instead
-   * or getSModelHeader().getPersistenceVersion() if you can afford tight coupling with DefaultSModel implementation
-   */
-  @Deprecated
-  @ToRemove(version = 3.2)
-  public int getPersistenceVersion() {
-    return myHeader.getPersistenceVersion();
   }
 
   @NotNull
@@ -96,9 +62,7 @@ public class DefaultSModel extends LazySModel {
 
   @Override
   public SModel createEmptyCopy() {
-    DefaultSModel result = new DefaultSModel(getReference());
-    result.setPersistenceVersion(getPersistenceVersion());
-    return result;
+    return new DefaultSModel(getReference());
   }
 
   @Override
--- core/persistence/source/jetbrains/mps/smodel/persistence/def/ModelPersistence.java
+++ core/persistence/source/jetbrains/mps/smodel/persistence/def/ModelPersistence.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2014 JetBrains s.r.o.
+ * Copyright 2003-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -308,7 +308,7 @@ public class ModelPersistence {
   public static Document saveModel(@NotNull SModel sourceModel) {
     int persistenceVersion = -1;
     if (sourceModel instanceof DefaultSModel) {
-      persistenceVersion = ((DefaultSModel) sourceModel).getPersistenceVersion();
+      persistenceVersion = ((DefaultSModel) sourceModel).getSModelHeader().getPersistenceVersion();
     }
     if (persistenceVersion == -1 || getModelPersistence(persistenceVersion) == null) {
       persistenceVersion = getCurrentPersistenceVersion();
