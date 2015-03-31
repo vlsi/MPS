5df156097a4d36b47f740329b00fc4ba6df5323d
Simon Alperovich <semen.alperovich@jetbrains.com>
2015-03-31 18:34:38 +0400

    MPS-21055 add AnalyzerRules

---
+++ core/kernel/dataFlowRuntime/source/jetbrains/mps/lang/dataFlow/framework/analyzers/AnalyzerRules.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2003-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jetbrains.mps.lang.dataFlow.framework.analyzers;
+
+import jetbrains.mps.lang.dataFlow.framework.DataFlowAnalyzer;
+import jetbrains.mps.lang.dataFlow.framework.DataFlowAspectDescriptor;
+import jetbrains.mps.lang.dataFlow.framework.DataFlowConstructor;
+import jetbrains.mps.lang.dataFlow.framework.Program;
+import jetbrains.mps.lang.smodel.generator.smodelAdapter.SNodeOperations;
+import jetbrains.mps.smodel.SModelInternal;
+import jetbrains.mps.smodel.language.LanguageRegistry;
+import jetbrains.mps.smodel.language.LanguageRuntime;
+import org.jetbrains.mps.openapi.language.SAbstractConcept;
+import org.jetbrains.mps.openapi.language.SLanguage;
+import org.jetbrains.mps.openapi.model.SNode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by simon on 30/03/15.
+ */
+public class AnalyzerRules {
+  private List<DataFlowConstructor> myConceptRules = new LinkedList<DataFlowConstructor>();
+  private Map<SNode, Set<DataFlowConstructor>> myConceptRulesCache = new HashMap<SNode, Set<DataFlowConstructor>>();
+  private DataFlowAnalyzer myAnalyzer;
+  public AnalyzerRules(DataFlowAnalyzer analyzer) {
+    myAnalyzer = analyzer;
+  }
+  public void apply(SNode nodeToApply, Program program) {
+    SModelInternal model = (SModelInternal) nodeToApply.getModel();
+    if (model == null) {
+      return;
+    }
+    for (SLanguage language : model.importedLanguageIds()) {
+      LanguageRuntime languageRuntime = LanguageRegistry.getInstance().getLanguage(language.getQualifiedName());
+      if (languageRuntime == null) {
+        continue;
+      }
+      DataFlowAspectDescriptor aspect = languageRuntime.getAspect(DataFlowAspectDescriptor.class);
+      if (aspect == null) {
+        continue;
+      }
+      for (DataFlowConstructor rule : aspect.getConstructors(myAnalyzer.getClass())) {
+        myConceptRules.add(rule);
+      }
+    }
+    for (SNode descendant : SNodeOperations.getNodeDescendants(nodeToApply, null, false, new SAbstractConcept[]{})) {
+      for (DataFlowConstructor rule : getRules(descendant)) {
+        rule.performActions(program, descendant);
+      }
+    }
+  }
+  private Set<DataFlowConstructor> getRules(SNode node) {
+    SNode concept = SNodeOperations.getConceptDeclaration(node);
+    Set<DataFlowConstructor> cachedResult = myConceptRulesCache.get(concept);
+    if (cachedResult != null) {
+      return cachedResult;
+    }
+    Set<DataFlowConstructor> result = new HashSet<DataFlowConstructor>();
+    for (DataFlowConstructor rule : myConceptRules) {
+      if (rule.isApplicable(node)) {
+        result.add(rule);
+      }
+    }
+    myConceptRulesCache.put(concept, result);
+    return result;
+  }
+}
