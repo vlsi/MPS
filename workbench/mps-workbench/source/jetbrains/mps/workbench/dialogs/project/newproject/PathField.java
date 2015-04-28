5e9fd2e75739566428db914272568005e4d5f9ea
Artem Tikhomirov <tikhomirov.artem@gmail.com>
2015-04-28 17:55:58 +0200

    Drop last use of org.jdesktop (beansbinding library), and the library as Idea project dependency as well. The library jar is kept as there's old UI language in 'MPS Contrib' project that still requires it.

--- workbench/mps-workbench/source/jetbrains/mps/workbench/dialogs/project/newproject/PathField.java
+++ workbench/mps-workbench/source/jetbrains/mps/workbench/dialogs/project/newproject/PathField.java
@@ -1,127 +1,105 @@
+/*
+ * Copyright 2003-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package jetbrains.mps.workbench.dialogs.project.newproject;
 
-
+import com.intellij.ui.DocumentAdapter;
 import jetbrains.mps.ide.ui.filechoosers.treefilechooser.TreeFileChooser;
-import jetbrains.mps.internal.collections.runtime.ListSequence;
 import jetbrains.mps.vfs.FileSystem;
 import jetbrains.mps.vfs.IFile;
 import jetbrains.mps.vfs.IFileUtils;
-import org.jdesktop.beansbinding.AutoBinding;
-import org.jdesktop.beansbinding.BeanProperty;
-import org.jdesktop.beansbinding.Bindings;
-import org.jdesktop.beansbinding.Property;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.List;
 
 public class PathField extends JPanel {
-  public PathField myThis;
-  private JTextField myPathField;
-  private JButton myButton;
+  private final JTextField myPathField;
+  private final JButton myButton;
   private String myPath;
   private int myMode;
-  public List<AutoBinding> myBindings = ListSequence.fromList(new ArrayList<AutoBinding>());
 
   public PathField() {
-    this.myThis = this;
-    PathField component = this;
-    component.setLayout(new BorderLayout());
-    component.add(this.createPathField(), BorderLayout.CENTER);
-    component.add(this.createButton(), BorderLayout.EAST);
-    myThis.setMode(TreeFileChooser.MODE_DIRECTORIES);
-  }
-
-  public void addNotify() {
-    super.addNotify();
-    this.bind();
-  }
-
-  public void removeNotify() {
-    this.unbind();
-    super.removeNotify();
-  }
-
-  private void bind() {
-    {
-      Object sourceObject = myThis;
-      Property sourceProperty = BeanProperty.create("path");
-      Object targetObject = this.myPathField;
-      Property targetProperty = BeanProperty.create("text");
-      AutoBinding binding = Bindings.createAutoBinding(
-          AutoBinding.UpdateStrategy.READ_WRITE, sourceObject, sourceProperty, targetObject, targetProperty);
-      binding.bind();
-      ListSequence.fromList(this.myBindings).addElement(binding);
-    }
-  }
-
-  private void unbind() {
-    for (AutoBinding binding : this.myBindings) {
-      if (binding.isBound()) {
-        binding.unbind();
-      }
-    }
+    setLayout(new BorderLayout());
+    add(myPathField = createPathField(), BorderLayout.CENTER);
+    add(myButton = createButton(), BorderLayout.EAST);
+    setMode(TreeFileChooser.MODE_DIRECTORIES);
   }
 
   private JTextField createPathField() {
-    JTextField component = new JTextField();
-    this.myPathField = component;
-    component.setColumns(40);
+    JTextField component = new JTextField(40);
+    component.getDocument().addDocumentListener(new DocumentAdapter() {
+      @Override
+      protected void textChanged(DocumentEvent e) {
+        pathFromField();
+      }
+    });
     return component;
   }
 
   private JButton createButton() {
     JButton component = new JButton();
-    this.myButton = component;
     component.setText("...");
     component.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent event) {
-        myThis.choosePathClicked();
+        choosePathClicked();
       }
     });
     return component;
   }
 
   public String getPath() {
-    return this.myPath;
+    return myPath;
   }
 
   public int getMode() {
-    return this.myMode;
+    return myMode;
   }
 
   public void setPath(String newValue) {
-    String oldValue = this.myPath;
-    this.myPath = newValue;
-    this.firePropertyChange("path", oldValue, newValue);
+    myPath = newValue;
+    myPathField.setText(newValue);
+  }
+
+  /*package*/ void pathFromField() {
+    myPath = myPathField.getText();
   }
 
   public void setMode(int newValue) {
-    int oldValue = this.myMode;
     this.myMode = newValue;
-    this.firePropertyChange("mode", oldValue, newValue);
   }
 
   /*package*/ void choosePathClicked() {
-    String oldPath = myThis.myPathField.getText();
+    String oldPath = myPathField.getText();
     TreeFileChooser chooser = new TreeFileChooser();
-    chooser.setMode(myThis.getMode());
+    chooser.setMode(getMode());
     if (oldPath != null) {
       chooser.setInitialFile(FileSystem.getInstance().getFileByPath(oldPath));
     }
     IFile result = chooser.showDialog();
     if (result != null) {
-      myThis.setPath(IFileUtils.getCanonicalPath(result));
+      setPath(IFileUtils.getCanonicalPath(result));
     }
   }
 
   public void setEnabled(boolean enabled) {
-    myThis.myPathField.setEnabled(enabled);
-    myThis.myButton.setEnabled(enabled);
+    myPathField.setEnabled(enabled);
+    myButton.setEnabled(enabled);
   }
 }
