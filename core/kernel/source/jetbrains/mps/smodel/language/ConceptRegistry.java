afba5825029080e617f37aea25985df4ff6338a3
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2015-05-13 01:05:18 +0200

    re-commit fix that somehow was lost during rebase

--- core/kernel/source/jetbrains/mps/smodel/language/ConceptRegistry.java
+++ core/kernel/source/jetbrains/mps/smodel/language/ConceptRegistry.java
@@ -108,7 +108,7 @@ public class ConceptRegistry implements CoreComponent, LanguageRegistryListener 
     if (concept instanceof SConceptAdapterById){
       return getConceptDescriptor(((SConceptAdapterById) concept).getId());
     }else{
-      return getConceptDescriptor(concept);
+      return getConceptDescriptor(concept.getQualifiedName()fix tesrt);
     }
   }
 
