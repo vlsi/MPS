vlsi/MPS

commit b2ffe239ccf7b7c4bf22077b96e0e8114d893cc8

TabbedEditor: drop use of compatibility editor class

workbench/mps-workbench/jetbrains.mps.ide/models/actions.mps
@@ -184,6 +184,7 @@
     <import index="s7nh" ref="742f6602-5a2f-4313-aa6e-ae1cd4ffdc61/f:java_stub#742f6602-5a2f-4313-aa6e-ae1cd4ffdc61#jetbrains.mps.ide.save(MPS.Platform/jetbrains.mps.ide.save@java_stub)" />
     <import index="auou" ref="498d89d2-c2e9-11e2-ad49-6cf049e62fe5/f:java_stub#498d89d2-c2e9-11e2-ad49-6cf049e62fe5#com.intellij.openapi(MPS.IDEA/com.intellij.openapi@java_stub)" />
     <import index="tpru" ref="r:00000000-0000-4000-0000-011c895904a6(jetbrains.mps.ide.embeddableEditor)" />
+    <import index="h0k" ref="86441d7a-e194-42da-81a5-2161ec62a379/f:java_stub#86441d7a-e194-42da-81a5-2161ec62a379#jetbrains.mps.ide.editor.tabs(MPS.Workbench/jetbrains.mps.ide.editor.tabs@java_stub)" />
   </imports>
   <registry>
     <language id="982eb8df-2c96-4bd7-9963-11712ea622e5" name="jetbrains.mps.lang.resources">
@@ -3386,8 +3387,8 @@
         <node concept="3clFbJ" id="2yoZns6MivH" role="3cqZAp">
           <node concept="3fqX7Q" id="2yoZns6MivI" role="3clFbw">
             <node concept="2ZW3vV" id="2yoZns6MivJ" role="3fr31v">
-              <node concept="3uibUv" id="1uR76MFXyAz" role="2ZW6by">
-                <ref role="3uigEE" to="pana:~TabbedEditor" resolve="TabbedEditor" />
+              <node concept="3uibUv" id="6rIkchcQVAP" role="2ZW6by">
+                <ref role="3uigEE" to="h0k:~TabbedEditor" resolve="TabbedEditor" />
               </node>
               <node concept="2OqwBi" id="2yoZns6MivL" role="2ZW6bz">
                 <node concept="2WthIp" id="2yoZns6MivM" role="2Oq$k0" />
@@ -3406,8 +3407,8 @@
         <node concept="3cpWs8" id="2yoZns6MivR" role="3cqZAp">
           <node concept="3cpWsn" id="2yoZns6MivS" role="3cpWs9">
             <property role="TrG5h" value="tabbedEditor" />
-            <node concept="3uibUv" id="1uR76MFXyAD" role="1tU5fm">
-              <ref role="3uigEE" to="pana:~TabbedEditor" resolve="TabbedEditor" />
+            <node concept="3uibUv" id="6rIkchcQWaD" role="1tU5fm">
+              <ref role="3uigEE" to="h0k:~TabbedEditor" resolve="TabbedEditor" />
             </node>
             <node concept="10QFUN" id="2yoZns6MivU" role="33vP2m">
               <node concept="2OqwBi" id="2yoZns6MivV" role="10QFUP">
@@ -3416,8 +3417,8 @@
                   <ref role="2WH_rO" node="2yoZns6MbOe" resolve="editor" />
                 </node>
               </node>
-              <node concept="3uibUv" id="1uR76MFXyAA" role="10QFUM">
-                <ref role="3uigEE" to="pana:~TabbedEditor" resolve="TabbedEditor" />
+              <node concept="3uibUv" id="6rIkchcQVSM" role="10QFUM">
+                <ref role="3uigEE" to="h0k:~TabbedEditor" resolve="TabbedEditor" />
               </node>
             </node>
           </node>
@@ -3427,25 +3428,25 @@
             <property role="TrG5h" value="editedNode" />
             <node concept="2OqwBi" id="41yWi8GWNh_" role="33vP2m">
               <node concept="liA8E" id="6SyhPqtSbSi" role="2OqNvi">
-                <ref role="37wK5l" to="cu2c:~SNodePointer.resolve(org.jetbrains.mps.openapi.module.SRepository):org.jetbrains.mps.openapi.model.SNode" resolve="resolve" />
-                <node concept="2YIFZM" id="6SyhPqtSbSj" role="37wK5m">
-                  <ref role="37wK5l" to="cu2c:~MPSModuleRepository.getInstance():jetbrains.mps.smodel.MPSModuleRepository" resolve="getInstance" />
-                  <ref role="1Pybhc" to="cu2c:~MPSModuleRepository" resolve="MPSModuleRepository" />
+                <ref role="37wK5l" to="ec5l:~SNodeReference.resolve(org.jetbrains.mps.openapi.module.SRepository):org.jetbrains.mps.openapi.model.SNode" resolve="resolve" />
+                <node concept="2OqwBi" id="6rIkchcQXgH" role="37wK5m">
+                  <node concept="2OqwBi" id="6rIkchcQWAL" role="2Oq$k0">
+                    <node concept="2WthIp" id="6rIkchcQWAO" role="2Oq$k0" />
+                    <node concept="1DTwFV" id="6rIkchcQWAQ" role="2OqNvi">
+                      <ref role="2WH_rO" node="6rIkchcPPez" resolve="mpsProject" />
+                    </node>
+                  </node>
+                  <node concept="liA8E" id="6rIkchcQXUe" role="2OqNvi">
+                    <ref role="37wK5l" to="vsqj:~Project.getRepository():org.jetbrains.mps.openapi.module.SRepository" resolve="getRepository" />
+                  </node>
                 </node>
               </node>
-              <node concept="1eOMI4" id="2q2JH_NuRxq" role="2Oq$k0">
-                <node concept="10QFUN" id="2q2JH_NuRxr" role="1eOMHV">
-                  <node concept="3uibUv" id="2q2JH_NuRxs" role="10QFUM">
-                    <ref role="3uigEE" to="cu2c:~SNodePointer" resolve="SNodePointer" />
-                  </node>
-                  <node concept="2OqwBi" id="2q2JH_NuRxt" role="10QFUP">
-                    <node concept="37vLTw" id="3GM_nagTrO2" role="2Oq$k0">
-                      <ref role="3cqZAo" node="2yoZns6MivS" resolve="tabbedEditor" />
-                    </node>
-                    <node concept="liA8E" id="2q2JH_NuRxv" role="2OqNvi">
-                      <ref role="37wK5l" to="1d7m:~BaseNodeEditor.getCurrentlyEditedNode():org.jetbrains.mps.openapi.model.SNodeReference" resolve="getCurrentlyEditedNode" />
-                    </node>
-                  </node>
+              <node concept="2OqwBi" id="2q2JH_NuRxt" role="2Oq$k0">
+                <node concept="37vLTw" id="3GM_nagTrO2" role="2Oq$k0">
+                  <ref role="3cqZAo" node="2yoZns6MivS" resolve="tabbedEditor" />
+                </node>
+                <node concept="liA8E" id="2q2JH_NuRxv" role="2OqNvi">
+                  <ref role="37wK5l" to="1d7m:~BaseNodeEditor.getCurrentlyEditedNode():org.jetbrains.mps.openapi.model.SNodeReference" resolve="getCurrentlyEditedNode" />
                 </node>
               </node>
             </node>
@@ -3493,12 +3494,12 @@
             <node concept="2OqwBi" id="hGApL1U" role="33vP2m">
               <node concept="2OqwBi" id="hGApL1V" role="2Oq$k0">
                 <node concept="2WthIp" id="hGApL1W" role="2Oq$k0" />
-                <node concept="1DTwFV" id="hHNVdJl" role="2OqNvi">
-                  <ref role="2WH_rO" node="hHNVdJk" resolve="context" />
+                <node concept="1DTwFV" id="6rIkchcPWkw" role="2OqNvi">
+                  <ref role="2WH_rO" node="6rIkchcPPOY" resolve="ideaProject" />
                 </node>
               </node>
               <node concept="liA8E" id="hGApL1Y" role="2OqNvi">
-                <ref role="37wK5l" to="cu2c:~IOperationContext.getComponent(java.lang.Class):java.lang.Object" resolve="getComponent" />
+                <ref role="37wK5l" to="iiw6:~ComponentManager.getComponent(java.lang.Class):java.lang.Object" resolve="getComponent" />
                 <node concept="3VsKOn" id="hGApL1Z" role="37wK5m">
                   <ref role="3VsUkX" to="rl1i:51NkKCgB397" resolve="HierarchyViewTool" />
                 </node>
@@ -3512,17 +3513,11 @@
               <ref role="3cqZAo" node="hGApL1S" resolve="tool" />
             </node>
             <node concept="liA8E" id="hGApL23" role="2OqNvi">
-              <ref role="37wK5l" to="rl1i:51NkKCgB3hW" resolve="showItemInHierarchy" />
+              <ref role="37wK5l" to="rl1i:3U4Yh9n2qoi" resolve="showItemInHierarchy" />
               <node concept="2OqwBi" id="2yoZns6Miwz" role="37wK5m">
                 <node concept="2WthIp" id="2yoZns6Miw$" role="2Oq$k0" />
                 <node concept="2XshWL" id="2yoZns6Miw_" role="2OqNvi">
                   <ref role="2WH_rO" node="2yoZns6MivB" resolve="getConceptNode" />
-                </node>
-              </node>
-              <node concept="2OqwBi" id="32reQvux0pf" role="37wK5m">
-                <node concept="2WthIp" id="32reQvux0pg" role="2Oq$k0" />
-                <node concept="1DTwFV" id="32reQvux0ph" role="2OqNvi">
-                  <ref role="2WH_rO" node="hHNVdJk" resolve="context" />
                 </node>
               </node>
             </node>
@@ -3543,10 +3538,15 @@
         </node>
       </node>
     </node>
-    <node concept="1DS2jV" id="hHNVdJk" role="1NuT2Z">
-      <property role="TrG5h" value="context" />
-      <ref role="1DUlNI" to="5xh9:~MPSCommonDataKeys.OPERATION_CONTEXT" resolve="OPERATION_CONTEXT" />
-      <node concept="1oajcY" id="7HZe2EwZDnA" role="1oa70y" />
+    <node concept="1DS2jV" id="6rIkchcPPOY" role="1NuT2Z">
+      <property role="TrG5h" value="ideaProject" />
+      <ref role="1DUlNI" to="nx1:~PlatformDataKeys.PROJECT_CONTEXT" resolve="PROJECT_CONTEXT" />
+      <node concept="1oajcY" id="6rIkchcPPOZ" role="1oa70y" />
+    </node>
+    <node concept="1DS2jV" id="6rIkchcPPez" role="1NuT2Z">
+      <property role="TrG5h" value="mpsProject" />
+      <ref role="1DUlNI" to="5xh9:~MPSCommonDataKeys.MPS_PROJECT" resolve="MPS_PROJECT" />
+      <node concept="1oajcY" id="6rIkchcPPe$" role="1oa70y" />
     </node>
     <node concept="1DS2jV" id="5purbfZh20R" role="1NuT2Z">
       <property role="TrG5h" value="editorCell" />

workbench/mps-workbench/jetbrains.mps.ide/source_gen.caches/jetbrains/mps/ide/actions/dependencies
@@ -3060,6 +3060,8 @@
   <dependency className="jetbrains.mps.ide.actions.ShowConceptInHierarchy_Action" file="ShowConceptInHierarchy_Action.java">
     <classNode dependClassName="com.intellij.icons.AllIcons" />
     <classNode dependClassName="com.intellij.openapi.actionSystem.AnActionEvent" />
+    <classNode dependClassName="com.intellij.openapi.actionSystem.PlatformDataKeys" />
+    <classNode dependClassName="com.intellij.openapi.project.Project" />
     <classNode dependClassName="java.lang.Object" />
     <classNode dependClassName="java.lang.Override" />
     <classNode dependClassName="java.lang.String" />
@@ -3067,16 +3069,14 @@
     <classNode dependClassName="javax.swing.Icon" />
     <classNode dependClassName="jetbrains.mps.ide.actions.MPSCommonDataKeys" />
     <classNode dependClassName="jetbrains.mps.ide.editor.MPSEditorDataKeys" />
-    <classNode dependClassName="jetbrains.mps.ide.editorTabs.TabbedEditor" />
+    <classNode dependClassName="jetbrains.mps.ide.editor.tabs.TabbedEditor" />
     <classNode dependClassName="jetbrains.mps.ide.hierarchy.HierarchyViewTool" />
     <classNode dependClassName="jetbrains.mps.internal.collections.runtime.MapSequence" />
     <classNode dependClassName="jetbrains.mps.lang.smodel.generator.smodelAdapter.SNodeOperations" />
     <classNode dependClassName="jetbrains.mps.nodeEditor.cells.APICellAdapter" />
     <classNode dependClassName="jetbrains.mps.openapi.editor.Editor" />
     <classNode dependClassName="jetbrains.mps.openapi.editor.cells.EditorCell" />
-    <classNode dependClassName="jetbrains.mps.smodel.IOperationContext" />
-    <classNode dependClassName="jetbrains.mps.smodel.MPSModuleRepository" />
-    <classNode dependClassName="jetbrains.mps.smodel.SNodePointer" />
+    <classNode dependClassName="jetbrains.mps.project.MPSProject" />
     <classNode dependClassName="jetbrains.mps.smodel.adapter.structure.MetaAdapterFactory" />
     <classNode dependClassName="org.jetbrains.annotations.NotNull" />
     <classNode dependClassName="org.jetbrains.mps.openapi.model.SNode" />

workbench/mps-workbench/jetbrains.mps.ide/source_gen.caches/jetbrains/mps/ide/actions/generated
@@ -1,3 +1,3 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<dependencies version="2" modelHash="d75smzb0prms30fo4rnpqb86vzfna4a" />
+<dependencies version="2" modelHash="-bgf6gxflp2rg2on9gmtbfpnc6zpqrj5" />
 

workbench/mps-workbench/jetbrains.mps.ide/source_gen/jetbrains/mps/ide/actions/ShowConceptInHierarchy_Action.java
@@ -8,8 +8,10 @@ import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import java.util.Map;
 import org.jetbrains.annotations.NotNull;
-import jetbrains.mps.smodel.IOperationContext;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import jetbrains.mps.internal.collections.runtime.MapSequence;
+import jetbrains.mps.project.MPSProject;
 import jetbrains.mps.openapi.editor.cells.EditorCell;
 import jetbrains.mps.ide.editor.MPSEditorDataKeys;
 import org.jetbrains.mps.openapi.model.SNode;
@@ -19,8 +21,6 @@ import jetbrains.mps.nodeEditor.cells.APICellAdapter;
 import jetbrains.mps.lang.smodel.generator.smodelAdapter.SNodeOperations;
 import jetbrains.mps.smodel.adapter.structure.MetaAdapterFactory;
 import jetbrains.mps.ide.editor.tabs.TabbedEditor;
-import jetbrains.mps.smodel.SNodePointer;
-import jetbrains.mps.smodel.MPSModuleRepository;
 
 public class ShowConceptInHierarchy_Action extends BaseAction {
   private static final Icon ICON = AllIcons.Toolwindows.ToolWindowHierarchy;
@@ -47,8 +47,15 @@ public class ShowConceptInHierarchy_Action extends BaseAction {
       return false;
     }
     {
-      IOperationContext p = event.getData(MPSCommonDataKeys.OPERATION_CONTEXT);
-      MapSequence.fromMap(_params).put("context", p);
+      Project p = event.getData(PlatformDataKeys.PROJECT_CONTEXT);
+      MapSequence.fromMap(_params).put("ideaProject", p);
+      if (p == null) {
+        return false;
+      }
+    }
+    {
+      MPSProject p = event.getData(MPSCommonDataKeys.MPS_PROJECT);
+      MapSequence.fromMap(_params).put("mpsProject", p);
       if (p == null) {
         return false;
       }
@@ -78,8 +85,8 @@ public class ShowConceptInHierarchy_Action extends BaseAction {
   }
   @Override
   public void doExecute(@NotNull final AnActionEvent event, final Map<String, Object> _params) {
-    HierarchyViewTool tool = ((IOperationContext) MapSequence.fromMap(_params).get("context")).getComponent(HierarchyViewTool.class);
-    tool.showItemInHierarchy(ShowConceptInHierarchy_Action.this.getConceptNode(_params), ((IOperationContext) MapSequence.fromMap(_params).get("context")));
+    HierarchyViewTool tool = ((Project) MapSequence.fromMap(_params).get("ideaProject")).getComponent(HierarchyViewTool.class);
+    tool.showItemInHierarchy(ShowConceptInHierarchy_Action.this.getConceptNode(_params));
     tool.openToolLater(true);
   }
   private SNode getConceptNode(final Map<String, Object> _params) {
@@ -102,7 +109,7 @@ public class ShowConceptInHierarchy_Action extends BaseAction {
       return null;
     }
     TabbedEditor tabbedEditor = (TabbedEditor) ((Editor) MapSequence.fromMap(_params).get("editor"));
-    SNode editedNode = ((SNodePointer) tabbedEditor.getCurrentlyEditedNode()).resolve(MPSModuleRepository.getInstance());
+    SNode editedNode = tabbedEditor.getCurrentlyEditedNode().resolve(((MPSProject) MapSequence.fromMap(_params).get("mpsProject")).getRepository());
     if (!(SNodeOperations.isInstanceOf(editedNode, MetaAdapterFactory.getConcept(0xc72da2b97cce4447L, 0x8389f407dc1158b7L, 0x1103553c5ffL, "jetbrains.mps.lang.structure.structure.AbstractConceptDeclaration")))) {
       return null;
     }

workbench/mps-workbench/jetbrains.mps.ide/source_gen/jetbrains/mps/ide/actions/trace.info
@@ -1696,71 +1696,77 @@
       <node id="6223531255410204610" at="37,87,38,80" concept="13" />
       <node id="1216120033386" at="41,89,42,85" concept="6" />
       <node id="1216120033386" at="46,53,47,19" concept="13" />
-      <node id="1216120033386" at="49,5,50,79" concept="12" />
-      <node id="1216120033386" at="50,79,51,53" concept="6" />
+      <node id="1216120033386" at="49,5,50,66" concept="12" />
+      <node id="1216120033386" at="50,66,51,57" concept="6" />
       <node id="1216120033386" at="52,22,53,21" concept="13" />
       <node id="1216120033386" at="56,5,57,66" concept="12" />
       <node id="1216120033386" at="57,66,58,56" concept="6" />
       <node id="1216120033386" at="59,22,60,21" concept="13" />
-      <node id="1216120033386" at="63,5,64,57" concept="12" />
-      <node id="1216120033386" at="64,57,65,53" concept="6" />
-      <node id="1216120033386" at="66,25,67,21" concept="13" />
-      <node id="1216120033386" at="70,5,71,61" concept="12" />
-      <node id="1216120033386" at="71,61,72,52" concept="6" />
-      <node id="1216120033386" at="73,22,74,21" concept="13" />
-      <node id="1216120033386" at="76,5,77,16" concept="13" />
-      <node id="1216120033399" at="80,96,81,133" concept="12" />
-      <node id="1216120033408" at="81,133,82,156" concept="6" />
-      <node id="1216120033418" at="82,156,83,29" concept="6" />
-      <node id="6223531255410204736" at="85,67,86,119" concept="12" />
-      <node id="6223531255410204746" at="87,206,88,201" concept="13" />
-      <node id="6223531255410204755" at="90,208,91,235" concept="12" />
-      <node id="6223531255410204765" at="92,28,93,23" concept="13" />
-      <node id="6223531255410204774" at="95,5,96,280" concept="12" />
-      <node id="6223531255410204886" at="97,31,98,26" concept="13" />
-      <node id="6223531255410204792" at="99,5,100,0" concept="15" />
-      <node id="2925366645633918965" at="101,91,102,18" concept="13" />
-      <node id="2925366645633918967" at="103,5,104,101" concept="12" />
-      <node id="2925366645633918975" at="104,101,105,121" concept="12" />
-      <node id="2925366645633918989" at="106,212,107,18" concept="13" />
-      <node id="2925366645633918994" at="108,5,109,202" concept="13" />
+      <node id="1216120033386" at="63,5,64,66" concept="12" />
+      <node id="1216120033386" at="64,66,65,56" concept="6" />
+      <node id="1216120033386" at="66,22,67,21" concept="13" />
+      <node id="1216120033386" at="70,5,71,57" concept="12" />
+      <node id="1216120033386" at="71,57,72,53" concept="6" />
+      <node id="1216120033386" at="73,25,74,21" concept="13" />
+      <node id="1216120033386" at="77,5,78,61" concept="12" />
+      <node id="1216120033386" at="78,61,79,52" concept="6" />
+      <node id="1216120033386" at="80,22,81,21" concept="13" />
+      <node id="1216120033386" at="83,5,84,16" concept="13" />
+      <node id="1216120033399" at="87,96,88,127" concept="12" />
+      <node id="1216120033408" at="88,127,89,89" concept="6" />
+      <node id="1216120033418" at="89,89,90,29" concept="6" />
+      <node id="6223531255410204736" at="92,67,93,119" concept="12" />
+      <node id="6223531255410204746" at="94,206,95,201" concept="13" />
+      <node id="6223531255410204755" at="97,208,98,235" concept="12" />
+      <node id="6223531255410204765" at="99,28,100,23" concept="13" />
+      <node id="6223531255410204774" at="102,5,103,280" concept="12" />
+      <node id="6223531255410204886" at="104,31,105,26" concept="13" />
+      <node id="6223531255410204792" at="106,5,107,0" concept="15" />
+      <node id="2925366645633918965" at="108,91,109,18" concept="13" />
+      <node id="2925366645633918967" at="110,5,111,101" concept="12" />
+      <node id="2925366645633918975" at="111,101,112,148" concept="12" />
+      <node id="2925366645633918989" at="113,212,114,18" concept="13" />
+      <node id="2925366645633918994" at="115,5,116,202" concept="13" />
       <node id="1216120033386" at="45,95,48,5" concept="10" />
-      <node id="1216120033386" at="51,53,54,7" concept="10" />
+      <node id="1216120033386" at="51,57,54,7" concept="10" />
       <node id="1216120033386" at="58,56,61,7" concept="10" />
-      <node id="1216120033386" at="65,53,68,7" concept="10" />
-      <node id="1216120033386" at="72,52,75,7" concept="10" />
-      <node id="6223531255410204744" at="86,119,89,5" concept="10" />
-      <node id="6223531255410204763" at="91,235,94,7" concept="10" />
-      <node id="6223531255410204858" at="96,280,99,5" concept="10" />
-      <node id="2925366645633918957" at="100,0,103,5" concept="10" />
-      <node id="2925366645633918982" at="105,121,108,5" concept="10" />
+      <node id="1216120033386" at="65,56,68,7" concept="10" />
+      <node id="1216120033386" at="72,53,75,7" concept="10" />
+      <node id="1216120033386" at="79,52,82,7" concept="10" />
+      <node id="6223531255410204744" at="93,119,96,5" concept="10" />
+      <node id="6223531255410204763" at="98,235,101,7" concept="10" />
+      <node id="6223531255410204858" at="103,280,106,5" concept="10" />
+      <node id="2925366645633918957" at="107,0,110,5" concept="10" />
+      <node id="2925366645633918982" at="112,148,115,5" concept="10" />
       <node id="1216120033386" at="32,0,36,0" concept="11" trace="isDumbAware#()Z" />
       <node id="1216120033386" at="36,0,40,0" concept="11" trace="isApplicable#(Lcom/intellij/openapi/actionSystem/AnActionEvent;Ljava/util/Map;)Z" />
       <node id="1216120033386" at="40,0,44,0" concept="11" trace="doUpdate#(Lcom/intellij/openapi/actionSystem/AnActionEvent;Ljava/util/Map;)V" />
       <node id="1216120033386" at="27,0,32,0" concept="4" trace="ShowConceptInHierarchy_Action#()V" />
-      <node id="1216120033386" at="79,0,85,0" concept="11" trace="doExecute#(Lcom/intellij/openapi/actionSystem/AnActionEvent;Ljava/util/Map;)V" />
-      <node id="6223531255410204753" at="89,5,95,5" concept="10" />
+      <node id="1216120033386" at="86,0,92,0" concept="11" trace="doExecute#(Lcom/intellij/openapi/actionSystem/AnActionEvent;Ljava/util/Map;)V" />
+      <node id="6223531255410204753" at="96,5,102,5" concept="10" />
       <node id="1216120033386" at="48,5,55,5" concept="1" />
       <node id="1216120033386" at="55,5,62,5" concept="1" />
       <node id="1216120033386" at="62,5,69,5" concept="1" />
       <node id="1216120033386" at="69,5,76,5" concept="1" />
-      <node id="2925366645633918951" at="85,0,111,0" concept="11" trace="getConceptNode#(Ljava/util/Map;)Lorg/jetbrains/mps/openapi/model/SNode;" />
-      <node id="1216120033386" at="44,0,79,0" concept="11" trace="collectActionData#(Lcom/intellij/openapi/actionSystem/AnActionEvent;Ljava/util/Map;)Z" />
+      <node id="1216120033386" at="76,5,83,5" concept="1" />
+      <node id="2925366645633918951" at="92,0,118,0" concept="11" trace="getConceptNode#(Ljava/util/Map;)Lorg/jetbrains/mps/openapi/model/SNode;" />
+      <node id="1216120033386" at="44,0,86,0" concept="11" trace="collectActionData#(Lcom/intellij/openapi/actionSystem/AnActionEvent;Ljava/util/Map;)Z" />
       <scope id="1216120033386" at="33,32,34,16" />
       <scope id="2925366645633918865" at="37,87,38,80" />
       <scope id="1216120033386" at="41,89,42,85" />
       <scope id="1216120033386" at="46,53,47,19" />
       <scope id="1216120033386" at="52,22,53,21" />
       <scope id="1216120033386" at="59,22,60,21" />
-      <scope id="1216120033386" at="66,25,67,21" />
-      <scope id="1216120033386" at="73,22,74,21" />
-      <scope id="6223531255410204745" at="87,206,88,201" />
-      <scope id="6223531255410204764" at="92,28,93,23" />
-      <scope id="6223531255410204859" at="97,31,98,26" />
-      <scope id="2925366645633918964" at="101,91,102,18" />
-      <scope id="2925366645633918988" at="106,212,107,18" />
+      <scope id="1216120033386" at="66,22,67,21" />
+      <scope id="1216120033386" at="73,25,74,21" />
+      <scope id="1216120033386" at="80,22,81,21" />
+      <scope id="6223531255410204745" at="94,206,95,201" />
+      <scope id="6223531255410204764" at="99,28,100,23" />
+      <scope id="6223531255410204859" at="104,31,105,26" />
+      <scope id="2925366645633918964" at="108,91,109,18" />
+      <scope id="2925366645633918988" at="113,212,114,18" />
       <scope id="1216120033386" at="27,42,30,41" />
-      <scope id="1216120033386" at="80,96,83,29">
+      <scope id="1216120033386" at="87,96,90,29">
         <var name="tool" id="1216120033400" />
       </scope>
       <scope id="1216120033386" at="32,0,36,0" />
@@ -1772,7 +1778,7 @@
         <var name="_params" id="1216120033386" />
         <var name="event" id="1216120033386" />
       </scope>
-      <scope id="6223531255410204754" at="90,208,94,7">
+      <scope id="6223531255410204754" at="97,208,101,7">
         <var name="concept" id="6223531255410204756" />
       </scope>
       <scope id="1216120033386" at="27,0,32,0" />
@@ -1783,30 +1789,33 @@
         <var name="p" id="1216120033386" />
       </scope>
       <scope id="1216120033386" at="63,5,68,7">
-        <var name="node" id="1216120033386" />
-      </scope>
-      <scope id="1216120033386" at="70,5,75,7">
         <var name="p" id="1216120033386" />
       </scope>
-      <scope id="1216120033386" at="79,0,85,0">
+      <scope id="1216120033386" at="70,5,75,7">
+        <var name="node" id="1216120033386" />
+      </scope>
+      <scope id="1216120033386" at="77,5,82,7">
+        <var name="p" id="1216120033386" />
+      </scope>
+      <scope id="1216120033386" at="86,0,92,0">
         <var name="_params" id="1216120033386" />
         <var name="event" id="1216120033386" />
       </scope>
-      <scope id="2925366645633918953" at="85,67,109,202">
+      <scope id="2925366645633918953" at="92,67,116,202">
         <var name="editedNode" id="2925366645633918976" />
         <var name="outerConcept" id="6223531255410204775" />
         <var name="refNode" id="6223531255410204737" />
         <var name="tabbedEditor" id="2925366645633918968" />
       </scope>
-      <scope id="2925366645633918951" at="85,0,111,0">
+      <scope id="2925366645633918951" at="92,0,118,0">
         <var name="_params" id="2925366645633918951" />
       </scope>
-      <scope id="1216120033386" at="45,95,77,16" />
-      <scope id="1216120033386" at="44,0,79,0">
+      <scope id="1216120033386" at="45,95,84,16" />
+      <scope id="1216120033386" at="44,0,86,0">
         <var name="_params" id="1216120033386" />
         <var name="event" id="1216120033386" />
       </scope>
-      <unit id="1216120033386" at="25,0,112,0" name="jetbrains.mps.ide.actions.ShowConceptInHierarchy_Action" />
+      <unit id="1216120033386" at="25,0,119,0" name="jetbrains.mps.ide.actions.ShowConceptInHierarchy_Action" />
     </file>
   </root>
   <root nodeRef="r:00000000-0000-4000-0000-011c895904a4(jetbrains.mps.ide.actions)/1216120752903">

commit a1f955ac3ee8e4dea6d37c3d77806e6b588ff092

fix mistake introduced by b2ffe23, which makes "show concept in hierarchy" action unusable

workbench/mps-workbench/jetbrains.mps.ide/models/actions.mps
@@ -3540,7 +3540,7 @@
     </node>
     <node concept="1DS2jV" id="6rIkchcPPOY" role="1NuT2Z">
       <property role="TrG5h" value="ideaProject" />
-      <ref role="1DUlNI" to="nx1:~PlatformDataKeys.PROJECT_CONTEXT" resolve="PROJECT_CONTEXT" />
+      <ref role="1DUlNI" to="nx1:~CommonDataKeys.PROJECT" resolve="PROJECT" />
       <node concept="1oajcY" id="6rIkchcPPOZ" role="1oa70y" />
     </node>
     <node concept="1DS2jV" id="6rIkchcPPez" role="1NuT2Z">

workbench/mps-workbench/jetbrains.mps.ide/source_gen.caches/jetbrains/mps/ide/actions/dependencies
@@ -3060,7 +3060,7 @@
   <dependency className="jetbrains.mps.ide.actions.ShowConceptInHierarchy_Action" file="ShowConceptInHierarchy_Action.java">
     <classNode dependClassName="com.intellij.icons.AllIcons" />
     <classNode dependClassName="com.intellij.openapi.actionSystem.AnActionEvent" />
-    <classNode dependClassName="com.intellij.openapi.actionSystem.PlatformDataKeys" />
+    <classNode dependClassName="com.intellij.openapi.actionSystem.CommonDataKeys" />
     <classNode dependClassName="com.intellij.openapi.project.Project" />
     <classNode dependClassName="java.lang.Object" />
     <classNode dependClassName="java.lang.Override" />

workbench/mps-workbench/jetbrains.mps.ide/source_gen.caches/jetbrains/mps/ide/actions/generated
@@ -1,3 +1,3 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<dependencies version="2" modelHash="-bgf6gxflp2rg2on9gmtbfpnc6zpqrj5" />
+<dependencies version="2" modelHash="bfpfdk6w802udc168643a0lp1eemubs" />
 

workbench/mps-workbench/jetbrains.mps.ide/source_gen/jetbrains/mps/ide/actions/ShowConceptInHierarchy_Action.java
@@ -9,7 +9,7 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import java.util.Map;
 import org.jetbrains.annotations.NotNull;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
 import jetbrains.mps.internal.collections.runtime.MapSequence;
 import jetbrains.mps.project.MPSProject;
 import jetbrains.mps.openapi.editor.cells.EditorCell;
@@ -47,7 +47,7 @@ public class ShowConceptInHierarchy_Action extends BaseAction {
       return false;
     }
     {
-      Project p = event.getData(PlatformDataKeys.PROJECT_CONTEXT);
+      Project p = event.getData(CommonDataKeys.PROJECT);
       MapSequence.fromMap(_params).put("ideaProject", p);
       if (p == null) {
         return false;

workbench/mps-workbench/jetbrains.mps.ide/source_gen/jetbrains/mps/ide/actions/trace.info
@@ -1696,8 +1696,8 @@
       <node id="6223531255410204610" at="37,87,38,80" concept="13" />
       <node id="1216120033386" at="41,89,42,85" concept="6" />
       <node id="1216120033386" at="46,53,47,19" concept="13" />
-      <node id="1216120033386" at="49,5,50,66" concept="12" />
-      <node id="1216120033386" at="50,66,51,57" concept="6" />
+      <node id="1216120033386" at="49,5,50,56" concept="12" />
+      <node id="1216120033386" at="50,56,51,57" concept="6" />
       <node id="1216120033386" at="52,22,53,21" concept="13" />
       <node id="1216120033386" at="56,5,57,66" concept="12" />
       <node id="1216120033386" at="57,66,58,56" concept="6" />