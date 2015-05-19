917b3ccee068adb55bd4204202d9e1ce11bb1762
Fedor Isakov <fisakov@gmail.com>
2015-05-19 11:54:21 +0200

    Rebuilt j.m.typesystem lang

--- languages/languageDesign/typesystem/source_gen/jetbrains/mps/lang/typesystem/editor/SubstituteTypeRule_Editor.java
+++ languages/languageDesign/typesystem/source_gen/jetbrains/mps/lang/typesystem/editor/SubstituteTypeRule_Editor.java
@@ -77,7 +77,7 @@ public class SubstituteTypeRule_Editor extends DefaultNodeEditor {
     Class attributeKind = provider.getRoleAttributeClass();
     if (attributeConcept != null) {
       EditorManager manager = EditorManager.getInstanceFromContext(editorContext);
-      return manager.createNodeRoleAttributeCell(editorContext, attributeConcept, attributeKind, editorCell);
+      return manager.createNodeRoleAttributeCell(attributeConcept, attributeKind, editorCell);
     } else
     return editorCell;
   }
@@ -171,7 +171,7 @@ public class SubstituteTypeRule_Editor extends DefaultNodeEditor {
     Class attributeKind = provider.getRoleAttributeClass();
     if (attributeConcept != null) {
       EditorManager manager = EditorManager.getInstanceFromContext(editorContext);
-      return manager.createNodeRoleAttributeCell(editorContext, attributeConcept, attributeKind, editorCell);
+      return manager.createNodeRoleAttributeCell(attributeConcept, attributeKind, editorCell);
     } else
     return editorCell;
   }
@@ -209,7 +209,7 @@ public class SubstituteTypeRule_Editor extends DefaultNodeEditor {
     Class attributeKind = provider.getRoleAttributeClass();
     if (attributeConcept != null) {
       EditorManager manager = EditorManager.getInstanceFromContext(editorContext);
-      return manager.createNodeRoleAttributeCell(editorContext, attributeConcept, attributeKind, editorCell);
+      return manager.createNodeRoleAttributeCell(attributeConcept, attributeKind, editorCell);
     } else
     return editorCell;
   }
