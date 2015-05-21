9a9acc4c9ca839cbe5927d2b5544994533f30a41
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2015-05-21 17:20:13 +0200

    rebuild language (fixes tests)

--- languages/languageDesign/generator/languages/descriptor/source_gen/jetbrains/mps/lang/descriptor/behavior/BehaviorAspectDescriptor.java
+++ languages/languageDesign/generator/languages/descriptor/source_gen/jetbrains/mps/lang/descriptor/behavior/BehaviorAspectDescriptor.java
@@ -11,11 +11,13 @@ public class BehaviorAspectDescriptor implements jetbrains.mps.smodel.runtime.Be
   }
   public BehaviorDescriptor getDescriptor(String fqName) {
     switch (Arrays.binarySearch(stringSwitchCases_846f5o_a0a0b, fqName)) {
-      case 0:
+      case 1:
         return new LanguageDescriptor_BehaviorDescriptor();
+      case 0:
+        return new GeneratorInternal_Aspect_BehaviorDescriptor();
       default:
         return BehaviorAspectInterpreted.getInstance().getDescriptor(fqName);
     }
   }
-  private static String[] stringSwitchCases_846f5o_a0a0b = new String[]{"jetbrains.mps.lang.descriptor.structure.LanguageDescriptor"};
+  private static String[] stringSwitchCases_846f5o_a0a0b = new String[]{"jetbrains.mps.lang.descriptor.structure.GeneratorInternal_Aspect", "jetbrains.mps.lang.descriptor.structure.LanguageDescriptor"};
 }
