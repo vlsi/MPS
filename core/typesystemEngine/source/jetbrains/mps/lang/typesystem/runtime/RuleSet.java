vlsi/MPS

commit 199ca4e59019df4572190f32b03ca709274ced7c

migrating to SConcept in typesystem rules (isApplicable): runtime

core/typesystemEngine/source/jetbrains/mps/lang/typesystem/runtime/RuleSet.java
@@ -16,9 +16,12 @@
 package jetbrains.mps.lang.typesystem.runtime;
 
 import gnu.trove.THashSet;
+import jetbrains.mps.lang.smodel.generator.smodelAdapter.SConceptOperations;
 import jetbrains.mps.newTypesystem.rules.LanguageScope;
 import jetbrains.mps.newTypesystem.rules.SingleTermRules;
+import jetbrains.mps.smodel.adapter.structure.MetaAdapterFactoryByName;
 import jetbrains.mps.smodel.language.ConceptRegistry;
+import org.jetbrains.mps.openapi.language.SAbstractConcept;
 import org.jetbrains.mps.openapi.model.SNode;
 
 import java.util.ArrayList;
@@ -34,25 +37,25 @@ import java.util.concurrent.ConcurrentMap;
 public class RuleSet<T extends IApplicableToConcept> {
 
   private static final String TYPESYSTEM_SUFFIX = ".typesystem";
-  private ConcurrentMap<String, Set<T>> myRules = new ConcurrentHashMap<String, /* synchronized */ Set<T>>();
+  private ConcurrentMap<SAbstractConcept, Set<T>> myRules = new ConcurrentHashMap<SAbstractConcept, /* synchronized */ Set<T>>();
 
   private SingleTermRules<T> mySingleTermRules = new SingleTermRules<T>() {
 
     @Override
-    protected List<String> getParents(String nextConceptFQName) {
-      return ConceptRegistry.getInstance().getConceptDescriptor(nextConceptFQName).getParentsNames();
+    protected List<SAbstractConcept> getParents(SAbstractConcept nextConcept) {
+      return SConceptOperations.getAllSuperConcepts(nextConcept, false);
     }
 
     @Override
-    protected Iterable<T> allForConcept(String conceptFQName, LanguageScope langScope) {
-      return getAllApplicableTo(conceptFQName, langScope);
+    protected Iterable<T> allForConcept(SAbstractConcept concept, LanguageScope langScope) {
+      return getAllApplicableTo(concept, langScope);
     }
 
     @Override
     protected boolean isOverriding(T rule) {
       return rule instanceof ICheckingRule_Runtime && ((ICheckingRule_Runtime) rule).overrides();
     }
-   };
+  };
 
 
   public void addRuleSetItem(Set<T> rules) {
@@ -69,7 +72,7 @@ public class RuleSet<T extends IApplicableToConcept> {
   }
 
   private void addRule_internal(T rule) {
-    String concept = rule.getApplicableConceptFQName();
+    SAbstractConcept concept = rule.getApplicableConcept();
     Set<T> existingRules = myRules.get(concept);
     while (existingRules == null) {
       myRules.putIfAbsent(concept, Collections.synchronizedSet(new THashSet<T>(2)));
@@ -82,13 +85,13 @@ public class RuleSet<T extends IApplicableToConcept> {
     return mySingleTermRules.lookupRules(term);
   }
 
-  private Iterable<T> getAllApplicableTo(String conceptFQName, LanguageScope scope) {
-    if (!myRules.containsKey(conceptFQName)) return Collections.emptyList();
+  private Iterable<T> getAllApplicableTo(SAbstractConcept concept, LanguageScope scope) {
+    if (!myRules.containsKey(concept)) return Collections.emptyList();
 
     List<T> result = new ArrayList<T>(4);
-    Set<T> rules = myRules.get(conceptFQName);
+    Set<T> rules = myRules.get(concept);
     synchronized (rules) {
-      for (T rule: rules) {
+      for (T rule : rules) {
         if (scope.containsNamespace(getNamespace(rule))) {
           result.add(rule);
         }
@@ -97,10 +100,10 @@ public class RuleSet<T extends IApplicableToConcept> {
     return Collections.unmodifiableList(result);
   }
 
-  private String getNamespace (T rule) {
+  private String getNamespace(T rule) {
     String pkg = rule.getClass().getPackage().getName();
     if (pkg.endsWith(TYPESYSTEM_SUFFIX)) {
-      return pkg.substring(0, pkg.length()-TYPESYSTEM_SUFFIX.length());
+      return pkg.substring(0, pkg.length() - TYPESYSTEM_SUFFIX.length());
     }
     return pkg;
   }

core/kernel/source/jetbrains/mps/smodel/ConceptDescendantsCache.java
@@ -21,6 +21,7 @@ import jetbrains.mps.smodel.adapter.ids.MetaIdFactory;
 import jetbrains.mps.smodel.adapter.ids.SConceptId;
 import jetbrains.mps.smodel.adapter.structure.MetaAdapterFactory;
 import jetbrains.mps.smodel.adapter.structure.MetaAdapterFactoryByName;
+import jetbrains.mps.smodel.language.ConceptRegistry;
 import jetbrains.mps.smodel.language.LanguageRegistry;
 import jetbrains.mps.smodel.language.LanguageRegistryListener;
 import jetbrains.mps.smodel.language.LanguageRuntime;
@@ -110,11 +111,8 @@ public class ConceptDescendantsCache implements CoreComponent {
     List<String> pnames = concept.getParentsNames();
     assert pids.size() == pnames.size() : pids.size() + "/" + pnames.size();
 
-    Iterator<SConceptId> pi = pids.iterator();
-    Iterator<String> ni = pnames.iterator();
-
-    while (pi.hasNext()) {
-      SAbstractConcept parentConcept = MetaAdapterFactory.getConcept(pi.next(), ni.next());
+    for(SConceptId id:pids) {
+      SAbstractConcept parentConcept = MetaAdapterFactory.getAbstractConcept(ConceptRegistry.getInstance().getConceptDescriptor(id));
       Set<SAbstractConcept> descendants = new HashSet<SAbstractConcept>(getDirectDescendants(parentConcept));
       descendants.add(MetaAdapterFactory.getAbstractConcept(concept));
       myDescendantsCache.put(parentConcept, Collections.unmodifiableSet(descendants));
@@ -126,11 +124,8 @@ public class ConceptDescendantsCache implements CoreComponent {
     List<String> pnames = concept.getParentsNames();
     assert pids.size() == pnames.size() : pids.size() + "/" + pnames.size();
 
-    Iterator<SConceptId> pi = pids.iterator();
-    Iterator<String> ni = pnames.iterator();
-
-    while (pi.hasNext()) {
-      SAbstractConcept parentConcept = MetaAdapterFactory.getConcept(pi.next(), ni.next());
+    for(SConceptId id:pids) {
+      SAbstractConcept parentConcept = MetaAdapterFactory.getAbstractConcept(ConceptRegistry.getInstance().getConceptDescriptor(id));
       Set<SAbstractConcept> descendants = new HashSet<SAbstractConcept>(getDirectDescendants(parentConcept));
       descendants.remove(MetaAdapterFactory.getAbstractConcept(concept));
       myDescendantsCache.put(parentConcept, Collections.unmodifiableSet(descendants));
@@ -267,7 +262,7 @@ public class ConceptDescendantsCache implements CoreComponent {
   @ToRemove(version = 3.3)
   public Set<String> getDirectDescendants(String conceptFqName) {
     myModuleRepository.getModelAccess().checkReadAccess();
-    Set<SAbstractConcept> fromCache = myDescendantsCache.get(MetaAdapterFactoryByName.getConcept(conceptFqName));
+    Set<SAbstractConcept> fromCache = myDescendantsCache.get(MetaAdapterFactoryByName.getTypedConcept_DoNotUse(conceptFqName));
     if (fromCache==null) return Collections.emptySet();
 
     Set<String> result = new HashSet<String>();

core/kernel/source/jetbrains/mps/smodel/adapter/structure/MetaAdapterFactoryByName.java
@@ -21,6 +21,11 @@ import jetbrains.mps.smodel.adapter.structure.language.SLanguageAdapterByName;
 import jetbrains.mps.smodel.adapter.structure.link.SContainmentLinkAdapterByName;
 import jetbrains.mps.smodel.adapter.structure.property.SPropertyAdapterByName;
 import jetbrains.mps.smodel.adapter.structure.ref.SReferenceLinkAdapterByName;
+import jetbrains.mps.smodel.language.ConceptRegistry;
+import jetbrains.mps.smodel.runtime.ConceptDescriptor;
+import jetbrains.mps.smodel.runtime.illegal.IllegalConceptDescriptor;
+import jetbrains.mps.util.annotation.ToRemove;
+import org.jetbrains.mps.openapi.language.SAbstractConcept;
 import org.jetbrains.mps.openapi.language.SConcept;
 import org.jetbrains.mps.openapi.language.SContainmentLink;
 import org.jetbrains.mps.openapi.language.SInterfaceConcept;
@@ -36,6 +41,7 @@ public class MetaAdapterFactoryByName {
   public static SLanguage getLanguage(String langName) {
     return new SLanguageAdapterByName(langName);
   }
+
   public static SLanguage getLanguage(String langName, int version) {
     return new SLanguageAdapterByName(langName, version);
   }
@@ -69,4 +75,13 @@ public class MetaAdapterFactoryByName {
     return new SContainmentLinkAdapterByName(conceptName, linkName);
   }
 
+  @Deprecated
+  @ToRemove(version = 3.3)
+  //not used in MPS
+  //this is to use only for compatibility reasons between 3.2 and 3.3. This code does not run normally at all
+  public static SAbstractConcept getTypedConcept_DoNotUse(String conceptName) {
+    final ConceptDescriptor cd = ConceptRegistry.getInstance().getConceptDescriptor(conceptName);
+    if (cd instanceof IllegalConceptDescriptor) return MetaAdapterFactoryByName.getConcept(conceptName);
+    return MetaAdapterFactory.getAbstractConcept(cd);
+  }
 }

core/kernel/source_gen/jetbrains/mps/scope/ModelsScope.java
@@ -31,12 +31,7 @@ public class ModelsScope extends Scope {
   public ModelsScope(Iterable<SModel> models, boolean rootsOnly, String targetConcept) {
     myModels = SetSequence.fromSetWithValues(new HashSet<SModel>(), models);
     myRootsOnly = rootsOnly;
-    ConceptDescriptor cd = ConceptRegistryUtil.getConceptDescriptor(targetConcept);
-    if (cd == null) {
-      myTargetConcept = MetaAdapterFactoryByName.getConcept(targetConcept);
-    } else {
-      myTargetConcept = (cd.isInterfaceConcept() ? MetaAdapterFactoryByName.getInterfaceConcept(targetConcept) : MetaAdapterFactoryByName.getConcept(targetConcept));
-    }
+    myTargetConcept=MetaAdapterFactoryByName.getTypedConcept_DoNotUse(targetConcept);
   }
   public ModelsScope(Iterable<SModel> models, boolean rootsOnly, SAbstractConcept targetConcept) {
     myModels = SetSequence.fromSetWithValues(new HashSet<SModel>(), models);

core/typesystemEngine/source/jetbrains/mps/lang/typesystem/runtime/AbstractInferenceRule_Runtime.java
@@ -15,10 +15,22 @@
  */
 package jetbrains.mps.lang.typesystem.runtime;
 
+import jetbrains.mps.smodel.adapter.structure.MetaAdapterFactoryByName;
+import org.jetbrains.mps.openapi.language.SAbstractConcept;
 import org.jetbrains.mps.openapi.model.SNode;
 import jetbrains.mps.typesystem.inference.TypeCheckingContext;
 
 public abstract class AbstractInferenceRule_Runtime implements InferenceRule_Runtime {
+  @Override
+  //body is needed for compatibility only
+  public SAbstractConcept getApplicableConcept() {
+    return MetaAdapterFactoryByName.getTypedConcept_DoNotUse(getApplicableConceptFQName());
+  }
+
+  @Override
+  public String getApplicableConceptFQName() {
+    return null;
+  }
 
   @Override
   public IsApplicableStatus isApplicableAndPattern(SNode argument) {

core/typesystemEngine/source/jetbrains/mps/lang/typesystem/runtime/AbstractNonTypesystemRule_Runtime.java
@@ -15,10 +15,23 @@
  */
 package jetbrains.mps.lang.typesystem.runtime;
 
+import jetbrains.mps.smodel.adapter.structure.MetaAdapterFactoryByName;
+import org.jetbrains.mps.openapi.language.SAbstractConcept;
 import org.jetbrains.mps.openapi.model.SNode;
 import jetbrains.mps.typesystem.inference.TypeCheckingContext;
 
 public abstract class AbstractNonTypesystemRule_Runtime implements NonTypesystemRule_Runtime {
+
+  @Override
+  //body is needed for compatibility only
+  public SAbstractConcept getApplicableConcept() {
+    return MetaAdapterFactoryByName.getTypedConcept_DoNotUse(getApplicableConceptFQName());
+  }
+
+  @Override
+  public String getApplicableConceptFQName() {
+    return null;
+  }
 
   @Override
   public IsApplicableStatus isApplicableAndPattern(SNode argument) {

core/typesystemEngine/source/jetbrains/mps/lang/typesystem/runtime/AbstractSubtypingRule_Runtime.java
@@ -15,6 +15,8 @@
  */
 package jetbrains.mps.lang.typesystem.runtime;
 
+import jetbrains.mps.smodel.adapter.structure.MetaAdapterFactoryByName;
+import org.jetbrains.mps.openapi.language.SAbstractConcept;
 import org.jetbrains.mps.openapi.model.SNode;
 import jetbrains.mps.typesystem.inference.TypeCheckingContext;
 
@@ -23,7 +25,6 @@ import java.util.Collections;
 import java.util.List;
 
 public abstract class AbstractSubtypingRule_Runtime implements ISubtypingRule_Runtime {
-
   @Override
   public List<SNode> getSubOrSuperTypes(SNode type, TypeCheckingContext typeCheckingContext, IsApplicableStatus status) {
     SNode subOrSuperType = getSubOrSuperType(type, typeCheckingContext, status);
@@ -31,6 +32,17 @@ public abstract class AbstractSubtypingRule_Runtime implements ISubtypingRule_Ru
       return Arrays.asList(subOrSuperType);
     }
     return Collections.emptyList();
+  }
+
+  @Override
+  //body is needed for compatibility only
+  public SAbstractConcept getApplicableConcept() {
+    return MetaAdapterFactoryByName.getTypedConcept_DoNotUse(getApplicableConceptFQName());
+  }
+
+  @Override
+  public String getApplicableConceptFQName() {
+    return null;
   }
 
   public SNode getSubOrSuperType(SNode type, TypeCheckingContext typeCheckingContext, IsApplicableStatus status) {

core/typesystemEngine/source/jetbrains/mps/lang/typesystem/runtime/ComparisonRule_Runtime.java
@@ -23,13 +23,23 @@ public abstract class ComparisonRule_Runtime implements IRuleWithTwoApplicableNo
   @Override
   //todo remove body after 3.3, needed for compatibility only
   public SAbstractConcept getApplicableConcept1() {
-    return MetaAdapterFactoryByName.getConcept(getApplicableConceptFQName1());
+    return MetaAdapterFactoryByName.getTypedConcept_DoNotUse(getApplicableConceptFQName1());
   }
 
   @Override
   //todo remove body after 3.3, needed for compatibility only
   public SAbstractConcept getApplicableConcept2() {
-    return MetaAdapterFactoryByName.getConcept(getApplicableConceptFQName2());
+    return MetaAdapterFactoryByName.getTypedConcept_DoNotUse(getApplicableConceptFQName2());
+  }
+
+  @Override
+  public String getApplicableConceptFQName1() {
+    return null;
+  }
+
+  @Override
+  public String getApplicableConceptFQName2() {
+    return null;
   }
 
   //todo generate this method

commit 2be7099c8498cf258cdc4c8395de8e3c15eef9a7

fix problem introduced in 199ca4e59019df4572190f32b03ca709274ced7c not allowing to write code: String a="abc"; a+='d'; (fails with typesystem error)

core/typesystemEngine/source/jetbrains/mps/lang/typesystem/runtime/RuleSet.java
@@ -46,7 +46,7 @@ public class RuleSet<T extends IApplicableToConcept> {
 
     @Override
     protected List<SAbstractConcept> getParents(SAbstractConcept nextConcept) {
-      return SConceptOperations.getAllSuperConcepts(nextConcept, false);
+      return SConceptOperations.getDirectSuperConcepts(nextConcept, false);
     }
 
     @Override