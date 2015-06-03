55956c9036044a7be4360cc6cc4e16fa8d4381be
AlexeyP <lexpyh@gmail.com>
2015-06-03 11:19:37 +0300

    Adding Facet correctness test to GlobalTestSuite

--- testbench/tests/GlobalTestSuite.java
+++ testbench/tests/GlobalTestSuite.java
@@ -57,7 +57,8 @@ import org.junit.runners.Suite;
     jetbrains.mps.smodel.adapter.SAdapterEqualityTest.class,
     jetbrains.mps.smodel.ModelListenerTest.class,
     jetbrains.mps.smodel.ModelUndoTest.class,
-    jetbrains.mps.classloading.ProjectMPSDependenciesTest.class
+    jetbrains.mps.classloading.ProjectMPSDependenciesTest.class,
+    jetbrains.mps.classloading.ProjectMPSFacetCorrectnessTest.class
 })
 public class GlobalTestSuite {
 }
