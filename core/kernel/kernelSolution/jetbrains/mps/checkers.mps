vlsi/MPS

commit 68baf000b9f1601223ecfb8cdc7603fa03461962

Avoid suppressing errors in the inspector code under siblings of a node with ReferenceMacro, MPS-17028

core/kernel/kernelSolution/jetbrains/mps/checkers.mps
@@ -5946,6 +5946,25 @@
                 </node>
               </node>
             </node>
+            <node role="statement" roleId="tpee.1068581517665" type="tpee.IfStatement" typeId="tpee.1068580123159" id="3772633333667264475" nodeInfo="nn">
+              <node role="ifTrue" roleId="tpee.1068580123161" type="tpee.StatementList" typeId="tpee.1068580123136" id="3772633333667264478" nodeInfo="sn">
+                <node role="statement" roleId="tpee.1068581517665" type="tpee.ReturnStatement" typeId="tpee.1068581242878" id="3772633333667267144" nodeInfo="nn">
+                  <node role="expression" roleId="tpee.1068581517676" type="tpee.BooleanConstant" typeId="tpee.1068580123137" id="3772633333667267633" nodeInfo="nn">
+                    <property name="value" nameId="tpee.1068580123138" value="true" />
+                  </node>
+                </node>
+              </node>
+              <node role="condition" roleId="tpee.1068580123160" type="tpee.DotExpression" typeId="tpee.1197027756228" id="3772633333667265294" nodeInfo="nn">
+                <node role="operand" roleId="tpee.1197027771414" type="tpee.VariableReference" typeId="tpee.1068498886296" id="3772633333667264934" nodeInfo="nn">
+                  <link role="variableDeclaration" roleId="tpee.1068581517664" targetNodeId="6631111943583857196" resolveInfo="parent" />
+                </node>
+                <node role="operation" roleId="tpee.1197027833540" type="tp25.Node_IsInstanceOfOperation" typeId="tp25.1139621453865" id="3772633333667266922" nodeInfo="nn">
+                  <node role="conceptArgument" roleId="tp25.1177027386292" type="tp25.RefConcept_Reference" typeId="tp25.1177026924588" id="3772633333667267004" nodeInfo="nn">
+                    <link role="conceptDeclaration" roleId="tp25.1177026940964" targetNodeId="tpck.5169995583184591161" resolveInfo="Attribute" />
+                  </node>
+                </node>
+              </node>
+            </node>
             <node role="statement" roleId="tpee.1068581517665" type="tpee.IfStatement" typeId="tpee.1068580123159" id="6631111943583880195" nodeInfo="nn">
               <node role="ifTrue" roleId="tpee.1068580123161" type="tpee.StatementList" typeId="tpee.1068580123136" id="6631111943583880196" nodeInfo="sn">
                 <node role="statement" roleId="tpee.1068581517665" type="tpee.ReturnStatement" typeId="tpee.1068581242878" id="6631111943583880240" nodeInfo="nn">

core/kernel/source_gen.caches/jetbrains/mps/checkers/generated
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<dependencies version="2" modelHash="-109pbsduwfvw9in7gcsc97w730hly5i">
+<dependencies version="2" modelHash="-4n58bvjjhjrbckp0k13qyta7f5v25h7">
   <dep model="f:java_stub#6354ebe7-c22a-4a0f-ac54-50b52ab9b065#java.io(JDK/java.io@java_stub)" />
   <dep model="f:java_stub#6354ebe7-c22a-4a0f-ac54-50b52ab9b065#java.lang(JDK/java.lang@java_stub)" />
   <dep model="f:java_stub#6354ebe7-c22a-4a0f-ac54-50b52ab9b065#java.lang.reflect(JDK/java.lang.reflect@java_stub)" />
@@ -27,12 +27,12 @@
   <dep model="r:00000000-0000-4000-0000-011c89590282(jetbrains.mps.lang.core.behavior)" hash="c1xbedyalk4nw2czpsk8cfko8ym7qhu" />
   <dep model="r:00000000-0000-4000-0000-011c89590288(jetbrains.mps.lang.core.structure)" hash="-8he8jux7qalbw33m2tcd7ucej0ikmq3" />
   <dep model="r:00000000-0000-4000-0000-011c89590292(jetbrains.mps.lang.structure.structure)" hash="4fpv2xr3ihc6ydyum9jowto74hclgxr" />
-  <dep model="r:00000000-0000-4000-0000-011c895902ca(jetbrains.mps.baseLanguage.structure)" hash="-bj3r7xrkx1tokdu56absbnx3f3f389" />
+  <dep model="r:00000000-0000-4000-0000-011c895902ca(jetbrains.mps.baseLanguage.structure)" hash="-4crpde5db3ynzfcen40ahgk4az0f9nx" />
   <dep model="r:00000000-0000-4000-0000-011c895902e8(jetbrains.mps.lang.generator.structure)" hash="-brl2bgkky74xjyauouufkoy6p3yk0z4" />
   <dep model="r:00000000-0000-4000-0000-011c89590301(jetbrains.mps.lang.smodel.structure)" hash="-1lugkap5r9645u9hztq33busleneuk2" />
   <dep model="r:00000000-0000-4000-0000-011c89590303(jetbrains.mps.lang.smodel.generator.baseLanguage.template.main@generator)" hash="-48vxm6lpdyencmht9ofqk4s4yqtnzq5" />
   <dep model="r:00000000-0000-4000-0000-011c8959030d(jetbrains.mps.lang.constraints.structure)" hash="-62izvc5x4wpn0xbaqpps9np85umaxdl" />
-  <dep model="r:00000000-0000-4000-0000-011c8959033a(jetbrains.mps.baseLanguage.closures.generator.baseLanguage.template.main@generator)" hash="4pvn854ev5xvesh6lweapu81i6sed5d" />
+  <dep model="r:00000000-0000-4000-0000-011c8959033a(jetbrains.mps.baseLanguage.closures.generator.baseLanguage.template.main@generator)" hash="-a4t5qmgcxttosxakd1703tbglug3df0" />
   <dep model="r:00000000-0000-4000-0000-011c89590580(jetbrains.mps.baseLanguage.logging.generator.baseLanguage.template.main@generator)" hash="-achotqmank8lbn0qyr9phfzza4hbi1t" />
   <dep model="r:35e808a0-0758-4b03-9053-4675a7ced44c(jetbrains.mps.baseLanguage.closures.runtime)" hash="b0kikcuz4xhl7u2yvnhyms2uffn8cx2" />
   <dep model="r:b45707fc-ff12-4ef7-8bef-69287f7e33f2(jetbrains.mps.baseLanguage.checkedDots.generator.template.main@generator)" hash="gl25jjwfjfu4wby24mt254hot7fpb3" />
@@ -163,7 +163,7 @@
     <dep model="r:00000000-0000-4000-0000-011c8959033a(jetbrains.mps.baseLanguage.closures.generator.baseLanguage.template.main@generator)" />
     <file name="LanguageChecker.java" />
   </source>
-  <source id="6803901086853929087" name="ErrorReportUtil" hash="-999gm2qx0uypkdkfaq4fpq8y3wjjmye">
+  <source id="6803901086853929087" name="ErrorReportUtil" hash="-9bmsgtf1auamvs69lbrlnpqj14xxytq">
     <dep model="f:java_stub#6354ebe7-c22a-4a0f-ac54-50b52ab9b065#java.io(JDK/java.io@java_stub)" />
     <dep model="f:java_stub#6354ebe7-c22a-4a0f-ac54-50b52ab9b065#java.lang(JDK/java.lang@java_stub)" />
     <dep model="f:java_stub#6354ebe7-c22a-4a0f-ac54-50b52ab9b065#java.lang.reflect(JDK/java.lang.reflect@java_stub)" />

core/kernel/source_gen/jetbrains/mps/checkers/ErrorReportUtil.java
@@ -26,6 +26,9 @@ public class ErrorReportUtil {
       if (jetbrains.mps.lang.smodel.generator.smodelAdapter.SNodeOperations.isInstanceOf(parent, "jetbrains.mps.lang.core.structure.ISuppressErrors") && BehaviorReflection.invokeVirtual(Boolean.TYPE, jetbrains.mps.lang.smodel.generator.smodelAdapter.SNodeOperations.cast(parent, "jetbrains.mps.lang.core.structure.ISuppressErrors"), "virtual_suppress_3393165121846091591", new Object[]{_node.value})) {
         return false;
       }
+      if (jetbrains.mps.lang.smodel.generator.smodelAdapter.SNodeOperations.isInstanceOf(parent, "jetbrains.mps.lang.core.structure.Attribute")) {
+        return true;
+      }
       if (ListSequence.fromList(AttributeOperations.getAttributeList(parent, new IAttributeDescriptor.AllAttributes())).any(new IWhereFilter<SNode>() {
         public boolean accept(SNode attr) {
           return jetbrains.mps.lang.smodel.generator.smodelAdapter.SNodeOperations.isInstanceOf(attr, "jetbrains.mps.lang.core.structure.ISuppressErrors") && BehaviorReflection.invokeVirtual(Boolean.TYPE, jetbrains.mps.lang.smodel.generator.smodelAdapter.SNodeOperations.cast(attr, "jetbrains.mps.lang.core.structure.ISuppressErrors"), "virtual_suppress_3393165121846091591", new Object[]{_node.value}) && attr != _node.value;

commit 75fc180cf560ee9a60b87c04c2fc6355b69cb3e9

remove wrong fix of MPS-17028 introduced in 68baf000b

core/kernel/kernelSolution/jetbrains/mps/checkers.mps
@@ -294,6 +294,9 @@
       <concept id="1138676077309" name="jetbrains.mps.lang.smodel.structure.EnumMemberReference" flags="nn" index="uoxfO">
         <reference id="1138676095763" name="enumMember" index="uo_Cq" />
       </concept>
+      <concept id="4693937538533521280" name="jetbrains.mps.lang.smodel.structure.OfConceptOperation" flags="ng" index="v3k3i">
+        <child id="4693937538533538124" name="requestedConcept" index="v3oSu" />
+      </concept>
       <concept id="7453996997717780434" name="jetbrains.mps.lang.smodel.structure.Node_GetSConceptOperation" flags="nn" index="2yIwOk" />
       <concept id="8758390115029295477" name="jetbrains.mps.lang.smodel.structure.SReferenceType" flags="in" index="2z4iKi" />
       <concept id="8758390115028452779" name="jetbrains.mps.lang.smodel.structure.Node_GetReferencesOperation" flags="nn" index="2z74zc" />
@@ -5554,23 +5557,9 @@
                 </node>
               </node>
             </node>
-            <node concept="3clFbJ" id="3hr5wtp2fvr" role="3cqZAp">
-              <node concept="3clFbS" id="3hr5wtp2fvu" role="3clFbx">
-                <node concept="3cpWs6" id="3hr5wtp2g98" role="3cqZAp">
-                  <node concept="3clFbT" id="3hr5wtp2ggL" role="3cqZAk">
-                    <property role="3clFbU" value="true" />
-                  </node>
-                </node>
-              </node>
-              <node concept="2OqwBi" id="3hr5wtp2fGe" role="3clFbw">
-                <node concept="37vLTw" id="3hr5wtp2fAA" role="2Oq$k0">
-                  <ref role="3cqZAo" node="5K6siRRaBCG" resolve="parent" />
-                </node>
-                <node concept="1mIQ4w" id="3hr5wtp2g5E" role="2OqNvi">
-                  <node concept="chp4Y" id="3hr5wtp2g6W" role="cj9EA">
-                    <ref role="cht4Q" to="tpck:4uZwTti3_$T" resolve="Attribute" />
-                  </node>
-                </node>
+            <node concept="3SKdUt" id="3uH03eoyOal" role="3cqZAp">
+              <node concept="3SKdUq" id="3uH03eoyOdw" role="3SKWNk">
+                <property role="3SKdUp" value="annotation can suppress errors in parent, but not in other annotations on the same level" />
               </node>
             </node>
             <node concept="3clFbJ" id="5K6siRRaHg3" role="3cqZAp">
@@ -5581,60 +5570,56 @@
                   </node>
                 </node>
               </node>
-              <node concept="2OqwBi" id="5K6siRRaHgh" role="3clFbw">
-                <node concept="2OqwBi" id="5K6siRRaHg8" role="2Oq$k0">
-                  <node concept="37vLTw" id="3GM_nagTu08" role="2Oq$k0">
-                    <ref role="3cqZAo" node="5K6siRRaBCG" resolve="parent" />
+              <node concept="1Wc70l" id="3uH03eoyOe4" role="3clFbw">
+                <node concept="2OqwBi" id="3uH03eoyIRn" role="3uHU7w">
+                  <node concept="2OqwBi" id="5K6siRRaHgh" role="2Oq$k0">
+                    <node concept="2OqwBi" id="5K6siRRaHg8" role="2Oq$k0">
+                      <node concept="37vLTw" id="3GM_nagTu08" role="2Oq$k0">
+                        <ref role="3cqZAo" node="5K6siRRaBCG" resolve="parent" />
+                      </node>
+                      <node concept="3CFZ6_" id="5K6siRRaHge" role="2OqNvi">
+                        <node concept="3CFTEB" id="5K6siRRaHgg" role="3CFYIz" />
+                      </node>
+                    </node>
+                    <node concept="v3k3i" id="3uH03eoyIIY" role="2OqNvi">
+                      <node concept="chp4Y" id="3uH03eoyNfT" role="v3oSu">
+                        <ref role="cht4Q" to="tpck:2WmWrdnSpX3" resolve="ISuppressErrors" />
+                      </node>
+                    </node>
                   </node>
-                  <node concept="3CFZ6_" id="5K6siRRaHge" role="2OqNvi">
-                    <node concept="3CFTEB" id="5K6siRRaHgg" role="3CFYIz" />
-                  </node>
-                </node>
-                <node concept="2HwmR7" id="5K6siRRaHgl" role="2OqNvi">
-                  <node concept="1bVj0M" id="5K6siRRaHgm" role="23t8la">
-                    <node concept="3clFbS" id="5K6siRRaHgn" role="1bW5cS">
-                      <node concept="3clFbF" id="5K6siRRaHgq" role="3cqZAp">
-                        <node concept="1Wc70l" id="3oi8Zj89eDi" role="3clFbG">
-                          <node concept="3y3z36" id="3oi8Zj89eDE" role="3uHU7w">
-                            <node concept="37vLTw" id="2BHiRxgmIz4" role="3uHU7w">
-                              <ref role="3cqZAo" node="5TGjZPCODy8" resolve="node" />
-                            </node>
-                            <node concept="37vLTw" id="2BHiRxgl3kU" role="3uHU7B">
+                  <node concept="2HwmR7" id="5K6siRRaHgl" role="2OqNvi">
+                    <node concept="1bVj0M" id="5K6siRRaHgm" role="23t8la">
+                      <node concept="3clFbS" id="5K6siRRaHgn" role="1bW5cS">
+                        <node concept="3clFbF" id="5K6siRRaHgq" role="3cqZAp">
+                          <node concept="2OqwBi" id="7Hf6HLtlPLZ" role="3clFbG">
+                            <node concept="37vLTw" id="2BHiRxgmP7x" role="2Oq$k0">
                               <ref role="3cqZAo" node="5K6siRRaHgo" resolve="attr" />
                             </node>
-                          </node>
-                          <node concept="1Wc70l" id="7Hf6HLtlOao" role="3uHU7B">
-                            <node concept="2OqwBi" id="5K6siRRaHgD" role="3uHU7B">
-                              <node concept="37vLTw" id="2BHiRxghis9" role="2Oq$k0">
-                                <ref role="3cqZAo" node="5K6siRRaHgo" resolve="attr" />
-                              </node>
-                              <node concept="1mIQ4w" id="5K6siRRaHgH" role="2OqNvi">
-                                <node concept="chp4Y" id="2zn6YdLtwWe" role="cj9EA">
-                                  <ref role="cht4Q" to="tpck:2WmWrdnSpX3" resolve="ISuppressErrors" />
-                                </node>
-                              </node>
-                            </node>
-                            <node concept="2OqwBi" id="7Hf6HLtlPLZ" role="3uHU7w">
-                              <node concept="1PxgMI" id="7Hf6HLtlPLV" role="2Oq$k0">
-                                <ref role="1PxNhF" to="tpck:2WmWrdnSpX3" resolve="ISuppressErrors" />
-                                <node concept="37vLTw" id="2BHiRxgmP7x" role="1PxMeX">
-                                  <ref role="3cqZAo" node="5K6siRRaHgo" resolve="attr" />
-                                </node>
-                              </node>
-                              <node concept="2qgKlT" id="7Hf6HLtlPM5" role="2OqNvi">
-                                <ref role="37wK5l" to="tpcu:2WmWrdnSpX7" resolve="suppress" />
-                                <node concept="37vLTw" id="2BHiRxgmhDw" role="37wK5m">
-                                  <ref role="3cqZAo" node="5TGjZPCODy8" resolve="node" />
-                                </node>
+                            <node concept="2qgKlT" id="7Hf6HLtlPM5" role="2OqNvi">
+                              <ref role="37wK5l" to="tpcu:2WmWrdnSpX7" resolve="suppress" />
+                              <node concept="37vLTw" id="2BHiRxgmhDw" role="37wK5m">
+                                <ref role="3cqZAo" node="5TGjZPCODy8" resolve="node" />
                               </node>
                             </node>
                           </node>
                         </node>
                       </node>
+                      <node concept="Rh6nW" id="5K6siRRaHgo" role="1bW2Oz">
+                        <property role="TrG5h" value="attr" />
+                        <node concept="2jxLKc" id="5K6siRRaHgp" role="1tU5fm" />
+                      </node>
                     </node>
-                    <node concept="Rh6nW" id="5K6siRRaHgo" role="1bW2Oz">
-                      <property role="TrG5h" value="attr" />
-                      <node concept="2jxLKc" id="5K6siRRaHgp" role="1tU5fm" />
+                  </node>
+                </node>
+                <node concept="3fqX7Q" id="3uH03eoyRsv" role="3uHU7B">
+                  <node concept="2OqwBi" id="3uH03eoyRsx" role="3fr31v">
+                    <node concept="37vLTw" id="3uH03eoyRsy" role="2Oq$k0">
+                      <ref role="3cqZAo" node="5TGjZPCODy8" resolve="node" />
+                    </node>
+                    <node concept="1mIQ4w" id="3uH03eoyRsz" role="2OqNvi">
+                      <node concept="chp4Y" id="3uH03eoyRs$" role="cj9EA">
+                        <ref role="cht4Q" to="tpck:4uZwTti3_$T" resolve="Attribute" />
+                      </node>
                     </node>
                   </node>
                 </node>

core/kernel/source_gen.caches/jetbrains/mps/checkers/dependencies
@@ -41,7 +41,7 @@
     <classNode dependClassName="java.lang.Object" />
     <classNode dependClassName="jetbrains.mps.baseLanguage.closures.runtime.Wrappers" />
     <classNode dependClassName="jetbrains.mps.internal.collections.runtime.IWhereFilter" />
-    <classNode dependClassName="jetbrains.mps.internal.collections.runtime.ListSequence" />
+    <classNode dependClassName="jetbrains.mps.internal.collections.runtime.Sequence" />
     <classNode dependClassName="jetbrains.mps.lang.smodel.generator.smodelAdapter.AttributeOperations" />
     <classNode dependClassName="jetbrains.mps.lang.smodel.generator.smodelAdapter.IAttributeDescriptor" />
     <classNode dependClassName="jetbrains.mps.lang.smodel.generator.smodelAdapter.SNodeOperations" />

core/kernel/source_gen.caches/jetbrains/mps/checkers/generated
@@ -1,3 +1,3 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<dependencies version="2" modelHash="-d9j8j1l9wgkl0u574bm8nu2cs2cio5i" />
+<dependencies version="2" modelHash="-d0aqp8xk6838jzpwcmx5jfi9s9vxjkv" />
 

core/kernel/source_gen/jetbrains/mps/checkers/ErrorReportUtil.java
@@ -9,7 +9,7 @@ import jetbrains.mps.smodel.SModelStereotype;
 import jetbrains.mps.util.SNodeOperations;
 import jetbrains.mps.smodel.adapter.structure.MetaAdapterFactory;
 import jetbrains.mps.smodel.behaviour.BehaviorReflection;
-import jetbrains.mps.internal.collections.runtime.ListSequence;
+import jetbrains.mps.internal.collections.runtime.Sequence;
 import jetbrains.mps.lang.smodel.generator.smodelAdapter.AttributeOperations;
 import jetbrains.mps.lang.smodel.generator.smodelAdapter.IAttributeDescriptor;
 import jetbrains.mps.internal.collections.runtime.IWhereFilter;
@@ -31,12 +31,10 @@ public class ErrorReportUtil {
       if (jetbrains.mps.lang.smodel.generator.smodelAdapter.SNodeOperations.isInstanceOf(parent, MetaAdapterFactory.getInterfaceConcept(0xceab519525ea4f22L, 0x9b92103b95ca8c0cL, 0x2f16f1b357e19f43L, "jetbrains.mps.lang.core.structure.ISuppressErrors")) && BehaviorReflection.invokeVirtual(Boolean.TYPE, jetbrains.mps.lang.smodel.generator.smodelAdapter.SNodeOperations.cast(parent, MetaAdapterFactory.getInterfaceConcept(0xceab519525ea4f22L, 0x9b92103b95ca8c0cL, 0x2f16f1b357e19f43L, "jetbrains.mps.lang.core.structure.ISuppressErrors")), "virtual_suppress_3393165121846091591", new Object[]{_node.value})) {
         return false;
       }
-      if (jetbrains.mps.lang.smodel.generator.smodelAdapter.SNodeOperations.isInstanceOf(parent, MetaAdapterFactory.getConcept(0xceab519525ea4f22L, 0x9b92103b95ca8c0cL, 0x47bf8397520e5939L, "jetbrains.mps.lang.core.structure.Attribute"))) {
-        return true;
-      }
-      if (ListSequence.fromList(AttributeOperations.getAttributeList(parent, new IAttributeDescriptor.AllAttributes())).any(new IWhereFilter<SNode>() {
+      // annotation can suppress errors in parent, but not in other annotations on the same level 
+      if (!(jetbrains.mps.lang.smodel.generator.smodelAdapter.SNodeOperations.isInstanceOf(_node.value, MetaAdapterFactory.getConcept(0xceab519525ea4f22L, 0x9b92103b95ca8c0cL, 0x47bf8397520e5939L, "jetbrains.mps.lang.core.structure.Attribute"))) && Sequence.fromIterable(jetbrains.mps.lang.smodel.generator.smodelAdapter.SNodeOperations.ofConcept(AttributeOperations.getAttributeList(parent, new IAttributeDescriptor.AllAttributes()), MetaAdapterFactory.getInterfaceConcept(0xceab519525ea4f22L, 0x9b92103b95ca8c0cL, 0x2f16f1b357e19f43L, "jetbrains.mps.lang.core.structure.ISuppressErrors"))).any(new IWhereFilter<SNode>() {
         public boolean accept(SNode attr) {
-          return jetbrains.mps.lang.smodel.generator.smodelAdapter.SNodeOperations.isInstanceOf(attr, MetaAdapterFactory.getInterfaceConcept(0xceab519525ea4f22L, 0x9b92103b95ca8c0cL, 0x2f16f1b357e19f43L, "jetbrains.mps.lang.core.structure.ISuppressErrors")) && BehaviorReflection.invokeVirtual(Boolean.TYPE, jetbrains.mps.lang.smodel.generator.smodelAdapter.SNodeOperations.cast(attr, MetaAdapterFactory.getInterfaceConcept(0xceab519525ea4f22L, 0x9b92103b95ca8c0cL, 0x2f16f1b357e19f43L, "jetbrains.mps.lang.core.structure.ISuppressErrors")), "virtual_suppress_3393165121846091591", new Object[]{_node.value}) && attr != _node.value;
+          return BehaviorReflection.invokeVirtual(Boolean.TYPE, attr, "virtual_suppress_3393165121846091591", new Object[]{_node.value});
         }
       })) {
         return false;

core/kernel/source_gen/jetbrains/mps/checkers/trace.info
@@ -60,46 +60,44 @@
       <node id="6803901086853929119" at="26,92,27,19" concept="9" />
       <node id="6631111943583857195" at="28,5,29,31" concept="8" />
       <node id="6631111943583857281" at="31,608,32,21" concept="9" />
-      <node id="3772633333667267144" at="34,240,35,20" concept="9" />
-      <node id="6631111943583880218" at="38,43,39,632" concept="9" />
-      <node id="6631111943583880240" at="41,11,42,21" concept="9" />
-      <node id="1047408822409602302" at="44,258,45,20" concept="9" />
-      <node id="6631111943583857217" at="46,7,47,27" concept="3" />
-      <node id="6631111943583857224" at="47,27,48,104" concept="3" />
-      <node id="6803901086853929121" at="49,5,50,16" concept="9" />
+      <node id="4011863064659575445" at="33,7,34,98" concept="10" />
+      <node id="6631111943583880218" at="36,43,37,137" concept="9" />
+      <node id="6631111943583880240" at="39,11,40,21" concept="9" />
+      <node id="1047408822409602302" at="42,258,43,20" concept="9" />
+      <node id="6631111943583857217" at="44,7,45,27" concept="3" />
+      <node id="6631111943583857224" at="45,27,46,104" concept="3" />
+      <node id="6803901086853929121" at="47,5,48,16" concept="9" />
       <node id="6803901086853929089" at="18,0,20,0" concept="1" trace="ErrorReportUtil#()V" />
       <node id="414045414354372315" at="22,42,25,5" concept="6" />
       <node id="6803901086853929109" at="25,5,28,5" concept="6" />
       <node id="6631111943583857251" at="30,28,33,7" concept="6" />
-      <node id="3772633333667264475" at="33,7,36,7" concept="6" />
-      <node id="6631111943583880214" at="38,0,41,0" concept="7" trace="accept#(Lorg/jetbrains/mps/openapi/model/SNode;)Z" />
-      <node id="1047408822409602300" at="43,7,46,7" concept="6" />
-      <node id="6631111943583880195" at="36,7,43,7" concept="6" />
-      <node id="6631111943583857203" at="29,31,49,5" concept="15" />
-      <node id="6803901086853929093" at="20,0,52,0" concept="13" trace="shouldReportError#(Lorg/jetbrains/mps/openapi/model/SNode;)Z" />
+      <node id="6631111943583880214" at="36,0,39,0" concept="7" trace="accept#(Lorg/jetbrains/mps/openapi/model/SNode;)Z" />
+      <node id="1047408822409602300" at="41,7,44,7" concept="6" />
+      <node id="6631111943583880195" at="34,98,41,7" concept="6" />
+      <node id="6631111943583857203" at="29,31,47,5" concept="15" />
+      <node id="6803901086853929093" at="20,0,50,0" concept="13" trace="shouldReportError#(Lorg/jetbrains/mps/openapi/model/SNode;)Z" />
       <scope id="6803901086853929092" at="18,28,18,28" />
       <scope id="414045414354372318" at="23,24,24,19" />
       <scope id="6803901086853929118" at="26,92,27,19" />
       <scope id="6631111943583857252" at="31,608,32,21" />
-      <scope id="3772633333667264478" at="34,240,35,20" />
-      <scope id="6631111943583880215" at="38,43,39,632" />
-      <scope id="6631111943583880196" at="41,11,42,21" />
-      <scope id="1047408822409602301" at="44,258,45,20" />
+      <scope id="6631111943583880215" at="36,43,37,137" />
+      <scope id="6631111943583880196" at="39,11,40,21" />
+      <scope id="1047408822409602301" at="42,258,43,20" />
       <scope id="6803901086853929089" at="18,0,20,0" />
-      <scope id="6631111943583880214" at="38,0,41,0">
+      <scope id="6631111943583880214" at="36,0,39,0">
         <var name="attr" id="6631111943583880214" />
       </scope>
-      <scope id="6631111943583857205" at="30,28,48,104" />
-      <scope id="6803901086853929098" at="20,55,50,16">
+      <scope id="6631111943583857205" at="30,28,46,104" />
+      <scope id="6803901086853929098" at="20,55,48,16">
         <var name="_node" id="6803901086853929098" />
         <var name="model" id="414045414354340896" />
         <var name="parent" id="6631111943583857196" />
       </scope>
-      <scope id="6803901086853929093" at="20,0,52,0">
+      <scope id="6803901086853929093" at="20,0,50,0">
         <var name="node" id="6803901086853929096" />
       </scope>
-      <unit id="6631111943583880214" at="37,128,41,7" name="jetbrains.mps.checkers.ErrorReportUtil$1" />
-      <unit id="6803901086853929087" at="17,0,53,0" name="jetbrains.mps.checkers.ErrorReportUtil" />
+      <unit id="6631111943583880214" at="35,600,39,7" name="jetbrains.mps.checkers.ErrorReportUtil$1" />
+      <unit id="6803901086853929087" at="17,0,51,0" name="jetbrains.mps.checkers.ErrorReportUtil" />
     </file>
   </root>
   <root nodeRef="r:ba41e9c6-15ca-4a47-95f2-6a81c2318547(jetbrains.mps.checkers)/7390982340086718154">