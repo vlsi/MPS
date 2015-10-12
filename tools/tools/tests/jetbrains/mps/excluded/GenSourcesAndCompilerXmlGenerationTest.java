63853cc190968977ceddc90a1561246c024b07f1
Victor.Matchenko <Victor.Matchenko@jetbrains.com>
2015-10-12 17:51:45 +0200

    Revert "add tests/src_gen in IdeaPlugin to excluded folders"
    This reverts commit 13c1b35c318690f7e336f8583bccb9b176b25275.

--- tools/tools/tests/jetbrains/mps/excluded/GenSourcesAndCompilerXmlGenerationTest.java
+++ tools/tools/tests/jetbrains/mps/excluded/GenSourcesAndCompilerXmlGenerationTest.java
@@ -114,7 +114,6 @@ public class GenSourcesAndCompilerXmlGenerationTest {
 
       // Models in the plugin project are generated into an excluded source_gen folder
       if (isUnder(cp, "/IdeaPlugin/mps-java/source_gen/")) continue;
-      if (isUnder(cp, "/IdeaPlugin/tests/src_gen/")) continue;
 
       Assert.assertFalse("Java file " + cp + " is neither included in any MPS module, nor in any Idea source root", true);
     }
