dcb0c6e5c8d6b3b20daeccbb692d74a69391603f
Mihail Buryakov <Mihail.Buryakov@jetbrains.com>
2015-10-26 16:48:31 +0300

    project migration resaving all modules: bugfixes

--- plugins/mpsmigration/source_gen/jetbrains/mps/ide/mpsmigration/migration32/migrations/ReSaveModulesMigration.java
+++ plugins/mpsmigration/source_gen/jetbrains/mps/ide/mpsmigration/migration32/migrations/ReSaveModulesMigration.java
@@ -28,9 +28,7 @@ public class ReSaveModulesMigration extends BaseProjectMigration {
         return !(it.isReadOnly());
       }
     })) {
-      if (!(module.getModuleDescriptor().hasLanguageVersions())) {
-        module.setChanged();
-      }
+      module.setChanged();
     }
     MPSModuleRepository.getInstance().saveAll();
     return true;
