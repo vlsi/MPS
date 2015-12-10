500b5cb42a8444ccec5bd746aad0e8779122abce
Alexander Shatalin <Alexander.Shatalin@jetbrains.com>
2015-12-10 12:26:51 +0100

    MPS-23137 Cannot instantiate EditorComponent for INamedConcept

--- languages/languageDesign/editor/source_gen/jetbrains/mps/lang/editor/constraints/AbstractComponent_Constraints.java
+++ languages/languageDesign/editor/source_gen/jetbrains/mps/lang/editor/constraints/AbstractComponent_Constraints.java
@@ -42,7 +42,7 @@ public class AbstractComponent_Constraints extends BaseConstraintsDescriptor {
           }
           @Override
           public Scope createScope(final IOperationContext operationContext, final ReferenceConstraintsContext _context) {
-            return new LanguageConceptsScope(_context.getModel(), MetaAdapterFactory.getConcept(0xc72da2b97cce4447L, 0x8389f407dc1158b7L, 0x1103553c5ffL, "jetbrains.mps.lang.structure.structure.AbstractConceptDeclaration").getDeclarationNode());
+            return new LanguageConceptsScope(_context.getModel(), MetaAdapterFactory.getConcept(0xc72da2b97cce4447L, 0x8389f407dc1158b7L, 0x1103553c5ffL, "jetbrains.mps.lang.structure.structure.AbstractConceptDeclaration"));
           }
         };
       }
