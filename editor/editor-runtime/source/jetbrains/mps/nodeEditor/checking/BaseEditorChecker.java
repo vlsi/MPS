62656c67777627911f5769a282ea445de4af591c
Mihail Muhin <Mihail.Muhin@jetbrains.com>
2016-02-29 20:05:28 +0300

    BaseEditorChecker: comments, todos

--- editor/editor-runtime/source/jetbrains/mps/nodeEditor/checking/BaseEditorChecker.java
+++ editor/editor-runtime/source/jetbrains/mps/nodeEditor/checking/BaseEditorChecker.java
@@ -114,25 +114,58 @@ public abstract class BaseEditorChecker implements EditorMessageOwner {
     return createMessages(rootNode, events, wasCheckedOnce, editorContext);
   }
 
+  /**
+   * todo rename to "needsUpdate","isDirty"
+   * The checker is updated "by request".
+   * The request is issued on by-time basis, taking model writes, idle period and other stuff into account
+   * When there is a request for update, all checkers are asked on whether they are to update some messages based on the events happened.
+   * If this method returns true, the checker will be executed.
+   */
   protected abstract boolean hasDramaticalEvent(List<SModelEvent> events);
 
+  /**
+   * Priority of checking
+   */
   protected boolean isLaterThan(BaseEditorChecker editorChecker) {
     return false;
   }
 
+  /**
+   * If it is essential, it will be re-checked after a shorter period of time
+   */
   protected boolean isEssential () {
     return true;
   }
 
+  /**
+   * This is called just after creating new messages and should tell whether new messages differ from
+   * those returned the previous time. If any of the checkers returns true, all the messages will b redrawn.
+   *
+   * todo: remove implicit contract that this method is called for the same editor just after createMessages().
+   * todo: maybe this even can be done in Hihglighter, not forcing users to write the same code multiple times
+   */
   protected abstract boolean areMessagesChanged();
 
+  /**
+   * This method is called if some exceptions occur to notify the checker that it should be re-checked ;ater
+   * todo remove this method
+   */
   protected void clear(SNode node, EditorComponent editor) {
 
   }
 
+  /**
+   * Invoked on F5. Why should we do something special in this case?! Additionally, if Sergey will make checkers per-editor,
+   * this is definitely better to be performed by recreating checkers for the editor interested
+   * todo:remove this method
+   */
   protected void resetCheckerState() {
   }
 
+  /**
+   * This is called when checker is about to stop working. Should remove all listeners added by the checker
+   * todo: get rid f this methd, remove attached messages on unregistration
+   */
   protected void doDispose(){
 
   }
