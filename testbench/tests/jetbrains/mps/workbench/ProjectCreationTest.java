4c04f156621b6f198d0501c6e6522e4fdf9efbc8
Victor.Matchenko <Victor.Matchenko@jetbrains.com>
2016-03-08 18:06:43 +0100

    fix project creation test

--- testbench/tests/jetbrains/mps/workbench/ProjectCreationTest.java
+++ testbench/tests/jetbrains/mps/workbench/ProjectCreationTest.java
@@ -61,8 +61,7 @@ public class ProjectCreationTest {
   private static final List<String> PROJECT_PROPERTIES_DIR_CONTENT = Arrays.asList(
       PROJECT_PROPERTIES_DIR + "/workspace.xml",
       PROJECT_PROPERTIES_DIR + "/version.xml",
-      PROJECT_PROPERTIES_DIR + "/misc.xml",
-      PROJECT_PROPERTIES_DIR + "/encodings.xml");
+      PROJECT_PROPERTIES_DIR + "/misc.xml");
 
   private static final List<String> EMPTY_PROJECT_PATH_LIST_FB = Arrays.asList(
       PROJECT_NAME + "/" + PROJECT_NAME + ".iws", PROJECT_NAME + "/" + PROJECT_NAME + MPSExtentions.DOT_MPS_PROJECT);
