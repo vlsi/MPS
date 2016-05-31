9caa5374899e119089cb648a04ee7d1a2f6498e2
Mihail Buryakov <Mihail.Buryakov@jetbrains.com>
2016-05-31 19:15:23 +0300

    there is no reason to show indeterminate progress indicator during rerunning search

--- core/findUsages-runtime/source_gen/jetbrains/mps/ide/findusages/model/SearchTask.java
+++ core/findUsages-runtime/source_gen/jetbrains/mps/ide/findusages/model/SearchTask.java
@@ -5,6 +5,10 @@ package jetbrains.mps.ide.findusages.model;
 import org.jetbrains.mps.openapi.module.ModelAccess;
 import org.jetbrains.mps.openapi.util.ProgressMonitor;
 
+/**
+ * Abstraction of search activity one could execute several times.
+ * Use {@link jetbrains.mps.ide.findusages.model.SearchTask#canExecute() } and {@link jetbrains.mps.ide.findusages.model.SearchTask#execute(ModelAccess, ProgressMonitor) }.
+ */
 public interface SearchTask {
   boolean canExecute();
   SearchResults execute(ModelAccess modelAccess, ProgressMonitor progressMonitor);
