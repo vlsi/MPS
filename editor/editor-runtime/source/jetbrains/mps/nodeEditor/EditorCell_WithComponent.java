475eae50ebdae5f899a271710bc9173904cd53db
Sergej Koščejev <sergej@koscejev.cz>
2016-06-07 20:52:12 +0200

    EditorComponentLayoutManager: move the responsibility for sizing child components to EditorCell_WithComponent.

--- editor/editor-runtime/source/jetbrains/mps/nodeEditor/EditorCell_WithComponent.java
+++ editor/editor-runtime/source/jetbrains/mps/nodeEditor/EditorCell_WithComponent.java
@@ -4,6 +4,7 @@ import jetbrains.mps.openapi.editor.cells.EditorCell;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.JComponent;
+import java.awt.Dimension;
 
 /**
  * Created with IntelliJ IDEA.
@@ -15,4 +16,24 @@ import javax.swing.JComponent;
 public interface EditorCell_WithComponent extends EditorCell {
   @NotNull
   JComponent getComponent();
+
+  /**
+   * Update the component's size. Called by {@link EditorComponentLayoutManager} when the editor component is being laid out.
+   * If the cell size should change as a result, call {@link #requestRelayout()}.
+   * <p>
+   * The default implementation validates the component, then sets the component size to its preferred size if it differs.
+   */
+  default void layoutComponent() {
+    JComponent component = getComponent();
+    component.validate();
+    Dimension preferredSize = component.getPreferredSize();
+
+    if (component.getSize().equals(preferredSize)) {
+      return;
+    }
+
+    component.setSize(preferredSize);
+    requestRelayout();
+  }
+
 }
