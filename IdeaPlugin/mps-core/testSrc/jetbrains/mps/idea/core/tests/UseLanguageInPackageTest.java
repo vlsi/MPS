3d1e5917f5f15ead4339584afbf1ab1fc48cba91
Daniil Elovkov <daniil.elovkov@jetbrains.com>
2016-06-08 18:00:35 +0300

    IdeaPlugin: test rename of a package wherein a file-per-root model lives.

--- IdeaPlugin/mps-core/testSrc/jetbrains/mps/idea/core/tests/UseLanguageInPackageTest.java
+++ IdeaPlugin/mps-core/testSrc/jetbrains/mps/idea/core/tests/UseLanguageInPackageTest.java
@@ -16,66 +16,54 @@
 
 package jetbrains.mps.idea.core.tests;
 
-import com.intellij.ide.DataManager;
-import com.intellij.ide.projectView.impl.AbstractProjectViewPSIPane;
-import com.intellij.ide.projectView.impl.nodes.PsiDirectoryNode;
+import com.intellij.facet.FacetManager;
 import com.intellij.ide.util.gotoByName.ChooseByNamePopupComponent.Callback;
-import com.intellij.idea.IdeaTestApplication;
 import com.intellij.navigation.NavigationItem;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ModalityState;
+import com.intellij.openapi.command.CommandProcessor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ModuleRootManager;
 import com.intellij.openapi.util.Ref;
 import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.projectView.TestProjectTreeStructure;
+import com.intellij.psi.JavaPsiFacade;
 import com.intellij.psi.PsiDirectory;
-import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiManager;
+import com.intellij.psi.PsiPackage;
+import com.intellij.refactoring.rename.RenameProcessor;
 import com.intellij.testFramework.MapDataContext;
-import com.intellij.testFramework.PlatformTestUtil;
 import com.intellij.testFramework.TestActionEvent;
-import com.intellij.testFramework.fixtures.HeavyIdeaTestFixture;
 import jetbrains.mps.ide.project.ProjectHelper;
 import jetbrains.mps.ide.vfs.VirtualFileUtils;
 import jetbrains.mps.idea.core.actions.MakeDirAModel;
-import jetbrains.mps.idea.core.actions.MarkModelRootAction;
 import jetbrains.mps.idea.core.facet.MPSFacetConfiguration;
-import jetbrains.mps.idea.core.projectView.MPSTreeStructureProvider;
+import jetbrains.mps.idea.core.facet.MPSFacetType;
 import jetbrains.mps.persistence.DefaultModelRoot;
-import jetbrains.mps.project.LanguageImportHelper;
 import jetbrains.mps.project.LanguageImportHelper.Interaction;
 import jetbrains.mps.smodel.SModelFileTracker;
 import jetbrains.mps.smodel.SModelInternal;
-import jetbrains.mps.vfs.IFileUtils;
+import jetbrains.mps.util.IterableUtil;
 import jetbrains.mps.workbench.choose.modules.BaseModuleModel;
 import org.jetbrains.mps.openapi.language.SLanguage;
 import org.jetbrains.mps.openapi.model.SModel;
-import org.jetbrains.mps.openapi.module.SModuleReference;
+import org.jetbrains.mps.openapi.module.SModule;
 import org.jetbrains.mps.openapi.module.SRepository;
 
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 import java.util.Set;
 
 /**
  * Created by danilla on 03/06/16.
  */
 public class UseLanguageInPackageTest extends DataMPSFixtureTestCase {
-  private TestProjectTreeStructure myStructure;
   private VirtualFile packageDir;
-
-  @Override
-  protected void setUp() throws Exception {
-    super.setUp();
-    myStructure = new TestProjectTreeStructure(myProjectBuilder.getFixture().getProject(), myTestRootDisposable);
-    myStructure.setProviders(new MPSTreeStructureProvider());
-  }
 
   @Override
   protected void prepareTestData(MPSFacetConfiguration configuration) throws Exception {
@@ -85,9 +73,6 @@ public class UseLanguageInPackageTest extends DataMPSFixtureTestCase {
     assertEquals(sourceRoots.length, 1);
 
     String sourceRootPath = sourceRoots[0].getPath();
-
-    VirtualFile sourceRoot = sourceRoots[0];
-//    final IFile psiTestModel = copyResource(sourceRoot.getPath() + "/otherPsiTest.mps", "otherPsiTest.mps", "/tests/psiProject/models/jetbrains/mps/otherPsiTest.mps");
 
     DefaultModelRoot root = new DefaultModelRoot();
     root.setContentRoot(sourceRootPath);
@@ -100,15 +85,11 @@ public class UseLanguageInPackageTest extends DataMPSFixtureTestCase {
   protected void doTest(Interaction interaction) {
     Project project = myProjectBuilder.getFixture().getProject();
 
-    Ref<AbstractProjectViewPSIPane> pane = new Ref<>();
     Ref<PsiDirectory> dirElement = new Ref<>();
-
     ApplicationManager.getApplication().runReadAction(new Runnable() {
       @Override
       public void run() {
-        pane.set(myStructure.createPane());
         dirElement.set(PsiManager.getInstance(project).findDirectory(packageDir));
-        pane.get().select(new PsiDirectoryNode(project, dirElement.get(), null), packageDir, true);
       }
     });
 
@@ -117,8 +98,11 @@ public class UseLanguageInPackageTest extends DataMPSFixtureTestCase {
     // Couldn't make a more honest context to work. By more honest I mean the one taken from project pane,
     // either by DataManager or new TestDataContext. Hence, constructing an artificial data context.
 
+    // Doing it by project pane would require us to have TestProjectTreeStructure in this test
+
 //    AnActionEvent e = new TestActionEvent(DataManager.getInstance().getDataContext(pane.get().getTree()), action);
 //    AnActionEvent e = new TestActionEvent(new TestDataContext(pane.get(), project), action);
+    // where pane is Ref<AbstractProjectViewPSIPane>
 
     MapDataContext dataContext = new MapDataContext();
     dataContext.put(PlatformDataKeys.PROJECT, project);
@@ -138,24 +122,41 @@ public class UseLanguageInPackageTest extends DataMPSFixtureTestCase {
         action.actionPerformed(e);
       }
     }, ModalityState.NON_MODAL);
-
-
   }
 
-  public void testCancelCreationOfModelInDir() {
-    doTest(new Interaction() {
-      @Override
-      public void chooseLanguage(BaseModuleModel baseModuleModel, Callback callback) {
-        // closing it right away without choosing anything
-        // we _must_ close the "window", real UI probably always calls this method
-        callback.onClose();
-      }
+  protected final Interaction cancelInteraction = new Interaction() {
+    @Override
+    public void chooseLanguage(BaseModuleModel baseModuleModel, Callback callback) {
+      // closing it right away without choosing anything
+      // we _must_ close the "window", real UI probably always calls this method
+      callback.onClose();
+    }
 
-      @Override
-      public Set<SLanguage> chooseAdditionalLanguages(Set<SLanguage> set) {
-        return null;
-      }
-    });
+    @Override
+    public Set<SLanguage> chooseAdditionalLanguages(Set<SLanguage> set) {
+      return null;
+    }
+  };
+
+  protected final Interaction chooseOnlyBaseLanguageInteraction = new Interaction() {
+    @Override
+    public void chooseLanguage(BaseModuleModel baseModuleModel, Callback callback) {
+      NavigationItem[] navigationItems = baseModuleModel.getElementsByName("baseLanguage", true, null);
+      assertTrue(navigationItems.length > 0);
+      callback.elementChosen(navigationItems[0]);
+      // we _must_ close the "window", real UI probably always calls this method
+      callback.onClose();
+    }
+
+    @Override
+    public Set<SLanguage> chooseAdditionalLanguages(Set<SLanguage> set) {
+      // equivalent of hitting 'none' in the dialog box asking if we want to import more langs
+      return Collections.emptySet();
+    }
+  };
+
+  public void testCancelCreationOfModelInDir() {
+    doTest(cancelInteraction);
 
     // check no model has been created
     SRepository repository = ProjectHelper.getProjectRepository(myProjectBuilder.getFixture().getProject());
@@ -169,22 +170,7 @@ public class UseLanguageInPackageTest extends DataMPSFixtureTestCase {
   }
 
   public void testCreateModelInDir() {
-    doTest(new Interaction() {
-      @Override
-      public void chooseLanguage(BaseModuleModel baseModuleModel, Callback callback) {
-        NavigationItem[] navigationItems = baseModuleModel.getElementsByName("baseLanguage", true, null);
-        assertTrue(navigationItems.length > 0);
-        callback.elementChosen(navigationItems[0]);
-        // we _must_ close the "window", real UI probably always calls this method
-        callback.onClose();
-      }
-
-      @Override
-      public Set<SLanguage> chooseAdditionalLanguages(Set<SLanguage> set) {
-        // equivalent of hitting 'none' in the dialog box asking if we want to import more langs
-        return Collections.emptySet();
-      }
-    });
+    doTest(chooseOnlyBaseLanguageInteraction);
 
     SRepository repository = ProjectHelper.getProjectRepository(myProjectBuilder.getFixture().getProject());
     repository.getModelAccess().runReadAction(new Runnable() {
@@ -198,4 +184,38 @@ public class UseLanguageInPackageTest extends DataMPSFixtureTestCase {
       }
     });
   }
+
+  public void testRenamePackage() {
+    doTest(chooseOnlyBaseLanguageInteraction);
+
+    Project project = myProjectBuilder.getFixture().getProject();
+    ApplicationManager.getApplication().invokeAndWait(new Runnable() {
+      @Override
+      public void run() {
+        CommandProcessor.getInstance().executeCommand(project, new Runnable() {
+          @Override
+          public void run() {
+            PsiPackage psiPackage = JavaPsiFacade.getInstance(project).findPackage("com.jetbrains.pkg");
+            assertNotNull(psiPackage);
+            new RenameProcessor(project, psiPackage, "com.jetbrains.pkgRenamed", false, false).run();
+          }
+        }, null, null);
+      }
+    }, ModalityState.NON_MODAL);
+
+
+    ProjectHelper.getModelAccess(project).runReadAction(new Runnable() {
+      @Override
+      public void run() {
+        SModule module = FacetManager.getInstance(myModule).getFacetByType(MPSFacetType.ID).getSolution();
+        List<SModel> smodels = IterableUtil.asList(module.getModels());
+        assertEquals("Exactly one model must be in the idea solution after package rename", 1, smodels.size());
+        SModel smodel = smodels.get(0);
+        assertEquals("com.jetbrains.pkgRenamed", smodel.getName().getLongName());
+      }
+    });
+
+
+  }
+
 }
