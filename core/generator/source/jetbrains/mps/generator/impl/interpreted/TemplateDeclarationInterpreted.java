d3c72dccaa14f20d4b0dd5b4cd73f7043edb429c
Artem Tikhomirov <tikhomirov.artem@gmail.com>
2016-08-15 17:50:26 +0200

    MPS-24406 Generation of custom aspect descriptor fails -- broken reference: use context with arguments, if any.

--- core/generator/source/jetbrains/mps/generator/impl/interpreted/TemplateDeclarationInterpreted.java
+++ core/generator/source/jetbrains/mps/generator/impl/interpreted/TemplateDeclarationInterpreted.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2015 JetBrains s.r.o.
+ * Copyright 2003-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -90,7 +90,7 @@ public class TemplateDeclarationInterpreted extends TemplateDeclarationBase {
 
     if (myIsTemplateDeclNode) {
       final TemplateContainer tc = getTemplates();
-      return tc.processRuleConsequence(context);
+      return tc.processRuleConsequence(applyContext);
     } else {
       return environment.getTemplateProcessor().apply(myTemplateNode, applyContext);
     }
