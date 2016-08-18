eff54966d2374c339af66286002fd42dbd6aec6f
Alexander Shatalin <Alexander.Shatalin@jetbrains.com>
2016-08-18 15:04:44 +0200

    MPS-23427 Focus is lost on attempt to delete 'model' type

--- languages/languageDesign/smodel/source_gen/jetbrains/mps/lang/smodel/editor/SModelType_Editor.java
+++ languages/languageDesign/smodel/source_gen/jetbrains/mps/lang/smodel/editor/SModelType_Editor.java
@@ -25,6 +25,7 @@ public class SModelType_Editor extends DefaultNodeEditor {
     Style style = new StyleImpl();
     BaseLanguageStyle_StyleSheet.apply_KeyWord(style, editorCell);
     style.set(StyleAttributes.PADDING_RIGHT, 0, new Padding(0.0, Measure.SPACES));
+    style.set(StyleAttributes.EDITABLE, 0, true);
     editorCell.getStyle().putAll(style);
     editorCell.setDefaultText("");
     return editorCell;
