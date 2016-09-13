eedbe7df9b0821d1b38df937feb3e01a26acc974
Mihail Buryakov <Mihail.Buryakov@jetbrains.com>
2016-09-13 15:06:48 +0300

    MPS-24583 Duplicated "correct id" intention

--- languages/languageDesign/structure/source_gen/jetbrains/mps/lang/structure/typesystem/check_AbstractConceptDeclaration_Ids_NonTypesystemRule.java
+++ languages/languageDesign/structure/source_gen/jetbrains/mps/lang/structure/typesystem/check_AbstractConceptDeclaration_Ids_NonTypesystemRule.java
@@ -50,8 +50,7 @@ public class check_AbstractConceptDeclaration_Ids_NonTypesystemRule extends Abst
             _reporter_2309309498.addIntentionProvider(intentionProvider);
           }
         }
-      }
-      if (neq_hcbjaj_a0b0a0a1(SPropertyOperations.getString(acd, MetaAdapterFactory.getProperty(0xc72da2b97cce4447L, 0x8389f407dc1158b7L, 0x1103553c5ffL, 0x5d2e6079771f8cc0L, "conceptId")), ConceptIdHelper.getNodeIdString(acd))) {
+      } else if (neq_hcbjaj_a0a0a0a0b(SPropertyOperations.getString(acd, MetaAdapterFactory.getProperty(0xc72da2b97cce4447L, 0x8389f407dc1158b7L, 0x1103553c5ffL, 0x5d2e6079771f8cc0L, "conceptId")), ConceptIdHelper.getNodeIdString(acd))) {
         {
           MessageTarget errorTarget = new NodeMessageTarget();
           IErrorReporter _reporter_2309309498 = typeCheckingContext.reportWarning(acd, "Node id and concept id differ. It's recommended to keep ids synchronised until MPS 3.5. Use quickfix to set corrected id", "r:00000000-0000-4000-0000-011c8959028f(jetbrains.mps.lang.structure.typesystem)", "2819660830273583910", null, errorTarget);
@@ -90,8 +89,7 @@ public class check_AbstractConceptDeclaration_Ids_NonTypesystemRule extends Abst
               _reporter_2309309498.addIntentionProvider(intentionProvider);
             }
           }
-        }
-        if (neq_hcbjaj_a0b0a0a2a1(SPropertyOperations.getString(p, MetaAdapterFactory.getProperty(0xc72da2b97cce4447L, 0x8389f407dc1158b7L, 0xf979bd086bL, 0x35a81382d82a4d9L, "propertyId")), ConceptIdHelper.getNodeIdString(p))) {
+        } else if (neq_hcbjaj_a0a0a0a0c0b(SPropertyOperations.getString(p, MetaAdapterFactory.getProperty(0xc72da2b97cce4447L, 0x8389f407dc1158b7L, 0xf979bd086bL, 0x35a81382d82a4d9L, "propertyId")), ConceptIdHelper.getNodeIdString(p))) {
           {
             MessageTarget errorTarget = new NodeMessageTarget();
             IErrorReporter _reporter_2309309498 = typeCheckingContext.reportWarning(p, "Node id and property id differ. It's recommended to keep ids synchronised until MPS 3.5. Use quickfix to set corrected id", "r:00000000-0000-4000-0000-011c8959028f(jetbrains.mps.lang.structure.typesystem)", "2819660830273589312", null, errorTarget);
@@ -131,8 +129,7 @@ public class check_AbstractConceptDeclaration_Ids_NonTypesystemRule extends Abst
               _reporter_2309309498.addIntentionProvider(intentionProvider);
             }
           }
-        }
-        if (neq_hcbjaj_a0b0a0a4a1(SPropertyOperations.getString(l, MetaAdapterFactory.getProperty(0xc72da2b97cce4447L, 0x8389f407dc1158b7L, 0xf979bd086aL, 0x35a81382d82a4e4L, "linkId")), ConceptIdHelper.getNodeIdString(l))) {
+        } else if (neq_hcbjaj_a0a0a0a0e0b(SPropertyOperations.getString(l, MetaAdapterFactory.getProperty(0xc72da2b97cce4447L, 0x8389f407dc1158b7L, 0xf979bd086aL, 0x35a81382d82a4e4L, "linkId")), ConceptIdHelper.getNodeIdString(l))) {
           {
             MessageTarget errorTarget = new NodeMessageTarget();
             IErrorReporter _reporter_2309309498 = typeCheckingContext.reportWarning(l, "Node id and property id differ. It's recommended to keep ids synchronised until MPS 3.5. Use quickfix to set corrected id", "r:00000000-0000-4000-0000-011c8959028f(jetbrains.mps.lang.structure.typesystem)", "2819660830273606558", null, errorTarget);
@@ -158,7 +155,7 @@ public class check_AbstractConceptDeclaration_Ids_NonTypesystemRule extends Abst
   private static boolean eq_hcbjaj_a0a0a0a0a0a0a0a0b(Object a, Object b) {
     return (a != null ? a.equals(b) : a == b);
   }
-  private static boolean neq_hcbjaj_a0b0a0a1(Object a, Object b) {
+  private static boolean neq_hcbjaj_a0a0a0a0b(Object a, Object b) {
     return !(((a != null ? a.equals(b) : a == b)));
   }
   private static boolean isEmptyString(String str) {
@@ -167,13 +164,13 @@ public class check_AbstractConceptDeclaration_Ids_NonTypesystemRule extends Abst
   private static boolean eq_hcbjaj_a0a0a0a0a0a0a0a0c0b(Object a, Object b) {
     return (a != null ? a.equals(b) : a == b);
   }
-  private static boolean neq_hcbjaj_a0b0a0a2a1(Object a, Object b) {
+  private static boolean neq_hcbjaj_a0a0a0a0c0b(Object a, Object b) {
     return !(((a != null ? a.equals(b) : a == b)));
   }
   private static boolean eq_hcbjaj_a0a0a0a0a0a0a0a0e0b(Object a, Object b) {
     return (a != null ? a.equals(b) : a == b);
   }
-  private static boolean neq_hcbjaj_a0b0a0a4a1(Object a, Object b) {
+  private static boolean neq_hcbjaj_a0a0a0a0e0b(Object a, Object b) {
     return !(((a != null ? a.equals(b) : a == b)));
   }
 }
