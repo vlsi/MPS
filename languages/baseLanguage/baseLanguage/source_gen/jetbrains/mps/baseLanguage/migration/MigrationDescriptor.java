6fdcf5ce2210ed80252dd7acd8cb7c6bf0600b23
Vaclav <vaclav.pech@gmail.com>
2016-12-16 11:21:03 +0100

    MPS-24981 - rebuild migrations

--- languages/baseLanguage/baseLanguage/source_gen/jetbrains/mps/baseLanguage/migration/MigrationDescriptor.java
+++ languages/baseLanguage/baseLanguage/source_gen/jetbrains/mps/baseLanguage/migration/MigrationDescriptor.java
@@ -9,7 +9,7 @@ import jetbrains.mps.internal.collections.runtime.MapSequence;
 import java.util.HashMap;
 
 public class MigrationDescriptor implements MigrationAspectDescriptor {
-  private Map<Integer, MigrationScript> myScripts = MapSequence.<Integer, MigrationScript>fromMapAndKeysArray(new HashMap<Integer, MigrationScript>(), 0, 1, 2, 3).withValues(new AddNecessaryParenthsToNotExpressions(), new ReplaceSingleLineCommentsWithGenericComments(), new RemoveUsagesOfDeprecatedProperty(), new ReplaceOldCommentWithGenericComment_CommentedStatementsBlock());
+  private Map<Integer, MigrationScript> myScripts = MapSequence.<Integer, MigrationScript>fromMapAndKeysArray(new HashMap<Integer, MigrationScript>(), 0, 1, 2, 3, 4).withValues(new AddNecessaryParenthsToNotExpressions(), new ReplaceSingleLineCommentsWithGenericComments(), new RemoveUsagesOfDeprecatedProperty(), new ReplaceOldCommentWithGenericComment_CommentedStatementsBlock(), new ReplaceDeprecatedVarReferencesWithVariableReference());
   public MigrationScript getScript(int fromVersion) {
     return MapSequence.fromMap(myScripts).get(fromVersion);
   }
