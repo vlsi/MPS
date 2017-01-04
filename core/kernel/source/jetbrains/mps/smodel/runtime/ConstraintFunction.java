b5b277ab1246bea5a332f2dbcf0a89f5eec4b3b8
Radimir.Sorokin <rad.sorokin@gmail.com>
2017-01-04 14:44:43 +0300

    MPS-25033 Determine & Implement Constraints Function inheritance contracts -- add constraint inheritance notes

--- core/kernel/source/jetbrains/mps/smodel/runtime/ConstraintFunction.java
+++ core/kernel/source/jetbrains/mps/smodel/runtime/ConstraintFunction.java
@@ -22,11 +22,34 @@ import org.jetbrains.mps.openapi.language.SAbstractConcept;
 /**
  * Presents a constraint function.
  *
+ * INHERITANCE NOTES:
+ *
+ * If constraint isn't defined externally, it should be calculated as composition of constraints defined in super concepts.
+ * For boolean-returning constraints it means that constraint composition returns true only if all super-concept constraints returns true.
+ * For scope-returning constraints it means that constraint composition returns a scope that contains nodes which are contained in all scopes
+ * provided via super-concept constraints.
+ * For other kinds it depends on implementation.
+ *
+ * Example:
+ *
+ * Assuming, there is concepts A, B, C, D:
+ *  A extends B, C;
+ *  B extends D;
+ *  C extends D.
+ *
+ * There is no externally-defined 'can be child' constraint function (from now, CF) in concept A. Let predict calculated constraint function for cases:
+ * 1) CF defined only in B  =>  CF for A returns same as CF for B
+ * 2) CF defined only in D  =>  CF for A returns same as CF for D
+ * 3) CF defined in B and C  =>  CF for A returns true only if CF for B & C returns true
+ * 4) CF defined in D and redefined in B  =>  CF for A returns true only if CF for B & D returns true (but not only CF for B)
+ *
+ *
  * @param <Context> arguments of constraint function.
  * @param <Result> resulting constraint.
  *
  * @see ConstraintFunctions
  * @author Radimir.Sorokin
+ * @since 3.5
  */
 public interface ConstraintFunction<Context, Result> {
 
